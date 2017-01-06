d2aba288ed7a9b861b1014f76f58e01543a153de
Gabor <kisvegabor@gmail.com>
2017-01-06 13:31:40 +0100

    lv_dispi: wait for release attribute added

--- lv_obj/lv_dispi.h
+++ lv_obj/lv_dispi.h
@@ -33,6 +33,7 @@ typedef struct
     /*Flags*/
     uint8_t drag_in_prog :1;
     uint8_t long_press_sent :1;
+    uint8_t wait_release :1;
 }lv_dispi_t;
 
 
@@ -52,6 +53,7 @@ void lv_dispi_reset(void);
 bool lv_dispi_is_dragging(lv_dispi_t * dispi_p);
 void lv_dispi_get_point(lv_dispi_t * dispi_p, point_t * point_p);
 void lv_dispi_get_vect(lv_dispi_t * dispi_p, point_t * point_p);
+void lv_dispi_wait_release(lv_dispi_t * dispi_p);
 
 /**********************
  *      MACROS
