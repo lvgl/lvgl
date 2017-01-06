c299dd9d3098c5c39610848dfe72905f6e2ae476
Gabor <kisvegabor@gmail.com>
2017-01-06 13:39:40 +0100

    lv_app: Files application added

---
+++ lv_appx/lv_app_files.h
@@ -0,0 +1,39 @@
+/**
+ * @file lv_app_files.h
+ *
+ */
+
+#ifndef LV_APP_FILES_H
+#define LV_APP_FILES_H
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl/lv_app/lv_app.h"
+
+#if LV_APP_ENABLE != 0 && USE_LV_APP_FILES != 0
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+typedef struct
+{
+
+}lv_app_files_conf_t;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+const lv_app_dsc_t * lv_app_files_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*LV_APP_ENABLE != 0 && USE_LV_APP_FILES != 0*/
+
+#endif /* LV_APP_EXAMPLE_H */
