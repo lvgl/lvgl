ac39928a015a198280ea4db22831a1c7e173dc68
Ajay Bhargav <contact@rickeyworld.info>
2017-01-13 10:34:19 +0530

    lv_misc: Move SYMBOL definition to font.h for easy access
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- lv_misc/font.h
+++ lv_misc/font.h
@@ -16,6 +16,34 @@
 /*********************
  *      DEFINES
  *********************/
+#if USE_FONT_SYMBOL_30 || USE_FONT_SYMBOL_60
+#define SYMBOL_DRIVE	"a"
+#define SYMBOL_FILE	"b"
+#define SYMBOL_FOLDER	"c"
+#define SYMBOL_DELETE	"d"
+#define SYMBOL_SAVE	"e"
+#define SYMBOL_EDIT	"f"
+#define SYMBOL_OK	"g"
+#define SYMBOL_CLOSE	"h"
+#define SYMBOL_DOWN	"i"
+#define SYMBOL_LEFT	"j"
+#define SYMBOL_RIGHT	"k"
+#define SYMBOL_UP	"l"
+#define SYMBOL_BT	"m"
+#define SYMBOL_THERM	"n"
+#define SYMBOL_GPS	"o"
+#define SYMBOL_WARN	"p"
+#define SYMBOL_INFO	"q"
+#define SYMBOL_BATT1	"r"
+#define SYMBOL_BATT2	"s"
+#define SYMBOL_BATT3	"t"
+#define SYMBOL_BATT4	"u"
+#define SYMBOL_BATTCH	"v"
+#define SYMBOL_HELP	"w"
+#define SYMBOL_POWER	"x"
+#define SYMBOL_SETUP	"y"
+#define SYMBOL_WIFI	"z"
+#endif
 
 /**********************
  *      TYPEDEFS
--- lv_misc/fonts/symbol_30.h
+++ lv_misc/fonts/symbol_30.h
@@ -10,33 +10,6 @@
 #include <stdint.h>
 #include "../font.h"
 
-#define SYMBOL_30_DRIVE		"a"
-#define SYMBOL_30_FILE		"b"
-#define SYMBOL_30_FOLDER	"c"
-#define SYMBOL_30_DELETE	"d"
-#define SYMBOL_30_SAVE		"e"
-#define SYMBOL_30_EDIT		"f"
-#define SYMBOL_30_OK		"g"
-#define SYMBOL_30_CLOSE		"h"
-#define SYMBOL_30_DOWN		"i"
-#define SYMBOL_30_LEFT		"j"
-#define SYMBOL_30_RIGHT		"k"
-#define SYMBOL_30_UP		"l"
-#define SYMBOL_30_BT		"m"
-#define SYMBOL_30_THERM		"n"
-#define SYMBOL_30_GPS		"o"
-#define SYMBOL_30_WARN		"p"
-#define SYMBOL_30_INFO		"q"
-#define SYMBOL_30_BATT1		"r"
-#define SYMBOL_30_BATT2		"s"
-#define SYMBOL_30_BATT3		"t"
-#define SYMBOL_30_BATT4		"u"
-#define SYMBOL_30_BATTCH	"v"
-#define SYMBOL_30_HELP		"w"
-#define SYMBOL_30_POWER		"x"
-#define SYMBOL_30_SETUP		"y"
-#define SYMBOL_30_WIFI		"z"
-
 const font_t * symbol_30_get_dsc(void);
 
 #endif
