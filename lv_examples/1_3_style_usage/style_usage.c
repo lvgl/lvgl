d69c18dba43ac7210e48009741e526fc0445f6d8
Gabor <kisvegabor@gmail.com>
2017-08-12 10:10:16 +0200

    examples: style_usage: skeleton added

---
+++ lv_examples/1_3_style_usage/style_usage.c
@@ -0,0 +1,64 @@
+/**
+ * @file style_usage.c
+ *
+ */
+
+/*
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "style_usage.h"
+
+#if USE_LV_EXAMPLE != 0
+
+#include "lvgl/lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+/**
+ * Create a simple 'Hello world!' label
+ */
+void style_usage_init(void)
+{
+
+    /*3 buttons*/
+    /*copy default style and mod, create new style*/
+
+
+
+    /*3 labels font + symbol font*/
+
+    /*3 leds*/
+
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+#endif /*USE_LV_EXAMPLE != 0*/
---
+++ lv_examples/1_3_style_usage/style_usage.h
@@ -0,0 +1,42 @@
+/**
+ * @file style_usage.h
+ *
+ */
+
+#ifndef STYLE_USAGE_H
+#define STYLE_USAGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_conf.h"
+#if USE_LV_EXAMPLE != 0
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void style_usage_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*USE_LV_EXAMPLE != 0*/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_HELLO_WORLD_H*/
