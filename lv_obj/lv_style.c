dfb078919192e10646a168235a52056b5e23f2bd
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2017-11-29 13:40:43 +0100

    makefile revert

--- lv_obj/lv_style.c
+++ lv_obj/lv_style.c
@@ -162,7 +162,7 @@ void lv_style_init (void)
     lv_style_btn_rel.body.padding.ver = LV_DPI / 6;
     lv_style_btn_rel.body.padding.inner = LV_DPI / 10;
     lv_style_btn_rel.body.border.color = LV_COLOR_MAKE(0x0b, 0x19, 0x28);
-    lv_style_btn_rel.body.border.width = 1;//LV_DPI / 50 >= 1 ? LV_DPI / 50  : 1;
+    lv_style_btn_rel.body.border.width = LV_DPI / 50 >= 1 ? LV_DPI / 50  : 1;
     lv_style_btn_rel.body.border.opa = LV_OPA_70;
     lv_style_btn_rel.text.color = LV_COLOR_MAKE(0xff, 0xff, 0xff);
     lv_style_btn_rel.body.shadow.color = LV_COLOR_GRAY;
