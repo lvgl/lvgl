fb75153d292b83e5f95ed4285935c5250d4a0bea
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-01-19 13:28:14 +0100

    lv_list: group focus fixes

--- lv_objx/lv_list.c
+++ lv_objx/lv_list.c
@@ -527,14 +527,14 @@ static lv_res_t lv_list_signal(lv_obj_t * list, lv_signal_t sign, void * param)
             lv_list_ext_t *ext = lv_obj_get_ext_attr(list);
             btn = get_next_btn(list, btn);
             while(btn != NULL) {
-                if(lv_btn_get_state(btn) == LV_BTN_STATE_PR) break;
+                if(lv_btn_get_state(btn) == LV_BTN_STATE_TGL_REL) break;
                 btn_prev = btn;
                 btn = get_next_btn(list, btn);
             }
 
             if(btn_prev != NULL && btn != NULL) {
                 lv_btn_set_state(btn, LV_BTN_STATE_REL);
-                lv_btn_set_state(btn_prev, LV_BTN_STATE_PR);
+                lv_btn_set_state(btn_prev, LV_BTN_STATE_TGL_REL);
                 lv_page_focus(list, btn_prev, ext->anim_time);
             }
         }
@@ -544,7 +544,7 @@ static lv_res_t lv_list_signal(lv_obj_t * list, lv_signal_t sign, void * param)
             lv_list_ext_t *ext = lv_obj_get_ext_attr(list);
             btn = get_next_btn(list, btn);
             while(btn != NULL) {
-                if(lv_btn_get_state(btn) == LV_BTN_STATE_PR) break;
+                if(lv_btn_get_state(btn) == LV_BTN_STATE_TGL_REL) break;
                 btn = get_next_btn(list, btn);
             }
 
@@ -552,7 +552,7 @@ static lv_res_t lv_list_signal(lv_obj_t * list, lv_signal_t sign, void * param)
                 lv_obj_t * btn_prev = get_next_btn(list, btn);
                 if(btn_prev != NULL) {
                     lv_btn_set_state(btn, LV_BTN_STATE_REL);
-                    lv_btn_set_state(btn_prev, LV_BTN_STATE_PR);
+                    lv_btn_set_state(btn_prev, LV_BTN_STATE_TGL_REL);
                     lv_page_focus(list, btn_prev, ext->anim_time);
                 }
             }
@@ -561,7 +561,7 @@ static lv_res_t lv_list_signal(lv_obj_t * list, lv_signal_t sign, void * param)
             lv_obj_t * btn = NULL;
             btn = get_next_btn(list, btn);
             while(btn != NULL) {
-                if(lv_btn_get_state(btn) == LV_BTN_STATE_PR) break;
+                if(lv_btn_get_state(btn) == LV_BTN_STATE_TGL_REL) break;
                 btn = get_next_btn(list, btn);
             }
 
