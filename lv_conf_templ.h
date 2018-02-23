6e6a7de1a8d65964d92aa79ccf48419839d75e21
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-02-23 17:07:02 +0100

    lv_conf_templ.h update

--- lv_conf_templ.h
+++ lv_conf_templ.h
@@ -11,10 +11,9 @@
 /*----------------
  * Dynamic memory
  *----------------*/
-/*
- * Memory size which will be used by the library
- * to store the graphical objects and other data
- */
+
+/* Memory size which will be used by the library
+ * to store the graphical objects and other data */
 #define LV_MEM_CUSTOM      0                /*1: use custom malloc/free, 0: use the built-in lv_mem_alloc/lv_mem_free*/
 #if LV_MEM_CUSTOM == 0
 #define LV_MEM_SIZE    (32U * 1024U)        /*Size memory used by mem_alloc (in bytes)*/
@@ -103,7 +102,10 @@
 /*==================
  *    FONT USAGE
  *===================*/
-/*More info about fonts: https://littlevgl.com/basics#fonts*/
+
+/* More info about fonts: https://littlevgl.com/basics#fonts
+ * To enable a built-in font use 1,2,4 or 8 values
+ * which will determine the bit-per-pixel */
 #define LV_FONT_DEFAULT        &lv_font_dejavu_20     /*Always set a default font from the built-in fonts*/
 
 #define USE_LV_FONT_DEJAVU_10              0
