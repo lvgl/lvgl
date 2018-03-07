d068c85e8018d34f7aad061565eeefde3b375983
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-03-07 09:22:37 +0100

    minor updates on LV_ATTRIBUTE_...

--- lv_hal/lv_hal_tick.h
+++ lv_hal/lv_hal_tick.h
@@ -35,7 +35,7 @@ extern "C" {
  * You have to call this function periodically
  * @param tick_period the call period of this function in milliseconds
  */
-void lv_tick_inc(uint32_t tick_period);
+LV_ATTRIBUTE_TICK_INC void lv_tick_inc(uint32_t tick_period);
 
 /**
  * Get the elapsed milliseconds since start up
