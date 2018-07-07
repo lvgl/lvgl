41dbff4d76a62f12a9948a8a453ea6b618a4e2d1
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-07-07 12:41:56 +0200

    initialize the built-in fonts in a spearete file

--- lv_misc/lv_font.h
+++ lv_misc/lv_font.h
@@ -24,7 +24,7 @@ extern "C" {
 #include <stddef.h>
 #include <stdbool.h>
 
-#include "../lv_misc/lv_symbol_def.h"
+#include "lv_symbol_def.h"
 
 /*********************
  *      DEFINES
@@ -66,7 +66,7 @@ typedef struct _lv_font_struct
  **********************/
 
 /**
- * Initialize the built-in fonts
+ * Initialize the fonts
  */
 void lv_font_init(void);
 
@@ -162,79 +162,14 @@ int16_t lv_font_get_width_sparse(const lv_font_t * font, uint32_t unicode_letter
 /**********************
  *      MACROS
  **********************/
+
 #define LV_FONT_DECLARE(font_name) extern lv_font_t font_name;
 
-/******************************
- *  FONT DECLARATION INCLUDES
- *****************************/
 
-/*10 px */
-#if USE_LV_FONT_DEJAVU_10
-LV_FONT_DECLARE(lv_font_dejavu_10);
-#endif
-
-#if USE_LV_FONT_DEJAVU_10_LATIN_SUP
-LV_FONT_DECLARE(lv_font_dejavu_10_latin_sup);
-#endif
-
-#if USE_LV_FONT_DEJAVU_10_CYRILLIC
-LV_FONT_DECLARE(lv_font_dejavu_10_cyrillic);
-#endif
-
-#if USE_LV_FONT_SYMBOL_10
-LV_FONT_DECLARE(lv_font_symbol_10);
-#endif
-
-/*20 px */
-#if USE_LV_FONT_DEJAVU_20
-LV_FONT_DECLARE(lv_font_dejavu_20);
-#endif
-
-#if USE_LV_FONT_DEJAVU_20_LATIN_SUP
-LV_FONT_DECLARE(lv_font_dejavu_20_latin_sup);
-#endif
-
-#if USE_LV_FONT_DEJAVU_20_CYRILLIC
-LV_FONT_DECLARE(lv_font_dejavu_20_cyrillic);
-#endif
-
-#if USE_LV_FONT_SYMBOL_20
-LV_FONT_DECLARE(lv_font_symbol_20);
-#endif
-
-/*30 px */
-#if USE_LV_FONT_DEJAVU_30
-LV_FONT_DECLARE(lv_font_dejavu_30);
-#endif
-
-#if USE_LV_FONT_DEJAVU_30_LATIN_SUP
-LV_FONT_DECLARE(lv_font_dejavu_30_latin_sup);
-#endif
-
-#if USE_LV_FONT_DEJAVU_30_CYRILLIC
-LV_FONT_DECLARE(lv_font_dejavu_30_cyrillic);
-#endif
-
-#if USE_LV_FONT_SYMBOL_30
-LV_FONT_DECLARE(lv_font_symbol_30);
-#endif
-
-/*40 px */
-#if USE_LV_FONT_DEJAVU_40
-LV_FONT_DECLARE(lv_font_dejavu_40);
-#endif
-
-#if USE_LV_FONT_DEJAVU_40_LATIN_SUP
-LV_FONT_DECLARE(lv_font_dejavu_40_latin_sup);
-#endif
-
-#if USE_LV_FONT_DEJAVU_40_CYRILLIC
-LV_FONT_DECLARE(lv_font_dejavu_40_cyrillic);
-#endif
-
-#if USE_LV_FONT_SYMBOL_40
-LV_FONT_DECLARE(lv_font_symbol_40);
-#endif
+/**********************
+ * ADD BUILT IN FONTS
+ **********************/
+#include "../lv_fonts/lv_font_builtin.h"
 
 #ifdef __cplusplus
 } /* extern "C" */
