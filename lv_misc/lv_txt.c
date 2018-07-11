fde3ee73af8fe4427c6aedcaafdbed1eb2972bbc
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-07-11 18:46:35 +0200

    lv_txt_get_width: don't trim the closing spaces because it would result unwanted text wrap

--- lv_misc/lv_txt.c
+++ lv_misc/lv_txt.c
@@ -198,17 +198,7 @@ lv_coord_t lv_txt_get_width(const char * txt, uint16_t length,
             width += letter_space;
         }
 
-        width -= letter_space;  /*Trim the last letter space because there is no letter after it*/
-
-        /*Trim closing spaces. Important when the text is aligned to the middle */
-        for(i = length - 1; i > 0; i--) {
-            if(txt[i] == ' ' || txt[i] == '\n' || txt[i] == '\r') {
-                width -= lv_font_get_width(font, txt[i]);
-                width -= letter_space;
-            } else {
-                break;
-            }
-        }
+        width -= letter_space;  /*Trim the last letter space. Important if the text is center aligned */
     }
 
     return width;
