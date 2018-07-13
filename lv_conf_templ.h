0a4c00c5f224af6cd04a7a29c615bac9a7fac698
kisvegabor <kisvegabor@gmail.com>
2018-07-13 21:40:56 +0200

    lv_conf_templ: add config option for Visual Studio

--- lv_conf_templ.h
+++ lv_conf_templ.h
@@ -82,9 +82,11 @@
 #define USE_LV_FILESYSTEM       1               /*1: Enable file system (required by images*/
 
 /*Compiler settings*/
-#define LV_ATTRIBUTE_TICK_INC                 /* Define a custom attribute to `lv_tick_inc` function */
-#define LV_ATTRIBUTE_TASK_HANDLER             /* Define a custom attribute to `lv_task_handler` function */
-#define LV_COMPILER_VLA_SUPPORTED    1        /* 1: Variable length array is supported*/
+#define LV_ATTRIBUTE_TICK_INC					/* Define a custom attribute to `lv_tick_inc` function */
+#define LV_ATTRIBUTE_TASK_HANDLER				/* Define a custom attribute to `lv_task_handler` function */
+#define LV_COMPILER_VLA_SUPPORTED    0			/* 1: Variable length array is supported. (In Visual studio it is not supported)*/
+#define LV_COMPILER_NON_CONST_INIT_SUPPORTED 0	/* 1: Initialziation woth non constant values are supproted (In Visual studio it is not supported)*/
+//#define _CRT_SECURE_NO_WARNINGS			    /* Visual Studio needs it to use `strcpy`, `sprintf` etc*/
 
 /*================
  *  THEME USAGE
