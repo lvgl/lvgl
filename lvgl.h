96f8392555688a09f1a5c911a723b1b1165c44a5
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-10-05 15:16:04 +0200

    set version number

--- lvgl.h
+++ lvgl.h
@@ -62,7 +62,7 @@ extern "C" {
 #define LVGL_VERSION_MAJOR   5
 #define LVGL_VERSION_MINOR   2
 #define LVGL_VERSION_PATCH   0
-#define LVGL_VERSION_INFO    "beta"
+#define LVGL_VERSION_INFO    ""
 
 /**********************
  *      TYPEDEFS
