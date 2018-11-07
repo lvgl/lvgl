b3c159d968710cafc841902e64daebe3991e6a39
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-11-07 20:59:03 +0100

    lv_list: add API for scroll propagation

--- lv_objx/lv_list.h
+++ lv_objx/lv_list.h
@@ -147,6 +147,16 @@ static inline void lv_list_set_sb_mode(lv_obj_t * list, lv_sb_mode_t mode)
 }
 
 /**
+ * Enable the scroll propagation feature. If enabled then the List will move its parent if there is no more space to scroll.
+ * @param list pointer to a List
+ * @param en true or false to enable/disable scroll propagation
+ */
+static inline void lv_list_set_scroll_propagation(lv_obj_t * list, bool en)
+{
+    lv_page_set_scroll_propagation(list, en);
+}
+
+/**
  * Set a style of a list
  * @param list pointer to a list object
  * @param type which style should be set
@@ -235,6 +245,16 @@ uint16_t lv_list_get_anim_time(const lv_obj_t *list);
 static inline lv_sb_mode_t lv_list_get_sb_mode(const lv_obj_t * list)
 {
     return lv_page_get_sb_mode(list);
+}
+
+/**
+ * Get the scroll propagation property
+ * @param list pointer to a List
+ * @return true or false
+ */
+static inline bool lv_list_get_scroll_propagation(lv_obj_t * list)
+{
+    return lv_page_get_scroll_propagation(list);
 }
 
 /**
