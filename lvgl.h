7528e2a9b2282078d185465ada6e002594e40c7a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-11-15 00:30:42 +0100

    start v5.2.1-dev

--- lvgl.h
+++ lvgl.h
@@ -62,8 +62,8 @@ extern "C" {
 /*Current version of LittlevGL*/
 #define LVGL_VERSION_MAJOR   5
 #define LVGL_VERSION_MINOR   2
-#define LVGL_VERSION_PATCH   0
-#define LVGL_VERSION_INFO    ""
+#define LVGL_VERSION_PATCH   1
+#define LVGL_VERSION_INFO    "dev"
 
 /**********************
  *      TYPEDEFS
