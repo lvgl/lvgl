6f12166af24e35e0d9846f8a55a791c910461c48
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-01-14 15:21:04 +0100

    lv_task_handler: revert the addition of return value. See: #708

--- lv_misc/lv_task.c
+++ lv_misc/lv_task.c
@@ -56,22 +56,21 @@ void lv_task_init(void)
 
 /**
  * Call it  periodically to handle lv_tasks.
- * @return true: no error; false: error ocurred
  */
-LV_ATTRIBUTE_TASK_HANDLER bool lv_task_handler(void)
+LV_ATTRIBUTE_TASK_HANDLER void lv_task_handler(void)
 {
     LV_LOG_TRACE("lv_task_handler started");
 
     /*Avoid concurrent running of the task handler*/
     static bool task_handler_mutex = false;
-    if(task_handler_mutex) return true;
+    if(task_handler_mutex) return;
     task_handler_mutex = true;
 
     static uint32_t idle_period_start = 0;
     static uint32_t handler_start = 0;
     static uint32_t busy_time = 0;
 
-    if(lv_task_run == false) return true;
+    if(lv_task_run == false) return;
 
     handler_start = lv_tick_get();
 
@@ -149,7 +148,6 @@ LV_ATTRIBUTE_TASK_HANDLER bool lv_task_handler(void)
     task_handler_mutex = false;     /*Release the mutex*/
 
     LV_LOG_TRACE("lv_task_handler ready");
-    return true;
 }
 
 /**
