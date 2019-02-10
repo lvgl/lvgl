d64902b2a5e1b5453f1e9b094f20c533546a0bb3
embeddedt <42941056+embeddedt@users.noreply.github.com>
2019-02-10 06:52:17 -0500

    Add note about LV_INDEV_DRAG_THROW to lv_conf_templ.h

--- lv_conf_templ.h
+++ lv_conf_templ.h
@@ -107,7 +107,7 @@
 #define LV_INDEV_READ_PERIOD            50                     /*Input device read period in milliseconds*/
 #define LV_INDEV_POINT_MARKER           0                      /*Mark the pressed points  (required: USE_LV_REAL_DRAW = 1)*/
 #define LV_INDEV_DRAG_LIMIT             10                     /*Drag threshold in pixels */
-#define LV_INDEV_DRAG_THROW             20                     /*Drag throw slow-down in [%]. Greater value means faster slow-down */
+#define LV_INDEV_DRAG_THROW             20                     /*Drag throw slow-down in [%] (must be > 0). Greater value means faster slow-down */
 #define LV_INDEV_LONG_PRESS_TIME        400                    /*Long press time in milliseconds*/
 #define LV_INDEV_LONG_PRESS_REP_TIME    100                    /*Repeated trigger period in long press [ms] */
 
