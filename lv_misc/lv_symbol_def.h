lvgl/lvgl

commit 1e8ea6b15bea6dbd8c9314d58c70a3ba49f9ac2b

gc: add undescore to roots, arranged lv_mem.c regarding gc defines. Added support to Symbols using the numstr technique of encoding the characters as a number in an enum. Globals: register them directly witout the need for constructor to access them. Updated example code. Updated lv_mpy.c

lv_misc/lv_symbol_def.h
@@ -22,115 +22,182 @@ extern "C" {
 
 #if LV_TXT_UTF8 == 0
 #define LV_SYMBOL_GLYPH_FIRST  0xC0
-#define SYMBOL_AUDIO           "\xC0"
-#define SYMBOL_VIDEO           "\xC1"
-#define SYMBOL_LIST            "\xC2"
-#define SYMBOL_OK              "\xC3"
-#define SYMBOL_CLOSE           "\xC4"
-#define SYMBOL_POWER           "\xC5"
-#define SYMBOL_SETTINGS        "\xC6"
-#define SYMBOL_TRASH           "\xC7"
-#define SYMBOL_HOME            "\xC8"
-#define SYMBOL_DOWNLOAD        "\xC9"
-#define SYMBOL_DRIVE           "\xCA"
-#define SYMBOL_REFRESH         "\xCB"
-#define SYMBOL_MUTE            "\xCC"
-#define SYMBOL_VOLUME_MID      "\xCD"
-#define SYMBOL_VOLUME_MAX      "\xCE"
-#define SYMBOL_IMAGE           "\xCF"
-#define SYMBOL_EDIT            "\xD0"
-#define SYMBOL_PREV            "\xD1"
-#define SYMBOL_PLAY            "\xD2"
-#define SYMBOL_PAUSE           "\xD3"
-#define SYMBOL_STOP            "\xD4"
-#define SYMBOL_NEXT            "\xD5"
-#define SYMBOL_EJECT           "\xD6"
-#define SYMBOL_LEFT            "\xD7"
-#define SYMBOL_RIGHT           "\xD8"
-#define SYMBOL_PLUS            "\xD9"
-#define SYMBOL_MINUS           "\xDA"
-#define SYMBOL_WARNING         "\xDB"
-#define SYMBOL_SHUFFLE         "\xDC"
-#define SYMBOL_UP              "\xDD"
-#define SYMBOL_DOWN            "\xDE"
-#define SYMBOL_LOOP            "\xDF"
-#define SYMBOL_DIRECTORY       "\xE0"
-#define SYMBOL_UPLOAD          "\xE1"
-#define SYMBOL_CALL            "\xE2"
-#define SYMBOL_CUT             "\xE3"
-#define SYMBOL_COPY            "\xE4"
-#define SYMBOL_SAVE            "\xE5"
-#define SYMBOL_CHARGE          "\xE6"
-#define SYMBOL_BELL            "\xE7"
-#define SYMBOL_KEYBOARD        "\xE8"
-#define SYMBOL_GPS             "\xE9"
-#define SYMBOL_FILE            "\xEA"
-#define SYMBOL_WIFI            "\xEB"
-#define SYMBOL_BATTERY_FULL    "\xEC"
-#define SYMBOL_BATTERY_3       "\xED"
-#define SYMBOL_BATTERY_2       "\xEE"
-#define SYMBOL_BATTERY_1       "\xEF"
-#define SYMBOL_BATTERY_EMPTY   "\xF0"
-#define SYMBOL_BLUETOOTH       "\xF1"
+#define SYMBOL_AUDIO           _SYMBOL_VALUE1(C0)
+#define SYMBOL_VIDEO           _SYMBOL_VALUE1(C1)
+#define SYMBOL_LIST            _SYMBOL_VALUE1(C2)
+#define SYMBOL_OK              _SYMBOL_VALUE1(C3)
+#define SYMBOL_CLOSE           _SYMBOL_VALUE1(C4)
+#define SYMBOL_POWER           _SYMBOL_VALUE1(C5)
+#define SYMBOL_SETTINGS        _SYMBOL_VALUE1(C6)
+#define SYMBOL_TRASH           _SYMBOL_VALUE1(C7)
+#define SYMBOL_HOME            _SYMBOL_VALUE1(C8)
+#define SYMBOL_DOWNLOAD        _SYMBOL_VALUE1(C9)
+#define SYMBOL_DRIVE           _SYMBOL_VALUE1(CA)
+#define SYMBOL_REFRESH         _SYMBOL_VALUE1(CB)
+#define SYMBOL_MUTE            _SYMBOL_VALUE1(CC)
+#define SYMBOL_VOLUME_MID      _SYMBOL_VALUE1(CD)
+#define SYMBOL_VOLUME_MAX      _SYMBOL_VALUE1(CE)
+#define SYMBOL_IMAGE           _SYMBOL_VALUE1(CF)
+#define SYMBOL_EDIT            _SYMBOL_VALUE1(D0)
+#define SYMBOL_PREV            _SYMBOL_VALUE1(D1)
+#define SYMBOL_PLAY            _SYMBOL_VALUE1(D2)
+#define SYMBOL_PAUSE           _SYMBOL_VALUE1(D3)
+#define SYMBOL_STOP            _SYMBOL_VALUE1(D4)
+#define SYMBOL_NEXT            _SYMBOL_VALUE1(D5)
+#define SYMBOL_EJECT           _SYMBOL_VALUE1(D6)
+#define SYMBOL_LEFT            _SYMBOL_VALUE1(D7)
+#define SYMBOL_RIGHT           _SYMBOL_VALUE1(D8)
+#define SYMBOL_PLUS            _SYMBOL_VALUE1(D9)
+#define SYMBOL_MINUS           _SYMBOL_VALUE1(DA)
+#define SYMBOL_WARNING         _SYMBOL_VALUE1(DB)
+#define SYMBOL_SHUFFLE         _SYMBOL_VALUE1(DC)
+#define SYMBOL_UP              _SYMBOL_VALUE1(DD)
+#define SYMBOL_DOWN            _SYMBOL_VALUE1(DE)
+#define SYMBOL_LOOP            _SYMBOL_VALUE1(DF)
+#define SYMBOL_DIRECTORY       _SYMBOL_VALUE1(E0)
+#define SYMBOL_UPLOAD          _SYMBOL_VALUE1(E1)
+#define SYMBOL_CALL            _SYMBOL_VALUE1(E2)
+#define SYMBOL_CUT             _SYMBOL_VALUE1(E3)
+#define SYMBOL_COPY            _SYMBOL_VALUE1(E4)
+#define SYMBOL_SAVE            _SYMBOL_VALUE1(E5)
+#define SYMBOL_CHARGE          _SYMBOL_VALUE1(E6)
+#define SYMBOL_BELL            _SYMBOL_VALUE1(E7)
+#define SYMBOL_KEYBOARD        _SYMBOL_VALUE1(E8)
+#define SYMBOL_GPS             _SYMBOL_VALUE1(E9)
+#define SYMBOL_FILE            _SYMBOL_VALUE1(EA)
+#define SYMBOL_WIFI            _SYMBOL_VALUE1(EB)
+#define SYMBOL_BATTERY_FULL    _SYMBOL_VALUE1(EC)
+#define SYMBOL_BATTERY_3       _SYMBOL_VALUE1(ED)
+#define SYMBOL_BATTERY_2       _SYMBOL_VALUE1(EE)
+#define SYMBOL_BATTERY_1       _SYMBOL_VALUE1(EF)
+#define SYMBOL_BATTERY_EMPTY   _SYMBOL_VALUE1(F0)
+#define SYMBOL_BLUETOOTH       _SYMBOL_VALUE1(F1)
 #define LV_SYMBOL_GLYPH_LAST   0xF1
-#define SYMBOL_DUMMY           "\xFF"       /*Invalid symbol. If written before a string then `lv_img` will show it as a label*/
+#define SYMBOL_DUMMY           _SYMBOL_VALUE1(xFF)       /*Invalid symbol. If written before a string then `lv_img` will show it as a label*/
 
 #else
 #define LV_SYMBOL_GLYPH_FIRST  0xF800
-#define SYMBOL_AUDIO           "\xEF\xA0\x80"
-#define SYMBOL_VIDEO           "\xEF\xA0\x81"
-#define SYMBOL_LIST            "\xEF\xA0\x82"
-#define SYMBOL_OK              "\xEF\xA0\x83"
-#define SYMBOL_CLOSE           "\xEF\xA0\x84"
-#define SYMBOL_POWER           "\xEF\xA0\x85"
-#define SYMBOL_SETTINGS        "\xEF\xA0\x86"
-#define SYMBOL_TRASH           "\xEF\xA0\x87"
-#define SYMBOL_HOME            "\xEF\xA0\x88"
-#define SYMBOL_DOWNLOAD        "\xEF\xA0\x89"
-#define SYMBOL_DRIVE           "\xEF\xA0\x8A"
-#define SYMBOL_REFRESH         "\xEF\xA0\x8B"
-#define SYMBOL_MUTE            "\xEF\xA0\x8C"
-#define SYMBOL_VOLUME_MID      "\xEF\xA0\x8D"
-#define SYMBOL_VOLUME_MAX      "\xEF\xA0\x8E"
-#define SYMBOL_IMAGE           "\xEF\xA0\x8F"
-#define SYMBOL_EDIT            "\xEF\xA0\x90"
-#define SYMBOL_PREV            "\xEF\xA0\x91"
-#define SYMBOL_PLAY            "\xEF\xA0\x92"
-#define SYMBOL_PAUSE           "\xEF\xA0\x93"
-#define SYMBOL_STOP            "\xEF\xA0\x94"
-#define SYMBOL_NEXT            "\xEF\xA0\x95"
-#define SYMBOL_EJECT           "\xEF\xA0\x96"
-#define SYMBOL_LEFT            "\xEF\xA0\x97"
-#define SYMBOL_RIGHT           "\xEF\xA0\x98"
-#define SYMBOL_PLUS            "\xEF\xA0\x99"
-#define SYMBOL_MINUS           "\xEF\xA0\x9A"
-#define SYMBOL_WARNING         "\xEF\xA0\x9B"
-#define SYMBOL_SHUFFLE         "\xEF\xA0\x9C"
-#define SYMBOL_UP              "\xEF\xA0\x9D"
-#define SYMBOL_DOWN            "\xEF\xA0\x9E"
-#define SYMBOL_LOOP            "\xEF\xA0\x9F"
-#define SYMBOL_DIRECTORY       "\xEF\xA0\xA0"
-#define SYMBOL_UPLOAD          "\xEF\xA0\xA1"
-#define SYMBOL_CALL            "\xEF\xA0\xA2"
-#define SYMBOL_CUT             "\xEF\xA0\xA3"
-#define SYMBOL_COPY            "\xEF\xA0\xA4"
-#define SYMBOL_SAVE            "\xEF\xA0\xA5"
-#define SYMBOL_CHARGE          "\xEF\xA0\xA6"
-#define SYMBOL_BELL            "\xEF\xA0\xA7"
-#define SYMBOL_KEYBOARD        "\xEF\xA0\xA8"
-#define SYMBOL_GPS             "\xEF\xA0\xA9"
-#define SYMBOL_FILE            "\xEF\xA0\xAA"
-#define SYMBOL_WIFI            "\xEF\xA0\xAB"
-#define SYMBOL_BATTERY_FULL    "\xEF\xA0\xAC"
-#define SYMBOL_BATTERY_3       "\xEF\xA0\xAD"
-#define SYMBOL_BATTERY_2       "\xEF\xA0\xAE"
-#define SYMBOL_BATTERY_1       "\xEF\xA0\xAF"
-#define SYMBOL_BATTERY_EMPTY   "\xEF\xA0\xB0"
-#define SYMBOL_BLUETOOTH       "\xEF\xA0\xB1"
+#define SYMBOL_AUDIO           _SYMBOL_VALUE3(EF,A0,80)
+#define SYMBOL_VIDEO           _SYMBOL_VALUE3(EF,A0,81)
+#define SYMBOL_LIST            _SYMBOL_VALUE3(EF,A0,82)
+#define SYMBOL_OK              _SYMBOL_VALUE3(EF,A0,83)
+#define SYMBOL_CLOSE           _SYMBOL_VALUE3(EF,A0,84)
+#define SYMBOL_POWER           _SYMBOL_VALUE3(EF,A0,85)
+#define SYMBOL_SETTINGS        _SYMBOL_VALUE3(EF,A0,86)
+#define SYMBOL_TRASH           _SYMBOL_VALUE3(EF,A0,87)
+#define SYMBOL_HOME            _SYMBOL_VALUE3(EF,A0,88)
+#define SYMBOL_DOWNLOAD        _SYMBOL_VALUE3(EF,A0,89)
+#define SYMBOL_DRIVE           _SYMBOL_VALUE3(EF,A0,8A)
+#define SYMBOL_REFRESH         _SYMBOL_VALUE3(EF,A0,8B)
+#define SYMBOL_MUTE            _SYMBOL_VALUE3(EF,A0,8C)
+#define SYMBOL_VOLUME_MID      _SYMBOL_VALUE3(EF,A0,8D)
+#define SYMBOL_VOLUME_MAX      _SYMBOL_VALUE3(EF,A0,8E)
+#define SYMBOL_IMAGE           _SYMBOL_VALUE3(EF,A0,8F)
+#define SYMBOL_EDIT            _SYMBOL_VALUE3(EF,A0,90)
+#define SYMBOL_PREV            _SYMBOL_VALUE3(EF,A0,91)
+#define SYMBOL_PLAY            _SYMBOL_VALUE3(EF,A0,92)
+#define SYMBOL_PAUSE           _SYMBOL_VALUE3(EF,A0,93)
+#define SYMBOL_STOP            _SYMBOL_VALUE3(EF,A0,94)
+#define SYMBOL_NEXT            _SYMBOL_VALUE3(EF,A0,95)
+#define SYMBOL_EJECT           _SYMBOL_VALUE3(EF,A0,96)
+#define SYMBOL_LEFT            _SYMBOL_VALUE3(EF,A0,97)
+#define SYMBOL_RIGHT           _SYMBOL_VALUE3(EF,A0,98)
+#define SYMBOL_PLUS            _SYMBOL_VALUE3(EF,A0,99)
+#define SYMBOL_MINUS           _SYMBOL_VALUE3(EF,A0,9A)
+#define SYMBOL_WARNING         _SYMBOL_VALUE3(EF,A0,9B)
+#define SYMBOL_SHUFFLE         _SYMBOL_VALUE3(EF,A0,9C)
+#define SYMBOL_UP              _SYMBOL_VALUE3(EF,A0,9D)
+#define SYMBOL_DOWN            _SYMBOL_VALUE3(EF,A0,9E)
+#define SYMBOL_LOOP            _SYMBOL_VALUE3(EF,A0,9F)
+#define SYMBOL_DIRECTORY       _SYMBOL_VALUE3(EF,A0,A0)
+#define SYMBOL_UPLOAD          _SYMBOL_VALUE3(EF,A0,A1)
+#define SYMBOL_CALL            _SYMBOL_VALUE3(EF,A0,A2)
+#define SYMBOL_CUT             _SYMBOL_VALUE3(EF,A0,A3)
+#define SYMBOL_COPY            _SYMBOL_VALUE3(EF,A0,A4)
+#define SYMBOL_SAVE            _SYMBOL_VALUE3(EF,A0,A5)
+#define SYMBOL_CHARGE          _SYMBOL_VALUE3(EF,A0,A6)
+#define SYMBOL_BELL            _SYMBOL_VALUE3(EF,A0,A7)
+#define SYMBOL_KEYBOARD        _SYMBOL_VALUE3(EF,A0,A8)
+#define SYMBOL_GPS             _SYMBOL_VALUE3(EF,A0,A9)
+#define SYMBOL_FILE            _SYMBOL_VALUE3(EF,A0,AA)
+#define SYMBOL_WIFI            _SYMBOL_VALUE3(EF,A0,AB)
+#define SYMBOL_BATTERY_FULL    _SYMBOL_VALUE3(EF,A0,AC)
+#define SYMBOL_BATTERY_3       _SYMBOL_VALUE3(EF,A0,AD)
+#define SYMBOL_BATTERY_2       _SYMBOL_VALUE3(EF,A0,AE)
+#define SYMBOL_BATTERY_1       _SYMBOL_VALUE3(EF,A0,AF)
+#define SYMBOL_BATTERY_EMPTY   _SYMBOL_VALUE3(EF,A0,B0)
+#define SYMBOL_BLUETOOTH       _SYMBOL_VALUE3(EF,A0,B1)
 #define LV_SYMBOL_GLYPH_LAST   0xF831
-#define SYMBOL_DUMMY           "\xEF\xA3\xBF"       /*Invalid symbol at (U+F831). If written before a string then `lv_img` will show it as a label*/
+#define SYMBOL_DUMMY           _SYMBOL_VALUE3(EF,A3,BF)       /*Invalid symbol at (U+F831). If written before a string then `lv_img` will show it as a label*/
 #endif
 
+#define _SYMBOL_VALUE1(x) (0x ## x)
+#define _SYMBOL_VALUE3(x, y, z) (0x ## z ## y ## x)
+#define _SYMBOL_NUMSTR(sym) LV_ ## sym ## _NUMSTR = sym
+
+enum
+{
+    _SYMBOL_NUMSTR(SYMBOL_AUDIO),
+    _SYMBOL_NUMSTR(SYMBOL_VIDEO),
+    _SYMBOL_NUMSTR(SYMBOL_LIST),
+    _SYMBOL_NUMSTR(SYMBOL_OK),
+    _SYMBOL_NUMSTR(SYMBOL_CLOSE),
+    _SYMBOL_NUMSTR(SYMBOL_POWER),
+    _SYMBOL_NUMSTR(SYMBOL_SETTINGS),
+    _SYMBOL_NUMSTR(SYMBOL_TRASH),
+    _SYMBOL_NUMSTR(SYMBOL_HOME),
+    _SYMBOL_NUMSTR(SYMBOL_DOWNLOAD),
+    _SYMBOL_NUMSTR(SYMBOL_DRIVE),
+    _SYMBOL_NUMSTR(SYMBOL_REFRESH),
+    _SYMBOL_NUMSTR(SYMBOL_MUTE),
+    _SYMBOL_NUMSTR(SYMBOL_VOLUME_MID),
+    _SYMBOL_NUMSTR(SYMBOL_VOLUME_MAX),
+    _SYMBOL_NUMSTR(SYMBOL_IMAGE),
+    _SYMBOL_NUMSTR(SYMBOL_EDIT),
+    _SYMBOL_NUMSTR(SYMBOL_PREV),
+    _SYMBOL_NUMSTR(SYMBOL_PLAY),
+    _SYMBOL_NUMSTR(SYMBOL_PAUSE),
+    _SYMBOL_NUMSTR(SYMBOL_STOP),
+    _SYMBOL_NUMSTR(SYMBOL_NEXT),
+    _SYMBOL_NUMSTR(SYMBOL_EJECT),
+    _SYMBOL_NUMSTR(SYMBOL_LEFT),
+    _SYMBOL_NUMSTR(SYMBOL_RIGHT),
+    _SYMBOL_NUMSTR(SYMBOL_PLUS),
+    _SYMBOL_NUMSTR(SYMBOL_MINUS),
+    _SYMBOL_NUMSTR(SYMBOL_WARNING),
+    _SYMBOL_NUMSTR(SYMBOL_SHUFFLE),
+    _SYMBOL_NUMSTR(SYMBOL_UP),
+    _SYMBOL_NUMSTR(SYMBOL_DOWN),
+    _SYMBOL_NUMSTR(SYMBOL_LOOP),
+    _SYMBOL_NUMSTR(SYMBOL_DIRECTORY),
+    _SYMBOL_NUMSTR(SYMBOL_UPLOAD),
+    _SYMBOL_NUMSTR(SYMBOL_CALL),
+    _SYMBOL_NUMSTR(SYMBOL_CUT),
+    _SYMBOL_NUMSTR(SYMBOL_COPY),
+    _SYMBOL_NUMSTR(SYMBOL_SAVE),
+    _SYMBOL_NUMSTR(SYMBOL_CHARGE),
+    _SYMBOL_NUMSTR(SYMBOL_BELL),
+    _SYMBOL_NUMSTR(SYMBOL_KEYBOARD),
+    _SYMBOL_NUMSTR(SYMBOL_GPS),
+    _SYMBOL_NUMSTR(SYMBOL_FILE),
+    _SYMBOL_NUMSTR(SYMBOL_WIFI),
+    _SYMBOL_NUMSTR(SYMBOL_BATTERY_FULL),
+    _SYMBOL_NUMSTR(SYMBOL_BATTERY_3),
+    _SYMBOL_NUMSTR(SYMBOL_BATTERY_2),
+    _SYMBOL_NUMSTR(SYMBOL_BATTERY_1),
+    _SYMBOL_NUMSTR(SYMBOL_BATTERY_EMPTY),
+    _SYMBOL_NUMSTR(SYMBOL_BLUETOOTH),
+    _SYMBOL_NUMSTR(SYMBOL_DUMMY),
+};
+
+#undef _SYMBOL_VALUE1
+#undef _SYMBOL_VALUE3
+
+#define _SYMBOL_STR_(x) #x
+#define _SYMBOL_STR(x) _SYMBOL_STR_(x)
+#define _SYMBOL_CHAR(c) \x ## c
+#define _SYMBOL_VALUE1(x) _SYMBOL_STR(_SYMBOL_CHAR(x))
+#define _SYMBOL_VALUE3(x, y, z) _SYMBOL_STR(_SYMBOL_CHAR(x)_SYMBOL_CHAR(y)_SYMBOL_CHAR(z))
 
 #ifdef __cplusplus
 } /* extern "C" */

lv_core/lv_obj.c
@@ -62,10 +62,10 @@ static lv_res_t lv_obj_signal(lv_obj_t * obj, lv_signal_t sign, void * param);
  */
 void lv_init(void)
 {
-    LV_GC_ROOT(def_scr) = NULL;
-    LV_GC_ROOT(act_scr) = NULL;
-    LV_GC_ROOT(top_layer) = NULL;
-    LV_GC_ROOT(sys_layer) = NULL;
+    LV_GC_ROOT(_def_scr) = NULL;
+    LV_GC_ROOT(_act_scr) = NULL;
+    LV_GC_ROOT(_top_layer) = NULL;
+    LV_GC_ROOT(_sys_layer) = NULL;
 
     LV_LOG_TRACE("lv_init started");
 
@@ -90,19 +90,19 @@ void lv_init(void)
     lv_refr_init();
 
     /*Create the default screen*/
-    lv_ll_init(&LV_GC_ROOT(scr_ll), sizeof(lv_obj_t));
-    LV_GC_ROOT(def_scr) = lv_obj_create(NULL, NULL);
+    lv_ll_init(&LV_GC_ROOT(_scr_ll), sizeof(lv_obj_t));
+    LV_GC_ROOT(_def_scr) = lv_obj_create(NULL, NULL);
 
-    LV_GC_ROOT(act_scr) = LV_GC_ROOT(def_scr);
+    LV_GC_ROOT(_act_scr) = LV_GC_ROOT(_def_scr);
 
-    LV_GC_ROOT(top_layer) = lv_obj_create(NULL, NULL);
-    lv_obj_set_style(LV_GC_ROOT(top_layer), &lv_style_transp_fit);
+    LV_GC_ROOT(_top_layer) = lv_obj_create(NULL, NULL);
+    lv_obj_set_style(LV_GC_ROOT(_top_layer), &lv_style_transp_fit);
 
-    LV_GC_ROOT(sys_layer) = lv_obj_create(NULL, NULL);
-    lv_obj_set_style(LV_GC_ROOT(sys_layer), &lv_style_transp_fit);
+    LV_GC_ROOT(_sys_layer) = lv_obj_create(NULL, NULL);
+    lv_obj_set_style(LV_GC_ROOT(_sys_layer), &lv_style_transp_fit);
 
     /*Refresh the screen*/
-    lv_obj_invalidate(LV_GC_ROOT(act_scr));
+    lv_obj_invalidate(LV_GC_ROOT(_act_scr));
 
 #if LV_INDEV_READ_PERIOD != 0
     /*Init the input device handling*/
@@ -132,7 +132,7 @@ lv_obj_t * lv_obj_create(lv_obj_t * parent, const  lv_obj_t * copy)
     if(parent == NULL) {
         LV_LOG_TRACE("Screen create started");
 
-        new_obj = lv_ll_ins_head(&LV_GC_ROOT(scr_ll));
+        new_obj = lv_ll_ins_head(&LV_GC_ROOT(_scr_ll));
         lv_mem_assert(new_obj);
         if(new_obj == NULL) return NULL;
 
@@ -355,7 +355,7 @@ lv_res_t lv_obj_del(lv_obj_t * obj)
     /*Remove the object from parent's children list*/
     lv_obj_t * par = lv_obj_get_parent(obj);
     if(par == NULL) { /*It is a screen*/
-        lv_ll_rem(&LV_GC_ROOT(scr_ll), obj);
+        lv_ll_rem(&LV_GC_ROOT(_scr_ll), obj);
     } else {
         lv_ll_rem(&(par->child_ll), obj);
     }
@@ -411,7 +411,7 @@ void lv_obj_invalidate(const lv_obj_t * obj)
 {
     if(lv_obj_get_hidden(obj)) return;
 
-    /*Invalidate the object only if it belongs to the 'LV_GC_ROOT(act_scr)'*/
+    /*Invalidate the object only if it belongs to the 'LV_GC_ROOT(_act_scr)'*/
     lv_obj_t * obj_scr = lv_obj_get_screen(obj);
     if(obj_scr == lv_scr_act() ||
             obj_scr == lv_layer_top() ||
@@ -456,9 +456,9 @@ void lv_obj_invalidate(const lv_obj_t * obj)
  */
 void lv_scr_load(lv_obj_t * scr)
 {
-    LV_GC_ROOT(act_scr) = scr;
+    LV_GC_ROOT(_act_scr) = scr;
 
-    lv_obj_invalidate(LV_GC_ROOT(act_scr));
+    lv_obj_invalidate(LV_GC_ROOT(_act_scr));
 }
 
 /*--------------------
@@ -1005,7 +1005,7 @@ void lv_obj_refresh_style(lv_obj_t * obj)
 void lv_obj_report_style_mod(lv_style_t * style)
 {
     lv_obj_t * i;
-    LL_READ(LV_GC_ROOT(scr_ll), i) {
+    LL_READ(LV_GC_ROOT(_scr_ll), i) {
         if(i->style_p == style || style == NULL) {
             lv_obj_refresh_style(i);
         }
@@ -1302,7 +1302,7 @@ void lv_obj_animate(lv_obj_t * obj, lv_anim_builtin_t type, uint16_t time, uint1
  */
 lv_obj_t * lv_scr_act(void)
 {
-    return LV_GC_ROOT(act_scr);
+    return LV_GC_ROOT(_act_scr);
 }
 
 /**
@@ -1311,7 +1311,7 @@ lv_obj_t * lv_scr_act(void)
  */
 lv_obj_t * lv_layer_top(void)
 {
-    return LV_GC_ROOT(top_layer);
+    return LV_GC_ROOT(_top_layer);
 }
 
 /**
@@ -1321,7 +1321,7 @@ lv_obj_t * lv_layer_top(void)
  */
 lv_obj_t * lv_layer_sys(void)
 {
-    return LV_GC_ROOT(sys_layer);
+    return LV_GC_ROOT(_sys_layer);
 }
 
 /**

lv_hal/lv_hal_disp.c
@@ -54,7 +54,7 @@ static lv_disp_t * active;
  */
 void lv_disp_drv_init(lv_disp_drv_t * driver)
 {
-    LV_GC_ROOT(disp_list) = NULL;
+    LV_GC_ROOT(_disp_list) = NULL;
     driver->disp_fill = NULL;
     driver->disp_map = NULL;
     driver->disp_flush = NULL;
@@ -87,12 +87,12 @@ lv_disp_t * lv_disp_drv_register(lv_disp_drv_t * driver)
     node->next = NULL;
 
     /* Set first display as active by default */
-    if(LV_GC_ROOT(disp_list) == NULL) {
-        LV_GC_ROOT(disp_list) = node;
+    if(LV_GC_ROOT(_disp_list) == NULL) {
+        LV_GC_ROOT(_disp_list) = node;
         active = node;
         lv_obj_invalidate(lv_scr_act());
     } else {
-        ((lv_disp_t*)LV_GC_ROOT(disp_list))->next = node;
+        ((lv_disp_t*)LV_GC_ROOT(_disp_list))->next = node;
     }
 
     return node;
@@ -126,10 +126,10 @@ lv_disp_t * lv_disp_get_active(void)
 lv_disp_t * lv_disp_next(lv_disp_t * disp)
 {
     if(disp == NULL) {
-        return LV_GC_ROOT(disp_list);
+        return LV_GC_ROOT(_disp_list);
     } else {
-        if(((lv_disp_t*)LV_GC_ROOT(disp_list))->next == NULL) return NULL;
-        else return ((lv_disp_t*)LV_GC_ROOT(disp_list))->next;
+        if(((lv_disp_t*)LV_GC_ROOT(_disp_list))->next == NULL) return NULL;
+        else return ((lv_disp_t*)LV_GC_ROOT(_disp_list))->next;
     }
 }
 

lv_hal/lv_hal_indev.c
@@ -49,7 +49,7 @@
  */
 void lv_indev_drv_init(lv_indev_drv_t * driver)
 {
-    LV_GC_ROOT(indev_list) = NULL;
+    LV_GC_ROOT(_indev_list) = NULL;
     driver->read = NULL;
     driver->type = LV_INDEV_TYPE_NONE;
     driver->user_data = NULL;
@@ -76,10 +76,10 @@ lv_indev_t * lv_indev_drv_register(lv_indev_drv_t * driver)
     node->group = NULL;
     node->btn_points = NULL;
 
-    if(LV_GC_ROOT(indev_list) == NULL) {
-        LV_GC_ROOT(indev_list) = node;
+    if(LV_GC_ROOT(_indev_list) == NULL) {
+        LV_GC_ROOT(_indev_list) = node;
     } else {
-        lv_indev_t * last = LV_GC_ROOT(indev_list);
+        lv_indev_t * last = LV_GC_ROOT(_indev_list);
         while(last->next)
             last = last->next;
 
@@ -98,7 +98,7 @@ lv_indev_t * lv_indev_next(lv_indev_t * indev)
 {
 
     if(indev == NULL) {
-        return LV_GC_ROOT(indev_list);
+        return LV_GC_ROOT(_indev_list);
     } else {
         if(indev->next == NULL) return NULL;
         else return indev->next;

lv_misc/lv_anim.c
@@ -56,7 +56,7 @@ static bool anim_list_changed;
  */
 void lv_anim_init(void)
 {
-    lv_ll_init(&LV_GC_ROOT(anim_ll), sizeof(lv_anim_t));
+    lv_ll_init(&LV_GC_ROOT(_anim_ll), sizeof(lv_anim_t));
     last_task_run = lv_tick_get();
     lv_task_create(anim_task, LV_REFR_PERIOD, LV_TASK_PRIO_MID, NULL);
 }
@@ -72,7 +72,7 @@ void lv_anim_create(lv_anim_t * anim_p)
     if(anim_p->fp != NULL) lv_anim_del(anim_p->var, anim_p->fp);       /*fp == NULL would delete all animations of var*/
 
     /*Add the new animation to the animation linked list*/
-    lv_anim_t * new_anim = lv_ll_ins_head(&LV_GC_ROOT(anim_ll));
+    lv_anim_t * new_anim = lv_ll_ins_head(&LV_GC_ROOT(_anim_ll));
     lv_mem_assert(new_anim);
     if(new_anim == NULL) return;
 
@@ -102,13 +102,13 @@ bool lv_anim_del(void * var, lv_anim_fp_t fp)
     lv_anim_t * a;
     lv_anim_t * a_next;
     bool del = false;
-    a = lv_ll_get_head(&LV_GC_ROOT(anim_ll));
+    a = lv_ll_get_head(&LV_GC_ROOT(_anim_ll));
     while(a != NULL) {
         /*'a' might be deleted, so get the next object while 'a' is valid*/
-        a_next = lv_ll_get_next(&LV_GC_ROOT(anim_ll), a);
+        a_next = lv_ll_get_next(&LV_GC_ROOT(_anim_ll), a);
 
         if(a->var == var && (a->fp == fp || fp == NULL)) {
-            lv_ll_rem(&LV_GC_ROOT(anim_ll), a);
+            lv_ll_rem(&LV_GC_ROOT(_anim_ll), a);
             lv_mem_free(a);
             anim_list_changed = true;       /*Read by `anim_task`. It need to know if a delete occurred in the linked list*/
             del = true;
@@ -357,12 +357,12 @@ static void anim_task(void * param)
     (void)param;
 
     lv_anim_t * a;
-    LL_READ(LV_GC_ROOT(anim_ll), a) {
+    LL_READ(LV_GC_ROOT(_anim_ll), a) {
         a->has_run = 0;
     }
 
     uint32_t elaps = lv_tick_elaps(last_task_run);
-    a = lv_ll_get_head(&LV_GC_ROOT(anim_ll));
+    a = lv_ll_get_head(&LV_GC_ROOT(_anim_ll));
 
     while(a != NULL) {
         /*It can be set by `lv_anim_del()` typically in `end_cb`. If set then an animation delete happened in `anim_ready_handler`
@@ -390,8 +390,8 @@ static void anim_task(void * param)
 
         /* If the linked list changed due to anim. delete then it's not safe to continue
          * the reading of the list from here -> start from the head*/
-        if(anim_list_changed) a = lv_ll_get_head(&LV_GC_ROOT(anim_ll));
-        else a = lv_ll_get_next(&LV_GC_ROOT(anim_ll), a);
+        if(anim_list_changed) a = lv_ll_get_head(&LV_GC_ROOT(_anim_ll));
+        else a = lv_ll_get_next(&LV_GC_ROOT(_anim_ll), a);
     }
 
     last_task_run = lv_tick_get();
@@ -401,7 +401,7 @@ static void anim_task(void * param)
  * Called when an animation is ready to do the necessary thinks
  * e.g. repeat, play back, delete etc.
  * @param a pointer to an animation descriptor
- * @return true: animation delete occurred nnd the `LV_GC_ROOT(anim_ll)` has changed
+ * @return true: animation delete occurred nnd the `LV_GC_ROOT(_anim_ll)` has changed
  * */
 static bool anim_ready_handler(lv_anim_t * a)
 {
@@ -413,7 +413,7 @@ static bool anim_ready_handler(lv_anim_t * a)
             (a->repeat == 0 && a->playback == 1 && a->playback_now == 1)) {
         void (*cb)(void *) = a->end_cb;
         void * p = a->var;
-        lv_ll_rem(&LV_GC_ROOT(anim_ll), a);
+        lv_ll_rem(&LV_GC_ROOT(_anim_ll), a);
         lv_mem_free(a);
         anim_list_changed = true;
 

lv_misc/lv_fs.c
@@ -49,7 +49,7 @@ static lv_fs_drv_t * lv_fs_get_drv(char letter);
  */
 void lv_fs_init(void)
 {
-    lv_ll_init(&LV_GC_ROOT(drv_ll), sizeof(lv_fs_drv_t));
+    lv_ll_init(&LV_GC_ROOT(_drv_ll), sizeof(lv_fs_drv_t));
 }
 
 
@@ -453,7 +453,7 @@ void lv_fs_add_drv(lv_fs_drv_t * drv_p)
 {
     /*Save the new driver*/
     lv_fs_drv_t * new_drv;
-    new_drv =  lv_ll_ins_head(&LV_GC_ROOT(drv_ll));
+    new_drv =  lv_ll_ins_head(&LV_GC_ROOT(_drv_ll));
     lv_mem_assert(new_drv);
     if(new_drv == NULL) return;
 
@@ -470,7 +470,7 @@ char  * lv_fs_get_letters(char * buf)
     lv_fs_drv_t * drv;
     uint8_t i = 0;
 
-    LL_READ(LV_GC_ROOT(drv_ll), drv) {
+    LL_READ(LV_GC_ROOT(_drv_ll), drv) {
         buf[i] = drv->letter;
         i++;
     }
@@ -593,7 +593,7 @@ static lv_fs_drv_t * lv_fs_get_drv(char letter)
 {
     lv_fs_drv_t * drv;
 
-    LL_READ(LV_GC_ROOT(drv_ll), drv) {
+    LL_READ(LV_GC_ROOT(_drv_ll), drv) {
         if(drv->letter == letter) {
             return drv;
         }

lv_misc/lv_gc.h
@@ -30,18 +30,18 @@ extern "C" {
  *********************/
 
 #define LV_GC_ROOTS(prefix) \
-    prefix lv_ll_t lv_task_ll;      /*Linked list to store the lv_tasks*/ \
-    prefix lv_ll_t scr_ll;          /*Linked list of screens*/ \
-    prefix lv_ll_t drv_ll;\
-    prefix lv_ll_t file_ll;\
-    prefix lv_ll_t anim_ll;\
-    prefix void * def_scr;\
-    prefix void * act_scr;\
-    prefix void * top_layer;\
-    prefix void * sys_layer;\
-    prefix void * task_act;\
-    prefix void * indev_list;\
-    prefix void * disp_list;\
+    prefix lv_ll_t _lv_task_ll;      /*Linked list to store the lv_tasks*/ \
+    prefix lv_ll_t _scr_ll;          /*Linked list of screens*/ \
+    prefix lv_ll_t _drv_ll;\
+    prefix lv_ll_t _file_ll;\
+    prefix lv_ll_t _anim_ll;\
+    prefix void * _def_scr;\
+    prefix void * _act_scr;\
+    prefix void * _top_layer;\
+    prefix void * _sys_layer;\
+    prefix void * _task_act;\
+    prefix void * _indev_list;\
+    prefix void * _disp_list;\
     
     
 #define LV_NO_PREFIX

lv_misc/lv_mem.c
@@ -31,7 +31,9 @@
 /**********************
  *      TYPEDEFS
  **********************/
+
 #if LV_ENABLE_GC == 0 /*gc custom allocations must not include header*/
+
 /*The size of this union must be 4 bytes (uint32_t)*/
 typedef union {
     struct {
@@ -45,7 +47,9 @@ typedef struct {
     lv_mem_header_t header;
     uint8_t first_data;        /*First data byte in the allocated data (Just for easily create a pointer)*/
 } lv_mem_ent_t;
-#endif
+
+#endif /* LV_ENABLE_GC */
+
 /**********************
  *  STATIC PROTOTYPES
  **********************/
@@ -128,14 +132,11 @@ void * lv_mem_alloc(uint32_t size)
         //End if there is not next entry OR the alloc. is successful
     } while(e != NULL && alloc == NULL);
 
-#if LV_MEM_ADD_JUNK
-    if(alloc != NULL) memset(alloc, 0xaa, size);
-#endif
 
 #else  /*Use custom, user defined malloc function*/
 #if LV_ENABLE_GC == 1 /*gc must not include header*/
     alloc = LV_MEM_CUSTOM_ALLOC(size);
-#else
+#else /* LV_ENABLE_GC */
     /*Allocate a header too to store the size*/
     alloc = LV_MEM_CUSTOM_ALLOC(size + sizeof(lv_mem_header_t));
     if(alloc != NULL) {
@@ -143,7 +144,11 @@ void * lv_mem_alloc(uint32_t size)
         ((lv_mem_ent_t *) alloc)->header.used = 1;
         alloc = &((lv_mem_ent_t *) alloc)->first_data;
     }
-#endif
+#endif /* LV_ENABLE_GC */
+#endif /* LV_MEM_CUSTOM */
+
+#if LV_MEM_ADD_JUNK
+    if(alloc != NULL) memset(alloc, 0xaa, size);
 #endif
 
     if(alloc == NULL) LV_LOG_WARN("Couldn't allocate memory");
@@ -202,12 +207,11 @@ void lv_mem_free(const void * data)
  * @param new_size the desired new size in byte
  * @return pointer to the new memory
  */
+
+#if LV_ENABLE_GC==0
+
 void * lv_mem_realloc(void * data_p, uint32_t new_size)
 {
-#if LV_ENABLE_GC==1
-    return LV_MEM_CUSTOM_REALLOC(data_p, new_size);
-#else
-
     /*data_p could be previously freed pointer (in this case it is invalid)*/
     if(data_p != NULL) {
         lv_mem_ent_t * e = (lv_mem_ent_t *)((uint8_t *) data_p - sizeof(lv_mem_header_t));
@@ -244,8 +248,18 @@ void * lv_mem_realloc(void * data_p, uint32_t new_size)
     if(new_p == NULL) LV_LOG_WARN("Couldn't allocate memory");
 
     return new_p;
-#endif /*LV_ENABLE_GC*/
 }
+
+#else /* LV_ENABLE_GC */
+
+void * lv_mem_realloc(void * data_p, uint32_t new_size)
+{
+    void * new_p = LV_MEM_CUSTOM_REALLOC(data_p, new_size);
+    if(new_p == NULL) LV_LOG_WARN("Couldn't allocate memory");
+    return new_p;
+}
+
+#endif /* lv_enable_gc */
 
 /**
  * Join the adjacent free memory blocks
@@ -329,19 +343,27 @@ void lv_mem_monitor(lv_mem_monitor_t * mon_p)
  * @param data pointer to an allocated memory
  * @return the size of data memory in bytes
  */
+
+#if LV_ENABLE_GC==0
+
 uint32_t lv_mem_get_size(const void * data)
 {
-#if LV_ENABLE_GC==1
-    return LV_MEM_CUSTOM_GET_SIZE(data);
-#else
     if(data == NULL) return 0;
     if(data == &zero_mem) return 0;
 
     lv_mem_ent_t * e = (lv_mem_ent_t *)((uint8_t *) data - sizeof(lv_mem_header_t));
 
     return e->header.d_size;
-#endif /*LV_ENABLE_GC*/
 }
+
+#else /* LV_ENABLE_GC */
+
+uint32_t lv_mem_get_size(const void * data)
+{
+    return LV_MEM_CUSTOM_GET_SIZE(data);
+}
+
+#endif /*LV_ENABLE_GC*/
 
 /**********************
  *   STATIC FUNCTIONS

commit 19ea8ac55f79cd6b557348dc0001e3e3fbf4e6f8

Fix compilation error introduced in 1e8ea6b15bea6dbd8c9314d58c70a3ba49f9ac2b

lv_misc/lv_symbol_def.h
@@ -73,7 +73,7 @@ extern "C" {
 #define SYMBOL_BATTERY_EMPTY   _SYMBOL_VALUE1(F0)
 #define SYMBOL_BLUETOOTH       _SYMBOL_VALUE1(F1)
 #define LV_SYMBOL_GLYPH_LAST   0xF1
-#define SYMBOL_DUMMY           _SYMBOL_VALUE1(xFF)       /*Invalid symbol. If written before a string then `lv_img` will show it as a label*/
+#define SYMBOL_DUMMY           _SYMBOL_VALUE1(FF)       /*Invalid symbol. If written before a string then `lv_img` will show it as a label*/
 
 #else
 #define LV_SYMBOL_GLYPH_FIRST  0xF800