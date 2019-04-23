1e5f690a45bde7e6f3b1dbe14a8b0f1a0c329ca9
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-04-23 15:09:37 +0200

    lv_txt: do not call lv_font_get_width twice

--- src/lv_misc/lv_txt.c
+++ src/lv_misc/lv_txt.c
@@ -335,7 +335,7 @@ lv_coord_t lv_txt_get_width(const char * txt, uint16_t length, const lv_font_t *
 
             lv_coord_t char_width = lv_font_get_width(font, letter);
             if(char_width > 0) {
-                width += lv_font_get_width(font, letter);
+                width += char_width;
                 width += letter_space;
             }
         }
