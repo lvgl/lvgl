7631d41c8873415546dc2a9933b3e862b547835e
Brian Pugh <bnp117@gmail.com>
2019-05-09 09:02:11 -0700

    lv_obj_user_data_ptr initial commit. Also deleted an unused variable in lv_indev to prevent compiler warnings

--- src/lv_core/lv_obj.h
+++ src/lv_core/lv_obj.h
@@ -909,11 +909,18 @@ void lv_obj_get_type(lv_obj_t * obj, lv_obj_type_t * buf);
 
 #if LV_USE_USER_DATA_SINGLE
 /**
+ * Get the object's user data
+ * @param obj pointer to an object
+ * @return user data
+ */
+lv_obj_user_data_t lv_obj_get_user_data(lv_obj_t * obj);
+
+/**
  * Get a pointer to the object's user data
  * @param obj pointer to an object
  * @return pointer to the user data
  */
-lv_obj_user_data_t lv_obj_get_user_data(lv_obj_t * obj);
+lv_obj_user_data_t *lv_obj_get_user_data_ptr(lv_obj_t * obj);
 
 /**
  * Set the object's user data. The data will be copied.
