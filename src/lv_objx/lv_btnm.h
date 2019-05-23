a3e88e1ecdec6a38737f82c08f5d9561b75b99c4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2019-05-23 10:42:45 -0400

    Update lv_btnm_set_one_toggle docs (#1079)

--- src/lv_objx/lv_btnm.h
+++ src/lv_objx/lv_btnm.h
@@ -170,6 +170,9 @@ void lv_btnm_set_btn_width(const lv_obj_t * btnm, uint16_t btn_id, uint8_t width
 
 /**
  * Make the button matrix like a selector widget (only one button may be toggled at a time).
+ *
+ * Toggling must be enabled on the buttons you want to be selected with `lv_btnm_set_ctrl` or `lv_btnm_set_btn_ctrl_all`.
+ *
  * @param btnm Button matrix object
  * @param one_toggle Whether "one toggle" mode is enabled
  */
