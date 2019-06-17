b9692ee798970f0654d4c02d7b98e9af94df84f0
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-06-17 16:07:34 +0200

    add missing 'iniline'

--- src/lv_objx/lv_slider.h
+++ src/lv_objx/lv_slider.h
@@ -97,7 +97,7 @@ static inline void lv_slider_set_range(lv_obj_t * slider, int16_t min, int16_t m
  * @param slider pointer to a bar object
  * @param anim_time the animation time in milliseconds.
  */
-static void lv_slider_set_anim_time(lv_obj_t * slider, uint16_t anim_time)
+static inline void lv_slider_set_anim_time(lv_obj_t * slider, uint16_t anim_time)
 {
     lv_bar_set_anim_time(slider, anim_time);
 }
