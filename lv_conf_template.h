a88cacb71e8810a196541c490d38f2463f29e1b8
Brian Pugh <bnp117@gmail.com>
2019-06-23 16:21:03 -0700

    Restore LV_TXT_LINE_BREAK_LONG_* defines in lv_conf_template.h and checker

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -290,6 +290,16 @@ typedef void * lv_font_user_data_t;
  /*Can break (wrap) texts on these chars*/
 #define LV_TXT_BREAK_CHARS                  " ,.;:-_"
 
+/* If a character is at least this long, will break wherever "prettiest" */
+#define LV_TXT_LINE_BREAK_LONG_LEN          12
+
+/* Minimum number of characters of a word to put on a line before a break */
+#define LV_TXT_LINE_BREAK_LONG_PRE_MIN_LEN  3
+
+/* Minimum number of characters of a word to put on a line after a break */
+#define LV_TXT_LINE_BREAK_LONG_POST_MIN_LEN 3
+
+
 /*===================
  *  LV_OBJ SETTINGS
  *==================*/
