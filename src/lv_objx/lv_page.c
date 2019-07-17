1c859fc3d69b3d18ce3201db298dc96dcd93a357
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-07-17 12:43:50 +0200

    lv_page: propagate the DRAG_START/END events too from scrollable to the bancground

--- src/lv_objx/lv_page.c
+++ src/lv_objx/lv_page.c
@@ -1081,7 +1081,8 @@ static void scrl_def_event_cb(lv_obj_t * scrl, lv_event_t event)
     /*clang-format off*/
     if(event == LV_EVENT_PRESSED || event == LV_EVENT_PRESSING || event == LV_EVENT_PRESS_LOST ||
        event == LV_EVENT_RELEASED || event == LV_EVENT_SHORT_CLICKED || event == LV_EVENT_CLICKED ||
-       event == LV_EVENT_LONG_PRESSED || event == LV_EVENT_LONG_PRESSED_REPEAT) {
+       event == LV_EVENT_LONG_PRESSED || event == LV_EVENT_LONG_PRESSED_REPEAT ||
+       event == LV_EVENT_DRAG_BEGIN || event == LV_EVENT_DRAG_END || event == LV_EVENT_DRAG_THROW_BEGIN) {
         lv_event_send(page, event, lv_event_get_data());
     }
     /*clang-format on*/
