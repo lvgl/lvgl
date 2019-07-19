18d61a49ab95310fa79eefa8c979d164d34e7a42
patricklaf <patrick.lafarguette@gmail.com>
2019-07-19 17:05:21 +0200

    Button matrix hidden buttons
    Update button matrix to really hide buttons with LV_BTNM_CTRL_HIDDEN control parameter.

--- src/lv_objx/lv_btnm.c
+++ src/lv_objx/lv_btnm.c
@@ -195,8 +195,11 @@ void lv_btnm_set_map(const lv_obj_t * btnm, const char * map[])
         btn_cnt  = 0;
         /*Count the buttons in a line*/
         while(strcmp(map_p_tmp[btn_cnt], "\n") != 0 && strlen(map_p_tmp[btn_cnt]) != 0) { /*Check a line*/
-            unit_cnt += get_button_width(ext->ctrl_bits[btn_i + btn_cnt]);
-            btn_cnt++;
+            /*Only deal with visible buttons*/
+            if (!button_is_hidden(ext->ctrl_bits[btn_i + btn_cnt])) {
+				unit_cnt += get_button_width(ext->ctrl_bits[btn_i + btn_cnt]);
+        	}
+			btn_cnt++;
         }
 
         /*Make sure the last row is at the bottom of 'btnm'*/
@@ -215,28 +218,30 @@ void lv_btnm_set_map(const lv_obj_t * btnm, const char * map[])
             lv_coord_t act_unit_w;
             unit_act_cnt = 0;
             for(i = 0; i < btn_cnt; i++) {
-                /* one_unit_w = all_unit_w / unit_cnt
-                 * act_unit_w = one_unit_w * button_width
-                 * do this two operations but the multiply first to divide a greater number */
-                act_unit_w = (all_unit_w * get_button_width(ext->ctrl_bits[btn_i])) / unit_cnt;
-                act_unit_w--; /*-1 because e.g. width = 100 means 101 pixels (0..100)*/
+                /*Only deal with visible buttons*/
+            	if(!button_is_hidden(ext->ctrl_bits[btn_i])) {
+					/* one_unit_w = all_unit_w / unit_cnt
+					 * act_unit_w = one_unit_w * button_width
+					 * do this two operations but the multiply first to divide a greater number */
+					act_unit_w = (all_unit_w * get_button_width(ext->ctrl_bits[btn_i])) / unit_cnt;
+					act_unit_w--; /*-1 because e.g. width = 100 means 101 pixels (0..100)*/
 
-                /*Always recalculate act_x because of rounding errors */
-                act_x = (unit_act_cnt * all_unit_w) / unit_cnt + i * style_bg->body.padding.inner +
-                        style_bg->body.padding.left;
+					/*Always recalculate act_x because of rounding errors */
+					act_x = (unit_act_cnt * all_unit_w) / unit_cnt + i * style_bg->body.padding.inner +
+							style_bg->body.padding.left;
 
-                /* Set the button's area.
-                 * If inner padding is zero then use the prev. button x2 as x1 to avoid rounding
-                 * errors*/
-                if(style_bg->body.padding.inner == 0 && act_x != style_bg->body.padding.left) {
-                    lv_area_set(&ext->button_areas[btn_i], ext->button_areas[btn_i - 1].x2, act_y, act_x + act_unit_w,
-                                act_y + btn_h);
-                } else {
-                    lv_area_set(&ext->button_areas[btn_i], act_x, act_y, act_x + act_unit_w, act_y + btn_h);
-                }
+					/* Set the button's area.
+					 * If inner padding is zero then use the prev. button x2 as x1 to avoid rounding
+					 * errors*/
+					if(style_bg->body.padding.inner == 0 && act_x != style_bg->body.padding.left) {
+						lv_area_set(&ext->button_areas[btn_i], ext->button_areas[btn_i - 1].x2, act_y, act_x + act_unit_w,
+									act_y + btn_h);
+					} else {
+						lv_area_set(&ext->button_areas[btn_i], act_x, act_y, act_x + act_unit_w, act_y + btn_h);
+					}
 
-                unit_act_cnt += get_button_width(ext->ctrl_bits[btn_i]);
-
+					unit_act_cnt += get_button_width(ext->ctrl_bits[btn_i]);
+            	}
                 i_tot++;
                 btn_i++;
             }
