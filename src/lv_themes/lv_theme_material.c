786e7936341a93040ea169dc1d58a75e4ed41bd7
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-07-27 17:40:20 +0200

    theme_material: improve group style mod funvtions

--- src/lv_themes/lv_theme_material.c
+++ src/lv_themes/lv_theme_material.c
@@ -807,18 +807,20 @@ static void style_mod(lv_group_t * group, lv_style_t * style)
 {
     (void)group; /*Unused*/
 #if LV_COLOR_DEPTH != 1
+    uint16_t hue2 = (_hue + 60) % 360;
+
     /*Make the style to be a little bit orange*/
     style->body.border.opa   = LV_OPA_COVER;
-    style->body.border.color = lv_color_hsv_to_rgb(_hue, 90, 70);
+    style->body.border.color = lv_color_hsv_to_rgb(hue2, 90, 70);
 
     /*If not empty or has border then emphasis the border*/
-    if(style->body.opa != LV_OPA_TRANSP || style->body.border.width != 0) style->body.border.width = LV_DPI / 20;
+    if(style->body.opa != LV_OPA_TRANSP || style->body.border.width != 0) style->body.border.width = LV_DPI / 30;
 
-    style->body.main_color   = lv_color_mix(style->body.main_color, lv_color_hsv_to_rgb(_hue, 90, 70), LV_OPA_70);
-    style->body.grad_color   = lv_color_mix(style->body.grad_color, lv_color_hsv_to_rgb(_hue, 90, 70), LV_OPA_70);
-    style->body.shadow.color = lv_color_mix(style->body.shadow.color, lv_color_hsv_to_rgb(_hue, 90, 70), LV_OPA_60);
+    style->body.main_color   = lv_color_mix(style->body.main_color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
+    style->body.grad_color   = lv_color_mix(style->body.grad_color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
+    style->body.shadow.color = lv_color_mix(style->body.shadow.color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_60);
 
-    style->text.color = lv_color_mix(style->text.color, lv_color_hsv_to_rgb(_hue, 90, 70), LV_OPA_70);
+    style->text.color = lv_color_mix(style->text.color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
 #else
     style->body.border.opa   = LV_OPA_COVER;
     style->body.border.color = LV_COLOR_BLACK;
@@ -828,6 +830,9 @@ static void style_mod(lv_group_t * group, lv_style_t * style)
 
 static void style_mod_edit(lv_group_t * group, lv_style_t * style)
 {
+
+    uint16_t hue2 = (_hue + 300) % 360;
+
     (void)group; /*Unused*/
 #if LV_COLOR_DEPTH != 1
     /*Make the style to be a little bit orange*/
@@ -835,13 +840,14 @@ static void style_mod_edit(lv_group_t * group, lv_style_t * style)
     style->body.border.color = LV_COLOR_GREEN;
 
     /*If not empty or has border then emphasis the border*/
-    if(style->body.opa != LV_OPA_TRANSP || style->body.border.width != 0) style->body.border.width = LV_DPI / 20;
+    if(style->body.opa != LV_OPA_TRANSP || style->body.border.width != 0) style->body.border.width = LV_DPI / 30;
 
-    style->body.main_color   = lv_color_mix(style->body.main_color, LV_COLOR_GREEN, LV_OPA_70);
-    style->body.grad_color   = lv_color_mix(style->body.grad_color, LV_COLOR_GREEN, LV_OPA_70);
-    style->body.shadow.color = lv_color_mix(style->body.shadow.color, LV_COLOR_GREEN, LV_OPA_60);
 
-    style->text.color = lv_color_mix(style->text.color, LV_COLOR_GREEN, LV_OPA_70);
+    style->body.main_color   = lv_color_mix(style->body.main_color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
+    style->body.grad_color   = lv_color_mix(style->body.grad_color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
+    style->body.shadow.color = lv_color_mix(style->body.shadow.color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_60);
+
+    style->text.color = lv_color_mix(style->text.color, lv_color_hsv_to_rgb(hue2, 90, 70), LV_OPA_70);
 #else
     style->body.border.opa   = LV_OPA_COVER;
     style->body.border.color = LV_COLOR_BLACK;
