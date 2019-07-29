8361a9c23d40981d87adc01d43b2b3226753b407
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-07-29 15:09:21 +0200

    font: add const to lv_font_fmt_txt_dsc_t

--- src/lv_font/lv_font_roboto_12.c
+++ src/lv_font/lv_font_roboto_12.c
@@ -1677,7 +1677,7 @@ static const lv_font_fmt_txt_kern_pair_t kern_pairs =
  *--------------------*/
 
 /*Store all the custom data of the font*/
-static lv_font_fmt_txt_dsc_t font_dsc = {
+static const lv_font_fmt_txt_dsc_t font_dsc = {
     .glyph_bitmap = gylph_bitmap,
     .glyph_dsc = glyph_dsc,
     .cmaps = cmaps,
