833849c006265015d2d2aef8a53f9f7d4f740a97
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-11-04 08:49:30 +0100

    move version to lvgl.h

--- lvgl.h
+++ lvgl.h
@@ -76,6 +76,11 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
+/*Current version of LittlevGL*/
+#define LVGL_VERSION_MAJOR   7
+#define LVGL_VERSION_MINOR   0
+#define LVGL_VERSION_PATCH   0
+#define LVGL_VERSION_INFO    "dev"
 
 /**********************
  *      TYPEDEFS
@@ -88,6 +93,30 @@ extern "C" {
 /**********************
  *      MACROS
  **********************/
+
+/** Gives 1 if the x.y.z version is supported in the current version
+ * Usage:
+ *
+ * - Require v6
+ * #if LV_VERSION_CHECK(6,0,0)
+ *   new_func_in_v6();
+ * #endif
+ *
+ *
+ * - Require at least v5.3
+ * #if LV_VERSION_CHECK(5,3,0)
+ *   new_feature_from_v5_3();
+ * #endif
+ *
+ *
+ * - Require v5.3.2 bugfixes
+ * #if LV_VERSION_CHECK(5,3,2)
+ *   bugfix_in_v5_3_2();
+ * #endif
+ *
+ * */
+#define LV_VERSION_CHECK(x,y,z) (x == LVGL_VERSION_MAJOR && (y < LVGL_VERSION_MINOR || (y == LVGL_VERSION_MINOR && z <= LVGL_VERSION_PATCH)))
+
 
 #ifdef __cplusplus
 }
