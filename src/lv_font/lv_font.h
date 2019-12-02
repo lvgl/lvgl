0b12f934ecf07525796ed444de4ce2add26b7814
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-12-02 09:16:47 +0100

    lv_font.h add update comments

--- src/lv_font/lv_font.h
+++ src/lv_font/lv_font.h
@@ -77,7 +77,7 @@ typedef struct _lv_font_struct
     uint8_t line_height;      /**< The real line height where any text fits*/
     uint8_t base_line;        /**< Base line measured from the top of the line_height*/
     uint8_t subpx  :2;        /**< An element of `lv_font_subpx_t`*/
-    void * dsc;               /**< Store implementation specific data here*/
+    void * dsc;               /**< Store implementation specific or run_time data or caching here*/
 #if LV_USE_USER_DATA
     lv_font_user_data_t user_data; /**< Custom user data for font. */
 #endif
