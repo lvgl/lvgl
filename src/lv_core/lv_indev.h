72b3cd80b90331c1b973a5b2e7a5cb74e59cf004
TridentTD <prote2000@gmail.com>
2019-12-04 04:19:25 +0700

    unify indev_search_obj()  to lv_indev_search_obj()

--- src/lv_core/lv_indev.h
+++ src/lv_core/lv_indev.h
@@ -128,6 +128,14 @@ bool lv_indev_is_dragging(const lv_indev_t * indev);
 void lv_indev_get_vect(const lv_indev_t * indev, lv_point_t * point);
 
 /**
+ * Manually finish dragging.
+ * `LV_SIGNAL_DRAG_END` and `LV_EVENT_DRAG_END` will be sent.
+ * @param indev pointer to an input device
+ * @return `LV_RES_INV` if the object being dragged was deleted. Else `LV_RES_OK`.
+ */
+lv_res_t lv_indev_finish_drag(lv_indev_t * indev);
+
+/**
  * Do nothing until the next release
  * @param indev pointer to an input device
  */
@@ -149,12 +157,12 @@ lv_task_t * lv_indev_get_read_task(lv_disp_t * indev);
 lv_obj_t * lv_indev_get_obj_act(void);
 
 /**
- * Get the most top, clickable object by a point
+ * Search the most top, clickable object by a point
  * @param obj pointer to a start object, typically the screen
  * @param point pointer to a point for searhing the most top child
  * @return pointer to the found object or NULL if there was no suitable object
  */
-lv_obj_t * lv_indev_get_obj(lv_obj_t * obj, lv_point_t *point);
+lv_obj_t * lv_indev_search_obj(lv_obj_t * obj, lv_point_t *point);
 
 /**********************
  *      MACROS
