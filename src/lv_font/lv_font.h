bc7efef53722023bf492456c70747e461a1960f6
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-02-07 14:03:51 +0100

    make base_line signed

--- src/lv_font/lv_font.h
+++ src/lv_font/lv_font.h
@@ -75,7 +75,7 @@ typedef struct _lv_font_struct
 
     /*Pointer to the font in a font pack (must have the same line height)*/
     uint8_t line_height;      /**< The real line height where any text fits*/
-    uint8_t base_line;        /**< Base line measured from the top of the line_height*/
+    int8_t base_line;        /**< Base line measured from the top of the line_height*/
     uint8_t subpx  :2;        /**< An element of `lv_font_subpx_t`*/
     void * dsc;               /**< Store implementation specific or run_time data or caching here*/
 #if LV_USE_USER_DATA
