f5e61fdb38c6d3cfd1a6edf4e6b90c7476e69d08
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-02-26 23:50:16 +0100

    Update lv_version.h

--- src/lv_version.h
+++ src/lv_version.h
@@ -16,7 +16,7 @@ extern "C" {
 /*Current version of LittlevGL*/
 #define LVGL_VERSION_MAJOR   6
 #define LVGL_VERSION_MINOR   1
-#define LVGL_VERSION_PATCH   1
+#define LVGL_VERSION_PATCH   2
 #define LVGL_VERSION_INFO    ""
 
 
