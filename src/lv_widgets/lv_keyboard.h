4fe85b5516d442e4b9c43654fef520ae167905d8
xennex22 <25083624+xennex22@users.noreply.github.com>
2020-02-28 08:16:11 -0800

    Moved define to header

--- src/lv_widgets/lv_keyboard.h
+++ src/lv_widgets/lv_keyboard.h
@@ -32,7 +32,8 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
-
+#define LV_KEYBOARD_CTRL_BTN_FLAGS (LV_BTNMATRIX_CTRL_NO_REPEAT | LV_BTNMATRIX_CTRL_CLICK_TRIG)
+    
 /**********************
  *      TYPEDEFS
  **********************/
