fe28c947c481e4a796d8490c42543b4c6c9daf33
hamidrm <hr_mehrabian@yahoo.com>
2020-03-14 21:43:09 +0330

    Arabic and Persian text support

--- src/lv_font/lv_font.mk
+++ src/lv_font/lv_font.mk
@@ -5,6 +5,7 @@ CSRCS += lv_font_roboto_16.c
 CSRCS += lv_font_roboto_22.c
 CSRCS += lv_font_roboto_28.c
 CSRCS += lv_font_unscii_8.c
+CSRCS += lv_font_ap_18.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_font
 VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_font
