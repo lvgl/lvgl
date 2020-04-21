c340a7b580013b915b6efd73dea7598136816428
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-04-21 22:21:42 +0200

    minor fixes

--- src/lv_draw/lv_draw_rect.c
+++ src/lv_draw/lv_draw_rect.c
@@ -274,10 +274,12 @@ static void draw_bg(const lv_area_t * coords, const lv_area_t * clip, lv_draw_re
                               grad_color, mask_buf, mask_res, opa, dsc->bg_blend_mode);
 
                 /*Center part*/
-                fill_area2.x1 = coords_bg.x1 + rout;
-                fill_area2.x2 = coords_bg.x2 - rout;
-                lv_blend_fill(clip, &fill_area2,
-                              grad_color, NULL, LV_DRAW_MASK_RES_FULL_COVER, opa, dsc->bg_blend_mode);
+                if(dsc->bg_grad_dir == LV_GRAD_DIR_VER) {
+                    fill_area2.x1 = coords_bg.x1 + rout;
+                    fill_area2.x2 = coords_bg.x2 - rout;
+                    lv_blend_fill(clip, &fill_area2,
+                                  grad_color, NULL, LV_DRAW_MASK_RES_FULL_COVER, opa, dsc->bg_blend_mode);
+                }
 
                 /*Right part*/
                 fill_area2.x1 = coords_bg.x2 - rout + 1;
