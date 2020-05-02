95d5298ad95dc572f8ba14ff401ef688d5d811f8
Themba Dube <embeddedthemba@gmail.com>
2020-05-02 18:36:52 -0400

    Add some more 6.x compability macros

--- src/lv_api_map.h
+++ src/lv_api_map.h
@@ -36,6 +36,12 @@ static inline void lv_task_once(lv_task_t * task)
     lv_task_set_repeat_count(task, 1);
 }
 
+#if LV_USE_CHECKBOX
+
+#define lv_checkbox_set_static_text lv_checkbox_set_text_static
+
+#endif
+
 #if LV_USE_CHART
 
 #define lv_chart_get_point_cnt lv_chart_get_point_count
@@ -56,6 +62,8 @@ static inline bool lv_dropdown_get_draw_arrow(lv_obj_t * ddlist)
     if(lv_dropdown_get_symbol(ddlist)) return true;
     else return false;
 }
+
+#define lv_dropdown_set_static_options lv_dropdown_set_options_static
 
 #endif
 
@@ -86,6 +94,12 @@ static inline bool lv_bar_get_sym(lv_obj_t * bar)
 {
     return lv_bar_get_type(bar) == LV_BAR_TYPE_SYMMETRICAL;
 }
+
+#endif
+
+#if LV_USE_LABEL
+
+#define lv_label_set_static_text lv_label_set_text_static
 
 #endif
 
