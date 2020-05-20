823348347796c6457e1bff0792fba4c01606392d
Amir Gonnen <amirgonnen@gmail.com>
2020-05-20 03:15:49 +0300

    Add missing LV_GC_INCLUDE (#1511)

--- src/lv_draw/lv_draw_mask.c
+++ src/lv_draw/lv_draw_mask.c
@@ -16,6 +16,10 @@
 #include "../lv_core/lv_debug.h"
 #include "../lv_misc/lv_gc.h"
 
+#if defined(LV_GC_INCLUDE)
+    #include LV_GC_INCLUDE
+#endif /* LV_ENABLE_GC */
+
 /*********************
  *      DEFINES
  *********************/
