af812b0c55e8e45ca129f02112c872401e87a0e6
Themba Dube <embeddedthemba@gmail.com>
2020-05-31 08:33:21 -0400

    keyboard: allow backspace to auto-repeat in numeric mode (lv_examples#58)

--- src/lv_widgets/lv_keyboard.c
+++ src/lv_widgets/lv_keyboard.c
@@ -67,7 +67,7 @@ static const char * const default_kb_map_spec[] = {"0", "1", "2", "3", "4", "5",
                                                   };
 
 static const lv_btnmatrix_ctrl_t default_kb_ctrl_spec_map[] = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, LV_KEYBOARD_CTRL_BTN_FLAGS | 2,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
     LV_KEYBOARD_CTRL_BTN_FLAGS | 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     LV_KEYBOARD_CTRL_BTN_FLAGS | 2, 2, 6, 2, LV_KEYBOARD_CTRL_BTN_FLAGS | 2
