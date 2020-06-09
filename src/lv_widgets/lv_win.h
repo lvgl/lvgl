2109450cd654979b317c24dd5c70bbcdfb308118
C47D <carlos.santiago.diaz@gmail.com>
2020-06-09 12:09:52 -0500

    lv_win: Define lv_win_btn_ext_t, adding an alignment_in_header property to buttons on the window header

--- src/lv_widgets/lv_win.h
+++ src/lv_widgets/lv_win.h
@@ -69,6 +69,21 @@ enum {
     _LV_WIN_PART_REAL_LAST
 };
 
+/** Extended data of win_btn*/
+typedef struct {
+    /** Ext. of ancestor*/
+    lv_btn_ext_t btn;
+
+    /** Which side of the header should the button be aligned to.
+     * 0: Align to right (default), 1: Align to left */
+    uint8_t alignment_in_header : 1;
+} lv_win_btn_ext_t;
+
+enum {
+	LV_WIN_BTN_ALIGNEMT_RIGHT = 0, 	/**< Align button to right of the header */
+	LV_WIN_BTN_ALIGNEMT_LEFT	/**< Align button to left of the header */
+};
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
