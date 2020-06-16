ad215c5b4b7f35ddbf0a6eb525b21aae1e6e576b
PeterB <PeterB@PETE-LT.otg.local>
2020-06-16 16:24:37 +0100

    Finalise changes for new Chart Functions

--- src/lv_widgets/lv_chart.h
+++ src/lv_widgets/lv_chart.h
@@ -79,7 +79,7 @@ typedef struct {
 typedef struct {
     /*No inherited ext*/ /*Ext. of ancestor*/
     /*New data for this type */
-    lv_ll_t series_ll;    /*Linked list for the data line pointers (stores lv_chart_dl_t)*/
+    lv_ll_t series_ll;    /*Linked list for the data line pointers (stores lv_chart_series_t)*/
     lv_coord_t ymin;      /*y min value (used to scale the data)*/
     lv_coord_t ymax;      /*y max value (used to scale the data)*/
     uint8_t hdiv_cnt;     /*Number of horizontal division lines*/
