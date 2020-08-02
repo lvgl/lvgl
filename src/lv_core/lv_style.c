28213adfa62f70fa3a25b10baf305b99bbc14436
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-08-02 12:04:10 +0200

    -fix using freed memory in _lv_style_list_remove_style Fixes: #1694

--- src/lv_core/lv_style.c
+++ src/lv_core/lv_style.c
@@ -257,7 +257,7 @@ void _lv_style_list_remove_style(lv_style_list_t * list, lv_style_t * style)
         return;
     }
 
-    lv_style_t ** new_classes = lv_mem_realloc(list->style_list, sizeof(lv_style_t *) * (list->style_cnt - 1));
+    lv_style_t ** new_classes = lv_mem_alloc(sizeof(lv_style_t *) * (list->style_cnt - 1));
     LV_ASSERT_MEM(new_classes);
     if(new_classes == NULL) {
         LV_LOG_WARN("lv_style_list_remove_style: couldn't reallocate class list");
@@ -270,6 +270,8 @@ void _lv_style_list_remove_style(lv_style_list_t * list, lv_style_t * style)
         j++;
 
     }
+
+    lv_mem_free(list->style_list);
 
     list->style_cnt--;
     list->style_list = new_classes;
