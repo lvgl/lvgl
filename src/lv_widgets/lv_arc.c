9148585d5d23cb4cea55055b46e69594ada06d72
Adam Martini <acumartini [at] gmail.com>
2020-08-24 09:18:57 -0700

    Fix func call name:

--- src/lv_widgets/lv_arc.c
+++ src/lv_widgets/lv_arc.c
@@ -463,7 +463,7 @@ void lv_arc_set_range(lv_obj_t * arc, int16_t min, int16_t max)
         ext->cur_value = max;
     }
 
-    update_value(arc) /* value has changed relative to the new range */
+    value_update(arc); /* value has changed relative to the new range */
 }
 
 /**
