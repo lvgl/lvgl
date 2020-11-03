2f40ca37df46e5f34f70d4644d2966fdb29ed8ab
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-11-03 10:02:00 +0100

    Update master version

--- lvgl.h
+++ lvgl.h
@@ -16,8 +16,8 @@ extern "C" {
  ***************************/
 #define LVGL_VERSION_MAJOR 7
 #define LVGL_VERSION_MINOR 7
-#define LVGL_VERSION_PATCH 1
-#define LVGL_VERSION_INFO ""
+#define LVGL_VERSION_PATCH 2
+#define LVGL_VERSION_INFO "dev"
 
 /*********************
  *      INCLUDES
