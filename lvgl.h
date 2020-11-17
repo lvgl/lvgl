4187286d9c6fd513f409318b62db6b643baf48f4
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-11-17 11:40:38 +0100

    Update dev version

--- lvgl.h
+++ lvgl.h
@@ -14,10 +14,10 @@ extern "C" {
 /***************************
  * CURRENT VERSION OF LVGL
  ***************************/
-#define LVGL_VERSION_MAJOR 7
-#define LVGL_VERSION_MINOR 8
+#define LVGL_VERSION_MAJOR 8
+#define LVGL_VERSION_MINOR 0
 #define LVGL_VERSION_PATCH 0
-#define LVGL_VERSION_INFO ""
+#define LVGL_VERSION_INFO "dev"
 
 /*********************
  *      INCLUDES
