ab76f4deda8013beeea68cdf703081ec80d6fca4
Benjamin Schilling <benjamin.schilling33@gmail.com>
2020-11-22 17:00:12 +0100

    fix(examples): Updated the Arduino example to compile using the latest Arduino libraries (7.7.2)
    Also apply the editorconfig to the Arduino Example to use the same indentation as for the library

--- examples/LVGL_Arduino.ino
+++ examples/LVGL_Arduino.ino
@@ -1,16 +1,22 @@
 #include <lvgl.h>
 #include <TFT_eSPI.h>
+/* If you want to use the LVGL examples,
+   make sure to install the lv_examples Arduino library */
+#include <lv_examples.h>
 
 TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
 static lv_disp_buf_t disp_buf;
 static lv_color_t buf[LV_HOR_RES_MAX * 10];
 
-#if USE_LV_LOG != 0
-/* Serial debugging */
-void my_print(lv_log_level_t level, const char * file, uint32_t line, const char * dsc)
-{
+/* Change to your screen resolution */
+static uint32_t screenWidth = 320;
+static uint32_t screenHeight = 240;
 
-    Serial.printf("%s@%d->%s\r\n", file, line, dsc);
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(lv_log_level_t level, const char *file, uint32_t line, const char *fn_name, const char *dsc)
+{
+    Serial.printf("%s(%s)@%d->%s\r\n", file, fn_name, line, dsc);
     Serial.flush();
 }
 #endif
@@ -30,42 +36,41 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 }
 
 /*Read the touchpad*/
-bool my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
+bool my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
     uint16_t touchX, touchY;
 
     bool touched = tft.getTouch(&touchX, &touchY, 600);
 
-    if(!touched)
+    if (!touched)
     {
-      data->state = LV_INDEV_STATE_REL;
-      return false;
+        data->state = LV_INDEV_STATE_REL;
+        return false;
     }
     else
     {
-      data->state = LV_INDEV_STATE_PR;
+        data->state = LV_INDEV_STATE_PR;
     }
 
-    if(touchX>screenWidth || touchY > screenHeight)
+    if (touchX > screenWidth || touchY > screenHeight)
     {
-      Serial.println("Y or y outside of expected parameters..");
-      Serial.print("y:");
-      Serial.print(touchX);
-      Serial.print(" x:");
-      Serial.print(touchY);
+        Serial.println("Y or y outside of expected parameters..");
+        Serial.print("y:");
+        Serial.print(touchX);
+        Serial.print(" x:");
+        Serial.print(touchY);
     }
     else
     {
-      /*Set the coordinates*/
-      data->point.x = touchX;
-      data->point.y = touchY;
-  
-      Serial.print("Data x");
-      Serial.println(touchX);
-      
-      Serial.print("Data y");
-      Serial.println(touchY);
+        /*Set the coordinates*/
+        data->point.x = touchX;
+        data->point.y = touchY;
 
+        Serial.print("Data x");
+        Serial.println(touchX);
+
+        Serial.print("Data y");
+        Serial.println(touchY);
     }
 
     return false; /*Return `false` because we are not buffering and no more data to read*/
@@ -77,43 +82,45 @@ void setup()
 
     lv_init();
 
-#if USE_LV_LOG != 0
+#if LV_USE_LOG != 0
     lv_log_register_print_cb(my_print); /* register print function for debugging */
 #endif
 
-    tft.begin(); /* TFT init */
+    tft.begin();        /* TFT init */
     tft.setRotation(1); /* Landscape orientation */
 
-    uint16_t calData[5] = { 275, 3620, 264, 3532, 1 };
+    /* Set the touchscreen calibration data,
+     the actual data for your display can be aquired using
+     the Generich -> Touch_calibrate example from the TFT_eSPI library */
+    uint16_t calData[5] = {275, 3620, 264, 3532, 1};
     tft.setTouch(calData);
-  
+
     lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
 
-    /*Initialize the display*/
+    /* Initialize the display */
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
     disp_drv.hor_res = 320;
     disp_drv.ver_res = 240;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
-    /*Initialize the (dummy) input device driver*/
+    /* Initialize the (dummy) input device driver */
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_touchpad_read;
     lv_indev_drv_register(&indev_drv);
 
-		/* Try an example from the lv_examples repository
-		 * https://github.com/lvgl/lv_examples*/
-		lv_ex_btn_1();
+    /* Try an example from the lv_examples repository
+     https://github.com/lvgl/lv_examples */
+    lv_ex_btn_1();
 }
-
 
 void loop()
 {
-
     lv_task_handler(); /* let the GUI do its work */
     delay(5);
 }
