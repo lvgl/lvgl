b04dea5971dd968a38bbfe24c58d245a5669ae3b
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-12-10 11:18:30 +0100

    fix warnings

--- src/lv_draw/lv_draw_img.c
+++ src/lv_draw/lv_draw_img.c
@@ -659,6 +659,8 @@ static void draw_cleanup(lv_img_cache_entry_t * cache)
 /*Automatically close images with no caching*/
 #if LV_IMG_CACHE_DEF_SIZE == 0
     lv_img_decoder_close(&cache->dec_dsc);
+#else
+    LV_UNUSED(cache);
 #endif
 }
 
