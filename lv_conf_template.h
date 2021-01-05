e3ad7918e2090ab8472a9c50878546a5f9a44457
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-01-05 16:04:29 +0100

    Update master version

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -1,6 +1,6 @@
 /**
  * @file lv_conf.h
- * Configuration file for v7.9.0
+ * Configuration file for v7.9.1
  */
 
 /*
--- lvgl.h
+++ lvgl.h
@@ -16,8 +16,8 @@ extern "C" {
  ***************************/
 #define LVGL_VERSION_MAJOR 7
 #define LVGL_VERSION_MINOR 9
-#define LVGL_VERSION_PATCH 0
-#define LVGL_VERSION_INFO ""
+#define LVGL_VERSION_PATCH 1
+#define LVGL_VERSION_INFO "dev"
 
 /*********************
  *      INCLUDES
