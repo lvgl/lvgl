9c1d81db90be1b9392b7742be2d36ad13a9928df
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-01-19 14:50:06 +0100

    Update dev version

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -1,6 +1,6 @@
 /**
  * @file lv_conf.h
- * Configuration file for v7.10.0-dev
+ * Configuration file for v7.11.0
  */
 
 /*
--- lvgl.h
+++ lvgl.h
@@ -14,7 +14,7 @@ extern "C" {
  * CURRENT VERSION OF LVGL
  ***************************/
 #define LVGL_VERSION_MAJOR 7
-#define LVGL_VERSION_MINOR 10
+#define LVGL_VERSION_MINOR 11
 #define LVGL_VERSION_PATCH 0
 #define LVGL_VERSION_INFO "dev"
 
