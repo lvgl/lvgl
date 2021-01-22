780e05be2ab976ec56fdf9975536e049f28273f6
scandermore <63395212+scandermore@users.noreply.github.com>
2021-01-22 17:04:25 +0100

    Update lv_indev.c

--- src/lv_core/lv_indev.c
+++ src/lv_core/lv_indev.c
@@ -1148,7 +1148,12 @@ lv_obj_t * lv_indev_search_obj(lv_obj_t * obj, lv_point_t * point)
                 hidden_i = lv_obj_get_parent(hidden_i);
             }
             /*No parent found with hidden == true*/
-            if(hidden_i == NULL && (lv_obj_get_state(obj, LV_OBJ_PART_MAIN) & LV_STATE_DISABLED) == false) found_p = obj;
+            if (indev_obj_act->disabled_noevent == 1){
+                if(hidden_i == NULL && (lv_obj_get_state(obj, LV_OBJ_PART_MAIN) & LV_STATE_DISABLED) == false) found_p = obj;
+            }
+            else {
+                if(hidden_i == NULL) found_p = obj;
+            }
         }
     }
 
