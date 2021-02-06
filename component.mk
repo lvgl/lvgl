7422154ac44a17a360823690540eb0448cfbd139
C47D <carlos.santiago.diaz@gmail.com>
2021-02-06 12:44:35 -0600

    component.mk: Check for IDF_VER
    Checks for IDF_VER when adding COMPONENT_SRCDIRS and COMPONENT_ADD_INCLUDEDIRS
    to avoid definig them when not using LVGL as ESP-IDF component.

--- component.mk
+++ component.mk
@@ -1,4 +1,14 @@
 # ESP-IDF component file for make based commands
 
+ifdef $(IDF_VER)
+
+$(info Adding LVGL as ESP-IDF component)
+
 COMPONENT_SRCDIRS := .
 COMPONENT_ADD_INCLUDEDIRS := .
+
+else
+
+$(info IDF_VER not defined)
+
+endif
