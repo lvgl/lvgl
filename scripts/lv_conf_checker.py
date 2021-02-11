9b52db053122a0bfa2ea497a6516b9c546901538
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-02-11 02:49:50 -0800

    fix(conf): remove esp specific inclusion from lv_conf_internal.h (#2065)
    * fix(lv_conf_internal.h): remove esp specific inclusion
    
    since the same thing is already included in lv_conf_kconfig.h
    
    * fix(library.json): change space to tab

--- scripts/lv_conf_checker.py
+++ scripts/lv_conf_checker.py
@@ -27,12 +27,6 @@ fout.write(
 
 #include <stdint.h>
 
-/* Add ESP-IDF related includes */
-#if defined (ESP_PLATFORM)
-#  include "sdkconfig.h"
-#  include "esp_attr.h"
-#endif
-
 /* Handle special Kconfig options */
 #include "lv_conf_kconfig.h"
 
