19c3613029d50bc740598ad3dd9db1812992d968
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-02-14 15:49:18 +0800

    fix(lv_*.mk): add the miss source code
    and reorder the entry to help finding the mismatch

--- lvgl.mk
+++ lvgl.mk
@@ -1,9 +1,9 @@
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_core/lv_core.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_hal/lv_hal.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_widgets/lv_widgets.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_font/lv_font.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_misc/lv_misc.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_draw/lv_draw.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_gpu/lv_gpu.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/extra/extra.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/examples/examples.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/extra/extra.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_core/lv_core.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_draw/lv_draw.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_font/lv_font.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_gpu/lv_gpu.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_hal/lv_hal.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_misc/lv_misc.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_widgets/lv_widgets.mk
--- src/lv_gpu/lv_gpu.mk
+++ src/lv_gpu/lv_gpu.mk
@@ -1,3 +1,6 @@
+CSRCS += lv_gpu_nxp_pxp.c
+CSRCS += lv_gpu_nxp_pxp_osa.c
+CSRCS += lv_gpu_nxp_vglite.c
 CSRCS += lv_gpu_stm32_dma2d.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_gpu
