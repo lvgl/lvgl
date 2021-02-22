20f7e4daef51ee609cde030469ef4e267aaab8ba
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-02-22 20:40:54 +0800

    fix(gc): Iterate the root variables only when they are really used
    to save the memory space

--- src/lv_misc/lv_gc.h
+++ src/lv_misc/lv_gc.h
@@ -25,21 +25,29 @@ extern "C" {
  *      DEFINES
  *********************/
 
-#define LV_ITERATE_ROOTS(f) \
-    f(lv_ll_t, _lv_timer_ll)  /*Linked list to store the lv_tmr-s*/ \
-    f(lv_ll_t, _lv_disp_ll)  /*Linked list of screens*/            \
-    f(lv_ll_t, _lv_indev_ll) /*Linked list of input device*/       \
-    f(lv_ll_t, _lv_fsdrv_ll)                                       \
-    f(lv_ll_t, _lv_anim_ll)                                        \
-    f(lv_ll_t, _lv_group_ll)                                       \
-    f(lv_ll_t, _lv_img_decoder_ll)                                 \
-    f(lv_ll_t, _lv_obj_style_trans_ll)                             \
-    f(lv_img_cache_entry_t*, _lv_img_cache_array)                  \
-    f(lv_timer_t*, _lv_timer_act)                                      \
-    f(lv_mem_buf_arr_t , lv_mem_buf)                              \
-    f(_lv_draw_mask_saved_arr_t , _lv_draw_mask_list)              \
-    f(void * , _lv_theme_default_styles)                          \
-    f(uint8_t *, _lv_font_decompr_buf)                             \
+#define LV_DISPATCH(f, t, n)            f(t, n)
+#define LV_DISPATCH_COND(f, t, n, m, v) LV_CONCAT3(LV_DISPATCH, m, v)(f, t, n)
+
+#define LV_DISPATCH00(f, t, n)          LV_DISPATCH(f, t, n)
+#define LV_DISPATCH01(f, t, n)
+#define LV_DISPATCH10(f, t, n)
+#define LV_DISPATCH11(f, t, n)          LV_DISPATCH(f, t, n)
+
+#define LV_ITERATE_ROOTS(f)                                                                 \
+    LV_DISPATCH(f, lv_ll_t, _lv_timer_ll) /*Linked list to store the lv_timers*/            \
+    LV_DISPATCH(f, lv_ll_t, _lv_disp_ll)  /*Linked list of display device*/                 \
+    LV_DISPATCH(f, lv_ll_t, _lv_indev_ll) /*Linked list of input device*/                   \
+    LV_DISPATCH(f, lv_ll_t, _lv_fsdrv_ll)                                                   \
+    LV_DISPATCH(f, lv_ll_t, _lv_anim_ll)                                                    \
+    LV_DISPATCH(f, lv_ll_t, _lv_group_ll)                                                   \
+    LV_DISPATCH(f, lv_ll_t, _lv_img_decoder_ll)                                             \
+    LV_DISPATCH(f, lv_ll_t, _lv_obj_style_trans_ll)                                         \
+    LV_DISPATCH(f, lv_img_cache_entry_t*, _lv_img_cache_array)                              \
+    LV_DISPATCH(f, lv_timer_t*, _lv_timer_act)                                              \
+    LV_DISPATCH(f, lv_mem_buf_arr_t , lv_mem_buf)                                           \
+    LV_DISPATCH_COND(f, _lv_draw_mask_saved_arr_t , _lv_draw_mask_list, LV_DRAW_COMPLEX, 1) \
+    LV_DISPATCH(f, void * , _lv_theme_default_styles)                                       \
+    LV_DISPATCH_COND(f, uint8_t *, _lv_font_decompr_buf, LV_USE_FONT_COMPRESSED, 1)
 
 #define LV_DEFINE_ROOT(root_type, root_name) root_type root_name;
 #define LV_ROOTS LV_ITERATE_ROOTS(LV_DEFINE_ROOT)
