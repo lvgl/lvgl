6b195371a1cee84ef5899bfce8925391014de0ab
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-02-26 14:01:09 +0800

    fix(image cache): move cache_temp to gc.h
    and rename it to _lv_img_cache_single

--- src/lv_misc/lv_gc.h
+++ src/lv_misc/lv_gc.h
@@ -24,6 +24,11 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
+#if LV_IMG_CACHE_DEF_SIZE
+#    define LV_IMG_CACHE_DEF            1
+#else
+#    define LV_IMG_CACHE_DEF            0
+#endif
 
 #define LV_DISPATCH(f, t, n)            f(t, n)
 #define LV_DISPATCH_COND(f, t, n, m, v) LV_CONCAT3(LV_DISPATCH, m, v)(f, t, n)
@@ -42,7 +47,8 @@ extern "C" {
     LV_DISPATCH(f, lv_ll_t, _lv_group_ll)                                                   \
     LV_DISPATCH(f, lv_ll_t, _lv_img_decoder_ll)                                             \
     LV_DISPATCH(f, lv_ll_t, _lv_obj_style_trans_ll)                                         \
-    LV_DISPATCH(f, lv_img_cache_entry_t*, _lv_img_cache_array)                              \
+    LV_DISPATCH_COND(f, lv_img_cache_entry_t*, _lv_img_cache_array, LV_IMG_CACHE_DEF, 1)    \
+    LV_DISPATCH_COND(f, lv_img_cache_entry_t, _lv_img_cache_single, LV_IMG_CACHE_DEF, 0)    \
     LV_DISPATCH(f, lv_timer_t*, _lv_timer_act)                                              \
     LV_DISPATCH(f, lv_mem_buf_arr_t , lv_mem_buf)                                           \
     LV_DISPATCH_COND(f, _lv_draw_mask_saved_arr_t , _lv_draw_mask_list, LV_DRAW_COMPLEX, 1) \
