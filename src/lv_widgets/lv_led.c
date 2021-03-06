1a287fa8af752af8ff07370dd1437e781709771b
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2021-03-06 11:13:08 +0200

    Cleanup LV_SIGNAL_GET_TYPE implementations
    Remove dead code in lv_label and lv_spinbox, there is already lv_obj_handle_get_type_signal() call higher up.
    Change lv_led to use lv_obj_handle_get_type_signal() like other widgets do.

--- src/lv_widgets/lv_led.c
+++ src/lv_widgets/lv_led.c
@@ -237,14 +237,7 @@ static lv_res_t lv_led_signal(lv_obj_t * led, lv_signal_t sign, void * param)
     res = ancestor_signal(led, sign, param);
     if(res != LV_RES_OK) return res;
 
-    if(sign == LV_SIGNAL_GET_TYPE) {
-        lv_obj_type_t * buf = param;
-        uint8_t i;
-        for(i = 0; i < LV_MAX_ANCESTOR_NUM - 1; i++) { /*Find the last set data*/
-            if(buf->type[i] == NULL) break;
-        }
-        buf->type[i] = "lv_led";
-    }
+    if(sign == LV_SIGNAL_GET_TYPE) return lv_obj_handle_get_type_signal(param, LV_OBJX_NAME);
 
     return res;
 }
