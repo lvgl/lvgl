4e669f40987e0a5c0d002328f9e96785386e0272
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-03-08 22:42:50 +0800

    fix(mem): change LV_MEM_ADD_JUNK to 0
    to speed up the normal operation

--- src/lv_misc/lv_mem.c
+++ src/lv_misc/lv_mem.c
@@ -23,7 +23,7 @@
  *********************/
 /*Add memory junk on alloc (0xaa) and free(0xbb) (just for testing purposes)*/
 #ifndef LV_MEM_ADD_JUNK
-#  define LV_MEM_ADD_JUNK  1
+#  define LV_MEM_ADD_JUNK  0
 #endif
 
 #ifdef LV_ARCH_64
