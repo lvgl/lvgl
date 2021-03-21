0f8d6075cd5164a2ddb87afbea4046b9f6edad1c
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-03-22 00:19:12 +0800

    fix(class): correct the typo error in comment

--- src/core/lv_obj_class.c
+++ src/core/lv_obj_class.c
@@ -63,7 +63,6 @@ lv_obj_t * lv_obj_create_from_class(const lv_obj_class_t * class_p, lv_obj_t * p
    }
 
     if(!copy) lv_theme_apply(obj);
-//    else lv_style_list_copy(&checkbox->style_indic, &checkbox_copy->style_indic);
 
     LV_TRACE_OBJ_CREATE("Object created at %p address with %p class on %p parent", obj, class_p, parent);
     return obj;
@@ -74,7 +73,7 @@ void _lv_obj_destruct(lv_obj_t * obj)
     if(obj->class_p->destructor_cb) obj->class_p->destructor_cb(obj);
 
     if(obj->class_p->base_class) {
-        /*Don't let the descendant methods run during constructing the ancestor type*/
+        /*Don't let the descendant methods run during destructing the ancestor type*/
         obj->class_p = obj->class_p->base_class;
 
         /*Call the base class's destructor too*/
@@ -114,9 +113,8 @@ static void lv_obj_construct(lv_obj_t * obj, lv_obj_t * parent, const lv_obj_t *
     obj->class_p = original_class_p;
 
     if(obj->class_p->constructor_cb) obj->class_p->constructor_cb(obj, copy);
-
-
 }
+
 static uint32_t get_instance_size(const lv_obj_class_t * class_p)
 {
     /*Find a base in which instance size is set*/
