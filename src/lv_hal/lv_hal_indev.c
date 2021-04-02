acf42d399977eecfc431f9dd0bbb070724bd8e0a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2021-04-02 06:51:49 -0400

    fix(indev) provide raw old points to read_cb, not rotated points (#2180)

--- src/lv_hal/lv_hal_indev.c
+++ src/lv_hal/lv_hal_indev.c
@@ -132,8 +132,8 @@ bool _lv_indev_read(lv_indev_t * indev, lv_indev_data_t * data)
     /* For touchpad sometimes users don't the last pressed coordinate on release.
      * So be sure a coordinates are initialized to the last point */
     if(indev->driver.type == LV_INDEV_TYPE_POINTER) {
-        data->point.x = indev->proc.types.pointer.act_point.x;
-        data->point.y = indev->proc.types.pointer.act_point.y;
+        data->point.x = indev->proc.types.pointer.last_raw_point.x;
+        data->point.y = indev->proc.types.pointer.last_raw_point.y;
     }
     /*Similarly set at least the last key in case of the user doesn't set it on release*/
     else if(indev->driver.type == LV_INDEV_TYPE_KEYPAD) {
