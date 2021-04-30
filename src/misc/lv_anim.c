f194079cd8f0a5fcae7d94221ac77c123da558e3
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-04-30 20:35:45 +0200

    fix(anim) fix bounce path

--- src/misc/lv_anim.c
+++ src/misc/lv_anim.c
@@ -335,7 +335,7 @@ int32_t lv_anim_path_overshoot(const lv_anim_t * a)
 int32_t lv_anim_path_bounce(const lv_anim_t * a)
 {
     /*Calculate the current step*/
-    uint32_t t = lv_map(a->act_time, 0, a->time, 0, 1024);
+    int32_t t = lv_map(a->act_time, 0, a->time, 0, 1024);
     int32_t diff = (a->end_value - a->start_value);
 
     /*3 bounces has 5 parts: 3 down and 2 up. One part is t / 5 long*/
@@ -372,6 +372,7 @@ int32_t lv_anim_path_bounce(const lv_anim_t * a)
     }
 
     if(t > 1024) t = 1024;
+    if(t < 0) t = 0;
     int32_t step = lv_bezier3(t, 1024, 800, 500, 0);
 
     int32_t new_value;
