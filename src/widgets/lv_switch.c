fb9789574681217fb8333ed75d0c1a08a89c6172
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-05-11 17:22:47 +0200

    fix(switch) add LV_OBJ_FLAG_SCROLL_ON_FOCUS

--- src/widgets/lv_switch.c
+++ src/widgets/lv_switch.c
@@ -77,6 +77,7 @@ static void lv_switch_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj
 
    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
    lv_obj_add_flag(obj, LV_OBJ_FLAG_CHECKABLE);
+   lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
 
    LV_TRACE_OBJ_CREATE("finished");
 }
