8bba029aa6bc74b5b29c7b6fdfc7ccfbb5a1046f
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-05-26 12:50:45 +0200

    fix warning

--- src/extra/themes/mono/lv_theme_mono.h
+++ src/extra/themes/mono/lv_theme_mono.h
@@ -38,9 +38,6 @@ extern "C" {
  */
 lv_theme_t * lv_theme_mono_init(lv_disp_t * disp, bool dark_bg, const lv_font_t * font);
 
-
-bool lv_theme_mono_is_inited(void);
-
 /**********************
  *      MACROS
  **********************/
