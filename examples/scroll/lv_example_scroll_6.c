66152438f6d67a3ed1d5a265a111565b95fe0581
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-05-30 20:39:35 +0200

    fix minor fix in a scroll example

--- examples/scroll/lv_example_scroll_6.c
+++ examples/scroll/lv_example_scroll_6.c
@@ -70,8 +70,10 @@ void lv_example_scroll_6(void)
     }
 
     /*Update the buttons position manually for first*/
-    lv_obj_update_layout(cont);
     lv_event_send(cont, LV_EVENT_SCROLL, NULL);
+
+    /*Be sure the fist button is in the middle*/
+    lv_obj_scroll_to_view(lv_obj_get_child(cont, 0), LV_ANIM_OFF);
 }
 
 #endif
