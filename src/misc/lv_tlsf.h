0d52b59cb16dda377f8a1ac581a851b830b7bf53
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-06-10 12:22:40 +0200

    fix(mem) add lv_ prefix to tlsf functions and types
    It avoids collision if tlsf is used by other libs in the project too
    Fixes https://github.com/lvgl/lvgl/issues/2116#issuecomment-857765919

--- src/misc/lv_tlsf.h
+++ src/misc/lv_tlsf.h
@@ -47,44 +47,44 @@
 extern "C" {
 #endif
 
-/* tlsf_t: a TLSF structure. Can contain 1 to N pools. */
-/* pool_t: a block of memory that TLSF can manage. */
-typedef void* tlsf_t;
-typedef void* pool_t;
+/* lv_tlsf_t: a TLSF structure. Can contain 1 to N pools. */
+/* lv_pool_t: a block of memory that TLSF can manage. */
+typedef void* lv_tlsf_t;
+typedef void* lv_pool_t;
 
 /* Create/destroy a memory pool. */
-tlsf_t tlsf_create(void* mem);
-tlsf_t tlsf_create_with_pool(void* mem, size_t bytes);
-void tlsf_destroy(tlsf_t tlsf);
-pool_t tlsf_get_pool(tlsf_t tlsf);
+lv_tlsf_t lv_tlsf_create(void* mem);
+lv_tlsf_t lv_tlsf_create_with_pool(void* mem, size_t bytes);
+void lv_tlsf_destroy(lv_tlsf_t tlsf);
+lv_pool_t lv_tlsf_get_pool(lv_tlsf_t tlsf);
 
 /* Add/remove memory pools. */
-pool_t tlsf_add_pool(tlsf_t tlsf, void* mem, size_t bytes);
-void tlsf_remove_pool(tlsf_t tlsf, pool_t pool);
+lv_pool_t lv_tlsf_add_pool(lv_tlsf_t tlsf, void* mem, size_t bytes);
+void lv_tlsf_remove_pool(lv_tlsf_t tlsf, lv_pool_t pool);
 
 /* malloc/memalign/realloc/free replacements. */
-void* tlsf_malloc(tlsf_t tlsf, size_t bytes);
-void* tlsf_memalign(tlsf_t tlsf, size_t align, size_t bytes);
-void* tlsf_realloc(tlsf_t tlsf, void* ptr, size_t size);
-void tlsf_free(tlsf_t tlsf, void* ptr);
+void* lv_tlsf_malloc(lv_tlsf_t tlsf, size_t bytes);
+void* lv_tlsf_memalign(lv_tlsf_t tlsf, size_t align, size_t bytes);
+void* lv_tlsf_realloc(lv_tlsf_t tlsf, void* ptr, size_t size);
+void lv_tlsf_free(lv_tlsf_t tlsf, void* ptr);
 
 /* Returns internal block size, not original request size */
-size_t tlsf_block_size(void* ptr);
+size_t lv_tlsf_block_size(void* ptr);
 
 /* Overheads/limits of internal structures. */
-size_t tlsf_size(void);
-size_t tlsf_align_size(void);
-size_t tlsf_block_size_min(void);
-size_t tlsf_block_size_max(void);
-size_t tlsf_pool_overhead(void);
-size_t tlsf_alloc_overhead(void);
+size_t lv_tlsf_size(void);
+size_t lv_tlsf_align_size(void);
+size_t lv_tlsf_block_size_min(void);
+size_t lv_tlsf_block_size_max(void);
+size_t lv_tlsf_pool_overhead(void);
+size_t lv_tlsf_alloc_overhead(void);
 
 /* Debugging. */
-typedef void (*tlsf_walker)(void* ptr, size_t size, int used, void* user);
-void tlsf_walk_pool(pool_t pool, tlsf_walker walker, void* user);
+typedef void (*lv_tlsf_walker)(void* ptr, size_t size, int used, void* user);
+void lv_tlsf_walk_pool(lv_pool_t pool, lv_tlsf_walker walker, void* user);
 /* Returns nonzero if any internal consistency check fails. */
-int tlsf_check(tlsf_t tlsf);
-int tlsf_check_pool(pool_t pool);
+int lv_tlsf_check(lv_tlsf_t tlsf);
+int lv_tlsf_check_pool(lv_pool_t pool);
 
 #if defined(__cplusplus)
 };
