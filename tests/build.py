d2e735ef36bd99c16ccaa281dcaa5f418e2dec98
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-06-17 12:39:56 +0200

    test add move tests to test_cases and test_runners directories

--- tests/build.py
+++ tests/build.py
@@ -24,6 +24,7 @@ def build(defines, test_name):
   d_all += '"'
   test_file_name = test_name + ".c"
   test_file_runner_name = test_name + "_Runner.c"
+  test_file_runner_name = test_file_runner_name.replace("/test_cases/", "/test_runners/")
   # -s makes it silence
   cmd = "make -s -j BIN=test.bin MAINSRC=" + test_file_name + " TEST_SRC=" + test_file_runner_name + "  LVGL_DIR_NAME=" + lvgldirname + " DEFINES=" + d_all + " OPTIMIZATION=" + optimization
 
--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,11 @@ import os
 import glob
 
 def prepare():
-  os.system("rm src/test_*_Runner.c")
+  os.system("rm src/test_runners/test_*_Runner.c")
   os.system("rm src/*.o")
-  files = glob.glob("./src/test_*.c")
+  files = glob.glob("./src/test_cases/test_*.c")
   for f in files:
-    os.system("ruby unity/generate_test_runner.rb " + f + " config.yml") 
+    r = f[:-2] + "_Runner.c"
+    r = r.replace("/test_cases/", "/test_runners/")
+    os.system("ruby unity/generate_test_runner.rb " + f + " " + r + " config.yml") 
   return files  
