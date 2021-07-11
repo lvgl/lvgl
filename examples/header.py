ef41450ed87f4f4dd936b63349b5a0c9ce880618
embeddedt <42941056+embeddedt@users.noreply.github.com>
2021-07-11 15:09:25 -0400

    refactor(examples) drop JS-specific code from header.py
    This logic was moved into the JS simulator itself

--- examples/header.py
+++ examples/header.py
@@ -3,9 +3,4 @@
 import lvgl as lv
 import usys as sys
 
-# JS requires a special import
-if sys.platform == 'javascript':
-  import imp
-  sys.path.append('https://raw.githubusercontent.com/lvgl/lv_binding_micropython/4c04dba836a5affcf86cef107b538e45278117ae/lib')
-
 import display_driver
