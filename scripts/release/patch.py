1626a0c029504f26e568677debcb7ab0f6053f83
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-07-16 17:00:16 +0200

    start to implement release/patch

--- scripts/release/patch.py
+++ scripts/release/patch.py
@@ -7,3 +7,64 @@
 #   - <branch-list>: branches where the commit should be applied. * can be used as wildchar 
 # -  p: push the changes to <brach-list>
 # -  t: increment version number and create a tag
+
+
+import os, subprocess, com, re
+
+# tag = True
+
+def clone(repo):
+  com.cmd("git clone  --recurse-submodules https://github.com/lvgl/" + repo)
+  os.chdir("./" + repo)
+  com.cmd("git checkout master")
+  com.cmd("git remote update origin --prune")
+  com.cmd("git pull origin --tags")
+  os.chdir("..")
+    
+# Get the list of realted minor version branches
+
+#clone("lvgl")
+os.chdir("lvgl")
+
+cmd = "git branch --remotes | grep origin/release/v8"
+branches, error = subprocess.Popen(cmd, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+branches = str(branches)
+branches = branches.replace("b'", "")
+branches = branches.replace("'", "")
+branches = branches.replace("origin/", "")
+branches = branches.replace("  ", " ")
+branches = branches.replace("\\n", "")
+branches = branches.split(" ")
+branches = list(filter(len, branches))
+
+
+commits = []
+with open('commits.txt') as f:
+    for line in f:
+        commits.append(line)
+
+print(commits)
+
+for br in branches:
+  ver = com.get_lvgl_version(br)
+  ver_new = ver.copy()
+  ver_new[2] = str(int(ver_new[2]) + 1)
+  print("Updating branch '" + br + "' from '" + com.ver_format(ver) + "' to '" + com.ver_format(ver_new) + "'")
+  com.update_version(ver_new)
+  com.cmd("git tag -a " + com.ver_format(ver_new) + "-m \"Release " + com.ver_format(ver_new) + "\"")
+  
+  if push:
+    com.cmd("git push origin " + br + "--tags")
+    
+com.cmd("git checkout master")    
+ver = com.get_lvgl_version("master")
+ver = com.get_lvgl_version(br)
+ver_new[2] = str(int(ver_new[2]) + 1)
+t = com.ver_format(ver_new) + "-dev"
+com.cmd("git tag -a " + t + "-m \"Start " + t + "\"")
+
+
+if push:
+  com.cmd("git push origin master --tags")
+
