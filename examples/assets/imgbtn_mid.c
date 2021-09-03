53b65a7964dd460f929223fd2ce2623ed284bf35
Johannes Marbach <n0-0ne+github@mailbox.org>
2021-09-03 15:01:01 +0200

    fix(examples) don't compile assets unless needed (#2523)
    This wraps the code for the assets under examples/assets in
    LV_BUILD_EXAMPLES so that it is not compiled when examples are
    deactivated in lv_conf.h.

--- examples/assets/imgbtn_mid.c
+++ examples/assets/imgbtn_mid.c
@@ -4,6 +4,7 @@
 #include "lvgl/lvgl.h"
 #endif
 
+#if LV_BUILD_EXAMPLES
 
 #ifndef LV_ATTRIBUTE_MEM_ALIGN
 #define LV_ATTRIBUTE_MEM_ALIGN
@@ -232,3 +233,5 @@ const lv_img_dsc_t imgbtn_mid = {
   .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
   .data = imgbtn_mid_map,
 };
+
+#endif /* LV_BUILD_EXAMPLES */
