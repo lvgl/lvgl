a33fe6a2d6eb0f2e9ff3f9f6ee955fa43e39ea0b
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-09-07 21:55:50 +0200

    fix(disp) be sure the pending scr load animation is finished in lv_scr_load_anim
    fixes #2502

--- src/lv_core/lv_disp.c
+++ src/lv_core/lv_disp.c
@@ -215,13 +215,15 @@ void lv_scr_load_anim(lv_obj_t * new_scr, lv_scr_load_anim_t anim_type, uint32_t
     lv_disp_t * d = lv_obj_get_disp(new_scr);
     lv_obj_t * act_scr = lv_scr_act();
 
-    if(d->del_prev && act_scr != d->scr_to_load && d->scr_to_load) {
-        lv_obj_del(act_scr);
+    if(d->scr_to_load && act_scr != d->scr_to_load) {
         lv_disp_load_scr(d->scr_to_load);
         lv_anim_del(d->scr_to_load, NULL);
         lv_obj_set_pos(d->scr_to_load, 0, 0);
         lv_style_remove_prop(lv_obj_get_local_style(d->scr_to_load, LV_OBJ_PART_MAIN), LV_STYLE_OPA_SCALE);
 
+        if(d->del_prev) {
+            lv_obj_del(act_scr);
+        }
         act_scr = d->scr_to_load;
     }
 
