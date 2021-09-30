3c2cac9f8050aba8a44bcc0db3807ca49d33efec
C47D <carlos.santiago.diaz@gmail.com>
2021-09-29 20:23:06 -0500

    test(arc): Add test for angle updating after changing to symmetrical mode and value is greater than middle range

--- tests/src/test_cases/test_arc.c
+++ tests/src/test_cases/test_arc.c
@@ -8,6 +8,7 @@ void test_arc_should_truncate_to_max_range_when_new_value_exceeds_it(void);
 void test_arc_should_truncate_to_min_range_when_new_value_is_inferior(void);
 void test_arc_should_update_value_after_updating_range(void);
 void test_arc_should_update_angles_when_changing_to_symmetrical_mode(void);
+void test_arc_should_update_angles_when_changing_to_symmetrical_mode_value_more_than_middle_range(void);
 void test_arc_angles_when_reversed(void);
 
 static lv_obj_t *active_screen = NULL;
@@ -74,6 +75,21 @@ void test_arc_should_update_angles_when_changing_to_symmetrical_mode(void)
     active_screen = lv_scr_act();
     /* start angle is 135, end angle is 45 at creation */
     arc = lv_arc_create(active_screen);
+    lv_arc_set_mode(arc, LV_ARC_MODE_SYMMETRICAL);
+
+    TEST_ASSERT_EQUAL_INT16(expected_angle_start, lv_arc_get_angle_start(arc));
+    TEST_ASSERT_EQUAL_INT16(expected_angle_end, lv_arc_get_angle_end(arc));
+}
+
+void test_arc_should_update_angles_when_changing_to_symmetrical_mode_value_more_than_middle_range(void)
+{
+    int16_t expected_angle_start = 270;
+    int16_t expected_angle_end = 45;
+
+    active_screen = lv_scr_act();
+    /* start angle is 135, end angle is 45 at creation */
+    arc = lv_arc_create(active_screen);
+    lv_arc_set_value(arc, 100);
     lv_arc_set_mode(arc, LV_ARC_MODE_SYMMETRICAL);
 
     TEST_ASSERT_EQUAL_INT16(expected_angle_start, lv_arc_get_angle_start(arc));
