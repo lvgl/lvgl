9cd62bc334784098628ac40ac25d073d179872fa
C47D <carlos.santiago.diaz@gmail.com>
2021-10-08 18:12:47 -0500

    test(txt): Add initial tests for _lv_txt_ins

--- tests/src/test_cases/test_txt.c
+++ tests/src/test_cases/test_txt.c
@@ -12,6 +12,8 @@ void test_txt_should_identify_scaped_command_in_parameter(void);
 void test_txt_should_skip_color_parameter_in_parameter(void);
 void test_txt_should_reset_state_when_receiving_color_cmd_while_processing_commands(void);
 void test_txt_should_identify_space_after_parameter(void);
+void test_txt_should_insert_string_into_another(void);
+void test_txt_should_handle_null_pointers_when_inserting(void);
 
 void test_txt_should_identify_valid_start_of_command(void)
 {
@@ -77,6 +79,33 @@ void test_txt_should_identify_space_after_parameter(void)
 
     TEST_ASSERT_TRUE(is_cmd);
     TEST_ASSERT_EQUAL_UINT8(state, LV_TEXT_CMD_STATE_IN);
+}
+
+void test_txt_should_insert_string_into_another(void)
+{
+    const char *msg = "Hello ";
+    const char *suffix = "World";
+    char target[20] = {0};
+    size_t msg_len = strlen(msg);
+
+    strcpy(target, msg);
+
+    _lv_txt_ins(target, msg_len, suffix);
+
+    TEST_ASSERT_EQUAL_STRING("Hello World", target);
+}
+
+void test_txt_should_handle_null_pointers_when_inserting(void)
+{
+    const char *msg = "Hello ";
+    char target[20] = {0};
+    size_t msg_len = strlen(msg);
+
+    strcpy(target, msg);
+
+    _lv_txt_ins(target, msg_len, NULL);
+
+    TEST_ASSERT_EQUAL_STRING("Hello ", target);
 }
 
 /* See #2615 for more information */
