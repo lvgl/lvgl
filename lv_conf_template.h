2f0fac13125be962da24a465f166d827196f1c83
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-10-11 04:09:25 +0800

    fix(Kconfig): typo error in the font and theme related setting
    and correct the default value and sequence
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -508,7 +508,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 # define LV_THEME_DEFAULT_GROW 1
 
 /*Default transition time in [ms]*/
-# define LV_THEME_DEFAULT_TRANSITON_TIME 80
+# define LV_THEME_DEFAULT_TRANSITION_TIME 80
 #endif /*LV_USE_THEME_DEFAULT*/
 
 /*A very simple theme that is a good starting point for a custom theme*/
