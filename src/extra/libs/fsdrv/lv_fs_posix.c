327135ea9ad4c716e8221b239c024571f2b95f13
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-10-18 04:58:51 +0800

    fix(extra/fs/posix): return file handle directly to avoid malloc
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- src/extra/libs/fsdrv/lv_fs_posix.c
+++ src/extra/libs/fsdrv/lv_fs_posix.c
@@ -98,7 +98,7 @@ void lv_fs_posix_init(void)
  * @param drv pointer to a driver where this function belongs
  * @param path path to the file beginning with the driver letter (e.g. S:/folder/file.txt)
  * @param mode read: FS_MODE_RD, write: FS_MODE_WR, both: FS_MODE_RD | FS_MODE_WR
- * @return LV_FS_RES_OK or any error from lv_fs_res_t enum
+ * @return a file handle or -1 in case of fail
  */
 static void * fs_open(lv_fs_drv_t * drv, const char * path, lv_fs_mode_t mode)
 {
@@ -120,33 +120,27 @@ static void * fs_open(lv_fs_drv_t * drv, const char * path, lv_fs_mode_t mode)
     /*Be sure we are the beginning of the file*/
     lseek(f, 0, SEEK_SET);
 
-    int * fp = lv_mem_alloc(sizeof(int));
-    if(fp == NULL) return NULL;
-    *fp = f;
-
-    return fp;
+    return (void *)(lv_uintptr_t)f;
 }
 
 /**
  * Close an opened file
  * @param drv pointer to a driver where this function belongs
- * @param file_p pointer to a file_t variable. (opened with fs_open)
+ * @param file_p a file handle. (opened with fs_open)
  * @return LV_FS_RES_OK: no error, the file is read
  *         any error from lv_fs_res_t enum
  */
 static lv_fs_res_t fs_close(lv_fs_drv_t * drv, void * file_p)
 {
     LV_UNUSED(drv);
-    int * fp = file_p;
-    close(*fp);
-    lv_mem_free(file_p);
+    close((lv_uintptr_t)file_p);
     return LV_FS_RES_OK;
 }
 
 /**
  * Read data from an opened file
  * @param drv pointer to a driver where this function belongs
- * @param file_p pointer to a file_t variable.
+ * @param file_p a file handle variable.
  * @param buf pointer to a memory block where to store the read data
  * @param btr number of Bytes To Read
  * @param br the real number of read bytes (Byte Read)
@@ -156,15 +150,14 @@ static lv_fs_res_t fs_close(lv_fs_drv_t * drv, void * file_p)
 static lv_fs_res_t fs_read(lv_fs_drv_t * drv, void * file_p, void * buf, uint32_t btr, uint32_t * br)
 {
     LV_UNUSED(drv);
-    int * fp = file_p;
-    *br = read(*fp, buf, btr);
+    *br = read((lv_uintptr_t)file_p, buf, btr);
     return LV_FS_RES_OK;
 }
 
 /**
  * Write into a file
  * @param drv pointer to a driver where this function belongs
- * @param file_p pointer to a file_t variable
+ * @param file_p a file handle variable
  * @param buf pointer to a buffer with the bytes to write
  * @param btr Bytes To Write
  * @param br the number of real written bytes (Bytes Written). NULL if unused.
@@ -173,15 +166,14 @@ static lv_fs_res_t fs_read(lv_fs_drv_t * drv, void * file_p, void * buf, uint32_
 static lv_fs_res_t fs_write(lv_fs_drv_t * drv, void * file_p, const void * buf, uint32_t btw, uint32_t * bw)
 {
     LV_UNUSED(drv);
-    int * fp = file_p;
-    *bw = write(*fp, buf, btw);
+    *bw = write((lv_uintptr_t)file_p, buf, btw);
     return LV_FS_RES_OK;
 }
 
 /**
  * Set the read write pointer. Also expand the file size if necessary.
  * @param drv pointer to a driver where this function belongs
- * @param file_p pointer to a file_t variable. (opened with fs_open )
+ * @param file_p a file handle variable. (opened with fs_open )
  * @param pos the new position of read write pointer
  * @return LV_FS_RES_OK: no error, the file is read
  *         any error from lv_fs_res_t enum
@@ -189,15 +181,14 @@ static lv_fs_res_t fs_write(lv_fs_drv_t * drv, void * file_p, const void * buf, 
 static lv_fs_res_t fs_seek(lv_fs_drv_t * drv, void * file_p, uint32_t pos, lv_fs_whence_t whence)
 {
     LV_UNUSED(drv);
-    int * fp = file_p;
-    lseek(*fp, pos, whence);
+    lseek((lv_uintptr_t)file_p, pos, whence);
     return LV_FS_RES_OK;
 }
 
 /**
  * Give the position of the read write pointer
  * @param drv pointer to a driver where this function belongs
- * @param file_p pointer to a file_t variable.
+ * @param file_p a file handle variable.
  * @param pos_p pointer to to store the result
  * @return LV_FS_RES_OK: no error, the file is read
  *         any error from lv_fs_res_t enum
@@ -205,8 +196,7 @@ static lv_fs_res_t fs_seek(lv_fs_drv_t * drv, void * file_p, uint32_t pos, lv_fs
 static lv_fs_res_t fs_tell(lv_fs_drv_t * drv, void * file_p, uint32_t * pos_p)
 {
     LV_UNUSED(drv);
-    int * fp = file_p;
-    *pos_p = lseek(*fp, 0, SEEK_CUR);
+    *pos_p = lseek((lv_uintptr_t)file_p, 0, SEEK_CUR);
     return LV_FS_RES_OK;
 }
 
