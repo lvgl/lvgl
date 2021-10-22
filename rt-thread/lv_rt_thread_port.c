9d7f53012c218205255d87d3b7b23c7a8a739185
Jianting (Meco) Man <920369182@qq.com>
2021-10-22 05:13:39 -0400

    feat(rtthread): prepare for porting the device-driver of rt-thread (#2719)

--- rt-thread/lv_rt_thread_port.c
+++ rt-thread/lv_rt_thread_port.c
@@ -16,6 +16,13 @@
 #define DBG_LVL    DBG_INFO
 #include <rtdbg.h>
 
+#ifndef PKG_USING_LVGL_DISP_DEVICE
+#  include <lv_port_disp.h>
+#endif
+#ifndef PKG_USING_LVGL_INDEV_DEVICE
+#  include <lv_port_indev.h>
+#endif
+
 #if LV_USE_LOG && LV_LOG_PRINTF
 static void lv_rt_log(const char *buf)
 {
@@ -27,6 +34,15 @@ static int lv_port_init(void)
 {
 #if LV_USE_LOG && LV_LOG_PRINTF
     lv_log_register_print_cb(lv_rt_log);
+#endif
+
+    lv_init();
+
+#ifndef PKG_USING_LVGL_DISP_DEVICE
+    lv_port_disp_init();
+#endif
+#ifndef PKG_USING_LVGL_INDEV_DEVICE
+    lv_port_indev_init();
 #endif
 
     return 0;
