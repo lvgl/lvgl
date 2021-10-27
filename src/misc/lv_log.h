17b8a76c4a887c9cf464484406a6631ea0194ad5
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-10-27 10:29:44 +0200

    feat(log) allow overwriting LV_LOG_... macros

--- src/misc/lv_log.h
+++ src/misc/lv_log.h
@@ -85,35 +85,44 @@ void _lv_log_add(lv_log_level_t level, const char * file, int line,
 /**********************
  *      MACROS
  **********************/
-
-#if LV_LOG_LEVEL <= LV_LOG_LEVEL_TRACE
-#define LV_LOG_TRACE(...) _lv_log_add(LV_LOG_LEVEL_TRACE, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#else
-#define LV_LOG_TRACE(...) do {}while(0)
+#ifndef LV_LOG_TRACE
+#  if LV_LOG_LEVEL <= LV_LOG_LEVEL_TRACE
+#    define LV_LOG_TRACE(...) _lv_log_add(LV_LOG_LEVEL_TRACE, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#  else
+#    define LV_LOG_TRACE(...) do {}while(0)
+#  endif
 #endif
 
-#if LV_LOG_LEVEL <= LV_LOG_LEVEL_INFO
-#define LV_LOG_INFO(...) _lv_log_add(LV_LOG_LEVEL_INFO, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#else
-#define LV_LOG_INFO(...) do {}while(0)
+#ifndef LV_LOG_INFO
+#  if LV_LOG_LEVEL <= LV_LOG_LEVEL_INFO
+#    define LV_LOG_INFO(...) _lv_log_add(LV_LOG_LEVEL_INFO, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#  else
+#    define LV_LOG_INFO(...) do {}while(0)
+#  endif
 #endif
 
-#if LV_LOG_LEVEL <= LV_LOG_LEVEL_WARN
-#define LV_LOG_WARN(...) _lv_log_add(LV_LOG_LEVEL_WARN, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#else
-#define LV_LOG_WARN(...) do {}while(0)
+#ifndef LV_LOG_WARN
+#  if LV_LOG_LEVEL <= LV_LOG_LEVEL_WARN
+#    define LV_LOG_WARN(...) _lv_log_add(LV_LOG_LEVEL_WARN, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#  else
+#    define LV_LOG_WARN(...) do {}while(0)
+#  endif
 #endif
 
-#if LV_LOG_LEVEL <= LV_LOG_LEVEL_ERROR
-#define LV_LOG_ERROR(...) _lv_log_add(LV_LOG_LEVEL_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#else
-#define LV_LOG_ERROR(...) do {}while(0)
+#ifndef LV_LOG_ERROR
+#  if LV_LOG_LEVEL <= LV_LOG_LEVEL_ERROR
+#    define LV_LOG_ERROR(...) _lv_log_add(LV_LOG_LEVEL_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#  else
+#    define LV_LOG_ERROR(...) do {}while(0)
+#  endif
 #endif
 
-#if LV_LOG_LEVEL <= LV_LOG_LEVEL_USER
-#define LV_LOG_USER(...) _lv_log_add(LV_LOG_LEVEL_USER, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#else
-#define LV_LOG_USER(...) do {}while(0)
+#ifndef LV_LOG_USER
+#  if LV_LOG_LEVEL <= LV_LOG_LEVEL_USER
+#    define LV_LOG_USER(...) _lv_log_add(LV_LOG_LEVEL_USER, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#  else
+#    define LV_LOG_USER(...) do {}while(0)
+#  endif
 #endif
 
 #else /*LV_USE_LOG*/
