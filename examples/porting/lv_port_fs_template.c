b57e0bb866870d302d77d372fe0279fcedf4ce81
Hotakus <553070703@qq.com>
2021-11-02 07:20:47 +0800

    fix prototype error

--- examples/porting/lv_port_fs_template.c
+++ examples/porting/lv_port_fs_template.c
@@ -25,7 +25,7 @@
  **********************/
 static void fs_init(void);
 
-static lv_fs_res_t fs_open(lv_fs_drv_t * drv, void * file_p, const char * path, lv_fs_mode_t mode);
+static void * fs_open(lv_fs_drv_t * drv, void * file_p, const char * path, lv_fs_mode_t mode);
 static lv_fs_res_t fs_close(lv_fs_drv_t * drv, void * file_p);
 static lv_fs_res_t fs_read(lv_fs_drv_t * drv, void * file_p, void * buf, uint32_t btr, uint32_t * br);
 static lv_fs_res_t fs_write(lv_fs_drv_t * drv, void * file_p, const void * buf, uint32_t btw, uint32_t * bw);
