a9e33c3f7508ea83c8b5413331b39638668cfbd7
C47D <carlos.santiago.diaz@gmail.com>
2021-11-18 21:39:29 -0600

    test(switch): Cleanup event bubbling test

--- tests/src/test_cases/test_switch.c
+++ tests/src/test_cases/test_switch.c
@@ -5,7 +5,6 @@
 
 #include "lv_test_indev.h"
 
-uint8_t state_changed = 0;
 uint8_t value_changed = 0;
 lv_obj_t *scr = NULL;
 lv_obj_t *sw = NULL;
@@ -21,7 +20,6 @@ void tearDown(void)
 {
     /* Function run after every test */
     value_changed = 0;
-    state_changed = 0;
 }
 
 static void event_handler(lv_event_t *e)
@@ -32,21 +30,6 @@ static void event_handler(lv_event_t *e)
         value_changed++;
     }
 
-}
-
-static void event_bubbling_handler(lv_event_t *e)
-{
-    lv_obj_t *original_target = lv_event_get_target(e);
-    lv_event_code_t event_code = lv_event_get_code(e);
-
-    if ((LV_EVENT_VALUE_CHANGED == event_code) &&
-        (original_target == sw))
-    {
-        if (lv_obj_has_state(original_target, LV_STATE_CHECKED))
-        {
-            state_changed++;
-        }
-    }
 }
 
 void test_switch_should_have_default_state_after_being_created(void)
@@ -68,12 +51,9 @@ void test_switch_should_trigger_value_changed_event_only_once(void)
 void test_switch_should_state_change_when_event_bubbling_is_enabled(void)
 {
     lv_obj_add_flag(sw, LV_OBJ_FLAG_EVENT_BUBBLE);
-
-    /* Call event_bubbling_handler only when the switch changes state */
-    lv_obj_add_event_cb(sw, event_bubbling_handler, LV_EVENT_ALL, NULL);
     lv_test_mouse_click_at(sw->coords.x1, sw->coords.y1);
 
-    TEST_ASSERT_EQUAL(1, state_changed);
+    TEST_ASSERT(lv_obj_has_state(sw, LV_STATE_CHECKED));
 }
 
 #endif
