4a38cb1a8820e2d2bbb9f8b7c57b4a16bcee4fd5
C47D <carlos.santiago.diaz@gmail.com>
2021-11-29 17:23:16 -0600

    test(bar) Update test for docummented default attributes

--- tests/src/test_cases/test_bar.c
+++ tests/src/test_cases/test_bar.c
@@ -18,7 +18,7 @@ void tearDown(void)
 
 void test_bar_should_have_valid_default_attributes(void)
 {
-    TEST_ASSERT_EQUAL(1, lv_bar_get_min_value(bar));
+    TEST_ASSERT_EQUAL(0, lv_bar_get_min_value(bar));
     TEST_ASSERT_EQUAL(100, lv_bar_get_max_value(bar));
     TEST_ASSERT_EQUAL(LV_BAR_MODE_NORMAL, lv_bar_get_mode(bar));
 }
