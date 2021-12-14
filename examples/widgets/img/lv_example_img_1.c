d72f52557b0f4f9511db719005a555ea25bdd123
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-12-14 14:02:08 +0100

    revert debug code from examples

--- examples/widgets/img/lv_example_img_1.c
+++ examples/widgets/img/lv_example_img_1.c
@@ -9,22 +9,10 @@ void lv_example_img_1(void)
     lv_img_set_src(img1, &img_cogwheel_argb);
     lv_obj_align(img1, LV_ALIGN_CENTER, 0, -20);
     lv_obj_set_size(img1, 200, 200);
-    lv_obj_set_style_blend_mode(img1, LV_BLEND_MODE_MULTIPLY, 0);
-
-
-
-    lv_obj_t * btn = lv_btn_create(lv_scr_act());
-    lv_obj_set_style_blend_mode(btn, LV_BLEND_MODE_MULTIPLY, 0);
-//    lv_obj_set_style_opa(btn, 130, 0);
-    lv_obj_set_size(btn, 100, 100);
-
 
     lv_obj_t * img2 = lv_img_create(lv_scr_act());
     lv_img_set_src(img2, LV_SYMBOL_OK "Accept");
     lv_obj_align_to(img2, img1, LV_ALIGN_OUT_BOTTOM_MID, 0, 20);
-
-
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0xff8888), 0);
 }
 
 #endif
