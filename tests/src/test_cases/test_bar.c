51d5dbe440e5c3ae39a7ccfbd50d95ef548b7fad
C47D <carlos.santiago.diaz@gmail.com>
2022-01-04 20:55:25 -0600

    test(bar): Indicator width tracks value

--- tests/src/test_cases/test_bar.c
+++ tests/src/test_cases/test_bar.c
@@ -3,6 +3,8 @@
 
 #include "unity/unity.h"
 
+#include "lv_test_indev.h"
+
 static lv_obj_t *active_screen = NULL;
 static lv_obj_t *bar = NULL;
 
@@ -23,4 +25,21 @@ void test_bar_should_have_valid_default_attributes(void)
     TEST_ASSERT_EQUAL(LV_BAR_MODE_NORMAL, lv_bar_get_mode(bar));
 }
 
+void test_bar_indicator_width_should_track_bar_value(void)
+{
+    lv_bar_t * bar_ptr = (lv_bar_t *) bar;
+
+    /* By default the bar is horizontal, so we get the indicator width */
+    int32_t indicator_width = lv_area_get_width(&bar_ptr->indic_area);
+
+    /* If x1 == x2 then width == 1 */
+    TEST_ASSERT_EQUAL_INT32(1, indicator_width);
+
+    /* Set a new value */
+    lv_bar_set_value(bar, 10, LV_ANIM_OFF);
+    lv_test_indev_wait(50);
+
+    indicator_width = lv_area_get_width(&bar_ptr->indic_area);
+    TEST_ASSERT_EQUAL_INT32(27 /* Value found by debugging */, indicator_width);
+}
 #endif
