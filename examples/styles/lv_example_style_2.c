093fbd71117b307dac43ea7422180e86c8cccc26
X-Ryl669 <boite.pour.spam@gmail.com>
2022-01-10 14:32:58 +0100

    Let's make an example

--- examples/styles/lv_example_style_2.c
+++ examples/styles/lv_example_style_2.c
@@ -12,13 +12,17 @@ void lv_example_style_2(void)
 
     /*Make a gradient*/
     lv_style_set_bg_opa(&style, LV_OPA_COVER);
-    lv_style_set_bg_color(&style, lv_palette_lighten(LV_PALETTE_GREY, 1));
-    lv_style_set_bg_grad_color(&style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_bg_grad_dir(&style, LV_GRAD_DIR_VER);
+    static lv_gradient_t grad;
+    grad.dir = LV_GRAD_DIR_VER;
+    grad.stops_count = 2;
+    grad.stops[0].color = lv_palette_lighten(LV_PALETTE_GREY, 1);
+    grad.stops[1].color = lv_palette_main(LV_PALETTE_BLUE);
 
     /*Shift the gradient to the bottom*/
-    lv_style_set_bg_main_stop(&style, 128);
-    lv_style_set_bg_grad_stop(&style, 192);
+    grad.stops[0].frac  = 128;
+    grad.stops[1].frac  = 192;
+
+    lv_style_set_bg_gradient(&style, &grad);
 
     /*Create an object with the new style*/
     lv_obj_t * obj = lv_obj_create(lv_scr_act());
