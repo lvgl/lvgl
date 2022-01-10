305284c2b5aadec7bcfa68c6517c98d44be7c8a9
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-01-10 11:16:02 +0100

    fix(conf) define LV_LOG_TRACE_... to 0 in lv_conf_internal.h to avoid warnings

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -2084,7 +2084,21 @@ LV_EXPORT_CONST_INT(LV_DPI_DEF);
 
 #undef _LV_KCONFIG_PRESENT
 
-/*If running without lv_conf.h add typdesf with default value*/
+
+/*Set some defines if a dependecy is disabled*/
+#if LV_USE_LOG == 0
+    #define LV_LOG_TRACE_MEM        0
+    #define LV_LOG_TRACE_TIMER      0
+    #define LV_LOG_TRACE_INDEV      0
+    #define LV_LOG_TRACE_DISP_REFR  0
+    #define LV_LOG_TRACE_EVENT      0
+    #define LV_LOG_TRACE_OBJ_CREATE 0
+    #define LV_LOG_TRACE_LAYOUT     0
+    #define LV_LOG_TRACE_ANIM       0
+#endif  /*LV_USE_LOG*/
+
+
+/*If running without lv_conf.h add typedefs with default value*/
 #ifdef LV_CONF_SKIP
     #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)    /*Disable warnings for Visual Studio*/
         #define _CRT_SECURE_NO_WARNINGS
