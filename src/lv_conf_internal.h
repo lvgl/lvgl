63266de87e1cd33be568a71b6e5ed9b9407426f1
Mariotaku <mariotaku.lee@gmail.com>
2022-01-13 23:08:08 +0900

    updated config header

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -430,6 +430,7 @@
             #define LV_GPU_SDL_INCLUDE_PATH <SDL2/SDL.h>
         #endif
     #endif
+    /*Texture cache size, 8MB by default*/
     #ifndef LV_GPU_SDL_LRU_SIZE
         #ifdef CONFIG_LV_GPU_SDL_LRU_SIZE
             #define LV_GPU_SDL_LRU_SIZE CONFIG_LV_GPU_SDL_LRU_SIZE
@@ -437,6 +438,7 @@
             #define LV_GPU_SDL_LRU_SIZE (1024 * 1024 * 8)
         #endif
     #endif
+    /*Custom blend mode for mask drawing, disable if you need to link with older SDL2 lib*/
     #ifndef LV_GPU_SDL_CUSTOM_BLEND_MODE
         #ifdef CONFIG_LV_GPU_SDL_CUSTOM_BLEND_MODE
             #define LV_GPU_SDL_CUSTOM_BLEND_MODE CONFIG_LV_GPU_SDL_CUSTOM_BLEND_MODE
@@ -2039,13 +2041,13 @@
     #endif
 #endif
 #if LV_USE_DEMO_WIDGETS
-    #ifndef LV_DEMO_WIDGETS_SLIDESHOW
-        #ifdef CONFIG_LV_DEMO_WIDGETS_SLIDESHOW
-            #define LV_DEMO_WIDGETS_SLIDESHOW CONFIG_LV_DEMO_WIDGETS_SLIDESHOW
-        #else
-            #define LV_DEMO_WIDGETS_SLIDESHOW  0
-        #endif
+#ifndef LV_DEMO_WIDGETS_SLIDESHOW
+    #ifdef CONFIG_LV_DEMO_WIDGETS_SLIDESHOW
+        #define LV_DEMO_WIDGETS_SLIDESHOW CONFIG_LV_DEMO_WIDGETS_SLIDESHOW
+    #else
+        #define LV_DEMO_WIDGETS_SLIDESHOW  0
     #endif
+#endif
 #endif
 
 /*Demonstrate the usage of encoder and keyboard*/
@@ -2084,41 +2086,41 @@
     #endif
 #endif
 #if LV_USE_DEMO_MUSIC
-    #ifndef LV_DEMO_MUSIC_SQUARE
-        #ifdef CONFIG_LV_DEMO_MUSIC_SQUARE
-            #define LV_DEMO_MUSIC_SQUARE CONFIG_LV_DEMO_MUSIC_SQUARE
-        #else
-            #define LV_DEMO_MUSIC_SQUARE       0
-        #endif
+#ifndef LV_DEMO_MUSIC_SQUARE
+    #ifdef CONFIG_LV_DEMO_MUSIC_SQUARE
+        #define LV_DEMO_MUSIC_SQUARE CONFIG_LV_DEMO_MUSIC_SQUARE
+    #else
+        #define LV_DEMO_MUSIC_SQUARE       0
     #endif
-    #ifndef LV_DEMO_MUSIC_LANDSCAPE
-        #ifdef CONFIG_LV_DEMO_MUSIC_LANDSCAPE
-            #define LV_DEMO_MUSIC_LANDSCAPE CONFIG_LV_DEMO_MUSIC_LANDSCAPE
-        #else
-            #define LV_DEMO_MUSIC_LANDSCAPE    0
-        #endif
+#endif
+#ifndef LV_DEMO_MUSIC_LANDSCAPE
+    #ifdef CONFIG_LV_DEMO_MUSIC_LANDSCAPE
+        #define LV_DEMO_MUSIC_LANDSCAPE CONFIG_LV_DEMO_MUSIC_LANDSCAPE
+    #else
+        #define LV_DEMO_MUSIC_LANDSCAPE    0
     #endif
-    #ifndef LV_DEMO_MUSIC_ROUND
-        #ifdef CONFIG_LV_DEMO_MUSIC_ROUND
-            #define LV_DEMO_MUSIC_ROUND CONFIG_LV_DEMO_MUSIC_ROUND
-        #else
-            #define LV_DEMO_MUSIC_ROUND        0
-        #endif
+#endif
+#ifndef LV_DEMO_MUSIC_ROUND
+    #ifdef CONFIG_LV_DEMO_MUSIC_ROUND
+        #define LV_DEMO_MUSIC_ROUND CONFIG_LV_DEMO_MUSIC_ROUND
+    #else
+        #define LV_DEMO_MUSIC_ROUND        0
     #endif
-    #ifndef LV_DEMO_MUSIC_LARGE
-        #ifdef CONFIG_LV_DEMO_MUSIC_LARGE
-            #define LV_DEMO_MUSIC_LARGE CONFIG_LV_DEMO_MUSIC_LARGE
-        #else
-            #define LV_DEMO_MUSIC_LARGE        0
-        #endif
+#endif
+#ifndef LV_DEMO_MUSIC_LARGE
+    #ifdef CONFIG_LV_DEMO_MUSIC_LARGE
+        #define LV_DEMO_MUSIC_LARGE CONFIG_LV_DEMO_MUSIC_LARGE
+    #else
+        #define LV_DEMO_MUSIC_LARGE        0
     #endif
-    #ifndef LV_DEMO_MUSIC_AUTO_PLAY
-        #ifdef CONFIG_LV_DEMO_MUSIC_AUTO_PLAY
-            #define LV_DEMO_MUSIC_AUTO_PLAY CONFIG_LV_DEMO_MUSIC_AUTO_PLAY
-        #else
-            #define LV_DEMO_MUSIC_AUTO_PLAY    0
-        #endif
+#endif
+#ifndef LV_DEMO_MUSIC_AUTO_PLAY
+    #ifdef CONFIG_LV_DEMO_MUSIC_AUTO_PLAY
+        #define LV_DEMO_MUSIC_AUTO_PLAY CONFIG_LV_DEMO_MUSIC_AUTO_PLAY
+    #else
+        #define LV_DEMO_MUSIC_AUTO_PLAY    0
     #endif
+#endif
 #endif
 
 
