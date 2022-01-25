2ff53832fa18c2c266991d430ec53a0b27ab63fd
C47D <carlos.santiago.diaz@gmail.com>
2022-01-24 18:17:17 -0600

    test(table): Add test for column increasing when setting a formatted cell value

--- tests/src/test_cases/test_table.c
+++ tests/src/test_cases/test_table.c
@@ -28,4 +28,14 @@ void test_table_should_return_assigned_cell_value(void)
     TEST_ASSERT_EQUAL_STRING(value, lv_table_get_cell_value(table, row, column));
 }
 
+void test_table_should_grow_columns_automatically_when_setting_formatted_cell_value(void)
+{
+    uint16_t original_column_count = lv_table_get_col_cnt(table);
+    uint16_t expected_column_count = original_column_count + 1;
+
+    lv_table_set_cell_value_fmt(table, 0, original_column_count + 1, "LVGL %s", "Rocks!");
+
+    TEST_ASSERT_EQUAL_UINT16(expected_column_count, lv_table_get_col_cnt(table));
+}
+
 #endif
