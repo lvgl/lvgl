7f1408529ac6fc1c3302504aba91c1501d6045f7
C47D <carlos.santiago.diaz@gmail.com>
2022-02-03 07:46:04 -0600

    fix(checkbox): Early return when reallocation fails

--- src/widgets/lv_checkbox.c
+++ src/widgets/lv_checkbox.c
@@ -80,6 +80,7 @@ void lv_checkbox_set_text(lv_obj_t * obj, const char * txt)
         cb->txt = lv_mem_realloc(cb->txt, len + 1);
         if(LV_RES_OK != lv_mem_realloc_safe(cb->txt, len + 1)) {
             LV_ASSERT_MALLOC(NULL);
+            return;
         }
     }
     else  cb->txt = lv_mem_alloc(len + 1);
