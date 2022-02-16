6cda25171ce4bed02b9e71879d6356dfbb4c637c
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2022-02-16 18:39:16 +0000

    pre-release for LVGL cmsis-pack v1.0.1 (#3091)
    * pre-release for LVGL cmsis-pack v1.0.1
    
    * update lv_conf_cmsis.h

--- env_support/cmsis-pack/lv_conf_cmsis.h
+++ env_support/cmsis-pack/lv_conf_cmsis.h
@@ -91,7 +91,12 @@
     #if LV_TICK_CUSTOM
         extern uint32_t SystemCoreClock;
         #define LV_TICK_CUSTOM_INCLUDE          "perf_counter.h" 
-        #define LV_TICK_CUSTOM_SYS_TIME_EXPR    (get_system_ticks() / (SystemCoreClock / 1000ul))
+        
+        #if __PER_COUNTER_VER__ < 10902ul
+            #define LV_TICK_CUSTOM_SYS_TIME_EXPR    ((uint32_t)get_system_ticks() / (SystemCoreClock / 1000ul))
+        #else
+            #define LV_TICK_CUSTOM_SYS_TIME_EXPR    get_system_ms()
+        #endif
     #endif   /*LV_TICK_CUSTOM*/
 #else
     #define LV_TICK_CUSTOM 0
