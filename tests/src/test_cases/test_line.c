88aeacfb4a9fde99f25bef224a9e5e798bc26059
C47D <carlos.santiago.diaz@gmail.com>
2022-02-17 22:20:18 -0600

    test(line): Validate documented default values

--- tests/src/test_cases/test_line.c
+++ tests/src/test_cases/test_line.c
@@ -6,6 +6,8 @@
 static lv_obj_t * active_screen = NULL;
 static lv_obj_t * line = NULL;
 
+static const uint16_t default_point_num = 0U;
+
 void setUp(void)
 {
     active_screen = lv_scr_act();
@@ -15,6 +17,14 @@ void setUp(void)
 void tearDown(void)
 {
     /* Function run after every test */
+}
+
+void test_line_should_have_valid_documented_defualt_values(void)
+{
+    lv_line_t * line_ptr = (lv_line_t *) line;
+    TEST_ASSERT_EQUAL_UINT16(default_point_num, line_ptr->point_num);
+    TEST_ASSERT_NULL(line_ptr->point_array);
+    TEST_ASSERT_FALSE(lv_line_get_y_invert(line));
 }
 
 void test_line_should_return_valid_y_invert(void)
