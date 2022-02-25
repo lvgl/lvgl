35624e8dce110043ede618aba0ea4521f0f100d0
Andres O. Vela <andresovela@gmail.com>
2022-02-25 10:54:04 +0100

    chore(label): ran code-format.py and added missing function prototype to lv_example_widgets.h

--- examples/widgets/label/lv_example_label_5.c
+++ examples/widgets/label/lv_example_label_5.c
@@ -12,12 +12,13 @@ void lv_example_label_5(void)
 
     lv_anim_init(&animation_template);
     lv_anim_set_delay(&animation_template, 1000);           /*Wait 1 second to start the first scroll*/
-    lv_anim_set_repeat_delay(&animation_template, 3000);    /*Repeat the scroll 3 seconds after the label scrolls back to the initial position*/
+    lv_anim_set_repeat_delay(&animation_template,
+                             3000);    /*Repeat the scroll 3 seconds after the label scrolls back to the initial position*/
 
     /*Initialize the label style with the animation template*/
     lv_style_init(&label_style);
     lv_style_set_anim(&label_style, &animation_template);
-    
+
     lv_obj_t * label1 = lv_label_create(lv_scr_act());
     lv_label_set_long_mode(label1, LV_LABEL_LONG_SCROLL_CIRCULAR);      /*Circular scroll*/
     lv_obj_set_width(label1, 150);
