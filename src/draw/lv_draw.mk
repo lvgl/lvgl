16ad5fbc19d426ca18d093b3a0d8ae92068518b8
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-03-04 01:45:39 +0800

    chore(lvgl.[h|mk]): don't include the grandchildren file
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- src/draw/lv_draw.mk
+++ src/draw/lv_draw.mk
@@ -16,3 +16,4 @@ VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw
 CFLAGS += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw"
 
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw/sdl/lv_draw_sdl.mk
+include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw/sw/lv_draw_sw.mk
