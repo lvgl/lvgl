d112a976fb106ef5d55b0c2b74861be1b62fa6c2
C47D <carlos.santiago.diaz@gmail.com>
2022-03-03 20:09:23 -0600

    test(slider): Add tests for LV_EVENT_RELEASED

--- tests/src/test_cases/test_slider.c
+++ tests/src/test_cases/test_slider.c
@@ -2,6 +2,7 @@
 #include "../lvgl.h"
 
 #include "unity/unity.h"
+#include "lv_test_indev.h"
 
 static lv_obj_t * active_screen = NULL;
 static lv_obj_t * slider = NULL;
@@ -68,6 +69,79 @@ void test_slider_event_invalid_key_should_not_change_values(void)
     lv_event_send(slider, LV_EVENT_KEY, (void *) &key);
 
     TEST_ASSERT_EQUAL_INT32(value, lv_slider_get_value(slider));
+}
+
+void test_slider_range_mode_should_leave_edit_mode_if_released(void)
+{
+    lv_slider_t * ptr = (lv_slider_t *) slider;
+    ptr->left_knob_focus = 1;
+    lv_slider_set_mode(slider, LV_SLIDER_MODE_RANGE);
+
+    /* Setup group and encoder indev */
+    lv_group_t * g = lv_group_create();
+    lv_indev_set_group(lv_test_encoder_indev, g);
+    lv_group_add_obj(g, slider);
+    lv_group_set_editing(g, true);
+
+    lv_test_encoder_click();
+
+    /* Always executed when handling LV_EVENT_RELEASED or
+     * LV_EVENT_PRESS_LOST */
+    TEST_ASSERT_FALSE(ptr->dragging);
+    TEST_ASSERT_NULL(ptr->value_to_set);
+    TEST_ASSERT_EQUAL(0U, ptr->left_knob_focus);
+
+    /* Group leaved edit mode */
+    TEST_ASSERT_FALSE(lv_group_get_editing(g));
+}
+
+void test_slider_range_mode_should_not_leave_edit_mode_if_released_with_no_left_knob_focus(void)
+{
+    lv_slider_t * ptr = (lv_slider_t *) slider;
+    ptr->left_knob_focus = 0;
+    lv_slider_set_mode(slider, LV_SLIDER_MODE_RANGE);
+
+    /* Setup group and encoder indev */
+    lv_group_t * g = lv_group_create();
+    lv_indev_set_group(lv_test_encoder_indev, g);
+    lv_group_add_obj(g, slider);
+    lv_group_set_editing(g, true);
+
+    lv_test_encoder_release();
+    lv_test_indev_wait(50);
+
+    /* Always executed when handling LV_EVENT_RELEASED or
+     * LV_EVENT_PRESS_LOST */
+    TEST_ASSERT_FALSE(ptr->dragging);
+    TEST_ASSERT_NULL(ptr->value_to_set);
+
+    /* Group leaved edit mode */
+    TEST_ASSERT_EQUAL(1U, ptr->left_knob_focus);
+    TEST_ASSERT(lv_group_get_editing(g));
+}
+
+void test_slider_normal_mode_should_leave_edit_mode_if_released(void)
+{
+    lv_slider_t * ptr = (lv_slider_t *) slider;
+    ptr->left_knob_focus = 1;
+    lv_slider_set_mode(slider, LV_SLIDER_MODE_NORMAL);
+
+    /* Setup group and encoder indev */
+    lv_group_t * g = lv_group_create();
+    lv_indev_set_group(lv_test_encoder_indev, g);
+    lv_group_add_obj(g, slider);
+    lv_group_set_editing(g, true);
+
+    lv_test_encoder_click();
+
+    /* Always executed when handling LV_EVENT_RELEASED or
+     * LV_EVENT_PRESS_LOST */
+    TEST_ASSERT_FALSE(ptr->dragging);
+    TEST_ASSERT_NULL(ptr->value_to_set);
+    TEST_ASSERT_EQUAL(0U, ptr->left_knob_focus);
+
+    /* Group leaved edit mode */
+    TEST_ASSERT_FALSE(lv_group_get_editing(g));
 }
 
 #endif
