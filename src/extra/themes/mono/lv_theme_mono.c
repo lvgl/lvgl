5ec6694f7874f3c99a764e7ee2d45a933865c91c
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-03-10 15:12:07 +0100

    fix(theme): fix mono theme init

--- src/extra/themes/mono/lv_theme_mono.c
+++ src/extra/themes/mono/lv_theme_mono.c
@@ -175,7 +175,7 @@ lv_theme_t * lv_theme_mono_init(lv_disp_t * disp, bool dark_bg, const lv_font_t 
     /*This trick is required only to avoid the garbage collection of
      *styles' data if LVGL is used in a binding (e.g. Micropython)
      *In a general case styles could be in simple `static lv_style_t my_style...` variables*/
-    if(!lv_theme_mono_is_inited()) {
+    if(!inited) {
         inited = false;
         LV_GC_ROOT(_lv_theme_default_styles) = lv_mem_alloc(sizeof(my_theme_styles_t));
         styles = (my_theme_styles_t *)LV_GC_ROOT(_lv_theme_default_styles);
