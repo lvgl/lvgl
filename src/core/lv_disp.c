88877cb2601f41493767a4e3fec59a463b6c4f8c
pengyiqiang <pengyiqiang@xiaomi.com>
2022-03-30 18:34:40 +0800

    fix(disp): fix missing null pointer judgment

--- src/core/lv_disp.c
+++ src/core/lv_disp.c
@@ -136,7 +136,12 @@ lv_obj_t * lv_disp_get_layer_sys(lv_disp_t * disp)
  */
 void lv_disp_set_theme(lv_disp_t * disp, lv_theme_t * th)
 {
-    if(disp == NULL) disp = lv_disp_get_default();
+    if(!disp) disp = lv_disp_get_default();
+    if(!disp) {
+        LV_LOG_WARN("no display registered");
+        return;
+    }
+
     disp->theme = th;
 
     if(disp->screen_cnt == 3 &&
