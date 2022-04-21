8481e3a33bc3313b679babac31e6193ec4319bcd
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-04-21 21:18:40 +0200

    ci(switch): fix mem leak test

--- tests/src/test_cases/test_switch.c
+++ tests/src/test_cases/test_switch.c
@@ -68,7 +68,7 @@ void test_switch_should_not_leak_memory_after_deletion(void)
     lv_mem_monitor(&monitor);
     final_available_memory = monitor.free_size;
 
-    LV_HEAP_CHECK(TEST_ASSERT_LESS_THAN(initial_available_memory, final_available_memory));
+    LV_HEAP_CHECK(TEST_ASSERT_LESS_OR_EQUAL(initial_available_memory, final_available_memory));
 }
 
 void test_switch_animation(void)
