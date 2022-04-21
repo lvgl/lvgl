98660a861d874d29e8356452baff21788b6a9ef1
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-04-21 20:45:23 +0200

    fix(fs): fix cached read and add unit test for lv_fs
    fixes: #3262

--- tests/src/lv_test_init.c
+++ tests/src/lv_test_init.c
@@ -30,65 +30,6 @@ void lv_test_deinit(void)
     lv_mem_deinit();
 }
 
-static void * open_cb(lv_fs_drv_t * drv, const char * path, lv_fs_mode_t mode)
-{
-    (void) drv;
-    (void) mode;
-
-    FILE * fp = fopen(path, "rb"); // only reading is supported
-
-    return fp;
-}
-
-static lv_fs_res_t close_cb(lv_fs_drv_t * drv, void * file_p)
-{
-    (void) drv;
-
-    fclose(file_p);
-    return LV_FS_RES_OK;
-}
-
-static lv_fs_res_t read_cb(lv_fs_drv_t * drv, void * file_p, void * buf, uint32_t btr, uint32_t * br)
-{
-    (void) drv;
-
-    *br = fread(buf, 1, btr, file_p);
-    return (*br <= 0) ? LV_FS_RES_UNKNOWN : LV_FS_RES_OK;
-}
-
-static lv_fs_res_t seek_cb(lv_fs_drv_t * drv, void * file_p, uint32_t pos, lv_fs_whence_t w)
-{
-    (void) drv;
-
-    uint32_t w2;
-    switch(w) {
-    case LV_FS_SEEK_SET:
-        w2 = SEEK_SET;
-        break;
-    case LV_FS_SEEK_CUR:
-        w2 = SEEK_CUR;
-        break;
-    case LV_FS_SEEK_END:
-        w2 = SEEK_END;
-        break;
-    default:
-        w2 = SEEK_SET;
-    }
-
-    fseek (file_p, pos, w2);
-
-    return LV_FS_RES_OK;
-}
-
-static lv_fs_res_t tell_cb(lv_fs_drv_t * drv, void * file_p, uint32_t * pos_p)
-{
-    (void) drv;
-
-    *pos_p = ftell(file_p);
-
-    return LV_FS_RES_OK;
-}
-
 static void hal_init(void)
 {
     static lv_disp_draw_buf_t draw_buf;
@@ -120,19 +61,6 @@ static void hal_init(void)
     indev_encoder_drv.type = LV_INDEV_TYPE_ENCODER;
     indev_encoder_drv.read_cb = lv_test_encoder_read_cb;
     lv_test_encoder_indev = lv_indev_drv_register(&indev_encoder_drv);
-
-
-    static lv_fs_drv_t drv;
-    lv_fs_drv_init(&drv);                     /*Basic initialization*/
-
-    drv.letter = 'F';                         /*An uppercase letter to identify the drive*/
-    drv.open_cb = open_cb;                 /*Callback to open a file*/
-    drv.close_cb = close_cb;               /*Callback to close a file*/
-    drv.read_cb = read_cb;                 /*Callback to read a file*/
-    drv.seek_cb = seek_cb;                 /*Callback to seek in a file (Move cursor)*/
-    drv.tell_cb = tell_cb;                 /*Callback to tell the cursor position*/
-
-    lv_fs_drv_register(&drv);                 /*Finally register the drive*/
 }
 
 static void dummy_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
