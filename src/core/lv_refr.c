cbbace4ca5fff50efb4580b8c6c91cc3192783dc
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-07-05 09:30:43 +0200

    chore: fix warning

--- src/core/lv_refr.c
+++ src/core/lv_refr.c
@@ -433,9 +433,6 @@ void _lv_disp_refr_timer(lv_timer_t * tmr)
         uint32_t used_size = mon.total_size - mon.free_size;;
         uint32_t used_kb = used_size / 1024;
         uint32_t used_kb_tenth = (used_size - (used_kb * 1024)) / 102;
-        uint32_t max_size = mon.max_used;
-        uint32_t max_kb = max_size / 1024;
-        uint32_t max_kb_tenth = (max_size - (max_kb * 1024)) / 102;
         lv_label_set_text_fmt(mem_label,
                               "%"LV_PRIu32 ".%"LV_PRIu32 " kB used (%d %%)\n"
                               "%d%% frag.",
