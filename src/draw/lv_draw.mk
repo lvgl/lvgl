b1c5883c0071dfd3614ec2bbb060c47f01ca305c
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-07-10 14:22:49 +0800

    chore(make): order the source files in *.mk
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- src/draw/lv_draw.mk
+++ src/draw/lv_draw.mk
@@ -1,12 +1,12 @@
-CSRCS += lv_draw_arc.c
 CSRCS += lv_draw.c
+CSRCS += lv_draw_arc.c
 CSRCS += lv_draw_img.c
 CSRCS += lv_draw_label.c
+CSRCS += lv_draw_layer.c
 CSRCS += lv_draw_line.c
 CSRCS += lv_draw_mask.c
 CSRCS += lv_draw_rect.c
 CSRCS += lv_draw_transform.c
-CSRCS += lv_draw_layer.c
 CSRCS += lv_draw_triangle.c
 CSRCS += lv_img_buf.c
 CSRCS += lv_img_cache.c
