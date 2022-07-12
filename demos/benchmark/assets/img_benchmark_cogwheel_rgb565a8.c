4133f1d3171242667182ade7f3154d17a44c4b5c
卢华东 <luhuadong@163.com>
2022-07-12 18:29:51 +0800

    fix(demo): can not found lvgl.h file (#3477)

--- demos/benchmark/assets/img_benchmark_cogwheel_rgb565a8.c
+++ demos/benchmark/assets/img_benchmark_cogwheel_rgb565a8.c
@@ -1,9 +1,4 @@
-#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
-#include "lvgl.h"
-#else
-#include "lvgl/lvgl.h"
-#endif
-
+#include "../../../lvgl.h"
 
 #ifndef LV_ATTRIBUTE_MEM_ALIGN
 #define LV_ATTRIBUTE_MEM_ALIGN
