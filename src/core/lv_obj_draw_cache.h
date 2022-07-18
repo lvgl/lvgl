345a410ddfbcda34c49e1754f94b565c50238949
pengyiqiang <pengyiqiang@xiaomi.com>
2022-07-18 18:28:49 +0800

    Add code comments

--- src/core/lv_obj_draw_cache.h
+++ src/core/lv_obj_draw_cache.h
@@ -27,14 +27,37 @@ extern "C" {
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
+
+/**
+ * Enable object draw cache function
+ * @param obj   pointer to an object
+ * @param en    true to enable, false to disable
+ */
 void lv_obj_draw_cache_set_enable(lv_obj_t * obj, bool en);
 
+/**
+ * Get object draw cache enable
+ * @param obj   pointer to an object
+ * @return      return true if enabled
+ */
 bool lv_obj_draw_cache_get_enable(lv_obj_t * obj);
 
+/**
+ * Mark the object as invalid to update cache
+ * @param obj    pointer to an object
+ */
 void lv_obj_draw_cache_invalidate(lv_obj_t * obj);
 
+/**
+ * Cache mode drawing processing
+ * @param e      darw event parameters
+ */
 lv_res_t _lv_obj_draw_cache(lv_event_t * e);
 
+/**
+ * Free the image from the draw cache
+ * @param obj     pointer to an object
+ */
 void _lv_obj_draw_cache_free(lv_obj_t * obj);
 
 /**********************
