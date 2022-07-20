e42f01a38ad43140cdeb99799c737e9f5e668103
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-07-20 11:39:43 +0200

    fix(make): update lvgl.mk and remove unnecessary mk files

--- lvgl.mk
+++ lvgl.mk
@@ -1,4 +1,6 @@
-CSRCS += $(shell find $(LVGL_DIR_NAME)/src -type f -name '*.c')
-CSRCS += $(shell find $(LVGL_DIR_NAME)/demos -type f -name '*.c')
-CSRCS += $(shell find $(LVGL_DIR_NAME)/examples -type f -name '*.c')
-CFLAGS += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)"
+LVGL_PATH ?= ${shell pwd}/lvgl
+
+CSRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
+CSRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
+CSRCS += $(shell find $(LVGL_PATH)/examples -type f -name '*.c')
+CFLAGS += "-I$(LVGL_PATH)"
