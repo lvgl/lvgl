5a315735fa869432b76d8dcd9c57a30de7a66f3c
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-07-23 06:27:48 +0800

    fix(config): remove LV_DISP_DEF_REFR_PERIOD related stuff (#3512)
    * fix(config): remove LV_DISP_DEF_REFR_PERIOD related stuff
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    
    * fix(config): remove LV_INDEV_DEF_READ_PERIOD related stuff
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- env_support/cmsis-pack/lv_conf_cmsis.h
+++ env_support/cmsis-pack/lv_conf_cmsis.h
@@ -62,12 +62,6 @@
    HAL SETTINGS
  *====================*/
 
-/*Default display refresh period. LVG will redraw changed areas with this period time*/
-#define LV_DISP_DEF_REFR_PERIOD 30      /*[ms]*/
-
-/*Input device read period in milliseconds*/
-#define LV_INDEV_DEF_READ_PERIOD 30     /*[ms]*/
-
 /*Use a custom tick source that tells the elapsed time in milliseconds.
  *It removes the need to manually update the tick with `lv_tick_inc()`)*/
 #ifdef __PERF_COUNTER__
