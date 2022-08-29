9cbcc8712e5cf0d961e4751b272877b5f2c4ed8c
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-08-29 12:25:11 +0200

    feat(demo): add benchmark modes (#3625)

--- demos/benchmark/lv_demo_benchmark.h
+++ demos/benchmark/lv_demo_benchmark.h
@@ -22,23 +22,29 @@ extern "C" {
 /**********************
  *      TYPEDEFS
  **********************/
-typedef void finished_cb_t(void);
+typedef enum {
+    /**Render the scenes and show them on the display.
+     * Measure rendering time but it might contain extra time when LVGL waits for the driver.
+     * Run each scenes for a few seconds so the performance can be seen by eye too.
+     * As only the rendering time is measured and converted to FPS, really high values (e.g. 1000 FPS)
+     * are possible.*/
+    LV_DEMO_BENCHMARK_MODE_RENDER_AND_DRIVER,
 
+    /**Similar to RENDER_AND_DRIVER but instead of measuring the rendering time only measure the real FPS of the system.
+     * E.g. even if a scene was rendered in 1 ms, but the screen is redrawn only in every 100 ms, the result will be 10 FPS.*/
+    LV_DEMO_BENCHMARK_MODE_REAL,
+
+    /**Temporarily display the `flush_cb` so the pure rendering time will be measured.
+     * The display is not updated during the benchmark, only at the end when the summary table is shown.
+     * Render a given number of frames from each scene adn calculate the FPS from them.*/
+    LV_DEMO_BENCHMARK_MODE_RENDER_ONLY,
+} lv_demo_benchmark_mode_t;
 
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
-void lv_demo_benchmark(void);
-
-void lv_demo_benchmark_run_scene(int_fast16_t scene_no);
-
-void lv_demo_benchmark_set_finished_cb(finished_cb_t * finished_cb);
-
-/**
- * Make the benchmark work at the highest frame rate
- * @param en true: highest frame rate; false: default frame rate
- */
-void lv_demo_benchmark_set_max_speed(bool en);
+void lv_demo_benchmark(lv_demo_benchmark_mode_t mode);
+void lv_demo_benchmark_run_scene(lv_demo_benchmark_mode_t mode, uint16_t scene_no);
 
 /**********************
  *      MACROS
