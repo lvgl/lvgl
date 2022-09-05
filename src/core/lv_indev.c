0c7f69ac91a3f4378f10313debdf8dd13b0435bb
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-09-05 10:09:30 +0200

    feat(event): add LV_EVENT_SCROLL_THROW_BEGIN

--- src/core/lv_indev.c
+++ src/core/lv_indev.c
@@ -982,6 +982,9 @@ static void indev_proc_release(_lv_indev_proc_t * proc)
 
             lv_event_send(indev_obj_act, LV_EVENT_CLICKED, indev_act);
             if(indev_reset_check(proc)) return;
+        } else {
+            lv_event_send(scroll_obj, LV_EVENT_SCROLL_THROW_BEGIN, indev_act);
+            if(indev_reset_check(proc)) return;
         }
 
         proc->types.pointer.act_obj = NULL;
@@ -990,8 +993,6 @@ static void indev_proc_release(_lv_indev_proc_t * proc)
 
     }
 
-    /*The reset can be set in the Call the ancestor's event handler function.
-     * In case of reset query ignore the remaining parts.*/
     if(scroll_obj) {
         _lv_indev_scroll_throw_handler(proc);
         if(indev_reset_check(proc)) return;
