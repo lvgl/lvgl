34a08ea88a0eb54f32cdbebdd3d64bdbd826cb70
Mahesh Murty <mahesh@linearcircuits.com>
2022-09-13 15:53:53 +0530

    test: Make use of public apis to get led brightness

--- tests/src/test_cases/test_led.c
+++ tests/src/test_cases/test_led.c
@@ -54,46 +54,43 @@ void test_led_set_color_works(void)
 void test_led_set_brightness_works(void)
 {
     lv_obj_t * led  = lv_led_create(active_screen);
-    lv_led_t * ledObj = (lv_led_t *)led;
-    
+
     /* Test by changing brightness randomly. */
     lv_led_set_brightness(led, 150);
-    TEST_ASSERT_EQUAL(150, ledObj->bright);
+    TEST_ASSERT_EQUAL(150, lv_led_get_brightness(led));
 
     lv_led_set_brightness(led, 10);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, lv_led_get_brightness(led));
 
     lv_led_set_brightness(led, 255);
-    TEST_ASSERT_EQUAL(255, ledObj->bright);
+    TEST_ASSERT_EQUAL(255, lv_led_get_brightness(led));
 }
 
 void test_led_on_off_works(void)
 {
     lv_obj_t * led  = lv_led_create(active_screen);
-    lv_led_t * ledObj = (lv_led_t *)led;
-    
+
     lv_led_on(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, lv_led_get_brightness(led));
 
     lv_led_off(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, lv_led_get_brightness(led);
 
-    lv_led_on(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, ledObj->bright);
+                      lv_led_on(led);
+                      TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, lv_led_get_brightness(led));
 }
 
 void test_led_toggle_works(void)
 {
     lv_obj_t * led  = lv_led_create(active_screen);
-    lv_led_t * ledObj = (lv_led_t *)led;
-    
+
     lv_led_on(led);
     lv_led_toggle(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, lv_led_get_brightness(led));
     lv_led_toggle(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MAX, lv_led_get_brightness(led));
     lv_led_toggle(led);
-    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, ledObj->bright);
+    TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, lv_led_get_brightness(led));
 }
 
 void test_led_get_brightness_works(void)
@@ -118,7 +115,7 @@ static void event_handler(lv_event_t * e)
 /* Added this test to increase code coverage. */
 void test_led_event_draw_main_works(void)
 {
-    lv_obj_t * led  = lv_led_create(active_screen);    
+    lv_obj_t * led  = lv_led_create(active_screen);
     lv_obj_add_event_cb(led, event_handler, LV_EVENT_DRAW_MAIN, NULL);
     /* Set expected event code before the event is raised. */
     exp_evt_code = LV_EVENT_DRAW_MAIN;
