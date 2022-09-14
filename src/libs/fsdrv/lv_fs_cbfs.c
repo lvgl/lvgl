fd749e150bf7eb5a92e1efd3b847ef7de4debd91
codewitch-honey-crisis <gaztoast@gmail.com>
2022-09-14 08:47:59 -0700

    nonsense. formatting a file that shouldn't exist

--- src/libs/fsdrv/lv_fs_cbfs.c
+++ src/libs/fsdrv/lv_fs_cbfs.c
@@ -1,9 +1,10 @@
 // this file should not exist
 #ifdef __GNUC__
-#define IS_NOT_USED __attribute__ ((unused))
+    #define IS_NOT_USED __attribute__ ((unused))
 #else
-#define IS_NOT_USED
+    #define IS_NOT_USED
 #endif
-IS_NOT_USED static void nothing(void) {
+IS_NOT_USED static void nothing(void)
+{
     // do nothing
 }
