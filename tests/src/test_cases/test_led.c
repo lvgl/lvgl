7d61304a053cb1f0a3ace24e4a232e9a7e642935
Mahesh Murty <mahesh@linearcircuits.com>
2022-09-15 15:23:51 +0530

    test: Remove draw main event test

--- tests/src/test_cases/test_led.c
+++ tests/src/test_cases/test_led.c
@@ -4,8 +4,6 @@
 #include "unity/unity.h"
 
 static lv_obj_t * active_screen = NULL;
-static lv_event_code_t exp_evt_code;
-static bool event_triggered = false;
 
 void setUp(void)
 {
@@ -103,24 +101,6 @@ void test_led_get_brightness_works(void)
     TEST_ASSERT_EQUAL(LV_LED_BRIGHT_MIN, lv_led_get_brightness(led));
     lv_led_set_brightness(led, 150);
     TEST_ASSERT_EQUAL(150, lv_led_get_brightness(led));
-}
-
-static void event_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    if(code == exp_evt_code) {
-        event_triggered = true;
-    }
-}
-/* Added this test to increase code coverage. */
-void test_led_event_draw_main_works(void)
-{
-    lv_obj_t * led  = lv_led_create(active_screen);
-    lv_obj_add_event_cb(led, event_handler, LV_EVENT_DRAW_MAIN, NULL);
-    /* Set expected event code before the event is raised. */
-    exp_evt_code = LV_EVENT_DRAW_MAIN;
-    lv_event_send(led, LV_EVENT_DRAW_MAIN, NULL);
-    TEST_ASSERT_TRUE(event_triggered);
 }
 
 #endif
