549a369374870714f11ef093ec58277c807bcc75
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-09-19 09:33:57 +0200

    fix header

--- src/libs/tiny_ttf/lv_tiny_ttf.h
+++ src/libs/tiny_ttf/lv_tiny_ttf.h
@@ -1,19 +1,50 @@
-// honey the codewitch (github codewitch-honey-crisis)
+/**
+ * @file lv_templ.h
+ *
+ */
+
 #ifndef LV_TINY_TTF_H
 #define LV_TINY_TTF_H
-#include "../../../lvgl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include "../../lv_conf_internal.h"
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../../../lvgl.h"
+
+#if LV_USE_TINY_TTF
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
 // create a font from the specified file or path with the specified line height.
 lv_font_t * lv_tiny_ttf_create_file(const char * path, lv_coord_t line_height);
+
 // create a font from the specified data pointer with the specified line height.
 lv_font_t * lv_tiny_ttf_create_data(const void * data, size_t data_size, lv_coord_t line_height);
+
 // destroy a font previously created with lv_tiny_ttf_create_xxxx()
 void lv_tiny_ttf_destroy(lv_font_t * font);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*LV_USE_TINY_TTF*/
+
 #ifdef __cplusplus
-}
+} /*extern "C"*/
 #endif
-#endif // LV_TINY_TTF_H
+
+#endif /*LV_TINY_TTF_H*/
