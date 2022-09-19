24893c37ae18618089eff8a9bf38f604ae23b96e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-09-19 14:00:39 +0200

    chore: fix warnings

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -153,7 +153,7 @@ void lv_init(void)
     lv_img_cache_set_size(LV_IMG_CACHE_DEF_SIZE);
 #endif
     /*Test if the IDE has UTF-8 encoding*/
-    char * txt = "Á";
+    const char * txt = "Á";
 
     uint8_t * txt_u8 = (uint8_t *)txt;
     if(txt_u8[0] != 0xc3 || txt_u8[1] != 0x81 || txt_u8[2] != 0x00) {
