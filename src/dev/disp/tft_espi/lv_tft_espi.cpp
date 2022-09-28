6d650bcafa5d01fa2043afdf325f0db011e7902b
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-09-28 13:38:55 +0200

    fix tft_espi, tested with Arduino

--- src/dev/disp/tft_espi/lv_tft_espi.cpp
+++ src/dev/disp/tft_espi/lv_tft_espi.cpp
@@ -19,7 +19,7 @@
  *      TYPEDEFS
  **********************/
 typedef struct {
-    TFT_eSPI tft;
+    TFT_eSPI * tft;
 } lv_tft_espi_t;
 
 /**********************
@@ -39,7 +39,7 @@ static void flush_cb(lv_disp_t * disp, const lv_area_t * area, lv_color_t * colo
  *   GLOBAL FUNCTIONS
  **********************/
 
-lv_disp_t * lv_tft_espi_create(uint32_t hor_res, uint32_t ver_res)
+lv_disp_t * lv_tft_espi_create(uint32_t hor_res, uint32_t ver_res, void * buf, uint32_t buf_size_px)
 {
     lv_tft_espi_t * dsc = (lv_tft_espi_t *)lv_malloc(sizeof(lv_tft_espi_t));
     LV_ASSERT_MALLOC(dsc);
@@ -52,12 +52,12 @@ lv_disp_t * lv_tft_espi_create(uint32_t hor_res, uint32_t ver_res)
         return NULL;
     }
 
-    dsc->tft = TFT_eSPI(hor_res, ver_res);
-    dsc->tft.begin();          /* TFT init */
-    dsc->tft.setRotation( 3 ); /* Landscape orientation, flipped */
+    dsc->tft = new TFT_eSPI(hor_res, ver_res);
+    dsc->tft->begin();          /* TFT init */
+    dsc->tft->setRotation( 3 ); /* Landscape orientation, flipped */
     lv_disp_set_user_data(disp, (void *)dsc);
     lv_disp_set_flush_cb(disp, flush_cb);
-
+    lv_disp_set_draw_buffers(disp, (void*)buf, NULL, buf_size_px, LV_DISP_RENDER_MODE_PARTIAL);
     return disp;
 }
 
@@ -72,10 +72,10 @@ static void flush_cb(lv_disp_t * disp, const lv_area_t * area, lv_color_t * colo
     uint32_t w = ( area->x2 - area->x1 + 1 );
     uint32_t h = ( area->y2 - area->y1 + 1 );
 
-    dsc->tft.startWrite();
-    dsc->tft.setAddrWindow( area->x1, area->y1, w, h );
-    dsc->tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
-    dsc->tft.endWrite();
+    dsc->tft->startWrite();
+    dsc->tft->setAddrWindow( area->x1, area->y1, w, h );
+    dsc->tft->pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    dsc->tft->endWrite();
 
     lv_disp_flush_ready( disp );
 
