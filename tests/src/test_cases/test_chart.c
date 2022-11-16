c905caf7da4cad00a8e4a8465072de59fe0f794e
C47D <carlos.santiago.diaz@gmail.com>
2022-11-16 00:37:47 -0600

    test(chart): Tests for lv_chart_set_point_count

--- tests/src/test_cases/test_chart.c
+++ tests/src/test_cases/test_chart.c
@@ -34,4 +34,70 @@ void test_chart_add_series(void)
     lv_chart_remove_series(chart, red_series);
 }
 
+void test_chart_set_point_count_increments(void)
+{
+    lv_chart_series_t * red_series;
+    red_series = lv_chart_add_series(chart, red_color, LV_CHART_AXIS_SECONDARY_Y);
+
+    uint16_t points_in_serie = lv_chart_get_point_count(chart);
+    uint16_t new_point_count = points_in_serie * 2;
+    lv_chart_set_point_count(chart, new_point_count);
+
+    TEST_ASSERT_EQUAL_MESSAGE(new_point_count, lv_chart_get_point_count(chart),
+                              "Actual points in chart are less than expected");
+    lv_chart_remove_series(chart, red_series);
+}
+
+void test_chart_set_point_count_decrements(void)
+{
+    lv_chart_series_t * red_series;
+    red_series = lv_chart_add_series(chart, red_color, LV_CHART_AXIS_SECONDARY_Y);
+    uint16_t points_in_serie = lv_chart_get_point_count(chart);
+    uint16_t new_point_count = points_in_serie / 2;
+
+    lv_chart_set_point_count(chart, new_point_count);
+
+    TEST_ASSERT_EQUAL_MESSAGE(new_point_count, lv_chart_get_point_count(chart),
+                              "Actual points in chart are more than expected");
+    lv_chart_remove_series(chart, red_series);
+}
+
+void test_chart_set_point_count_as_same(void)
+{
+    lv_chart_series_t * red_series;
+    red_series = lv_chart_add_series(chart, red_color, LV_CHART_AXIS_SECONDARY_Y);
+    uint16_t points_in_serie = lv_chart_get_point_count(chart);
+    uint16_t new_point_count = points_in_serie;
+
+    lv_chart_set_point_count(chart, new_point_count);
+
+    TEST_ASSERT_EQUAL_MESSAGE(new_point_count, lv_chart_get_point_count(chart),
+                              "Actual points is not equal to original point count");
+    lv_chart_remove_series(chart, red_series);
+}
+
+void test_chart_set_new_point_count_as_zero(void)
+{
+    lv_chart_series_t * red_series;
+    red_series = lv_chart_add_series(chart, red_color, LV_CHART_AXIS_SECONDARY_Y);
+
+    lv_chart_set_point_count(chart, 0u);
+
+    TEST_ASSERT_EQUAL_MESSAGE(1u, lv_chart_get_point_count(chart), "Actual points in chart are more than 1");
+    lv_chart_remove_series(chart, red_series);
+}
+
+void test_chart_point_is_added_at_the_end_of_a_serie(void)
+{
+#if 0
+    lv_chart_series_t * red_series;
+    red_series = lv_chart_add_series(chart, red_color, LV_CHART_AXIS_SECONDARY_Y);
+
+    TEST_ASSERT_NOT_NULL_MESSAGE(red_series, "Red series not added to chart");
+    TEST_ASSERT_NULL_MESSAGE(red_series->x_points, "X points in non scatter chart should not point to anything");
+
+    lv_chart_remove_series(chart, red_series);
+#endif
+}
+
 #endif
