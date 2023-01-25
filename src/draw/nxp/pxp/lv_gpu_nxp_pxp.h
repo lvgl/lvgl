efaeffbe166c6783cdbc926aabfcd9b1307b8e29
Nicușor Cîțu <nicusor.citu@nxp.com>
2023-01-25 10:24:18 +0200

    fix(pxp) Update copyright.
    Signed-off-by: Nicușor Cîțu <nicusor.citu@nxp.com>

--- src/draw/nxp/pxp/lv_gpu_nxp_pxp.h
+++ src/draw/nxp/pxp/lv_gpu_nxp_pxp.h
@@ -6,7 +6,7 @@
 /**
  * MIT License
  *
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
