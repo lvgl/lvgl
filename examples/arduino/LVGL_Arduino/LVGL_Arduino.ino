3e2f19b07b6c8d78bc3534dc532d4223dd7c946b
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-03-14 07:38:03 +0100

    docs(arduino): add note to not use lv_examles library

--- examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
+++ examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
@@ -1,12 +1,13 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/get-started/platforms/arduino.html  */
+
 #include <lvgl.h>
 #include <TFT_eSPI.h>
-/*If you want to use the LVGL examples,
-  make sure to install the lv_examples Arduino library
-  and uncomment the following line.
-#include <lv_examples.h>
-*/
 
-#include <lv_demo.h>
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ as the examples and demos are now part of the main LVGL library. */
 
 /*Change to your screen resolution*/
 static const uint16_t screenWidth  = 480;
@@ -27,7 +28,7 @@ void my_print(const char * buf)
 #endif
 
 /* Display flushing */
-void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+void my_disp_flush( lv_disp_t *disp, const lv_area_t *area, lv_color_t *color_p )
 {
     uint32_t w = ( area->x2 - area->x1 + 1 );
     uint32_t h = ( area->y2 - area->y1 + 1 );
@@ -41,7 +42,7 @@ void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *colo
 }
 
 /*Read the touchpad*/
-void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+void my_touchpad_read( lv_indev_t * indev_driver, lv_indev_data_t * data )
 {
     uint16_t touchX, touchY;
 
@@ -95,7 +96,7 @@ void setup()
     lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
 
     /*Initialize the display*/
-    static lv_disp_drv_t disp_drv;
+    static lv_disp_t disp_drv;
     lv_disp_drv_init( &disp_drv );
     /*Change the following line to your display resolution*/
     disp_drv.hor_res = screenWidth;
@@ -105,7 +106,7 @@ void setup()
     lv_disp_drv_register( &disp_drv );
 
     /*Initialize the (dummy) input device driver*/
-    static lv_indev_drv_t indev_drv;
+    static lv_indev_t indev_drv;
     lv_indev_drv_init( &indev_drv );
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_touchpad_read;
