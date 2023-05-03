6a6b0388bf234c4741ce5889db7c744478a4220d
C47D <carlos.santiago.diaz@gmail.com>
2023-05-02 23:54:41 -0600

    lv_scale: Add LV_PART_TICKS to drawing api

--- src/widgets/scale/lv_scale.c
+++ src/widgets/scale/lv_scale.c
@@ -30,7 +30,8 @@
 
 static void lv_scale_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj);
 static void lv_scale_event(const lv_obj_class_t * class_p, lv_event_t * event);
-static void lv_scale_draw(lv_event_t * event);
+
+static void draw_horizontal_scale(lv_obj_t *obj, lv_event_t * event);
 
 /**********************
  *  STATIC VARIABLES
@@ -71,6 +72,36 @@ lv_obj_t * lv_scale_create(lv_obj_t * parent)
  * Setter functions
  *====================*/
 
+void lv_scale_mode(lv_obj_t * obj, lv_scale_mode_t mode)
+{
+    LV_ASSERT_OBJ(obj, MY_CLASS);
+    lv_scale_t * scale = (lv_scale_t *)obj;
+
+    scale->mode = mode;
+
+    lv_obj_invalidate(obj);
+}
+
+void lv_scale_set_total_tick_count(lv_obj_t * obj, lv_coord_t total_tick_count)
+{
+    LV_ASSERT_OBJ(obj, MY_CLASS);
+    lv_scale_t * scale = (lv_scale_t *)obj;
+
+    scale->total_tick_count = total_tick_count;
+
+    lv_obj_invalidate(obj);
+}
+
+void lv_scale_set_major_tick_every(lv_obj_t * obj, lv_coord_t major_tick_every)
+{
+    LV_ASSERT_OBJ(obj, MY_CLASS);
+    lv_scale_t * scale = (lv_scale_t *)obj;
+
+    scale->major_tick_every = major_tick_every;
+
+    lv_obj_invalidate(obj);
+}
+
 /*=====================
  * Getter functions
  *====================*/
@@ -90,6 +121,13 @@ static void lv_scale_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
 
     lv_scale_t * scale = (lv_scale_t *)obj;
 
+    // scale->total_tick_count = LV_SCALE_TOTAL_TICK_COUNT_DEFAULT;
+    scale->total_tick_count = 4;
+    // scale->major_tick_every = LV_SCALE_MAJOR_TICK_EVERY_DEFAULT;
+    scale->major_tick_every = 2;
+    scale->mode = LV_SCALE_MODE_HORIZONTAL;
+    scale->label_enabled = LV_SCALE_LABEL_ENABLED_DEFAULT;
+
     LV_TRACE_OBJ_CREATE("finished");
 }
 
@@ -101,20 +139,106 @@ static void lv_scale_event(const lv_obj_class_t * class_p, lv_event_t * event)
     lv_res_t res = lv_obj_event_base(MY_CLASS, event);
     if(res != LV_RES_OK) return;
 
-    lv_event_code_t code = lv_event_get_code(event);
+    lv_event_code_t event_code = lv_event_get_code(event);
     lv_obj_t * obj = lv_event_get_target(event);
     lv_scale_t * scale = (lv_scale_t *)lv_event_get_target(event);
+
+    if(event_code == LV_EVENT_DRAW_MAIN) {
+        if (LV_SCALE_MODE_HORIZONTAL == scale->mode) {
+            draw_horizontal_scale(obj, event);
+        }
+        else if (LV_SCALE_MODE_VERTICAL == scale->mode) {
+
+        }
+        else if (LV_SCALE_MODE_ROUND == scale->mode) {
+            
+        }
+        else {
+            /* Invalid mode */
+        }
+    }
+    else {
+        /* TODO */
+    }
 }
 
-static void lv_scale_draw(lv_event_t * event)
+static void draw_horizontal_scale(lv_obj_t *obj, lv_event_t * event)
 {
-    lv_obj_t * obj = lv_event_get_target(event);
     lv_scale_t * scale = (lv_scale_t *)obj;
-
     lv_draw_ctx_t * draw_ctx = lv_event_get_draw_ctx(event);
+
+    lv_point_t point1;
+    lv_point_t point2;
+
+    lv_draw_label_dsc_t label_dsc;
+    lv_draw_label_dsc_init(&label_dsc);
+    lv_obj_init_draw_label_dsc(obj, LV_PART_TICKS, &label_dsc);
+
+    lv_draw_line_dsc_t line_dsc;
+    lv_draw_line_dsc_init(&line_dsc);
+    lv_obj_init_draw_line_dsc(obj, LV_PART_TICKS, &line_dsc);
+
+    lv_coord_t pad_left = lv_obj_get_style_pad_left(obj, LV_PART_MAIN) + lv_obj_get_style_border_width(obj, LV_PART_MAIN);
+    lv_coord_t w = (int32_t)lv_obj_get_content_width(obj);
+
+    lv_coord_t x_ofs = obj->coords.x1 + pad_left - lv_obj_get_scroll_left(obj);
+    lv_coord_t y_ofs = obj->coords.y2;
+    lv_coord_t label_gap;
+    label_gap = scale->label_enabled ? lv_obj_get_style_pad_bottom(obj, LV_PART_TICKS) : 0;
+
+    /* Line descriptor customization */
+    line_dsc.dash_gap = 0;
+    line_dsc.dash_width = 0;
 
     lv_obj_draw_part_dsc_t part_draw_dsc;
     lv_obj_draw_dsc_init(&part_draw_dsc, draw_ctx);
-}
+    part_draw_dsc.class_p = MY_CLASS;
+    part_draw_dsc.type = LV_SCALE_DRAW_PART_TICK_LABEL;
+    part_draw_dsc.id = LV_SCALE_MODE_HORIZONTAL;
+    part_draw_dsc.part = LV_PART_TICKS;
+    part_draw_dsc.label_dsc = &label_dsc;
+    part_draw_dsc.line_dsc = &line_dsc;
+
+    point1.y = y_ofs;
+    uint8_t tick_idx = 0;
+    for (tick_idx = 0; tick_idx < scale->total_tick_count; tick_idx++)
+    {
+        /*draw a line at moving x position*/
+        point2.x = point1.x = x_ofs + (int32_t)((int32_t)(w - line_dsc.width) * tick_idx) / 5;
+        point2.y = point1.y - 10; /* TODO */
+
+        LV_LOG_USER("Drawing tick %d at X: %d", tick_idx, point1.x);
+
+        part_draw_dsc.p1 = &point1;
+        part_draw_dsc.p2 = &point2;
+
+        part_draw_dsc.label_dsc = &label_dsc;
+        char text_buffer[20];
+        lv_snprintf(text_buffer, sizeof(text_buffer), "Hi %" LV_PRId32, tick_idx);
+        part_draw_dsc.label_dsc = &label_dsc;
+        part_draw_dsc.text = text_buffer;
+        part_draw_dsc.text_length = 10;
+
+        lv_event_send(obj, LV_EVENT_DRAW_PART_BEGIN, &part_draw_dsc);
+
+        /*reserve appropriate area*/
+        lv_point_t size;
+        lv_txt_get_size(&size, part_draw_dsc.text, label_dsc.font, label_dsc.letter_space, label_dsc.line_space, LV_COORD_MAX,
+                        LV_TEXT_FLAG_NONE);
+
+        lv_area_t a;
+        a.x1 = (point2.x - size.x / 2);
+        a.x2 = (point2.x + size.x / 2);
+        a.y1 = point2.y + label_gap;
+        a.y2 = a.y1 + size.y;
+
+        LV_LOG_USER("Drawing line P1 {%d,%d} P1 {%d,%d}", point1.x, point1.y, point2.x, point2.y);
+
+        lv_draw_label(draw_ctx, &label_dsc, &a, part_draw_dsc.text, NULL);
+        lv_draw_line(draw_ctx, &line_dsc, &point1, &point2);
+
+        lv_event_send(obj, LV_EVENT_DRAW_PART_END, &part_draw_dsc);
+    }
+}   
 
 #endif
--- src/widgets/scale/lv_scale.h
+++ src/widgets/scale/lv_scale.h
@@ -23,15 +23,50 @@ extern "C" {
  *      DEFINES
  *********************/
 
+/**Default value of total minor ticks. */
+#define LV_SCALE_TOTAL_TICK_COUNT_DEFAULT (10U)
+LV_EXPORT_CONST_INT(LV_SCALE_TOTAL_TICK_COUNT_DEFAULT);
+
+/**Default value of major tick every nth ticks. */
+#define LV_SCALE_MAJOR_TICK_EVERY_DEFAULT (5U)
+LV_EXPORT_CONST_INT(LV_SCALE_MAJOR_TICK_EVERY_DEFAULT);
+
+/**Default value of scale label enabled. */
+#define LV_SCALE_LABEL_ENABLED_DEFAULT (1U)
+LV_EXPORT_CONST_INT(LV_SCALE_LABEL_ENABLED_DEFAULT);
+
 /**********************
  *      TYPEDEFS
  **********************/
 
+/**
+ * Scale mode
+ */
+enum {
+    LV_SCALE_MODE_HORIZONTAL    = 0x00,
+    LV_SCALE_MODE_VERTICAL      = 0x01,
+    LV_SCALE_MODE_ROUND         = 0x02,
+    _LV_SCALE_MODE_LAST
+};
+typedef uint8_t lv_scale_mode_t;
+
 typedef struct {
     lv_obj_t obj;
+    uint32_t total_tick_count   : 15;
+    uint32_t major_tick_every   : 15;
+    lv_scale_mode_t mode;
+    uint32_t label_enabled      : 1;
 } lv_scale_t;
 
 extern const lv_obj_class_t lv_scale_class;
+
+/**
+ * `type` field in `lv_obj_draw_part_dsc_t` if `class_p = lv_scale_class`
+ * Used in `LV_EVENT_DRAW_PART_BEGIN` and `LV_EVENT_DRAW_PART_END`
+ */
+typedef enum {
+    LV_SCALE_DRAW_PART_TICK_LABEL,       /**< Used on tick lines and labels*/
+} lv_scale_draw_part_type_t;
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -51,6 +86,27 @@ lv_obj_t * lv_scale_create(lv_obj_t * parent);
 /*=====================
  * Setter functions
  *====================*/
+
+/**
+ * Set scale mode. See @ref lv_scale_mode_t
+ * @param   obj     pointer the scale object
+ * @param   mode    New scale mode
+ */
+void lv_scale_mode(lv_obj_t * obj, lv_scale_mode_t mode);
+
+/**
+ * Set scale total tick count (including minor and major ticks)
+ * @param   obj       pointer the scale object
+ * @param   total_tick_count    New total tick count
+ */
+void lv_scale_set_total_tick_count(lv_obj_t * obj, lv_coord_t total_tick_count);
+
+/**
+ * Sets how often the major tick will be drawn
+ * @param   obj       pointer the scale object
+ * @param   major_tick_every    New count for major tick drawing
+ */
+void lv_scale_set_major_tick_every(lv_obj_t * obj, lv_coord_t major_tick_every);
 
 /*=====================
  * Getter functions
