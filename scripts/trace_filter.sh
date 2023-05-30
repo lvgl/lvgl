13cca3e1e1e81f5ee4a58a98b42818adbd865e42
FASTSHIFT <vifextech@foxmail.com>
2023-05-31 01:36:47 +0800

    use chatGPT refact trace_filter.sh

--- scripts/trace_filter.sh
+++ scripts/trace_filter.sh
@@ -1,18 +1,36 @@
-#!/bin/env bash
+#!/bin/bash
 
-logfile=$1
-outfile=$2
+show_help() {
+    echo "Usage: $0 <log_file> [trace_file]"
+    echo "log_file: The input log file to process."
+    echo "trace_file (optional): The output trace file. If not provided, defaults to 'trace.systrace'."
+}
 
-echo file: $logfile
-if [ ! -f $logfile ]; then 
-    echo "File does not exist"
+log_file="$1"
+trace_file="$2"
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    show_help
+    exit 0
+fi
+
+if [ -z "$log_file" ]; then
+    echo "Missing log_file argument."
+    show_help
+    exit -1
+fi
+
+if [ ! -f "$log_file" ]; then 
+    echo "log_file does not exist"
     exit -2
 fi
 
-if [ ! -f "$outfile" ]; then 
-    outfile=trace.systrace
+if [ -z "$trace_file" ]; then 
+    trace_file="trace.systrace"
 fi
-echo "outfile: $outfile"
 
-printf "# tracer: nop\n#\n" > $outfile
-cat $logfile | grep -E ".*-[0-9]+\s[0-9]*" >> $outfile
+echo "log file: $log_file"
+echo "trace file: $trace_file"
+
+printf "# tracer: nop\n#\n" > "$trace_file"
+grep -E ".*-[0-9]+\s[0-9]*" "$log_file" >> "$trace_file"
