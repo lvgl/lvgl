94d1e69b7d560221adf6ee58cfca17a5a6201627
C47D <carlos.santiago.diaz@gmail.com>
2023-06-05 18:42:51 -0600

    lv_scale: Add support for label opa, letter space and font to sections

--- src/widgets/scale/lv_scale.c
+++ src/widgets/scale/lv_scale.c
@@ -526,18 +526,49 @@ static void scale_draw_indicator(lv_obj_t *obj, lv_event_t * event)
             _LV_LL_READ_BACK(&scale->section_ll, section) {
                 if(section->minor_range <= tick_value && section->major_range >= tick_value) {
                     lv_style_value_t value;
+
+                    /* Text color */
                     lv_res_t res = lv_style_get_prop(section->indicator_style, LV_STYLE_TEXT_COLOR, &value);
                     if(res == LV_RES_OK) {
-                        /* Section property found, overwrite value */
                         label_dsc.color = value.color;
                     }
                     else {
                         label_dsc.color = lv_obj_get_style_text_color(scale, LV_PART_INDICATOR);
                     }
+
+                    /* Text opa */
+                    res = lv_style_get_prop(section->indicator_style, LV_STYLE_TEXT_OPA, &value);
+                    if(res == LV_RES_OK) {
+                        label_dsc.opa = (lv_opa_t)value.num;
+                    }
+                    else {
+                        label_dsc.opa = lv_obj_get_style_text_opa(scale, LV_PART_INDICATOR);
+                    }
+
+                    /* Text letter space */
+                    res = lv_style_get_prop(section->indicator_style, LV_STYLE_TEXT_LETTER_SPACE, &value);
+                    if(res == LV_RES_OK) {
+                        label_dsc.letter_space = (lv_coord_t)value.num;
+                    }
+                    else {
+                        label_dsc.letter_space = lv_obj_get_style_text_letter_space(scale, LV_PART_INDICATOR);
+                    }
+
+                    /* Text font */
+                    res = lv_style_get_prop(section->indicator_style, LV_STYLE_TEXT_FONT, &value);
+                    if(res == LV_RES_OK) {
+                        label_dsc.font = (const lv_font_t *)value.ptr;
+                    }
+                    else {
+                        label_dsc.font = lv_obj_get_style_text_font(scale, LV_PART_INDICATOR);
+                    }
                 }
                 else {
                     /* If label is not within a range then get the indicator style */
                     label_dsc.color = lv_obj_get_style_text_color(scale, LV_PART_INDICATOR);
+                    label_dsc.opa = lv_obj_get_style_text_opa(scale, LV_PART_INDICATOR);
+                    label_dsc.letter_space = lv_obj_get_style_text_letter_space(scale, LV_PART_INDICATOR);
+                    label_dsc.font = lv_obj_get_style_text_font(scale, LV_PART_INDICATOR);
                 }
             }
 
