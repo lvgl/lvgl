6b19287afccfcc37917839dd43fbf0a5beca1f3c
C47D <carlos.santiago.diaz@gmail.com>
2023-07-18 22:07:54 -0600

    lv_scale: Run lv_conf_internal_gen.py for LV_USE_SCALE

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -469,8 +469,6 @@
 
 #define LV_USE_SCALE      1
 
-#define LV_USE_SCALE      1
-
 #define LV_USE_SLIDER     1   /*Requires: lv_bar*/
 
 #define LV_USE_SPAN       1
--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -1502,24 +1502,12 @@
 #ifndef LV_USE_SCALE
     #ifdef _LV_KCONFIG_PRESENT
         #ifdef CONFIG_LV_USE_SCALE
-            #define LV_USE_SLIDER CONFIG_LV_USE_SCALE
+            #define LV_USE_SCALE CONFIG_LV_USE_SCALE
         #else
             #define LV_USE_SCALE 0
         #endif
     #else
-        #define LV_USE_SCALE     1
-    #endif
-#endif
-
-#ifndef LV_USE_SCALE
-    #ifdef _LV_KCONFIG_PRESENT
-        #ifdef CONFIG_LV_USE_SCALE
-            #define LV_USE_SLIDER CONFIG_LV_USE_SCALE
-        #else
-            #define LV_USE_SCALE 0
-        #endif
-    #else
-        #define LV_USE_SCALE     1
+        #define LV_USE_SCALE      1
     #endif
 #endif
 
