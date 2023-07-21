d40dfaee0c600e03be8de651450bcfa142298a76
the-programmer <the-programmer@users.noreply.github.com>
2023-07-21 09:51:37 +0200

    fix(font): fix optimizer issue in  lv_font_fmt_txt.c (#4385)
    Co-authored-by: Gabor Kiss-Vamosi <kisvegabor@gmail.com>

--- src/extra/widgets/imgbtn/lv_imgbtn.c
+++ src/extra/widgets/imgbtn/lv_imgbtn.c
@@ -209,7 +209,8 @@ static void lv_imgbtn_event(const lv_obj_class_t * class_p, lv_event_t * e)
     else if(code == LV_EVENT_VALUE_CHANGED) {
         if(lv_obj_has_state(obj, LV_STATE_CHECKED)) {
             lv_imgbtn_set_state(obj, LV_IMGBTN_STATE_CHECKED_RELEASED);
-        } else {
+        }
+        else {
             lv_imgbtn_set_state(obj, LV_IMGBTN_STATE_RELEASED);
         }
     }
