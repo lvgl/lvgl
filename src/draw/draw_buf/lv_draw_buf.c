cb73a2ad62927af11cb01c04a00c9beed33272dc
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-07-23 19:55:47 +0200

    basic API

---
+++ src/draw/draw_buf/lv_draw_buf.c
@@ -0,0 +1,102 @@
+/**
+ * @file lv_draw_buf.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_draw_buf.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static uint8_t * buf_alloc(void * old_buf, lv_coord_t w, lv_coord_t h);
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+lv_draw_buf_t  * lv_draw_buf_malloc(lv_coord_t w, lv_coord_t h, lv_color_format_t color_format)
+{
+    lv_draw_buf_t * draw_buf = lv_malloc(sizeof(lv_draw_buf_t));
+    draw_buf->width = w;
+    draw_buf->height = h;
+    draw_buf->color_format = color_format;
+    draw_buf->buf = buf_alloc(NULL, w, h);
+
+    return draw_buf;
+}
+
+
+lv_draw_buf_t  * lv_draw_buf_realloc(lv_draw_buf_t  * draw_buf, lv_coord_t w, lv_coord_t h,
+                                     lv_color_format_t color_format)
+{
+    draw_buf->width = w;
+    draw_buf->height = h;
+    draw_buf->color_format = color_format;
+
+    draw_buf->buf = buf_alloc(draw_buf->buf, w, h);
+    return draw_buf;
+}
+
+void lv_draw_buf_free(lv_draw_buf_t  * draw_buf)
+{
+    lv_free(draw_buf->buf);
+    lv_free(draw_buf);
+}
+
+void lv_draw_buf_invalidate_cache(lv_draw_buf_t  * buf)
+{
+
+}
+
+void * lv_draw_buf_go_to_xy(lv_draw_buf_t * draw_buf, lv_coord_t x, lv_coord_t y)
+{
+    uint32_t px_size = lv_color_format_get_size(draw_buf->color_format);
+    uint8_t * buf_tmp = draw_buf->buf;
+    buf_tmp += draw_buf->stride * y * px_size;
+    buf_tmp += x * px_size;
+
+    return buf_tmp;
+}
+
+
+void lv_draw_buf_clear(lv_draw_buf_t * draw_buf, const lv_area_t * a)
+{
+    uint8_t px_size = lv_color_format_get_size(draw_buf->color_format);
+    lv_memzero(draw_buf->buf, draw_buf->stride * draw_buf->height * draw_buf->color_format);
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+static uint8_t * buf_alloc(void * old_buf, lv_coord_t w, lv_coord_t h)
+{
+    w = (w / LV_DRAW_BUF_STRIDE) * LV_DRAW_BUF_STRIDE;
+    uint8_t * buf;
+    size_t s = w * h + LV_DRAW_BUF_ALIGN - 1;
+    if(old_buf) buf = lv_realloc(old_buf, s);
+    else  buf = lv_malloc(s);
+
+    buf += LV_DRAW_BUF_ALIGN - 1;
+    buf = (lv_uintptr_t) buf & ~(LV_DRAW_BUF_ALIGN - 1);
+    return buf;
+}
---
+++ src/draw/draw_buf/lv_draw_buf.h
@@ -0,0 +1,54 @@
+/**
+ * @file lv_draw_buf.h
+ *
+ */
+
+#ifndef LV_DRAW_BUF_H
+#define LV_DRAW_BUF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../../misc/lv_area.h"
+#include "../../misc/lv_color.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+typedef struct {
+    void * buf;
+    lv_coord_t width;
+    lv_coord_t height;
+    lv_coord_t stride;
+    lv_color_format_t color_format;
+} lv_draw_buf_t;
+
+lv_draw_buf_t  * lv_draw_buf_malloc(lv_coord_t w, lv_coord_t h, lv_color_format_t color_format);
+lv_draw_buf_t  * lv_draw_buf_realloc(lv_draw_buf_t  * buf, lv_coord_t w, lv_coord_t h, lv_color_format_t color_format);
+void lv_draw_buf_free(lv_draw_buf_t  * buf);
+void lv_draw_buf_invalidate_cache(lv_draw_buf_t  * buf);
+
+void * lv_draw_buf_go_to_xy(lv_draw_buf_t * draw_buf, lv_coord_t x, lv_coord_t y);
+void lv_draw_buf_clear(lv_draw_buf_t * draw_buf, const lv_area_t * a);
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DRAW_BUF_H*/
