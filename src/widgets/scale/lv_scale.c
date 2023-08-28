b5ece96909dd5c5b46e44b74cd1f3f0d37c7c5fc
C47D <carlos.santiago.diaz@gmail.com>
2023-08-27 23:42:09 -0600

    lv_scale: Add missing LV_SCALE_DEFAULT_ZOOM  symbol

--- src/widgets/scale/lv_scale.c
+++ src/widgets/scale/lv_scale.c
@@ -20,7 +20,7 @@
 #define MY_CLASS &lv_scale_class
 
 #define LV_SCALE_LABEL_TXT_LEN  (20U)
-#define LV_SCALE_DEFAULT_ZOOM	((int32_t) 256U)
+#define LV_SCALE_DEFAULT_ZOOM   ((int32_t) 256U)
 
 /**********************
  *      TYPEDEFS
