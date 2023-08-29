42252396eb0bea0a5f9c9754aa839a1053099cf1
W-Mai <1341398182@qq.com>
2023-08-29 12:02:49 +0800

    feat(lv_lru_rb): add rb tree based lru cache manager

---
+++ src/misc/lv_lru_rb.c
@@ -0,0 +1,246 @@
+/**
+ * @file lv_lru_rb.c
+ *
+ */
+
+/**
+ * @brief cache model
+ * */
+
+/*************************************************************************\
+*                                                                         *
+*                                             ┏ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ┓ *
+* ┏ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━          ┌ ─ ─ ─ ┐              *
+*             ┌ ─ ─ ─ ─ ─ ─ ─            ┃    ┃      Cache   insert     ┃ *
+* ┃               RB Tree    │                     │Hitting│  head        *
+*             └ ─ ─ ─ ─ ─ ─ ─            ┃    ┃     ─ ─ ─ ─             ┃ *
+* ┃      ┌─┬─┬─┬─┐                                  ┌─────┐               *
+*     ┌──│◄│B│►│ │─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─┃─ ─ ╋ ─ ─▶│  B  │             ┃ *
+* ┃   │  └─┴─┴─┴─┘                                  └──▲──┘               *
+*     │       │                          ┃    ┃        │                ┃ *
+* ┃   │       │                                     ┌──┴──┐               *
+*     │       └──────┐                ┌ ─┃─ ─ ╋ ─ ─▶│  E  │             ┃ *
+* ┃   ▼         ┌ ─ ─│─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐   └──▲──┘               *
+*  ┌─┬─┬─┬─┐         ▼                │  ┃    ┃        │                ┃ *
+* ┃│◄│A│►│ │─ ─ ┘ ┌─┬─┬─┬─┐                     │   ┌──┴──┐               *
+*  └─┴─┴─┴─┘  ┌───│◄│D│►│ │─ ─ ─ ─ ─ ─│─ ╋ ┐  ┃  ─ ▶│  A  │ ┌ ─ ─ ─ ─ ┐ ┃ *
+* ┃           │   └─┴─┴─┴─┘                         └──▲──┘     LRU       *
+*             │        │              │  ┃ │  ┃        │    │  Cache  │ ┃ *
+* ┃           ▼        └──────┐                     ┌──┴──┐  ─ ─ ─ ─ ─    *
+*          ┌─┬─┬─┬─┐          ▼       │  ┃ └ ─┃─ ─ ▶│  D  │             ┃ *
+* ┃        │◄│C│►│ │─ ─    ┌─┬─┬─┬─┐                └──▲──┘               *
+*          └─┴─┴─┴─┘   │   │◄│E│►│ │─ ┘  ┃    ┃        │                ┃ *
+* ┃                        └─┴─┴─┴─┘                ┌──┴──┐               *
+*                      │        │      ─ ╋ ─ ─┃─ ─ ▶│  C  │             ┃ *
+* ┃                     ─ ─ ─ ─ ┼ ─ ─ ┘             └──▲──┘               *
+*                               ▼        ┃    ┃   ┌ ─ ─│─ ─ ┐           ┃ *
+* ┃                          ┌─┬─┬─┬─┐              ┌──┴──┐               *
+*                            │◄│F│►│ │─ ─┃─ ─ ╋ ─ ┼▶│  F  │ │           ┃ *
+* ┃                          └─┴─┴─┴─┘              └─────┘               *
+*                                        ┃    ┃   └ ─ ─ ─ ─ ┘           ┃ *
+* ┃                                                 remove                *
+*                                        ┃    ┃      tail               ┃ *
+* ┗ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━      ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━  *
+*                                                                         *
+\*************************************************************************/
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_lru_rb.h"
+#include "lv_rb.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+struct _lv_lru_rb_t {
+    lv_rb_t rb;
+    lv_ll_t lru_ll;
+
+    size_t max_size;
+    size_t size;
+    lv_lru_rb_alloc_cb_t alloc_cb;
+    lv_lru_rb_free_cb_t free_cb;
+};
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void* alloc_new_node(lv_lru_rb_t* lru, void* key, void* user_data);
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+lv_lru_rb_t* lv_lru_rb_create(size_t node_size, size_t max_size, lv_lru_rb_compare_cb_t compare_cb, lv_lru_rb_alloc_cb_t alloc_cb, lv_lru_rb_free_cb_t free_cb)
+{
+    LV_ASSERT_NULL(compare_cb);
+    LV_ASSERT_NULL(alloc_cb);
+    LV_ASSERT_NULL(free_cb);
+    LV_ASSERT(node_size > 0);
+    LV_ASSERT(max_size > 0);
+
+    if (node_size == 0 || max_size == 0 || compare_cb == NULL || alloc_cb == NULL || free_cb == NULL) {
+        return NULL;
+    }
+
+    lv_lru_rb_t* lru;
+    lru = lv_malloc(sizeof(lv_lru_rb_t));
+    LV_ASSERT_MALLOC(lru);
+    if (lru == NULL) {
+        return NULL;
+    }
+
+    lv_memzero(lru, sizeof(lv_rb_t));
+
+    // add void* to store the ll node pointer
+    if (!lv_rb_init(&lru->rb, (lv_rb_compare_t)compare_cb, node_size + sizeof(void*))) {
+        return false;
+    }
+    _lv_ll_init(&lru->lru_ll, sizeof(void*));
+
+    lv_lru_rb_set_max_size(lru, max_size, NULL);
+    lv_lru_rb_set_compare_cb(lru, compare_cb, NULL);
+    lv_lru_rb_set_alloc_cb(lru, alloc_cb, NULL);
+    lv_lru_rb_set_free_cb(lru, free_cb, NULL);
+
+    return lru;
+}
+
+void lv_lru_rb_destroy(lv_lru_rb_t* lru)
+{
+    LV_ASSERT_NULL(lru);
+
+    if (lru == NULL) {
+        return;
+    }
+
+    lv_rb_destroy(&lru->rb);
+    _lv_ll_clear(&lru->lru_ll);
+}
+
+void* lv_lru_rb_get(lv_lru_rb_t* lru, const void* key, void* user_data)
+{
+    LV_ASSERT_NULL(lru);
+    LV_ASSERT_NULL(key);
+
+    if (lru == NULL || key == NULL) {
+        return NULL;
+    }
+
+    lv_rb_node_t* node = lv_rb_find(&lru->rb, key);
+    // cache hit
+    if (node) {
+        // TODO: use lv_ll_move_to_head
+        return node->data;
+    }
+
+    // cache miss
+    lv_rb_node_t* new_node = alloc_new_node(lru, (void*)key, user_data);
+    if (new_node == NULL) {
+        return NULL;
+    }
+
+    // TODO: RETURN THE NEW NODE
+    return new_node->data;
+}
+
+void lv_lru_rb_put(lv_lru_rb_t* lru, const void* key, void* value, void* user_data)
+{
+}
+
+void lv_lru_rb_remove(lv_lru_rb_t* lru, const void* key, void* user_data)
+{
+}
+
+void lv_lru_rb_clear(lv_lru_rb_t* lru, void* user_data)
+{
+}
+
+void lv_lru_rb_set_max_size(lv_lru_rb_t* lru, size_t max_size, void* user_data)
+{
+    lru->max_size = max_size;
+}
+
+size_t lv_lru_rb_get_max_size(lv_lru_rb_t* lru, void* user_data)
+{
+    return lru->max_size;
+}
+
+size_t lv_lru_rb_get_size(lv_lru_rb_t* lru, void* user_data)
+{
+    return lru->size;
+}
+
+size_t lv_lru_rb_get_free_size(lv_lru_rb_t* lru, void* user_data)
+{
+    return lv_lru_rb_get_max_size(lru, user_data) - lv_lru_rb_get_size(lru, user_data);
+}
+
+void lv_lru_rb_set_compare_cb(lv_lru_rb_t* lru, lv_lru_rb_compare_cb_t compare_cb, void* user_data)
+{
+    lru->rb.compare = (lv_rb_compare_t)compare_cb;
+}
+
+void lv_lru_rb_set_alloc_cb(lv_lru_rb_t* lru, lv_lru_rb_alloc_cb_t alloc_cb, void* user_data)
+{
+    lru->alloc_cb = alloc_cb;
+}
+
+void lv_lru_rb_set_free_cb(lv_lru_rb_t* lru, lv_lru_rb_free_cb_t free_cb, void* user_data)
+{
+    lru->free_cb = free_cb;
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+static void* alloc_new_node(lv_lru_rb_t* lru, void* key, void* user_data)
+{
+    LV_ASSERT_NULL(lru);
+    LV_ASSERT_NULL(key);
+
+    if (lru == NULL || key == NULL) {
+        return NULL;
+    }
+
+    lv_rb_node_t* node = lv_rb_insert(&lru->rb, key);
+    if (node == NULL) {
+        return NULL;
+    }
+
+    lv_memzero(node->data, lru->rb.size);
+
+    bool alloc_res = lru->alloc_cb(node->data, user_data);
+    if (alloc_res == false) {
+        lv_rb_remove(&lru->rb, key);
+        return NULL;
+    }
+
+    void* lru_node = _lv_ll_ins_head(&lru->lru_ll);
+    if (lru_node == NULL) {
+        lru->free_cb(node->data, user_data);
+        lv_rb_remove(&lru->rb, key);
+        return NULL;
+    }
+
+    lv_memcpy(lru_node, &node, sizeof(void*));
+    lv_memcpy((char*)node->data - sizeof(void*), &lru_node, sizeof(void*));
+
+    // TODO: simplify error handling
+    return node;
+}
