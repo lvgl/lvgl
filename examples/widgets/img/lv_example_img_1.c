828d34aa3b221b218a37d4fcd323d1eea0154f32
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-08-31 22:03:31 +0200

    chore: revert debug code

--- examples/widgets/img/lv_example_img_1.c
+++ examples/widgets/img/lv_example_img_1.c
@@ -4,22 +4,15 @@
 
 void lv_example_img_1(void)
 {
+    LV_IMG_DECLARE(img_cogwheel_argb);
+    lv_obj_t * img1 = lv_img_create(lv_scr_act());
+    lv_img_set_src(img1, &img_cogwheel_argb);
+    lv_obj_align(img1, LV_ALIGN_CENTER, 0, -20);
+    lv_obj_set_size(img1, 200, 200);
 
-    LV_IMG_DECLARE(test_img_cogwheel_rgb565a8);
-
-    lv_obj_t * cont = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont, 150, LV_SIZE_CONTENT);
-    lv_obj_set_style_bg_color(cont, lv_palette_main(LV_PALETTE_LIGHT_BLUE), 0);
-
-    lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
-
-    lv_obj_t * img = lv_img_create(cont);
-    lv_img_set_src(img, &test_img_cogwheel_rgb565a8);
-    lv_obj_set_style_img_recolor(img, lv_palette_main(LV_PALETTE_RED), 0);   /*For A8*/
-    lv_obj_set_style_img_recolor_opa(img, LV_OPA_70, 0);
-
-    lv_obj_t * label = lv_label_create(cont);
-    lv_label_set_text(label, "RGB565");
+    lv_obj_t * img2 = lv_img_create(lv_scr_act());
+    lv_img_set_src(img2, LV_SYMBOL_OK "Accept");
+    lv_obj_align_to(img2, img1, LV_ALIGN_OUT_BOTTOM_MID, 0, 20);
 }
 
 #endif
