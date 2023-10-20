48baca0899255e0da8bf44c9ece1ab6d2efe64c3
C47D <carlos.santiago.diaz@gmail.com>
2023-10-19 21:30:59 -0600

    test(scale): Add skeleton for scale testing

---
+++ tests/src/test_cases/widgets/test_scale.c
@@ -0,0 +1,33 @@
+#if LV_BUILD_TEST
+#include "../lvgl.h"
+
+#include "unity/unity.h"
+
+void setUp(void)
+{
+    /* Function run before every test */
+}
+
+void tearDown(void)
+{
+    /* Function run after every test */
+}
+
+/* The width of the initial and last ticks of the section should be stored in order to compensate the main line drawing */
+void test_scale_linear_section_stores_initial_and_last_tick_widths(void)
+{
+    TEST_FAIL();
+}
+
+/* When adding a section to a linear scale we should find and store the initial and last ticks that the section covers */
+void test_scale_linear_section_finds_correct_initial_and_last_tick_indexes(void)
+{
+	TEST_FAIL();
+}
+
+void test_scale_round_section_finds_correct_initial_and_last_tick_indexes(void)
+{
+	TEST_FAIL();
+}
+
+#endif
