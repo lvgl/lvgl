f2041fd2a369ce22e362fdd38eb5c9a44f675c45
lhdjply <1029652979@qq.com>
2023-11-03 13:39:54 +0800

    (feat)add rt-thread for osal

--- src/osal/lv_os.h
+++ src/osal/lv_os.h
@@ -30,6 +30,8 @@ extern "C" {
 #include "lv_freertos.h"
 #elif LV_USE_OS == LV_OS_CMSIS_RTOS2
 #include "lv_cmsis_rtos2.h"
+#elif LV_USE_OS == LV_OS_RTTHREAD
+#include "lv_rtthread.h"
 #elif LV_USE_OS == LV_OS_CUSTOM
 #include LV_OS_CUSTOM_INCLUDE
 #endif
