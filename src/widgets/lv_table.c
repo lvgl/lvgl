6c86969ee002fb50099a3906de837d6aac023d80
Oleg Belousov <belousov.oleg@gmail.com>
2023-11-10 15:51:05 +0300

    fix(table) user data API functions renamed (#4769)
    Co-authored-by: Oleg Belousov <bov@silentwings.ru>

--- src/widgets/lv_table.c
+++ src/widgets/lv_table.c
@@ -378,7 +378,7 @@ void lv_table_clear_cell_ctrl(lv_obj_t * obj, uint16_t row, uint16_t col, lv_tab
 }
 
 #if LV_USE_USER_DATA
-void lv_table_set_user_data(lv_obj_t * obj, uint16_t row, uint16_t col, void * user_data)
+void lv_table_set_cell_user_data(lv_obj_t * obj, uint16_t row, uint16_t col, void * user_data)
 {
     LV_ASSERT_OBJ(obj, MY_CLASS);
 
@@ -481,7 +481,7 @@ void lv_table_get_selected_cell(lv_obj_t * obj, uint16_t * row, uint16_t * col)
 }
 
 #if LV_USE_USER_DATA
-void * lv_table_get_user_data(lv_obj_t * obj, uint16_t row, uint16_t col)
+void * lv_table_get_cell_user_data(lv_obj_t * obj, uint16_t row, uint16_t col)
 {
     LV_ASSERT_OBJ(obj, MY_CLASS);
 
--- src/widgets/lv_table.h
+++ src/widgets/lv_table.h
@@ -159,9 +159,9 @@ void lv_table_clear_cell_ctrl(lv_obj_t * obj, uint16_t row, uint16_t col, lv_tab
  * @param obj       pointer to a Table object
  * @param row       id of the row [0 .. row_cnt -1]
  * @param col       id of the column [0 .. col_cnt -1]
- * @param user_data pointer to the new user_data.
+ * @param user_data pointer to the new user_data. It must be allocated by user as it will be freed automatically
  */
-void lv_table_set_user_data(lv_obj_t * obj, uint16_t row, uint16_t col, void * user_data);
+void lv_table_set_cell_user_data(lv_obj_t * obj, uint16_t row, uint16_t col, void * user_data);
 #endif
 
 /*=====================
@@ -224,7 +224,7 @@ void lv_table_get_selected_cell(lv_obj_t * obj, uint16_t * row, uint16_t * col);
  * @param row       id of the row [0 .. row_cnt -1]
  * @param col       id of the column [0 .. col_cnt -1]
  */
-void * lv_table_get_user_data(lv_obj_t * obj, uint16_t row, uint16_t col);
+void * lv_table_get_cell_user_data(lv_obj_t * obj, uint16_t row, uint16_t col);
 #endif
 
 /**********************
