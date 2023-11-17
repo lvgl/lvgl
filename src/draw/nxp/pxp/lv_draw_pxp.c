ebdb8dcdb6fdf27c8f36e83207ab4fc2de813a77
Nicușor Cîțu <nicusor.citu@nxp.com>
2023-11-17 13:35:07 +0200

    feat(pxp) Add LV_USE_LAYER_DEBUG and LV_USE_PARALLEL_DRAW_DEBUG.
    Signed-off-by: Nicușor Cîțu <nicusor.citu@nxp.com>

--- src/draw/nxp/pxp/lv_draw_pxp.c
+++ src/draw/nxp/pxp/lv_draw_pxp.c
@@ -17,7 +17,11 @@
 
 #if LV_USE_DRAW_PXP
 #include "lv_pxp_cfg.h"
+
 #include "../../../display/lv_display_private.h"
+#if LV_USE_PARALLEL_DRAW_DEBUG
+    #include "../../../core/lv_global.h"
+#endif
 
 /*********************
  *      DEFINES
@@ -63,6 +67,10 @@ static void _pxp_execute_drawing(lv_draw_pxp_unit_t * u);
 /**********************
  *  STATIC VARIABLES
  **********************/
+
+#if LV_USE_PARALLEL_DRAW_DEBUG
+    #define _draw_info LV_GLOBAL_DEFAULT()->draw_info
+#endif
 
 /**********************
  *      MACROS
@@ -311,6 +319,51 @@ static void _pxp_execute_drawing(lv_draw_pxp_unit_t * u)
         default:
             break;
     }
+
+#if LV_USE_PARALLEL_DRAW_DEBUG
+    /*Layers manage it for themselves*/
+    if(t->type != LV_DRAW_TASK_TYPE_LAYER) {
+        lv_area_t draw_area;
+        if(!_lv_area_intersect(&draw_area, &t->area, u->base_unit.clip_area))
+            return;
+
+        int32_t idx = 0;
+        lv_draw_unit_t * draw_unit_tmp = _draw_info.unit_head;
+        while(draw_unit_tmp != (lv_draw_unit_t *)u) {
+            draw_unit_tmp = draw_unit_tmp->next;
+            idx++;
+        }
+        lv_draw_rect_dsc_t rect_dsc;
+        lv_draw_rect_dsc_init(&rect_dsc);
+        rect_dsc.bg_color = lv_palette_main(idx % _LV_PALETTE_LAST);
+        rect_dsc.border_color = rect_dsc.bg_color;
+        rect_dsc.bg_opa = LV_OPA_10;
+        rect_dsc.border_opa = LV_OPA_80;
+        rect_dsc.border_width = 1;
+        lv_draw_sw_fill((lv_draw_unit_t *)u, &rect_dsc, &draw_area);
+
+        lv_point_t txt_size;
+        lv_text_get_size(&txt_size, "W", LV_FONT_DEFAULT, 0, 0, 100, LV_TEXT_FLAG_NONE);
+
+        lv_area_t txt_area;
+        txt_area.x1 = draw_area.x1;
+        txt_area.y1 = draw_area.y1;
+        txt_area.x2 = draw_area.x1 + txt_size.x - 1;
+        txt_area.y2 = draw_area.y1 + txt_size.y - 1;
+
+        lv_draw_rect_dsc_init(&rect_dsc);
+        rect_dsc.bg_color = lv_color_white();
+        lv_draw_sw_fill((lv_draw_unit_t *)u, &rect_dsc, &txt_area);
+
+        char buf[8];
+        lv_snprintf(buf, sizeof(buf), "%d", idx);
+        lv_draw_label_dsc_t label_dsc;
+        lv_draw_label_dsc_init(&label_dsc);
+        label_dsc.color = lv_color_black();
+        label_dsc.text = buf;
+        lv_draw_sw_label((lv_draw_unit_t *)u, &label_dsc, &txt_area);
+    }
+#endif
 }
 
 #if LV_USE_OS
