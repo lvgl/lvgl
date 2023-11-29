4757d27afbe7d451e5325e885b9c1a04f0f71a9d
GGorAA <ggoraa1029@gmail.com>
2023-11-29 23:54:10 +0100

    format: ran code-format.py

--- src/core/lv_obj.h
+++ src/core/lv_obj.h
@@ -2,7 +2,7 @@
  * @file lv_obj.h
  *
  */
- // IWYU pragma: private, include <lvgl/lvgl.h">
+// IWYU pragma: private, include <lvgl/lvgl.h">
 
 #ifndef LV_OBJ_H
 #define LV_OBJ_H
--- src/display/lv_display.h
+++ src/display/lv_display.h
@@ -2,7 +2,7 @@
  * @file lv_disp.h
  *
  */
- // IWYU pragma: private, include <lvgl/lvgl.h">
+// IWYU pragma: private, include <lvgl/lvgl.h">
 
 #ifndef LV_DISPLAY_H
 #define LV_DISPLAY_H
