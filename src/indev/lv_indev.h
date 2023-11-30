94a70ebcd4b49a49cdba26b9a231c0154365a029
YanXiaowei <yanxiaowei@xiaomi.com>
2023-11-30 15:25:00 +0800

    feat(indev): use union for lv_indev_data_t
    Signed-off-by: YanXiaowei <yanxiaowei@xiaomi.com>

--- src/indev/lv_indev.h
+++ src/indev/lv_indev.h
@@ -49,10 +49,12 @@ typedef enum {
 
 /** Data structure passed to an input driver to fill*/
 typedef struct {
-    lv_point_t point; /**< For LV_INDEV_TYPE_POINTER the currently pressed point*/
-    uint32_t key;     /**< For LV_INDEV_TYPE_KEYPAD the currently pressed key*/
-    uint32_t btn_id;  /**< For LV_INDEV_TYPE_BUTTON the currently pressed button*/
-    int16_t enc_diff; /**< For LV_INDEV_TYPE_ENCODER number of steps since the previous read*/
+    union {
+        lv_point_t point; /**< For LV_INDEV_TYPE_POINTER the currently pressed point*/
+        uint32_t key;     /**< For LV_INDEV_TYPE_KEYPAD the currently pressed key*/
+        uint32_t btn_id;  /**< For LV_INDEV_TYPE_BUTTON the currently pressed button*/
+        int16_t enc_diff; /**< For LV_INDEV_TYPE_ENCODER number of steps since the previous read*/
+    };
 
     lv_indev_state_t state; /**< LV_INDEV_STATE_REL or LV_INDEV_STATE_PR*/
     bool continue_reading;  /**< If set to true, the read callback is invoked again*/
