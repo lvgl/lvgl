5929ef20efe5ed9add825d93c37b29acae1a0f1e
YanXiaowei <yanxiaowei@xiaomi.com>
2023-12-07 23:15:42 +0800

    fix(arc): fix code format for arc example
    VELAPLATFO-22362
    
    Change-Id: Ia3433c38c9a47b4098a62cc268b0ff373c6655b9
    Signed-off-by: YanXiaowei <yanxiaowei@xiaomi.com>

--- examples/widgets/arc/lv_example_arc_1.c
+++ examples/widgets/arc/lv_example_arc_1.c
@@ -26,7 +26,7 @@ static void value_changed_event_cb(lv_event_t * e)
     lv_obj_t * arc = lv_event_get_target(e);
     lv_obj_t * label = lv_event_get_user_data(e);
 
-    lv_label_set_text_fmt(label, "%d%%", lv_arc_get_value(arc));
+    lv_label_set_text_fmt(label, "%" LV_PRId32 "%%", lv_arc_get_value(arc));
 
     /*Rotate the label to the current position of the arc*/
     lv_arc_rotate_obj_to_angle(arc, label, 25);
