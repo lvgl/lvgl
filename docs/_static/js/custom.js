a200c137c805dc32da600c1edd93c7849c296f33
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-12-11 18:50:25 +0100

    docs: add banner

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -12,7 +12,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
             cppListing.classList.toggle("unexpanded");
             cppListing.classList.toggle("expanded");
         });
-        
+
         dt.insertBefore(button, dt.firstChild);
+    });
+
+    fetch('https://lvgl.io/home-banner.txt') // Replace with your URL
+  .then(response => {
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    // Read the response as text
+    return response.text();
+  })
+  .then(data => {
+
+    const section = document.querySelector('.wy-nav-content-wrap');
+
+    //Add a div
+    const newDiv = document.createElement('div');
+    newDiv.style="background-image: linear-gradient(45deg, black, #5e5e5e); color: white; border-bottom: 4px solid #e10010; padding-inline:3em"
+    section.insertBefore(newDiv, section.firstChild);
+
+
+    //Add a p to the div
+    const newP = document.createElement('p');
+    newP.style="padding-block:12px; margin-block:0px;align-content: center;align-items: center;"
+    newP.innerHTML = data
+    newDiv.insertBefore(newP, newDiv.firstChild);
+
+      }) .catch(error => {
+        console.error('Fetch error: ' + error.message);
     });
 })
