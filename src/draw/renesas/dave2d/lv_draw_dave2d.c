ad485bb2f649df7dff5efa66664295187d33ec04
Jeremy Baker <jeremy.baker@renesas.com>
2023-12-16 11:40:00 +0000

    Allocate buffer for layer in Dave2D dispatch

--- src/draw/renesas/dave2d/lv_draw_dave2d.c
+++ src/draw/renesas/dave2d/lv_draw_dave2d.c
@@ -391,6 +391,11 @@ static int32_t lv_draw_dave2d_dispatch(lv_draw_unit_t * draw_unit, lv_layer_t * 
         return 0;
     }
 
+    void * buf = lv_draw_layer_alloc_buf(layer);
+    if(buf == NULL) {
+        return -1;
+    }
+
 #if  (0 == D2_RENDER_EACH_OPERATION)
     ref_count += lv_draw_get_dependent_count(t);
 
