9fb20efffbf2452935ce2788dbf1ab96edb35a6e
pengyiqiang <pengyiqiang@xiaomi.com>
2023-12-18 12:02:30 +0800

    fix(draw_vector): fix build break when thorvg disabled
    VELAPLATFO-22913
    
    ```
    lvgl/demos/lv_demos.c:74:37: error: ‘lv_demo_vector_graphic’ undeclared here (not in a function)
       74 |     { "vector_graphic", .entry_cb = lv_demo_vector_graphic },
          |                                     ^~~~~~~~~~~~~~~~~~~~~
    
    /usr/bin/ld: lib/liblvgl.a(lv_draw_sw.c.o): in function `execute_drawing':
    lvgl/src/draw/sw/lv_draw_sw.c:338: undefined reference to `lv_draw_sw_vector'
    ```
    
    Change-Id: I3aff2ac3ed9d43d93bf1c5da1967481954601c90
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- demos/lv_demos.h
+++ demos/lv_demos.h
@@ -51,7 +51,7 @@ extern "C" {
 #include "multilang/lv_demo_multilang.h"
 #endif
 
-#if LV_USE_DEMO_VECTOR_GRAPHIC && LV_USE_VECTOR_GRAPHIC && (LV_USE_THORVG_INTERNAL || LV_USE_THORVG_EXTERNAL)
+#if LV_USE_DEMO_VECTOR_GRAPHIC && LV_USE_VECTOR_GRAPHIC
 #include "vector_graphic/lv_demo_vector_graphic.h"
 #endif
 
