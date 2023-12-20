0b76fd90b802ce5acef48c45f24374843f185c9f
Liu Zhongwei <liuzhongwei@espressif.com>
2023-12-20 17:13:37 +0800

    lv_disp: invoke scr_load_internal upon completion

--- tests/src/test_cases/test_screen_load.c
+++ tests/src/test_cases/test_screen_load.c
@@ -10,6 +10,12 @@ void test_screen_load_no_crash(void)
     lv_obj_del(screen);
     screen = lv_obj_create(NULL);
     lv_screen_load(screen);
+
+    /*Consecutively loading multiple screens with transition animations should not crash*/
+    lv_obj_t * screen_with_anim_1 = lv_obj_create(NULL);
+    lv_obj_t * screen_with_anim_2 = lv_obj_create(NULL);
+    lv_screen_load_anim(screen_with_anim_1, LV_SCR_LOAD_ANIM_OVER_LEFT, 2000, 0, false);
+    lv_screen_load_anim(screen_with_anim_2, LV_SCR_LOAD_ANIM_OVER_RIGHT, 1000, 500, false);
 }
 
 #endif
