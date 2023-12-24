a19d4611a0b11575cd01255434a000506fdc4cd1
Gabriel Wang <gabriel.wang@arm.com>
2023-12-24 05:21:47 +0000

    update macro name for arm-2d

--- env_support/cmsis-pack/lv_conf_cmsis.h
+++ env_support/cmsis-pack/lv_conf_cmsis.h
@@ -117,6 +117,7 @@
         /*turn-on helium acceleration when Arm-2D and the Helium-powered device are detected */
         #if defined(__ARM_FEATURE_MVE) && __ARM_FEATURE_MVE
             #define  LV_USE_DRAW_SW_ASM     LV_DRAW_SW_ASM_HELIUM
+            #define LV_USE_DRAW_ARM2D       1
         #endif
     #endif
 
