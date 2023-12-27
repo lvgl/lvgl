80b6d9cf5190bd72b7fe4f41fa7890c708e0a68b
Gabor Peresztegi <gabor.p1983@gmail.com>
2023-12-27 11:04:03 +0100

    fix(lodepng): fix LodePNG 8 bit palette image to RGBA data conversion bug

--- src/libs/lodepng/lodepng.c
+++ src/libs/lodepng/lodepng.c
@@ -5339,8 +5339,7 @@ unsigned lodepng_decode(unsigned char ** out, unsigned * w, unsigned * h,
         }
     }
     else {   /*color conversion needed*/
-        unsigned char * data = *out;
-        size_t outsize;
+        lv_draw_buf_t * old_buf = (lv_draw_buf_t *)*out;
 
         /*TODO: check if this works according to the statement in the documentation: "The converter can convert
         from grayscale input color type, to 8-bit grayscale or grayscale with alpha"*/
@@ -5349,14 +5348,23 @@ unsigned lodepng_decode(unsigned char ** out, unsigned * w, unsigned * h,
             return 56; /*unsupported color mode conversion*/
         }
 
-        outsize = lodepng_get_raw_size(*w, *h, &state->info_raw);
-        *out = (unsigned char *)lodepng_malloc(outsize);
-        if(!(*out)) {
+        lv_draw_buf_t * new_buf = lv_draw_buf_create(*w, *h, LV_COLOR_FORMAT_ARGB8888, 4 * *w);
+        if(new_buf == NULL) {
             state->error = 83; /*alloc fail*/
+            return state->error;
         }
-        else state->error = lodepng_convert(*out, data, &state->info_raw,
+
+        state->error = lodepng_convert(new_buf->data, old_buf->data, &state->info_raw,
                                                 &state->info_png.color, *w, *h);
-        lodepng_free(data);
+
+        if (state->error) {
+            lv_draw_buf_destroy(new_buf);
+        }
+        else {
+            *out = (unsigned char*)new_buf;
+        }
+
+        lv_draw_buf_destroy(old_buf);
     }
     return state->error;
 }
