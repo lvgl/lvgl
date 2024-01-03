4874af06fbfe5e2fa6a13d5dbd84d8e487bdd269
Jim Boström <jim.bostrom@indra.no>
2024-01-03 16:25:19 +0100

    fix(widgets): wrong casting in some widgets when calling lv_event_get_param
    Fixes: #5083

--- src/widgets/arc/lv_arc.c
+++ src/widgets/arc/lv_arc.c
@@ -604,7 +604,7 @@ static void lv_arc_event(const lv_obj_class_t * class_p, lv_event_t * e)
 
     }
     else if(code == LV_EVENT_KEY) {
-        char c = *((char *)lv_event_get_param(e));
+        uint32_t c = lv_event_get_key(e);
 
         int16_t old_value = arc->value;
         if(c == LV_KEY_RIGHT || c == LV_KEY_UP) {
