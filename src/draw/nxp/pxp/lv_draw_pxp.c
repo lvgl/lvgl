f6acd95086e80613f9b8491cbe40c24659107185
Nicușor Cîțu <nicusor.citu@nxp.com>
2024-01-05 10:35:46 +0200

    fix(pxp) Remove useless include.
    Signed-off-by: Nicușor Cîțu <nicusor.citu@nxp.com>

--- src/draw/nxp/pxp/lv_draw_pxp.c
+++ src/draw/nxp/pxp/lv_draw_pxp.c
@@ -19,7 +19,6 @@
 #include "lv_pxp_cfg.h"
 #include "lv_pxp_utils.h"
 
-#include "../../../display/lv_display_private.h"
 #if LV_USE_PARALLEL_DRAW_DEBUG
     #include "../../../core/lv_global.h"
 #endif
