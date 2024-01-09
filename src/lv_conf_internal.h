99818408cac253a88fb676ba25bf6f76cf79cbf4
_VIFEXTech <vifextech@foxmail.com>
2024-01-09 18:52:30 +0800

    chore(conf): remove LV_DISPLAY_ROT_MAX_BUF (#5239)
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Co-authored-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -746,16 +746,6 @@
  * Others
  *-----------*/
 
-/*Maximum buffer size to allocate for rotation.
- *Only used if software rotation is enabled in the display driver.*/
-#ifndef LV_DISPLAY_ROT_MAX_BUF
-    #ifdef CONFIG_LV_DISPLAY_ROT_MAX_BUF
-        #define LV_DISPLAY_ROT_MAX_BUF CONFIG_LV_DISPLAY_ROT_MAX_BUF
-    #else
-        #define LV_DISPLAY_ROT_MAX_BUF (10*1024)
-    #endif
-#endif
-
 #ifndef LV_ENABLE_GLOBAL_CUSTOM
     #ifdef CONFIG_LV_ENABLE_GLOBAL_CUSTOM
         #define LV_ENABLE_GLOBAL_CUSTOM CONFIG_LV_ENABLE_GLOBAL_CUSTOM
