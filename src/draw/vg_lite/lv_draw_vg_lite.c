f38e880747c2b54bbf9d53e37f31fc3015366cdb
pengyiqiang <pengyiqiang@xiaomi.com>
2024-01-12 18:42:07 +0800

    fix warning and crash

--- src/draw/vg_lite/lv_draw_vg_lite.c
+++ src/draw/vg_lite/lv_draw_vg_lite.c
@@ -152,11 +152,6 @@ static int32_t draw_dispatch(lv_draw_unit_t * draw_unit, lv_layer_t * layer)
         return 0;
     }
 
-    /* Return if target buffer format is not supported. */
-    if(!lv_vg_lite_is_dest_cf_supported(layer->draw_buf->header.cf)) {
-        return -1;
-    }
-
     /* Try to get an ready to draw. */
     lv_draw_task_t * t = lv_draw_get_next_available_task(layer, NULL, VG_LITE_DRAW_UNIT_ID);
 
@@ -167,6 +162,11 @@ static int32_t draw_dispatch(lv_draw_unit_t * draw_unit, lv_layer_t * layer)
 
     void * buf = lv_draw_layer_alloc_buf(layer);
     if(!buf) {
+        return -1;
+    }
+
+    /* Return if target buffer format is not supported. */
+    if(!lv_vg_lite_is_dest_cf_supported(layer->draw_buf->header.cf)) {
         return -1;
     }
 
