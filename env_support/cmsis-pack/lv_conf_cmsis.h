32828206d14722560e369f1281667938bce12426
_VIFEXTech <vifextech@foxmail.com>
2024-01-17 21:33:02 +0800

    chore(imgfont): simplify imgfont_dsc_t data structure (#5354)
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Co-authored-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- env_support/cmsis-pack/lv_conf_cmsis.h
+++ env_support/cmsis-pack/lv_conf_cmsis.h
@@ -712,16 +712,6 @@
     #define LV_PROFILER_END_TAG   LV_PROFILER_BUILTIN_END_TAG
 #endif
 
-
-/*1: Support using images as font in label or span widgets */
-#if LV_USE_IMGFONT
-    /*Imgfont image file path maximum length*/
-    #define LV_IMGFONT_PATH_MAX_LEN 64
-
-    /*1: Use img cache to buffer header information*/
-    #define LV_IMGFONT_USE_IMAGE_CACHE_HEADER 0
-#endif
-
 /*1: Enable an observer pattern implementation*/
 #define LV_USE_OBSERVER 1
 
