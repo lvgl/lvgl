214d72da88b53736adf5085cc615b7ffed4a55fd
Neo Xu <xuxingliang@xiaomi.com>
2024-01-19 17:38:30 +0800

    feat(test): automatically generate test image (#4976)
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>

--- tests/test_images/stride_align64/UNCOMPRESSED/test_ARGB8888_NONE_align64.c
+++ tests/test_images/stride_align64/UNCOMPRESSED/test_ARGB8888_NONE_align64.c
@@ -84,6 +84,7 @@ uint8_t test_ARGB8888_NONE_align64_map[] = {
 };
 
 const lv_img_dsc_t test_ARGB8888_NONE_align64 = {
+  .header.magic = LV_IMAGE_HEADER_MAGIC,
   .header.cf = LV_COLOR_FORMAT_ARGB8888,
   .header.flags = 0,
   .header.w = 71,
