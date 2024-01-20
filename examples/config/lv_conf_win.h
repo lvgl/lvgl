31989129437e1c11cb3b0219462ced1173b0e333
Julian Heni <jheni@testo.de>
2024-01-20 15:15:43 +0100

    remkve some unsed lines from Presets

--- examples/config/lv_conf_win.h
+++ examples/config/lv_conf_win.h
@@ -794,7 +794,7 @@
         *==================*/
 
         /*Use SDL to open window on PC and handle mouse and keyboard*/
-        #define LV_USE_SDL              1
+        #define LV_USE_SDL              0
         #if LV_USE_SDL
             #define LV_SDL_INCLUDE_PATH    <SDL2/SDL.h>
             #define LV_SDL_RENDER_MODE     LV_DISPLAY_RENDER_MODE_DIRECT   /*LV_DISPLAY_RENDER_MODE_DIRECT is recommended for best performance*/
