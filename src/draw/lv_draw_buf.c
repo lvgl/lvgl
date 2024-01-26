514968b5e8982cf72cfdceaa8c1f5b726f9a8db7
Xu Xingliang <xuxingliang@xiaomi.com>
2024-01-26 15:56:16 +0800

    fix(draw_buf): support indexed image stride adjustment
    VELAPLATFO-20122
    
    Change-Id: I98a33ffea2c811fbe1b62c3aa47338f116028795
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>

--- src/draw/lv_draw_buf.c
+++ src/draw/lv_draw_buf.c
@@ -296,8 +296,11 @@ lv_draw_buf_t * lv_draw_buf_adjust_stride(const lv_draw_buf_t * src, uint32_t st
     lv_draw_buf_t * dst = lv_draw_buf_create(header->w, header->h, header->cf, stride);
     if(dst == NULL) return NULL;
 
-    uint8_t * dst_data = dst->data;
-    const uint8_t * src_data = src->data;
+    uint32_t offset = LV_COLOR_INDEXED_PALETTE_SIZE(header->cf) * 4;
+    if(offset) lv_memcpy(dst->data, src->data, offset);
+
+    uint8_t * dst_data = dst->data + offset;
+    const uint8_t * src_data = src->data + offset;
     for(int32_t y = 0; y < src->header.h; y++) {
         lv_memcpy(dst_data, src_data, min_stride);
         src_data += src->header.stride;
