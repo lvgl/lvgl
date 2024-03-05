c326e31a87e29fe77a6403bd96b13bf9448d18a5
xaowang <wangxuedong@xiaomi.com>
2024-03-05 11:34:21 +0800

    fix(vg_lite):fix bug for wrong rendering in vertical or horizontal dir (#5789)
    Signed-off-by: wangxuedong <wangxuedong@xiaomi.com>

--- src/draw/vg_lite/lv_draw_vg_lite_vector.c
+++ src/draw/vg_lite/lv_draw_vg_lite_vector.c
@@ -194,14 +194,16 @@ static void task_draw_cb(void * ctx, const lv_vector_path_t * path, const lv_vec
                 vg_lite_gradient_spreadmode_t spreadmode = lv_spread_to_vg(dsc->fill_dsc.gradient.spread);
                 LV_UNUSED(spreadmode);
 
-                lv_matrix_t m = dsc->matrix;
-                lv_matrix_translate(&m, min_x, min_y);
-                lv_matrix_multiply(&m, &dsc->fill_dsc.matrix);
-
-                vg_lite_matrix_t grad_matrix;
-                lv_matrix_to_vg(&grad_matrix, &m);
-
                 if(style == LV_VECTOR_GRADIENT_STYLE_LINEAR) {
+                    vg_lite_matrix_t grad_matrix, fill_matrix;
+                    lv_area_t grad_area;
+                    lv_area_set(&grad_area, (int32_t)min_x, (int32_t)min_y, (int32_t)max_x, (int32_t)max_y);
+                    lv_vg_lite_grad_area_to_matrix(&grad_matrix, &grad_area, LV_GRAD_DIR_HOR);
+
+                    lv_matrix_to_vg(&fill_matrix, &dsc->fill_dsc.matrix);
+                    lv_vg_lite_matrix_multiply(&grad_matrix, &matrix);
+                    lv_vg_lite_matrix_multiply(&grad_matrix, &fill_matrix);
+
                     lv_vg_lite_draw_linear_grad(
                         u,
                         &u->target_buffer,
