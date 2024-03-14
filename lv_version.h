f4c414a0d39ecdc22c54b9a53d8e3f1d93fc076a
Marek Vasut <marex@denx.de>
2024-03-14 03:23:10 +0100

    fix(cmake): generate versioned shared libraries
    Add missing version suffix to shared libraries. Currently the filename of
    generated shared libraries is only liblvgl.so, which prevents coexistence
    of different versions of LVGL on the same system. Set VERSION and SOVERSION
    to make cmake add the version suffix to generated shared libraries. That
    changes the filename to liblvgl.so.9.1.1 and includes symlink with major
    ABI version, i.e. liblvgl.so.9 .
    
    Signed-off-by: Marek Vasut <marex@denx.de>

---
+++ lv_version.h
@@ -0,0 +1,14 @@
+/**
+ * @file version.h
+ * The current version of LVGL
+ */
+
+#ifndef LVGL_VERSION_H
+#define LVGL_VERSION_H
+
+#define LVGL_VERSION_MAJOR 9
+#define LVGL_VERSION_MINOR 1
+#define LVGL_VERSION_PATCH 1
+#define LVGL_VERSION_INFO "dev"
+
+#endif /*LVGL_VERSION_H*/
