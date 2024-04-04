29662d1c357c15135d076a2a924b9ef056c82120
Jaeyoon Jung <35471821+jaeyoonjung@users.noreply.github.com>
2024-04-04 15:55:34 +0900

    fix(fbdev): set resolution prior to buffer (#6004)
    Signed-off-by: Jaeyoon Jung <jaeyoon.jung@lge.com>

--- src/drivers/display/fb/lv_linux_fbdev.c
+++ src/drivers/display/fb/lv_linux_fbdev.c
@@ -233,8 +233,8 @@ void lv_linux_fbdev_set_file(lv_display_t * disp, const char * file)
         draw_buf_2 = malloc(draw_buf_size);
     }
 
-    lv_display_set_buffers(disp, draw_buf, draw_buf_2, draw_buf_size, LV_LINUX_FBDEV_RENDER_MODE);
     lv_display_set_resolution(disp, hor_res, ver_res);
+    lv_display_set_buffers(disp, draw_buf, draw_buf_2, draw_buf_size, LV_LINUX_FBDEV_RENDER_MODE);
 
     if(width > 0) {
         lv_display_set_dpi(disp, DIV_ROUND_UP(hor_res * 254, width * 10));
