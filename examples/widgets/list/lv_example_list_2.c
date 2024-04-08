a3df357297a73a2e6abb86a536bb1ecf80bdd6da
MiSimon <michael.simon.at@gmail.com>
2024-04-08 08:08:46 +0200

    use lv_rand instead of rand

--- examples/widgets/list/lv_example_list_2.c
+++ examples/widgets/list/lv_example_list_2.c
@@ -1,6 +1,5 @@
 #include "../../lv_examples.h"
 #if LV_USE_LIST && LV_BUILD_EXAMPLES
-#include <stdlib.h>
 
 static lv_obj_t * list1;
 static lv_obj_t * list2;
@@ -101,8 +100,8 @@ static void event_handler_swap(lv_event_t * e)
         uint32_t cnt = lv_obj_get_child_count(list1);
         for(int i = 0; i < 100; i++)
             if(cnt > 1) {
-                lv_obj_t * obj = lv_obj_get_child(list1, rand() % cnt);
-                lv_obj_move_to_index(obj, rand() % cnt);
+                lv_obj_t * obj = lv_obj_get_child(list1, lv_rand(0, cnt));
+                lv_obj_move_to_index(obj, lv_rand(0, cnt));
                 if(currentButton != NULL) {
                     lv_obj_scroll_to_view(currentButton, LV_ANIM_ON);
                 }
