4d0c02982758304fa39d9b4ddc4be0dc991ed00f
VIFEX <vifextech@foxmail.com>
2024-04-09 03:31:19 +0800

    fix(draw): fix the default draw thread stack is too large (#5951)
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Signed-off-by: FASTSHIFT <vifextech@foxmail.com>
    Co-authored-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -113,6 +113,11 @@
 /*The target buffer size for simple layer chunks.*/
 #define LV_DRAW_LAYER_SIMPLE_BUF_SIZE    (24 * 1024)   /*[bytes]*/
 
+/* The stack size of the drawing thread.
+ * NOTE: If FreeType or ThorVG is enabled, it is recommended to set it to 32KB or more.
+ */
+#define LV_DRAW_THREAD_STACK_SIZE    (8 * 1024)   /*[bytes]*/
+
 #define LV_USE_DRAW_SW 1
 #if LV_USE_DRAW_SW == 1
     /* Set the number of draw unit.
@@ -149,9 +154,6 @@
         #define  LV_DRAW_SW_ASM_CUSTOM_INCLUDE ""
     #endif
 #endif
-
-/* Set stack size of drawing thread. Unit is byte. If Thorvg is enabled, 128kB is required tested on simulator.*/
-#define LV_DRAW_THREAD_STACKSIZE 32768
 
 /* Use NXP's VG-Lite GPU on iMX RTxxx platforms. */
 #define LV_USE_DRAW_VGLITE 0
