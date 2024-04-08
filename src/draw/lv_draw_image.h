f92b0a961b98f08ae8b9e2efb61783cfee6358b1
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-04-08 15:14:48 +0200

    Update src/draw/lv_draw_image.h
    Co-authored-by: Neo Xu <neo.xu1990@gmail.com>

--- src/draw/lv_draw_image.h
+++ src/draw/lv_draw_image.h
@@ -59,10 +59,11 @@ typedef struct _lv_draw_image_dsc_t {
     uint16_t tile               : 1;
     lv_draw_image_sup_t * sup;
 
-    /** Used to indicate the original position size of the image.
-     * It's important for
-     *  1. layer rendering where it might happen the only a smaller area of the layer is rendered.
-     *  2. tiled images where the target draw area is larger then the image to tile.*/
+/** Used to indicate the entire original, non-clipped area where the image is to be drawn.
+ * This is important for:
+ *  1. Layer rendering, where it might happen that only a smaller area of the layer is rendered.
+ *  2. Tiled images, where the target draw area is larger than the image to be tiled.
+ */
     lv_area_t image_area;
     const lv_image_dsc_t * bitmap_mask_src;
 } lv_draw_image_dsc_t;
