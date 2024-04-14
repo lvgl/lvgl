a8b99a2fcf03adb2d327a436bb5bbc574a48d1a7
Zoltan Janosy <zjanosy@fishman.com>
2024-04-14 17:35:26 +0200

    Restore canvas examples to original. Fix canvas 6 -- added missing lv_canvas_finish_layer(canvas, &layer) at the end.

--- examples/widgets/canvas/lv_example_canvas_5.c
+++ examples/widgets/canvas/lv_example_canvas_5.c
@@ -1,8 +1,8 @@
 ﻿#include "../../lv_examples.h"
 #if LV_USE_CANVAS && LV_BUILD_EXAMPLES
 
-#define CANVAS_WIDTH  200
-#define CANVAS_HEIGHT 200
+#define CANVAS_WIDTH  50
+#define CANVAS_HEIGHT  50
 
 /**
  * Draw an arc to the canvas
@@ -10,13 +10,12 @@
 void lv_example_canvas_5(void)
 {
     /*Create a buffer for the canvas*/
-    //    LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
-    LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_L8);
+    LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
 
     /*Create a canvas and initialize its palette*/
     lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
     lv_canvas_set_draw_buf(canvas, &draw_buf);
-    lv_canvas_fill_bg(canvas, lv_color_hex3(0x0cc), LV_OPA_COVER);
+    lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
     lv_obj_center(canvas);
 
     lv_layer_t layer;
@@ -26,13 +25,12 @@ void lv_example_canvas_5(void)
     lv_draw_arc_dsc_init(&dsc);
     dsc.color = lv_palette_main(LV_PALETTE_RED);
     dsc.width = 5;
-    dsc.center.x = CANVAS_WIDTH / 2;
-    dsc.center.y = CANVAS_HEIGHT / 2;
-    dsc.width = CANVAS_WIDTH / 5;
-    dsc.radius = CANVAS_WIDTH / 2 - 10;
+    dsc.center.x = 25;
+    dsc.center.y = 25;
+    dsc.width = 10;
+    dsc.radius = 15;
     dsc.start_angle = 0;
     dsc.end_angle = 220;
-    dsc.opa = LV_OPA_100;
 
     lv_draw_arc(&layer, &dsc);
 
--- examples/widgets/canvas/lv_example_canvas_6.c
+++ examples/widgets/canvas/lv_example_canvas_6.c
@@ -1,16 +1,8 @@
 ﻿#include "../../lv_examples.h"
 #if LV_USE_CANVAS && LV_BUILD_EXAMPLES
 
-#if 1
-
-#define CANVAS_WIDTH  200
-#define CANVAS_HEIGHT  (9*40+10)
-#define DEMO_IMAGE_RGB img_star_grayscale_rgb
-#define DEMO_IMAGE_ARGB img_star_grayscale_argb
-#define DEMO_IMAGE_L8 img_star_grayscale_l8
-#define IMAGE_OPA       LV_OPA_10
-#define RECOLOR_OPA     LV_OPA_0
-#define RECOLOR_COLOR   0xff0
+#define CANVAS_WIDTH  50
+#define CANVAS_HEIGHT  50
 
 /**
  * Draw an image to the canvas
@@ -18,109 +10,26 @@
 void lv_example_canvas_6(void)
 {
     /*Create a buffer for the canvas*/
-    //LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_AL88);
-    LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
+    static uint8_t cbuf[LV_CANVAS_BUF_SIZE(CANVAS_WIDTH, CANVAS_HEIGHT, 32, LV_DRAW_BUF_STRIDE_ALIGN)];
 
     /*Create a canvas and initialize its palette*/
     lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
-    lv_canvas_set_draw_buf(canvas, &draw_buf);
+    lv_canvas_set_buffer(canvas, cbuf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_ARGB8888);
     lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
     lv_obj_center(canvas);
 
     lv_layer_t layer;
     lv_canvas_init_layer(canvas, &layer);
 
-    LV_IMAGE_DECLARE(DEMO_IMAGE_ARGB);
-    LV_IMAGE_DECLARE(DEMO_IMAGE_RGB);
-    LV_IMAGE_DECLARE(DEMO_IMAGE_L8);
-
+    LV_IMAGE_DECLARE(img_star);
     lv_draw_image_dsc_t dsc;
-
     lv_draw_image_dsc_init(&dsc);
-    dsc.opa = IMAGE_OPA;
-    dsc.recolor = lv_color_hex3(RECOLOR_COLOR);
-    dsc.rotation = 300;
-    dsc.pivot.x = 0;
-    dsc.pivot.y = 0;
+    dsc.src = &img_star;
 
-    lv_area_t coords1 = { 20, 10, 20 + DEMO_IMAGE_RGB.header.w - 1, 10 + DEMO_IMAGE_RGB.header.h - 1 };
-    lv_area_t coords2 = { 70, 10, 70 + DEMO_IMAGE_RGB.header.w - 1, 10 + DEMO_IMAGE_RGB.header.h - 1 };
-    lv_area_t coords3 = { 120, 10, 120 + DEMO_IMAGE_RGB.header.w - 1, 10 + DEMO_IMAGE_RGB.header.h - 1 };
+    lv_area_t coords = {10, 10, 10 + img_star.header.w - 1, 10 + img_star.header.h - 1};
 
-    for(int i = 0; i <= 256; i += 32) {
-
-        dsc.recolor_opa = RECOLOR_OPA;
-        dsc.opa = LV_MIN(i, 255);
-
-        dsc.src = &DEMO_IMAGE_ARGB;
-        lv_draw_image(&layer, &dsc, &coords1);
-
-        dsc.src = &DEMO_IMAGE_RGB;
-        lv_draw_image(&layer, &dsc, &coords2);
-
-        dsc.src = &DEMO_IMAGE_L8;
-        lv_draw_image(&layer, &dsc, &coords3);
-
-        coords1.y1 += 40;
-        coords1.y2 += 40;
-        coords2.y1 += 40;
-        coords2.y2 += 40;
-        coords3.y1 += 40;
-        coords3.y2 += 40;
-    }
+    lv_draw_image(&layer, &dsc, &coords);
 
     lv_canvas_finish_layer(canvas, &layer);
 }
-
-#else
-
-#define CANVAS_WIDTH  150
-#define CANVAS_HEIGHT  150
-#define DEMO_IMAGE_RGB img_cogwheel_argb //img_lenna_rgb565
-//#define DEMO_IMAGE_L8 img_lenna_l8
-#define IMAGE_OPA       LV_OPA_50
-#define RECOLOR_OPA     LV_OPA_0
-#define RECOLOR_COLOR   0xff0
-
-/**
- * Draw an image to the canvas
- */
-void lv_example_canvas_6(void)
-{
-    /*Create a buffer for the canvas*/
-    LV_DRAW_BUF_DEFINE(draw_buf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_COLOR_FORMAT_RGB888);
-
-    /*Create a canvas and initialize its palette*/
-    lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
-    lv_canvas_set_draw_buf(canvas, &draw_buf);
-    lv_canvas_fill_bg(canvas, lv_color_hex3(0xccc), LV_OPA_COVER);
-    lv_obj_center(canvas);
-
-    lv_layer_t layer;
-    lv_canvas_init_layer(canvas, &layer);
-
-    LV_IMAGE_DECLARE(DEMO_IMAGE_RGB);
-    //    LV_IMAGE_DECLARE(DEMO_IMAGE_L8);
-
-    lv_draw_image_dsc_t dsc;
-
-    lv_draw_image_dsc_init(&dsc);
-    dsc.opa = IMAGE_OPA;
-    dsc.recolor = lv_color_hex3(RECOLOR_COLOR);
-    dsc.recolor_opa = RECOLOR_OPA;
-    lv_area_t coords1 = { 10, 10, 10 + DEMO_IMAGE_RGB.header.w - 1, 10 + DEMO_IMAGE_RGB.header.h - 1 };
-    //    lv_area_t coords2 = { 60, 10, 60 + DEMO_IMAGE_RGB.header.w - 1, 10 + DEMO_IMAGE_RGB.header.h - 1 };
-
-    dsc.src = &DEMO_IMAGE_RGB;
-    lv_draw_image(&layer, &dsc, &coords1);
-
-    //    dsc.src = &DEMO_IMAGE_L8;
-    //    lv_draw_image(&layer, &dsc, &coords2);
-
-    lv_canvas_finish_layer(canvas, &layer);
-}
-
 #endif
-
-#endif
-
