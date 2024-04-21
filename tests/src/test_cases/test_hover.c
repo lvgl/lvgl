c55523ef1c219616f96ddc198ae37de54c5dd777
Samuel <turoksama@163.com>
2024-04-21 11:56:56 +0800

    Hover test cases.
    Add test cases for hover.

---
+++ tests/src/test_cases/test_hover.c
@@ -0,0 +1,83 @@
+//#if LV_BUILD_TEST
+#include "../lvgl.h"
+#include "../lv_test_indev.h"
+#include "unity/unity.h"
+
+#define TEST_HOVER_COUNTS   20
+
+typedef struct _test_hover_t {
+    /* data */
+    uint32_t id;
+    uint32_t counts;
+} test_hover_t;
+
+static test_hover_t label_hovered;
+static test_hover_t btn_hovered;
+
+static const lv_point_t pointer1[] = {
+    {0, 0}, {110, 20}, {150, 26}, {120, 19}, {0, 0},
+}
+
+static const lv_point_t pointer2[] = {
+    {0, 0}, {60, 100}, {80, 100}, {120, 120}, {0, 0},
+}
+
+void setUp(void)
+{
+    /* Function run before every test */
+}
+
+void tearDown(void)
+{
+    /* Function run after every test */
+    lv_obj_clean(lv_screen_active());
+}
+
+static void hovered_event_cb(lv_event_t * e)
+{
+    test_hover_t * hover = (test_hover_t *)e->user_data;
+    lv_log("Object(ID:%d) hovered %d times.\n", hover->id, hover->counts);
+}
+
+static void test_move_mouse(lv_point_t * point, uint8_t size)
+{
+    lv_point_t * p = point;
+
+    for(uint8_t j = 0; i < TEST_HOVER_COUNTS; j++) {
+        for(uint8_t i = 0; i < size; i++) {
+            lv_test_mouse_move_to(p[i]->x, p[i]->y);
+            lv_test_indev_wait(50);
+        }
+    }
+}
+
+void test_hover(void)
+{
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_obj_set_size(btn, 200, 20);
+    lv_label_set_text(label, "Clickable text can be hovered!");
+    lv_obj_set_pos(label, 100, 20);
+    lv_obj_add_flag(label, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_set_style_text_color(label, lv_color_hex(0x5be1b6), LV_PART_MAIN | LV_STATE_HOVERED);
+    //Set hover callback
+    label_hovered.id = (uint32_t)label->id;
+    label_hovered.counts = 0;
+    lv_obj_add_event_cb(label, hovered_event_cb, LV_EVENT_HOVERED, &label_hovered);
+
+    lv_obj_t * btn = lv_button_create(lv_screen_active());
+    lv_obj_set_pos(btn, 64, 100);
+    lv_obj_set_size(btn, 128, 48);
+    lv_obj_set_style_bg_opa(btn, 128, LV_PART_MAIN | LV_STATE_HOVERED);
+    //Set hover callback
+    btn_hovered.id = (uint32_t)btn->id;
+    btn_hovered.counts = 0;
+    lv_obj_add_event_cb(btn, hovered_event_cb, LV_EVENT_HOVERED, &btn_hovered);
+
+    test_move_mouse(pointer1);
+    test_move_mouse(pointer2);
+
+    TEST_ASSERT_EQUAL_INT32(TEST_HOVER_COUNTS, label_hovered.counts);
+    TEST_ASSERT_EQUAL_INT32(TEST_HOVER_COUNTS, btn_hovered.counts);
+}
+
+//#endif
