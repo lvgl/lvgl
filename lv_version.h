84af9d35b3e6d67cc60206551b23fb8a16bafb1c
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-04-23 16:16:15 +0200

    docs: fix example build break (#6114)

--- lv_version.h
+++ lv_version.h
@@ -1,14 +1,14 @@
 /**
- * @file version.h
+ * @file lv_version.h
  * The current version of LVGL
  */
 
-#ifndef LVGL_VERSION_H
-#define LVGL_VERSION_H
+#ifndef __LVGL_VERSION_H__
+#define __LVGL_VERSION_H__
 
 #define LVGL_VERSION_MAJOR 9
 #define LVGL_VERSION_MINOR 1
 #define LVGL_VERSION_PATCH 1
 #define LVGL_VERSION_INFO "dev"
 
-#endif /*LVGL_VERSION_H*/
+#endif /*__LVGL_VERSION_H__*/
