5e6a55d116b93a62136842d6046300a2744bb6ba
zipeartnes <75340184+zipeartnes@users.noreply.github.com>
2024-04-25 15:31:30 -0500

    Update lv_anim_timeline.c

--- src/misc/lv_anim_timeline.c
+++ src/misc/lv_anim_timeline.c
@@ -31,6 +31,7 @@ struct _lv_anim_timeline_t {
     uint32_t act_time;                  /**< Current time of the animation*/
     bool reverse;                       /**< Reverse playback*/
     uint32_t repeat_count;              /**< Repeat count*/
+    uint32_t repeat_delay;              /**< Wait before repeat*/
 };
 
 /**********************
@@ -88,6 +89,7 @@ uint32_t lv_anim_timeline_start(lv_anim_timeline_t * at)
 
     uint32_t playtime = lv_anim_timeline_get_playtime(at);
     uint32_t repeat = at->repeat_count;
+    uint32_t delay = at->repeat_delay;
     uint32_t start = at->act_time;
     uint32_t end = at->reverse ? 0 : playtime;
     uint32_t duration = end > start ? end - start : start - end;
@@ -100,6 +102,7 @@ uint32_t lv_anim_timeline_start(lv_anim_timeline_t * at)
     lv_anim_set_time(&a, duration);
     lv_anim_set_path_cb(&a, anim_timeline_path_cb);
     lv_anim_set_repeat_count(&a, repeat);
+    lv_anim_set_repeat_delay(&a, delay);
     lv_anim_start(&a);
     return playtime;
 }
@@ -161,6 +164,19 @@ uint16_t lv_anim_timeline_get_progress(lv_anim_timeline_t * at)
     LV_ASSERT_NULL(at);
     uint32_t playtime = lv_anim_timeline_get_playtime(at);
     return lv_map(at->act_time, 0, playtime, 0, LV_ANIM_TIMELINE_PROGRESS_MAX);
+}
+
+uint32_t lv_anim_timeline_get_repeat_count(lv_anim_timeline_t * at)
+{
+    LV_ASSERT_NULL(at);
+    return  at->repeat_count;
+}
+
+
+uint32_t lv_anim_timeline_get_repeat_delay(lv_anim_timeline_t * at)
+{   
+    LV_ASSERT_NULL(at);
+    return  at->repeat_delay;
 }
 
 /**********************
