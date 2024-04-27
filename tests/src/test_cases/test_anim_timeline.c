7377947d8e8764af5d97a58733523ad34db84c93
TridentTD <prote2000@gmail.com>
2024-04-27 07:43:57 +0700

    Update test_anim_timeline.c

--- tests/src/test_cases/test_anim_timeline.c
+++ tests/src/test_cases/test_anim_timeline.c
@@ -414,7 +414,7 @@ void test_anim_timeline_with_anim_start_cb_and_completed_cb(void)
     TEST_ASSERT_EQUAL(1, anim2_start_called);
     TEST_ASSERT_EQUAL(0, anim2_completed_called);
 
-    lv_test_wait(100); /*Now we are at 320ms */
+    lv_test_wait(90); /*Now we are at 310ms */
     TEST_ASSERT_EQUAL(1, anim1_start_called);
     TEST_ASSERT_EQUAL(1, anim1_completed_called);
     TEST_ASSERT_EQUAL(1, anim2_start_called);
