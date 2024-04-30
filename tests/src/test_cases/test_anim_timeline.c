b1957629e83048f87458e915028202aa7e1dd33f
TridentTD <prote2000@gmail.com>
2024-04-30 17:30:26 +0700

    Update test_anim_timeline.c
    add
    -----------------------
    lv_test_wait(200);
    -----------------------
    at the end of the code.

--- tests/src/test_cases/test_anim_timeline.c
+++ tests/src/test_cases/test_anim_timeline.c
@@ -418,6 +418,8 @@ void test_anim_timeline_with_anim_start_cb_and_completed_cb(void)
     TEST_ASSERT_EQUAL(1, anim1_completed_called);
     TEST_ASSERT_EQUAL(1, anim2_start_called);
     TEST_ASSERT_EQUAL(0, anim2_completed_called);
+
+    lv_test_wait(200); /*Now we are at 520ms */
 }
 
 #endif
