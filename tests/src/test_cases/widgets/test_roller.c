1d1b37ea453da11ff20e692bba61fb4c868eb72b
liamHowatt <liamjmh0@gmail.com>
2024-04-30 10:35:47 -0400

    clean up test

--- tests/src/test_cases/widgets/test_roller.c
+++ tests/src/test_cases/widgets/test_roller.c
@@ -255,14 +255,15 @@ void test_roller_appearance(void)
     lv_obj_add_flag(roller_mouse, LV_OBJ_FLAG_HIDDEN);
 
     lv_obj_t * rollers[10] = {roller, roller_infinite};
-    unsigned i = 2;
+    uint32_t i = 2;
 
-    /* main part and selected part same size font */
+    /* a normal and infinite roller with the same size font for the main and selected parts */
     lv_obj_set_pos(roller, 20, 20);
     lv_roller_set_options(roller, opts, LV_PART_MAIN);
     lv_obj_set_pos(roller_infinite, 20, 200);
     lv_roller_set_options(roller_infinite, opts, LV_ROLLER_MODE_INFINITE);
 
+    /* a normal and infinite roller with slightly different size fonts for the main and selected parts */
     lv_obj_t * r = lv_roller_create(active_screen);
     lv_obj_set_pos(r, 130, 20);
     lv_roller_set_options(r, opts, LV_ROLLER_MODE_NORMAL);
@@ -276,6 +277,7 @@ void test_roller_appearance(void)
     lv_obj_set_style_text_font(r, &lv_font_montserrat_24, LV_PART_SELECTED);
     rollers[i++] = r;
 
+    /* same as previous pair but the fonts are swapped for the main and selected parts */
     r = lv_roller_create(active_screen);
     lv_obj_set_pos(r, 270, 20);
     lv_roller_set_options(r, opts, LV_ROLLER_MODE_NORMAL);
@@ -289,6 +291,7 @@ void test_roller_appearance(void)
     lv_obj_set_style_text_font(r, &lv_font_montserrat_16, LV_PART_SELECTED);
     rollers[i++] = r;
 
+    /* a normal and infinite roller with extremely different size fonts for the main and selected parts */
     r = lv_roller_create(active_screen);
     lv_obj_set_pos(r, 410, 20);
     lv_roller_set_options(r, opts, LV_ROLLER_MODE_NORMAL);
@@ -302,6 +305,7 @@ void test_roller_appearance(void)
     lv_obj_set_style_text_font(r, &lv_font_montserrat_36, LV_PART_SELECTED);
     rollers[i++] = r;
 
+    /* same as previous pair but the fonts are swapped for the main and selected parts */
     r = lv_roller_create(active_screen);
     lv_obj_set_pos(r, 580, 20);
     lv_roller_set_options(r, opts, LV_ROLLER_MODE_NORMAL);
@@ -317,7 +321,8 @@ void test_roller_appearance(void)
 
     TEST_ASSERT_EQUAL_SCREENSHOT("widgets/roller_2.png");
 
-    for(i = 0; i < (sizeof(rollers) / sizeof(*rollers)); i++) {
+    /* test that the selected label stays in sync with the main label for scrolling */
+    for(i = 0; i < 10; i++) {
         lv_roller_set_selected(rollers[i], lv_roller_get_option_count(rollers[i]) - 1, LV_ANIM_OFF);
     }
 
