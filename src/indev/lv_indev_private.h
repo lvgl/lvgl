7208bc68435b5bbcfd7bd86b131bddbeba2458cb
Niklas Fiekas <niklas.fiekas@sartorius.com>
2024-05-06 16:09:19 +0200

    feat(indev): detect double and triple click

--- src/indev/lv_indev_private.h
+++ src/indev/lv_indev_private.h
@@ -90,7 +90,10 @@ struct _lv_indev_t {
         lv_area_t scroll_area;
         lv_point_t gesture_sum; /*Count the gesture pixels to check LV_INDEV_DEF_GESTURE_LIMIT*/
         int32_t diff;
-
+        /*Short click streaks*/
+        uint8_t short_click_streak;
+        lv_point_t last_short_click_point;
+        uint32_t last_short_click_timestamp;
         /*Flags*/
         uint8_t scroll_dir : 4;
         uint8_t gesture_dir : 4;
