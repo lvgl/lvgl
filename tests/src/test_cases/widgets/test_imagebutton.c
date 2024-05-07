3ca82c6a1a3df7b2454169c883a1c8545b8d8285
liamHowatt <liamjmh0@gmail.com>
2024-05-07 12:36:52 -0400

    also add music play button

--- tests/src/test_cases/widgets/test_imagebutton.c
+++ tests/src/test_cases/widgets/test_imagebutton.c
@@ -16,6 +16,10 @@ void tearDown(void)
 
 void test_imagebutton_basic_appearance(void)
 {
+    /**
+     * from lv_example_imagebutton_1
+    */
+
     LV_IMAGE_DECLARE(imagebutton_left);
     LV_IMAGE_DECLARE(imagebutton_right);
     LV_IMAGE_DECLARE(imagebutton_mid);
@@ -27,12 +31,26 @@ void test_imagebutton_basic_appearance(void)
     lv_obj_set_style_text_color(imagebutton1, lv_color_white(), 0);
 
     lv_obj_set_width(imagebutton1, 100);
-    lv_obj_align(imagebutton1, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(imagebutton1, LV_ALIGN_CENTER, -150, 0);
 
     /*Create a label on the image button*/
     lv_obj_t * label = lv_label_create(imagebutton1);
     lv_label_set_text(label, "Button");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, -4);
+
+    /**
+     * from lv_demo_music
+    */
+
+    LV_IMAGE_DECLARE(music_button_play);
+
+    lv_obj_t * play_button = lv_imagebutton_create(lv_screen_active());
+    lv_imagebutton_set_src(play_button, LV_IMAGEBUTTON_STATE_RELEASED, NULL, &music_button_play, NULL);
+    lv_obj_align(play_button, LV_ALIGN_CENTER, 100, 0);
+
+    /**
+     * compare screenshot
+    */
 
     TEST_ASSERT_EQUAL_SCREENSHOT("widgets/imagebutton_1.png");
 }
