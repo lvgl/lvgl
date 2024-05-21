83b1ad8d0e0ba5ae5572381d21bb2cba04afff82
liamHowatt <liamjmh0@gmail.com>
2024-05-21 10:52:19 -0400

    remove unnecessary memzero

--- src/others/file_explorer/lv_file_explorer.c
+++ src/others/file_explorer/lv_file_explorer.c
@@ -598,7 +598,6 @@ static void show_dir(lv_obj_t * obj, const char * path)
     /*Move the table to the top*/
     lv_obj_scroll_to_y(explorer->file_table, 0, LV_ANIM_OFF);
 
-    lv_memzero(explorer->current_path, sizeof(explorer->current_path));
     lv_strlcpy(explorer->current_path, path, sizeof(explorer->current_path));
     lv_label_set_text_fmt(explorer->path_label, LV_SYMBOL_EYE_OPEN" %s", path);
 
