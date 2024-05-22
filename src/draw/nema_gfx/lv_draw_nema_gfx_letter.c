54d0e68cea1f4d51b56c7053c6408d1a09bd43c4
Ioannis Markopoulos <Ioannis_Markopoulos@amat.com>
2024-05-22 12:55:59 +0300

    Licence correction

--- src/draw/nema_gfx/lv_draw_nema_gfx_letter.c
+++ src/draw/nema_gfx/lv_draw_nema_gfx_letter.c
@@ -8,7 +8,6 @@
  *
  * -----------------------------------------------------------------------------
  * Copyright (c) 2008-24 Think Silicon Single Member PC
- * Think Silicon Single Member PC Confidential Proprietary
  * -----------------------------------------------------------------------------
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
