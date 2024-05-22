71fa4f3ed5086903dcb542135a316962ae98b240
Benign X <1341398182@qq.com>
2024-05-22 20:18:36 +0800

    fix(freetype): fix potential multi-threaded data conflicts

--- src/libs/freetype/lv_freetype.c
+++ src/libs/freetype/lv_freetype.c
@@ -368,12 +368,14 @@ static bool cache_node_cache_create_cb(lv_freetype_cache_node_t * node, void * u
     }
 
     node->face = face;
+    lv_mutex_init(&node->face_lock);
 
     return true;
 }
 static void cache_node_cache_free_cb(lv_freetype_cache_node_t * node, void * user_data)
 {
     FT_Done_Face(node->face);
+    lv_mutex_delete(&node->face_lock);
 
     if(node->glyph_cache) {
         lv_cache_destroy(node->glyph_cache, user_data);
