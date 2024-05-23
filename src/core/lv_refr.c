79fb5eed82bd862e277d59c7e169fb154e608338
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-05-23 14:22:15 -0600

    removes the pausing of the refresh timer as this wasn't doing anything except going to cause an issue because it was backwards. It was set to pause when the performance monitor was running and it was supposed to be the op[posite of that. But seeing as how time spent doing a refresh is time spent it should not be paused to begin with. LVGL function fine without pausing it so there is no need to have it there to begin with. I was also unable to locate where the timer was being unpaused as well so if the performance monitor was running it would have caused issues with the refresh occuring when it should be.

--- src/core/lv_refr.c
+++ src/core/lv_refr.c
@@ -340,14 +340,7 @@ void _lv_display_refr_timer(lv_timer_t * tmr)
 
     if(tmr) {
         disp_refr = tmr->user_data;
-        /* Ensure the timer does not run again automatically.
-         * This is done before refreshing in case refreshing invalidates something else.
-         * However if the performance monitor is enabled keep the timer running to count the FPS.*/
-#if LV_USE_PERF_MONITOR
-        lv_timer_pause(tmr);
-#endif
-    }
-    else {
+    } else {
         disp_refr = lv_display_get_default();
     }
 
