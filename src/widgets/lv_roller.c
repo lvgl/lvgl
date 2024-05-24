1ff3c20fd76b08539063d5a2b482a02562f35611
ymh-masuda <160470322+ymh-masuda@users.noreply.github.com>
2024-05-24 17:01:01 +0900

    fix(roller): Avoid divided-by-zero during draw event (#6259)

--- src/widgets/lv_roller.c
+++ src/widgets/lv_roller.c
@@ -488,7 +488,10 @@ static void draw_main(lv_event_t * e)
 
             /*Proportional position from the middle line.
              *Will be 0 for the first option, and 1 for the last option (upscaled by << 14)*/
-            label_y_prop = (label_y_prop << 14) / (lv_obj_get_height(label) - normal_label_font->line_height);
+            lv_coord_t remain_h = lv_obj_get_height(label) - normal_label_font->line_height;
+            if(remain_h > 0) {
+                label_y_prop = (label_y_prop << 14) / remain_h;
+            }
 
             /*We don't want the selected label start and end exactly where the normal label is as
              *a larger font won't centered on selected area.*/
