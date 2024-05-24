201bbcc6456c8b5585b6aae5e528b226a6be19b2
ymh-masuda <kyosuke.masuda@music.yamaha.com>
2024-05-24 11:29:03 +0900

    fix code format

--- src/widgets/roller/lv_roller.c
+++ src/widgets/roller/lv_roller.c
@@ -475,7 +475,7 @@ static void draw_main(lv_event_t * e)
             /*Proportional position from the middle line.
              *Will be 0 for the first option, and 1 for the last option (upscaled by << 14)*/
             lv_coord_t label_h = lv_obj_get_height(label);
-            if ((label_h - normal_label_font->line_height) > 0) {
+            if((label_h - normal_label_font->line_height) > 0) {
                 label_y_prop = (label_y_prop << 14) / (label_h - normal_label_font->line_height);
             }
 
