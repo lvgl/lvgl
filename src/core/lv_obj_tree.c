6e6a427e1590ad1e6d69746ae480e9e7e2353357
jaeho83.lim <jaeho83.lim@lge.com>
2024-05-27 14:54:21 +0900

    fix(obj) search child object using dfs(depth-first search)
    Change-Id: I9a8b3dd664e7fb2701feaeb170e77283c28a5ff2

--- src/core/lv_obj_tree.c
+++ src/core/lv_obj_tree.c
@@ -623,7 +623,7 @@ static lv_obj_tree_walk_res_t dump_tree_core(lv_obj_t * obj, int32_t depth)
         for(uint32_t i = 0; i < obj->spec_attr->child_cnt; i++) {
             res = dump_tree_core(lv_obj_get_child(obj, i), depth + 1);
             if(res == LV_OBJ_TREE_WALK_END)
-                break;
+                continue;
         }
         return LV_OBJ_TREE_WALK_NEXT;
     }
