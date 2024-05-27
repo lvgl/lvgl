ba27cc445e08e9dc274f8e0a93a4d7254ad4d71e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-05-27 20:39:08 +0200

    fix(dave2d): make it work without software render too
    fixes https://github.com/lvgl/lv_port_renesas_ek-ra8d1/issues/9

--- src/draw/renesas/dave2d/lv_draw_dave2d.c
+++ src/draw/renesas/dave2d/lv_draw_dave2d.c
@@ -344,6 +344,10 @@ static int32_t lv_draw_dave2d_dispatch(lv_draw_unit_t * draw_unit, lv_layer_t * 
 
     lv_draw_task_t * t = NULL;
     t = lv_draw_get_next_available_task(layer, NULL, DRAW_UNIT_ID_DAVE2D);
+    while(t && t->preferred_draw_unit_id != DRAW_UNIT_ID_DAVE2D) {
+        t->state = LV_DRAW_TASK_STATE_READY;
+        t = lv_draw_get_next_available_task(layer, NULL, DRAW_UNIT_ID_DAVE2D);
+    }
 
     /* Return 0 is no selection, some tasks can be supported by other units. */
     if(t == NULL) {
@@ -356,9 +360,6 @@ static int32_t lv_draw_dave2d_dispatch(lv_draw_unit_t * draw_unit, lv_layer_t * 
         return 0;
     }
 
-    if(t->preferred_draw_unit_id != DRAW_UNIT_ID_DAVE2D) {
-        return 0;
-    }
 
     void * buf = lv_draw_layer_alloc_buf(layer);
     if(buf == NULL) {
