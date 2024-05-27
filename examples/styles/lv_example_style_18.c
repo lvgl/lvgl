3928e4fdec166b7495b4ac73a9e7a32e5777c2f0
lhdjply <lhdjply@126.com>
2024-05-27 13:44:27 +0800

    fix(example): fix when LV_USE_DRAW_SW_COMPLEX_GRADIENTS is 1, example is missing the relevant functions.
    Signed-off-by: lhdjply <lhdjply@126.com>

--- examples/styles/lv_example_style_18.c
+++ examples/styles/lv_example_style_18.c
@@ -83,6 +83,17 @@ void lv_example_style_18(void)
     lv_obj_center(label);
 }
 
+#else
+
+void lv_example_style_18(void)
+{
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_obj_set_width(label, LV_PCT(80));
+    lv_label_set_text(label, "LV_USE_DRAW_SW_COMPLEX_GRADIENTS is not enabled");
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_center(label);
+}
+
 #endif /*LV_USE_DRAW_SW_COMPLEX_GRADIENTS*/
 
 #endif /*LV_BUILD_EXAMPLES*/
