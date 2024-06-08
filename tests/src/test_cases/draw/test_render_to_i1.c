8ec15d32b672d1053db6cc783b9ff15b9529d2e3
faxe1008 <fabianblatz@gmail.com>
2024-06-08 21:46:08 +0200

    feat(draw/sw): added tests for LV_COLOR_FORMAT_I1

--- tests/src/test_cases/draw/test_render_to_i1.c
+++ tests/src/test_cases/draw/test_render_to_i1.c
@@ -15,30 +15,28 @@ void tearDown(void)
     lv_display_set_color_format(NULL, LV_COLOR_FORMAT_XRGB8888);
 }
 
-void test_render_to_xrgb8888(void)
+void test_render_to_i1(void)
 {
-    lv_display_set_color_format(NULL, LV_COLOR_FORMAT_XRGB8888);
+#if LV_BIN_DECODER_RAM_LOAD
+    lv_display_set_color_format(NULL, LV_COLOR_FORMAT_I1);
 
-    lv_opa_t opa_values[2] = {0xff, 0x80};
+    lv_opa_t opa_values[2] = {0xff, 0xc0};
     uint32_t opa;
     for(opa = 0; opa < 2; opa++) {
         uint32_t i;
         for(i = 0; i < _LV_DEMO_RENDER_SCENE_NUM; i++) {
-
-            /*Skip test with transformed indexed images if they are not loaded to RAM*/
-            if(LV_BIN_DECODER_RAM_LOAD == 0 &&
-               (i == LV_DEMO_RENDER_SCENE_IMAGE_NORMAL_2 ||
-                i == LV_DEMO_RENDER_SCENE_IMAGE_RECOLOR_2)) continue;
-
-
             lv_demo_render(i, opa_values[opa]);
 
             char buf[128];
-            lv_snprintf(buf, sizeof(buf), "draw/render/xrgb8888/demo_render_%s_opa_%d.png",
+            lv_snprintf(buf, sizeof(buf), "draw/render/i1/demo_render_%s_opa_%d.png",
                         lv_demo_render_get_scene_name(i), opa_values[opa]);
             TEST_ASSERT_EQUAL_SCREENSHOT(buf);
         }
     }
+#else
+    /*Without LV_BIN_DECODER_RAM_LOAD can't test rotated images*/
+    TEST_PASS();
+#endif
 }
 
 #endif
