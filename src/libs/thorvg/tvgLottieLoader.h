132f40566a11b27a3c0a3d0c182bd3097603d0d7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-06-18 09:23:30 -0400

    spelling: begin
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/libs/thorvg/tvgLottieLoader.h
+++ src/libs/thorvg/tvgLottieLoader.h
@@ -70,7 +70,7 @@ public:
     //Marker Supports
     uint32_t markersCnt();
     const char* markers(uint32_t index);
-    bool segment(const char* marker, float& beign, float& end);
+    bool segment(const char* marker, float& begin, float& end);
 
 private:
     bool header();
