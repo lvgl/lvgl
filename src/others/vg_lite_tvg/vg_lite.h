06ac2a95138318a37e32a31e80c9ba196c86776c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-06-18 09:50:50 -0400

    spelling: channel
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/others/vg_lite_tvg/vg_lite.h
+++ src/others/vg_lite_tvg/vg_lite.h
@@ -927,13 +927,13 @@ typedef unsigned int        vg_lite_color_t;
     typedef struct vg_lite_color_key
     {
         vg_lite_uint8_t enable;                 /*! The color key is effective only when "enable" is ture, */
-        vg_lite_uint8_t low_r;                  /*! The R chanel of low_rgb. */
-        vg_lite_uint8_t low_g;                  /*! The G chanel of low_rgb. */
-        vg_lite_uint8_t low_b;                  /*! The B chanel of low_rgb. */
+        vg_lite_uint8_t low_r;                  /*! The R channel of low_rgb. */
+        vg_lite_uint8_t low_g;                  /*! The G channel of low_rgb. */
+        vg_lite_uint8_t low_b;                  /*! The B channel of low_rgb. */
         vg_lite_uint8_t alpha;                  /*! The alpha channel to replace destination pixel alpha channel.*/
-        vg_lite_uint8_t hign_r;                 /*! The R chanel of hign_rgb. */
-        vg_lite_uint8_t hign_g;                 /*! The G chanel of hign_rgb. */
-        vg_lite_uint8_t hign_b;                 /*! The B chanel of hign_rgb. */
+        vg_lite_uint8_t hign_r;                 /*! The R channel of hign_rgb. */
+        vg_lite_uint8_t hign_g;                 /*! The G channel of hign_rgb. */
+        vg_lite_uint8_t hign_b;                 /*! The B channel of hign_rgb. */
     } vg_lite_color_key_t;
 
     /* Four colorkey definition.
