eb57be774b91f44d2bb2e671f9632989045375df
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-06-19 09:05:48 -0400

    spelling: subdivided
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/core/lv_refr.c
+++ src/core/lv_refr.c
@@ -896,7 +896,7 @@ void refr_obj(lv_layer_t * layer, lv_obj_t * obj)
         lv_result_t res = layer_get_area(layer, obj, layer_type, &layer_area_full, &obj_draw_size);
         if(res != LV_RESULT_OK) return;
 
-        /*Simple layers can be subdivied into smaller layers*/
+        /*Simple layers can be subdivided into smaller layers*/
         uint32_t max_rgb_row_height = lv_area_get_height(&layer_area_full);
         uint32_t max_argb_row_height = lv_area_get_height(&layer_area_full);
         if(layer_type == LV_LAYER_TYPE_SIMPLE) {
