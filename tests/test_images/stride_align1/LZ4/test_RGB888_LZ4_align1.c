dd70291e4cfe152c28750192d0241def8e012625
Neo Xu <neo.xu1990@gmail.com>
2024-06-26 11:01:46 +0800

    fix(script): update RLE compressed image raw len without padding
    On device, the decompress could ignore the padding bytes, thus we should store only the raw image data length as decompressed data length in image file.
    
    Signed-off-by: Neo Xu <neo.xu1990@gmail.com>

--- tests/test_images/stride_align1/LZ4/test_RGB888_LZ4_align1.c
+++ tests/test_images/stride_align1/LZ4/test_RGB888_LZ4_align1.c
@@ -12,12 +12,12 @@
 #define LV_ATTRIBUTE_MEM_ALIGN
 #endif
 
-#ifndef LV_ATTRIBUTE_IMG_DUST
-#define LV_ATTRIBUTE_IMG_DUST
+#ifndef LV_ATTRIBUTE_TEST_RGB888_LZ4_ALIGN1
+#define LV_ATTRIBUTE_TEST_RGB888_LZ4_ALIGN1
 #endif
 
 static const
-LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_DUST
+LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_TEST_RGB888_LZ4_ALIGN1
 uint8_t test_RGB888_LZ4_align1_map[] = {
 
     0x02,0x00,0x00,0x00,0xe1,0x0a,0x00,0x00,0x80,0x34,0x00,0x00,0x3f,0x00,0xfe,0x00,
@@ -198,14 +198,14 @@ uint8_t test_RGB888_LZ4_align1_map[] = {
 
 };
 
-const lv_img_dsc_t test_RGB888_LZ4_align1 = {
+const lv_image_dsc_t test_RGB888_LZ4_align1 = {
   .header.magic = LV_IMAGE_HEADER_MAGIC,
   .header.cf = LV_COLOR_FORMAT_RGB888,
   .header.flags = 0 | LV_IMAGE_FLAGS_COMPRESSED,
   .header.w = 71,
   .header.h = 60,
   .header.stride = 224,
-  .data_size = 2797,
+  .data_size = sizeof(test_RGB888_LZ4_align1_map),
   .data = test_RGB888_LZ4_align1_map,
 };
 
