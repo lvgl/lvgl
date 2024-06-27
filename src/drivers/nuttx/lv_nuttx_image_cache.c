0c4f0e6108e5eb7c8c297178db3187050376bb6a
apple <apple@appledeMacBook-Air.local>
2024-06-27 20:34:29 +0800

    update

--- src/drivers/nuttx/lv_nuttx_image_cache.c
+++ src/drivers/nuttx/lv_nuttx_image_cache.c
@@ -30,8 +30,7 @@ typedef struct {
     uint8_t * mem;
     uint32_t mem_size;
 
-    char name[sizeof(HEAP_NAME) + 10]; /**< UINT8_MAX is a maximum of 3 bits long in decimal notation,
-                                            and with the [] on both sides, 10 bytes must be enough */
+    char name[sizeof(HEAP_NAME) + 10]; /**< +10 characters to store task pid. */
 
     struct mm_heap_s * heap;
     uint32_t heap_size;
