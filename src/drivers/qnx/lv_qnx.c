fd43b56e86fc04757b4269f2e76a33861cf8c6e2
Elad Lahav <elahav@qnx.com>
2024-07-14 06:17:38 -0400

    Format code to LVGL style

--- src/drivers/qnx/lv_qnx.c
+++ src/drivers/qnx/lv_qnx.c
@@ -39,13 +39,12 @@ static uint32_t get_ticks(void);
 static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * color_p);
 static bool window_create(lv_display_t * disp);
 static bool init_display_from_window(lv_display_t * disp);
-static void get_pointer(lv_indev_t *indev_drv, lv_indev_data_t *data);
+static void get_pointer(lv_indev_t * indev_drv, lv_indev_data_t * data);
 static void release_disp_cb(lv_event_t * e);
 
 /***********************
  *   GLOBAL PROTOTYPES
  ***********************/
-lv_display_t * _lv_qnx_get_disp_from_win_id(uint32_t win_id);
 
 static screen_context_t context;
 static bool inited = false;
@@ -67,8 +66,8 @@ static int mouse_buttons;
 lv_display_t * lv_qnx_window_create(int32_t hor_res, int32_t ver_res)
 {
     if(!inited) {
-        if (screen_create_context(&context,
-                                  SCREEN_APPLICATION_CONTEXT) != 0) {
+        if(screen_create_context(&context,
+                                 SCREEN_APPLICATION_CONTEXT) != 0) {
             perror("screen_create_context");
             return NULL;
         }
@@ -88,14 +87,14 @@ lv_display_t * lv_qnx_window_create(int32_t hor_res, int32_t ver_res)
     }
     lv_display_add_event_cb(disp, release_disp_cb, LV_EVENT_DELETE, disp);
     lv_display_set_driver_data(disp, dsc);
-    if (!window_create(disp)) {
+    if(!window_create(disp)) {
         lv_free(dsc);
         return NULL;
     }
 
     lv_display_set_flush_cb(disp, flush_cb);
 
-    if (!init_display_from_window(disp)) {
+    if(!init_display_from_window(disp)) {
         screen_destroy_window(dsc->window);
         lv_free(dsc);
         return NULL;
@@ -103,23 +102,6 @@ lv_display_t * lv_qnx_window_create(int32_t hor_res, int32_t ver_res)
 
     return disp;
 }
-
-#if 0
-lv_display_t * _lv_qnx_get_disp_from_win_id(uint32_t win_id)
-{
-    lv_display_t * disp = lv_display_get_next(NULL);
-    if(win_id == UINT32_MAX) return disp;
-
-    while(disp) {
-        lv_qnx_window_t * dsc = lv_display_get_driver_data(disp);
-        if(QNX_GetWindowID(dsc->window) == win_id) {
-            return disp;
-        }
-        disp = lv_display_get_next(disp);
-    }
-    return NULL;
-}
-#endif
 
 void lv_qnx_window_set_title(lv_display_t * disp, const char * title)
 {
@@ -130,7 +112,9 @@ void lv_qnx_window_set_title(lv_display_t * disp, const char * title)
     char title_buf[64];
     snprintf(title_buf, sizeof(title), "Title=%s", title);
     screen_set_event_property_iv(event, SCREEN_PROPERTY_TYPE,
-                                 (const int[]){ SCREEN_EVENT_MANAGER });
+    (const int[]) {
+        SCREEN_EVENT_MANAGER
+    });
     screen_set_event_property_cv(event, SCREEN_PROPERTY_USER_DATA,
                                  sizeof(title_buf), title_buf);
     screen_set_event_property_pv(event, SCREEN_PROPERTY_WINDOW,
@@ -143,7 +127,7 @@ void lv_qnx_window_set_title(lv_display_t * disp, const char * title)
 
 bool lv_qnx_inputs_create(lv_display_t * disp)
 {
-    lv_indev_t *indev = lv_indev_create();
+    lv_indev_t * indev = lv_indev_create();
     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
     lv_indev_set_read_cb(indev, get_pointer);
     return true;
@@ -151,41 +135,41 @@ bool lv_qnx_inputs_create(lv_display_t * disp)
 
 int lv_qnx_event_loop(lv_display_t * disp)
 {
-    // Run the event loop.
+    /*Run the event loop*/
     screen_event_t  event;
-    if (screen_create_event(&event) != 0) {
+    if(screen_create_event(&event) != 0) {
         perror("screen_create_event");
         return EXIT_FAILURE;
     }
 
-    for (;;) {
-        // Wait for an event, timing out after 10ms.
-        if (screen_get_event(context, event, 10000000L) != 0) {
+    for(;;) {
+        /*Wait for an event, timing out after 10ms*/
+        if(screen_get_event(context, event, 10000000L) != 0) {
             perror("screen_get_event");
             return EXIT_FAILURE;
         }
 
-        // Get the event's type.
+        /*Get the event's type*/
         int type;
-        if (screen_get_event_property_iv(event, SCREEN_PROPERTY_TYPE, &type)
-            != 0) {
+        if(screen_get_event_property_iv(event, SCREEN_PROPERTY_TYPE, &type)
+           != 0) {
             perror("screen_get_event_property_iv(TYPE)");
             return EXIT_FAILURE;
         }
 
-        if (type == SCREEN_EVENT_POINTER) {
-            // Handle a mouse pointer event.
-            if (screen_get_event_property_iv(event,
-                                             SCREEN_PROPERTY_SOURCE_POSITION,
-                                             mouse_pos)
-                != 0) {
+        if(type == SCREEN_EVENT_POINTER) {
+            /*Handle a mouse pointer event*/
+            if(screen_get_event_property_iv(event,
+                                            SCREEN_PROPERTY_SOURCE_POSITION,
+                                            mouse_pos)
+               != 0) {
                 perror("screen_get_event_property_iv(SOURCE_POSITION)");
                 return EXIT_FAILURE;
             }
 
-            if (screen_get_event_property_iv(event, SCREEN_PROPERTY_BUTTONS,
-                                             &mouse_buttons)
-                != 0) {
+            if(screen_get_event_property_iv(event, SCREEN_PROPERTY_BUTTONS,
+                                            &mouse_buttons)
+               != 0) {
                 perror("screen_get_event_property_iv(BUTTONS)");
                 return EXIT_FAILURE;
             }
@@ -220,8 +204,8 @@ get_ticks(void)
 static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * px_map)
 {
     lv_qnx_window_t * dsc = lv_display_get_driver_data(disp);
-    if (screen_post_window(dsc->window, dsc->buffers[dsc->bufidx], 0, NULL, 0)
-        != 0) {
+    if(screen_post_window(dsc->window, dsc->buffers[dsc->bufidx], 0, NULL, 0)
+       != 0) {
         perror("screen_post_window");
     }
 
@@ -234,59 +218,62 @@ static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * px_m
 
 static bool window_create(lv_display_t * disp)
 {
+    /*Create a window*/
     lv_qnx_window_t * dsc = lv_display_get_driver_data(disp);
-    if (screen_create_window(&dsc->window, context) != 0) {
+    if(screen_create_window(&dsc->window, context) != 0) {
         perror("screen_create_window");
         return false;
     }
 
+    /*Set window properties*/
     int rect[] = { 0, 0, disp->hor_res, disp->ver_res };
-
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_POSITION,
-                                      &rect[0]) != 0) {
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_POSITION,
+                                     &rect[0]) != 0) {
         perror("screen_window_set_property_iv(POSITION)");
         return false;
     }
 
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_SIZE,
-                                      &rect[2]) != 0) {
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_SIZE,
+                                     &rect[2]) != 0) {
         perror("screen_window_set_property_iv(SIZE)");
         return false;
     }
 
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_SOURCE_SIZE,
-                                      &rect[2]) != 0) {
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_SOURCE_SIZE,
+                                     &rect[2]) != 0) {
         perror("screen_window_set_property_iv(SOURCE_SIZE)");
         return NULL;
     }
 
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_USAGE,
-                                      (int[]){ SCREEN_USAGE_WRITE }) != 0) {
+    int usage = SCREEN_USAGE_WRITE;
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_USAGE,
+                                     &usage) != 0) {
         perror("screen_window_set_property_iv(USAGE)");
         return NULL;
     }
 
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_FORMAT,
-                                      (int[]){ SCREEN_FORMAT_RGBA8888 }) != 0) {
+    int format = SCREEN_FORMAT_RGBA8888;
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_FORMAT,
+                                     &format) != 0) {
         perror("screen_window_set_property_iv(USAGE)");
         return NULL;
     }
 
-    // Initialize window buffers.
-    if (screen_create_window_buffers(dsc->window, LV_QNX_BUF_COUNT) != 0) {
+    /*Initialize window buffers*/
+    if(screen_create_window_buffers(dsc->window, LV_QNX_BUF_COUNT) != 0) {
         perror("screen_create_window_buffers");
         return false;
     }
 
-    if (screen_get_window_property_pv(dsc->window, SCREEN_PROPERTY_BUFFERS,
-                                      (void **)&dsc->buffers) != 0) {
+    if(screen_get_window_property_pv(dsc->window, SCREEN_PROPERTY_BUFFERS,
+                                     (void **)&dsc->buffers) != 0) {
         perror("screen_get_window_property_pv(BUFFERS)");
         return false;
     }
 
-    if (screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_VISIBLE,
-                                      (int[]){ 1 })
-        != 0) {
+    int visible = 1;
+    if(screen_set_window_property_iv(dsc->window, SCREEN_PROPERTY_VISIBLE,
+                                     &visible) != 0) {
         perror("screen_set_window_property_iv(VISIBLE)");
         return false;
     }
@@ -294,28 +281,28 @@ static bool window_create(lv_display_t * disp)
     return true;
 }
 
-static bool init_display_from_window(lv_display_t *disp)
+static bool init_display_from_window(lv_display_t * disp)
 {
     lv_qnx_window_t * dsc = lv_display_get_driver_data(disp);
 
     int bufsize;
-    if (screen_get_buffer_property_iv(dsc->buffers[0], SCREEN_PROPERTY_SIZE,
-                                      &bufsize) == -1) {
+    if(screen_get_buffer_property_iv(dsc->buffers[0], SCREEN_PROPERTY_SIZE,
+                                     &bufsize) == -1) {
         perror("screen_get_buffer_property_iv(SIZE)");
         return false;
     }
 
-    void *ptr1 = NULL;
-    if (screen_get_buffer_property_pv(dsc->buffers[0], SCREEN_PROPERTY_POINTER,
-                                      &ptr1) == -1) {
+    void * ptr1 = NULL;
+    if(screen_get_buffer_property_pv(dsc->buffers[0], SCREEN_PROPERTY_POINTER,
+                                     &ptr1) == -1) {
         perror("screen_get_buffer_property_pv(POINTER)");
         return false;
     }
 
-    void *ptr2 = NULL;
+    void * ptr2 = NULL;
 #if (LV_QNX_BUF_COUNT > 1)
-    if (screen_get_buffer_property_pv(dsc->buffers[1], SCREEN_PROPERTY_POINTER,
-                                      &ptr2) == -1) {
+    if(screen_get_buffer_property_pv(dsc->buffers[1], SCREEN_PROPERTY_POINTER,
+                                     &ptr2) == -1) {
         perror("screen_get_buffer_property_pv(POINTER)");
         return false;
     }
@@ -329,13 +316,14 @@ static void release_disp_cb(lv_event_t * e)
 {
 }
 
-static void get_pointer(lv_indev_t *indev_drv, lv_indev_data_t *data)
+static void get_pointer(lv_indev_t * indev_drv, lv_indev_data_t * data)
 {
     data->point.x = mouse_pos[0];
     data->point.y = mouse_pos[1];
-    if ((mouse_buttons & SCREEN_LEFT_MOUSE_BUTTON) != 0) {
+    if((mouse_buttons & SCREEN_LEFT_MOUSE_BUTTON) != 0) {
         data->state = LV_INDEV_STATE_PRESSED;
-    } else {
+    }
+    else {
         data->state = LV_INDEV_STATE_RELEASED;
     }
 }
