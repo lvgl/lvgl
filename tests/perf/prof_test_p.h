fc6f82996103e5d074d6e6fa5b57319ed159921d
Anthony I. Jaccard <anthony.jaccard@heig-vd.ch>
2024-07-15 17:22:20 +0200

    fix code style

--- tests/perf/prof_test_p.h
+++ tests/perf/prof_test_p.h
@@ -1,4 +1,4 @@
 #include <lvgl.h>
 
 uint32_t prof_lv_bezier3(uint32_t t, uint32_t u0, uint32_t u1, uint32_t u2, uint32_t u3);
-bool prof_lv_area_intersect(lv_area_t *res_p, lv_area_t *a1_p, lv_area_t *a2_p);
+bool prof_lv_area_intersect(lv_area_t * res_p, lv_area_t * a1_p, lv_area_t * a2_p);
