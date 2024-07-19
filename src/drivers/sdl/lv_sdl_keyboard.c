80b8c33f219d21b57b4cef0e2f149f471490cefa
Liu Yi <lhdjply@126.com>
2024-07-19 18:10:58 +0800

    ci(sdl): add sdl build to ci test (#6505)
    Signed-off-by: lhdjply <lhdjply@126.com>
    Signed-off-by: Liu Yi <lhdjply@126.com>

--- src/drivers/sdl/lv_sdl_keyboard.c
+++ src/drivers/sdl/lv_sdl_keyboard.c
@@ -12,7 +12,7 @@
 #include "../../indev/lv_indev.h"
 #include "../../core/lv_group.h"
 #include "../../stdlib/lv_string.h"
-#include LV_SDL_INCLUDE_PATH
+#include "lv_sdl_private.h"
 
 /*********************
  *      DEFINES
@@ -99,7 +99,7 @@ static void release_indev_cb(lv_event_t * e)
     }
 }
 
-void _lv_sdl_keyboard_handler(SDL_Event * event)
+void lv_sdl_keyboard_handler(SDL_Event * event)
 {
     uint32_t win_id = UINT32_MAX;
     switch(event->type) {
@@ -113,7 +113,7 @@ void _lv_sdl_keyboard_handler(SDL_Event * event)
             return;
     }
 
-    lv_display_t * disp = _lv_sdl_get_disp_from_win_id(win_id);
+    lv_display_t * disp = lv_sdl_get_disp_from_win_id(win_id);
 
 
     /*Find a suitable indev*/
