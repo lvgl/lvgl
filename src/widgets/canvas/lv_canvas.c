3ab26d08d742dcd8c3ff27b4e2955ffdccc93f82
Cosmin-Daniel Radu <cosmin.radu_1@nxp.com>
2024-07-29 15:17:54 +0300

    fix(widgets) wait_for_finish called in lv_canvas_finish
    Signed-off-by: Cosmin-Daniel Radu <cosmin.radu_1@nxp.com>

--- src/widgets/canvas/lv_canvas.c
+++ src/widgets/canvas/lv_canvas.c
@@ -385,10 +385,18 @@ void lv_canvas_init_layer(lv_obj_t * obj, lv_layer_t * layer)
 
 void lv_canvas_finish_layer(lv_obj_t * canvas, lv_layer_t * layer)
 {
+    bool task_dispatched;
+
     if(layer->draw_task_head == NULL) return;
+
     while(layer->draw_task_head) {
         lv_draw_dispatch_wait_for_request();
-        lv_draw_dispatch_layer(lv_obj_get_display(canvas), layer);
+        task_dispatched = lv_draw_dispatch_layer(lv_obj_get_display(canvas), layer);
+
+        if(!task_dispatched) {
+            lv_draw_wait_for_finish();
+            lv_draw_dispatch_request();
+        }
     }
     lv_obj_invalidate(canvas);
 }
