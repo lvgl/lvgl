c7ba82199a9bca006ca0b3646472f54806c28387
zipeartnes <75340184+zipeartnes@users.noreply.github.com>
2024-07-29 16:54:04 -0500

    Update test_anim_timeline.c

--- tests/src/test_cases/test_anim_timeline.c
+++ tests/src/test_cases/test_anim_timeline.c
@@ -363,7 +363,7 @@ void test_anim_timeline_repeat(void)
     TEST_ASSERT_EQUAL(998, lv_obj_get_x(obj));
 
     lv_test_wait(100);
-    TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
+    TEST_ASSERT_EQUAL(0, lv_obj_get_x(obj));
     
     lv_test_wait(100);
     TEST_ASSERT_EQUAL(99, lv_obj_get_x(obj));
