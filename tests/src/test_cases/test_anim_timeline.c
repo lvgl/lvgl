6766de9b67385e77e4dea7d423d0534c47dc8436
zipeartnes <75340184+zipeartnes@users.noreply.github.com>
2024-07-30 11:05:11 -0500

    Update test_anim_timeline.c

--- tests/src/test_cases/test_anim_timeline.c
+++ tests/src/test_cases/test_anim_timeline.c
@@ -342,16 +342,15 @@ void test_anim_timeline_repeat(void)
 
     anim_timeline = lv_anim_timeline_create();
     TEST_ASSERT_NOT_NULL(anim_timeline);
-
+    
     lv_anim_timeline_add(anim_timeline, 0, &a1);
     lv_anim_timeline_set_repeat_count(anim_timeline, 3);
-    lv_anim_timeline_set_repeat_delay(anim_timeline, 0);
+    lv_anim_timeline_set_repeat_delay(anim_timeline, 500);
     lv_anim_timeline_start(anim_timeline);
 
-    TEST_ASSERT_EQUAL(3, lv_anim_timeline_get_repeat_count(anim_timeline));
-
     lv_refr_now(NULL);
-    TEST_ASSERT_EQUAL(0, lv_obj_get_x(obj));
+
+    TEST_ASSERT_EQUAL(500, lv_anim_timeline_get_repeat_delay(anim_timeline));
 
     lv_test_wait(100);
     TEST_ASSERT_EQUAL(99, lv_obj_get_x(obj));
@@ -359,23 +358,60 @@ void test_anim_timeline_repeat(void)
     lv_test_wait(500);
     TEST_ASSERT_EQUAL(599, lv_obj_get_x(obj));
 
-    lv_test_wait(399);
-    TEST_ASSERT_EQUAL(998, lv_obj_get_x(obj));
+    lv_test_wait(400);
+    TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
 
-    lv_test_wait(100);
+    lv_test_wait(500);
     TEST_ASSERT_EQUAL(0, lv_obj_get_x(obj));
-    
-    lv_test_wait(100);
-    TEST_ASSERT_EQUAL(99, lv_obj_get_x(obj));
 
-    lv_test_wait(100);
-    TEST_ASSERT_EQUAL(199, lv_obj_get_x(obj));
+    lv_test_wait(500);
+    TEST_ASSERT_EQUAL(500, lv_obj_get_x(obj));
+
+    lv_test_wait(500);
+    TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
 
     lv_test_wait(1200);
-    TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
-
+    TEST_ASSERT_EQUAL(699, lv_obj_get_x(obj));
+    
     lv_test_wait(1000);
     TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
+    
+    lv_test_wait(1000);
+    TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
+
+    // lv_anim_timeline_add(anim_timeline, 0, &a1);
+    // lv_anim_timeline_set_repeat_count(anim_timeline, 3);
+    // lv_anim_timeline_set_repeat_delay(anim_timeline, 0);
+    // lv_anim_timeline_start(anim_timeline);
+
+    // TEST_ASSERT_EQUAL(3, lv_anim_timeline_get_repeat_count(anim_timeline));
+
+    // lv_refr_now(NULL);
+    // TEST_ASSERT_EQUAL(0, lv_obj_get_x(obj));
+
+    // lv_test_wait(100);
+    // TEST_ASSERT_EQUAL(99, lv_obj_get_x(obj));
+
+    // lv_test_wait(500);
+    // TEST_ASSERT_EQUAL(599, lv_obj_get_x(obj));
+
+    // lv_test_wait(399);
+    // TEST_ASSERT_EQUAL(998, lv_obj_get_x(obj));
+
+    // lv_test_wait(100);
+    // TEST_ASSERT_EQUAL(0, lv_obj_get_x(obj));
+    
+    // lv_test_wait(100);
+    // TEST_ASSERT_EQUAL(99, lv_obj_get_x(obj));
+
+    // lv_test_wait(100);
+    // TEST_ASSERT_EQUAL(199, lv_obj_get_x(obj));
+
+    // lv_test_wait(1200);
+    // TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
+
+    // lv_test_wait(1000);
+    // TEST_ASSERT_EQUAL(1000, lv_obj_get_x(obj));
 }
 
 
