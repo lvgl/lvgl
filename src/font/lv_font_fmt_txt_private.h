39345de1cd649f81b903c9701de5f14dca5e3286
Liam <30486941+liamHowatt@users.noreply.github.com>
2024-08-03 02:33:59 -0400

    fix(API): keep font struct lv_font_fmt_txt_kern_pair_t public (#6625)

--- src/font/lv_font_fmt_txt_private.h
+++ src/font/lv_font_fmt_txt_private.h
@@ -24,21 +24,6 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
-/** A simple mapping of kern values from pairs*/
-struct lv_font_fmt_txt_kern_pair_t {
-    /*To get a kern value of two code points:
-       1. Get the `glyph_id_left` and `glyph_id_right` from `lv_font_fmt_txt_cmap_t
-       2. for(i = 0; i < pair_cnt * 2; i += 2)
-             if(glyph_ids[i] == glyph_id_left &&
-                glyph_ids[i+1] == glyph_id_right)
-                 return values[i / 2];
-     */
-    const void * glyph_ids;
-    const int8_t * values;
-    uint32_t pair_cnt   : 30;
-    uint32_t glyph_ids_size : 2;    /*0: `glyph_ids` is stored as `uint8_t`; 1: as `uint16_t`*/
-};
-
 #if LV_USE_FONT_COMPRESSED
 typedef enum {
     RLE_STATE_SINGLE = 0,
