06601f3dbb7f9e5e6b5f7098903ee8b7ebdd6e8c
jaeho83.lim <jaeho83.lim@lge.com>
2024-09-03 10:45:49 +0900

    fix that CI test fail
    Change-Id: I6ed1b3d4127f9d2facf800f3d2d81d9875e84e65

--- tests/src/test_cases/libs/test_lodepng.c
+++ tests/src/test_cases/libs/test_lodepng.c
@@ -61,7 +61,7 @@ void test_lodepng_1(void)
     TEST_ASSERT_EQUAL_SCREENSHOT("libs/png_1.png");
 
     size_t mem_before = lv_test_get_free_mem();
-    for(uint32_t i = 0; i < 20; i++) {
+    for(uint32_t i = 0; i < 100; i++) {
         create_images();
 
         lv_obj_invalidate(lv_screen_active());
