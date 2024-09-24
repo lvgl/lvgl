6f3c26cc59223b7243dc95c7b6f59a34359f4b66
Cosmin-Daniel Radu <cosmindanielradu19@gmail.com>
2024-09-24 14:35:39 +0300

    fix(freertos): sync signal from isr fixed (#6793)
    Signed-off-by: Nicușor Cîțu <nicusor.citu@nxp.com>
    Signed-off-by: Cosmin-Daniel Radu <cosmin.radu_1@nxp.com>
    Co-authored-by: Nicușor Cîțu <nicusor.citu@nxp.com>

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -95,6 +95,15 @@
 #if LV_USE_OS == LV_OS_CUSTOM
     #define LV_OS_CUSTOM_INCLUDE <stdint.h>
 #endif
+#if LV_USE_OS == LV_OS_FREERTOS
+	/*
+	 * Unblocking an RTOS task with a direct notification is 45% faster and uses less RAM
+	 * than unblocking a task using an intermediary object such as a binary semaphore.
+	 *
+	 * RTOS task notifications can only be used when there is only one task that can be the recipient of the event.
+	 */
+	#define LV_USE_FREERTOS_TASK_NOTIFY 1
+#endif
 
 /*========================
  * RENDERING CONFIGURATION
