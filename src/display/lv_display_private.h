37e9b3abadcc8fcc4f7dcbdb76d8d5f721e58b39
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-09-26 11:36:48 +0200

    start

--- src/display/lv_display_private.h
+++ src/display/lv_display_private.h
@@ -63,11 +63,22 @@ struct _lv_display_t {
     /*---------------------
      * Buffering
      *--------------------*/
+    lv_draw_buf_t _static_buf1; /**< Used when user pass in a raw buffer as display draw buffer */
+    lv_draw_buf_t _static_buf2;
     lv_draw_buf_t * buf_1;
     lv_draw_buf_t * buf_2;
 
     /** Internal, used by the library*/
     lv_draw_buf_t * buf_act;
+
+
+    lv_draw_buf_t _static_frame_buf1; /**< Used when user pass in a raw buffer as display draw buffer */
+    lv_draw_buf_t _static_frame_buf2;
+    lv_draw_buf_t * frame_buf_1;
+    lv_draw_buf_t * frame_buf_2;
+
+    /** Internal, used by the library*/
+    lv_draw_buf_t * frame_buf_act;
 
     /** MANDATORY: Write the internal buffer (draw_buf) to the display. 'lv_display_flush_ready()' has to be
      * called when finished*/
@@ -108,8 +119,6 @@ struct _lv_display_t {
     /** Double buffer sync areas (redrawn during last refresh) */
     lv_ll_t sync_areas;
 
-    lv_draw_buf_t _static_buf1; /**< Used when user pass in a raw buffer as display draw buffer */
-    lv_draw_buf_t _static_buf2;
     /*---------------------
      * Layer
      *--------------------*/
