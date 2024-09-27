c6173f7a7c64ead44dcd3e853cf46cd825269131
Martin Vladic <martin.vladic@gmail.com>
2024-09-27 18:04:42 +0200

    fix get_indev in lv_group.c

--- src/core/lv_group.c
+++ src/core/lv_group.c
@@ -517,10 +517,10 @@ static lv_indev_t * get_indev(const lv_group_t * g)
     lv_indev_t * indev = lv_indev_get_next(NULL);
 
     while(indev) {
-        lv_indev_type_t indev_type = lv_indev_get_type(indev);
-        /*Prefer POINTER*/
-        if(indev_type == LV_INDEV_TYPE_POINTER) return indev;
         if(lv_indev_get_group(indev) == g) {
+            lv_indev_type_t indev_type = lv_indev_get_type(indev);
+            /*Prefer POINTER*/
+            if(indev_type == LV_INDEV_TYPE_POINTER) return indev;
             indev_guess = indev;
         }
         indev = lv_indev_get_next(indev);
