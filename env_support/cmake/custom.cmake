a33769da66def32ac2ea8ed1d95bdf77ed28be64
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-10-01 08:33:53 +0100

    chore(cmake): Support defconfig-based configs
    Adds a new CMake variable, `LV_CONF_DEFCONFIG_PATH`.
    If set to a path to a defconfig or a .config file,
    that file will be used to generate lvgl configuration.
    
    A defconfig file can be generated from a `.config` file
    using the kconfig-conf command:
    
    ```
    kconfig-conf --savedefconfig .config Kconfig
    ```

--- env_support/cmake/custom.cmake
+++ env_support/cmake/custom.cmake
@@ -8,11 +8,34 @@ option(LV_LVGL_H_INCLUDE_SIMPLE
 option(LV_CONF_INCLUDE_SIMPLE
        "Use #include \"lv_conf.h\" instead of #include \"../../lv_conf.h\"" ON)
 
-# Option LV_CONF_PATH, which should be the path for lv_conf.h
-# If set parent path LV_CONF_DIR is added to includes
-if( LV_CONF_PATH )
-    get_filename_component(LV_CONF_DIR ${LV_CONF_PATH} DIRECTORY)
-endif( LV_CONF_PATH )
+set(LV_CONF_PATH "" CACHE STRING
+    "Path to lv_conf.h.")
+set(LV_CONF_DEFCONFIG_PATH "" CACHE STRING
+    "Generate lv_conf.h from the given defconfig. If set, LV_CONF_PATH is unset and LV_CONF_SKIP is set to ON.")
+
+if(LV_CONF_DEFCONFIG_PATH)
+  set(LV_CONF_SKIP ON)
+  unset(LV_CONF_PATH)
+  set(KCONFIG_LV_CONF_DIR "${CMAKE_BINARY_DIR}/lv_kconfig_generated")
+  set(KCONFIG_LV_CONF_PATH "${KCONFIG_LV_CONF_DIR}/lv_kconfig_generated.h")
+  make_directory("${KCONFIG_LV_CONF_DIR}")
+  add_custom_command(
+    OUTPUT "${KCONFIG_LV_CONF_PATH}"
+    COMMAND
+    env KCONFIG_CONFIG='${LV_CONF_DEFCONFIG_PATH}'
+    '${LVGL_ROOT_DIR}/scripts/genconfig.py'
+    --header-path='${KCONFIG_LV_CONF_PATH}'
+    '${LVGL_ROOT_DIR}/Kconfig'
+    DEPENDS
+    "${LV_CONF_DEFCONFIG_PATH}"
+    "${LVGL_ROOT_DIR}/Kconfig"
+  )
+  add_custom_target(lv_kconfig_generate_command DEPENDS "${KCONFIG_LV_CONF_PATH}")
+  add_library(lv_kconfig_generated INTERFACE)
+  add_dependencies(lv_kconfig_generated lv_kconfig_generate_command)
+  target_compile_definitions(lv_kconfig_generated INTERFACE LV_CONF_KCONFIG_EXTERNAL_INCLUDE=<lv_kconfig_generated.h>)
+  target_include_directories(lv_kconfig_generated SYSTEM INTERFACE "${KCONFIG_LV_CONF_DIR}")
+endif()
 
 # Option to build shared libraries (as opposed to static), default: OFF
 option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
@@ -31,9 +54,15 @@ target_compile_definitions(
   lvgl PUBLIC $<$<BOOL:${LV_LVGL_H_INCLUDE_SIMPLE}>:LV_LVGL_H_INCLUDE_SIMPLE>
               $<$<BOOL:${LV_CONF_INCLUDE_SIMPLE}>:LV_CONF_INCLUDE_SIMPLE>)
 
-# Add definition of LV_CONF_PATH only if needed
+if(LV_CONF_DEFCONFIG_PATH)
+  target_link_libraries(lvgl PUBLIC lv_kconfig_generated)
+endif()
+
+# Add definition and include directory of LV_CONF_PATH only if needed
 if(LV_CONF_PATH)
   target_compile_definitions(lvgl PUBLIC LV_CONF_PATH=${LV_CONF_PATH})
+  get_filename_component(LV_CONF_DIR "${LV_CONF_PATH}" DIRECTORY)
+  target_include_directories(lvgl SYSTEM PUBLIC ${LV_CONF_DIR})
 endif()
 
 # Add definition of LV_CONF_SKIP only if needed
@@ -41,9 +70,7 @@ if(LV_CONF_SKIP)
   target_compile_definitions(lvgl PUBLIC LV_CONF_SKIP=1)
 endif()
 
-# Include root and optional parent path of LV_CONF_PATH
-target_include_directories(lvgl SYSTEM PUBLIC ${LVGL_ROOT_DIR} ${LV_CONF_DIR} ${CMAKE_CURRENT_BINARY_DIR})
-
+target_include_directories(lvgl SYSTEM PUBLIC ${LVGL_ROOT_DIR} ${CMAKE_CURRENT_BINARY_DIR})
 
 if(NOT LV_CONF_BUILD_DISABLE_THORVG_INTERNAL)
     add_library(lvgl_thorvg ${THORVG_SOURCES})
@@ -78,6 +105,10 @@ endif()
 file(GLOB LVGL_PUBLIC_HEADERS
     "${LVGL_ROOT_DIR}/lvgl.h"
     "${LVGL_ROOT_DIR}/lv_version.h")
+
+if(LV_CONF_DEFCONFIG_PATH)
+  list(APPEND LVGL_PUBLIC_HEADERS "${KCONFIG_LV_CONF_PATH}")
+endif()
 
 if(NOT LV_CONF_SKIP)
   if (LV_CONF_PATH)
