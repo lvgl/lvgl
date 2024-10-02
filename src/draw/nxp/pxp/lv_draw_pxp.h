37f41947f39d4ae61e20cd6829ea557d7ff29ef5
Cristian Stoica <cristianmarian.stoica@nxp.com>
2024-10-02 11:36:34 +0300

    fix(pxp,vglite) extend base structure to create vglite and pxp draw units
    This reflects the technical separation between SW and GPU units
    
    Signed-off-by: Cristian Stoica <cristianmarian.stoica@nxp.com>

--- src/draw/nxp/pxp/lv_draw_pxp.h
+++ src/draw/nxp/pxp/lv_draw_pxp.h
@@ -35,7 +35,16 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
-typedef lv_draw_sw_unit_t lv_draw_pxp_unit_t;
+typedef struct lv_draw_pxp_unit_t {
+    lv_draw_unit_t base_unit;
+    lv_draw_task_t * task_act;
+#if LV_USE_OS
+    lv_thread_sync_t sync;
+    lv_thread_t thread;
+    volatile bool inited;
+    volatile bool exit_status;
+#endif
+} lv_draw_pxp_unit_t;
 
 /**********************
  * GLOBAL PROTOTYPES
