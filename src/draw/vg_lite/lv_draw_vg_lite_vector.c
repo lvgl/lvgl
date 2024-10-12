4007df1128c88adf2ad6d2ce7199ed92971c63ee
pengyiqiang <pengyiqiang@xiaomi.com>
2024-10-12 14:24:07 +0800

    fix(vg_lite): fix thorvg 32bit rendering test coordinate calculation overflow
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/draw/vg_lite/lv_draw_vg_lite_vector.c
+++ src/draw/vg_lite/lv_draw_vg_lite_vector.c
@@ -25,9 +25,19 @@
  *      DEFINES
  *********************/
 
-/* Since thorvg has an upper limit on coordinates, choose a suitable value here */
-#define PATH_COORD_MAX (1 << 18)
-#define PATH_COORD_MIN (-PATH_COORD_MAX)
+#if LV_USE_VG_LITE_THORVG
+    /**
+    * It is found that thorvg cannot handle large coordinates well.
+    * When the coordinates are larger than 4096, the calculation of tvgSwRle module will overflow in 32-bit system.
+    * So we use FLT_MAX and FLT_MIN to write the mark to bonding_box to tell vg_lite_tvg not to add clip path to the current path.
+    */
+    #define PATH_COORD_MAX FLT_MAX
+    #define PATH_COORD_MIN FLT_MIN
+#else
+    /*  18 bits is enough to represent the coordinates of path bounding box */
+    #define PATH_COORD_MAX (1 << 18)
+    #define PATH_COORD_MIN (-PATH_COORD_MAX)
+#endif
 
 /**********************
  *      TYPEDEFS
