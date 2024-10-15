f1a3ddf147410254ac3cae4f92b88b7c048ebe77
Cristian Stoica <cristianmarian.stoica@nxp.com>
2024-10-15 14:11:18 +0300

    fix(sysmon) do not record refresh event data if there are no redraws
    This avoids calculating the average the FPS during a
    LV_SYSMON_REFR_PERIOD_DEF interval when some frames are rendered and
    others are not.
    
    Signed-off-by: Cristian Stoica <cristianmarian.stoica@nxp.com>

--- src/others/sysmon/lv_sysmon.c
+++ src/others/sysmon/lv_sysmon.c
@@ -190,8 +190,14 @@ static void perf_monitor_disp_event_cb(lv_event_t * e)
             info->measured.refr_start = lv_tick_get();
             break;
         case LV_EVENT_REFR_READY:
-            info->measured.refr_elaps_sum += lv_tick_elaps(info->measured.refr_start);
             info->measured.refr_cnt++;
+            /* For FPS purposes do not record a refresh event if there was no redraw */
+            if(info->measured.refr_cnt == info->measured.render_cnt) {
+                info->measured.refr_elaps_sum += lv_tick_elaps(info->measured.refr_start);
+            }
+            else {
+                info->measured.refr_cnt--;
+            }
             break;
         case LV_EVENT_RENDER_START:
             info->measured.render_in_progress = 1;
