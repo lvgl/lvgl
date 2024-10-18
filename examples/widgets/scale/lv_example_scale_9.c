9416cc46be4d3f0434765e5d6351cb0477963b33
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-10-18 20:43:40 +0200

    minor adjustments on the examples

--- examples/widgets/scale/lv_example_scale_9.c
+++ examples/widgets/scale/lv_example_scale_9.c
@@ -7,14 +7,14 @@
 void lv_example_scale_9(void)
 {
     lv_obj_t * scale = lv_scale_create(lv_screen_active());
-    lv_obj_set_size(scale, lv_pct(80), 100);
+    lv_obj_set_size(scale, 200, 100);
     lv_scale_set_mode(scale, LV_SCALE_MODE_HORIZONTAL_BOTTOM);
     lv_obj_center(scale);
 
     lv_scale_set_label_show(scale, true);
-    lv_obj_set_style_transform_rotation(scale, 200, LV_PART_INDICATOR);
+    lv_obj_set_style_transform_rotation(scale, 450, LV_PART_INDICATOR);
     lv_obj_set_style_length(scale, 30, LV_PART_INDICATOR);
-    lv_obj_set_style_translate_x(scale, -10, LV_PART_INDICATOR);
+    lv_obj_set_style_translate_x(scale, 5, LV_PART_INDICATOR);
 
     lv_scale_set_total_tick_count(scale, 31);
     lv_scale_set_major_tick_every(scale, 5);
