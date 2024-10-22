f89cfebd33e06f4135122ccd3a263e70ccef8cc2
kurisaw <2053731441@qq.com>
2024-10-22 10:51:10 +0800

    Fix ebike demo header file inclusion issue

--- demos/ebike/generated/font_ebike_56.c
+++ demos/ebike/generated/font_ebike_56.c
@@ -4,7 +4,15 @@
  * Opts:
  ******************************************************************************/
 
-#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#ifdef __has_include
+    #if __has_include("lvgl.h")
+        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
+            #define LV_LVGL_H_INCLUDE_SIMPLE
+        #endif
+    #endif
+#endif
+
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
     #include "lvgl.h"
 #else
     #include "lvgl/lvgl.h"
