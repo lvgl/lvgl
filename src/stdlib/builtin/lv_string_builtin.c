784a6f02d2e133f9cdc746231feae7eab6db25fb
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2024-10-25 20:16:31 +0200

    fix(roller): handle the case when len is 0

--- src/stdlib/builtin/lv_string_builtin.c
+++ src/stdlib/builtin/lv_string_builtin.c
@@ -234,6 +234,10 @@ int lv_strcmp(const char * s1, const char * s2)
 
 int lv_strncmp(const char * s1, const char * s2, size_t len)
 {
+    if(len == 0) {
+        return 0;
+    }
+
     while(len > 0 && *s1 && (*s1 == *s2)) {
         if(--len == 0) {
             return 0;
