72fdfc10620c4966974731a3fde68abb80fe356f
lizhaoming <13678462+lizhao-ming@user.noreply.gitee.com>
2024-10-29 22:11:06 +0800

    src(scale):add scale arc_image_src setting

--- src/widgets/scale/lv_scale.c
+++ src/widgets/scale/lv_scale.c
@@ -1303,12 +1303,22 @@ static void scale_set_arc_properties(lv_obj_t * obj, lv_draw_arc_dsc_t * arc_dsc
         else {
             arc_dsc->rounded = lv_obj_get_style_arc_rounded(obj, LV_PART_MAIN);
         }
+
+        /* arc image src */
+        res = lv_style_get_prop(section_style, LV_STYLE_ARC_IMAGE_SRC, &value);
+        if(res == LV_STYLE_RES_FOUND) {
+            arc_dsc->img_src = (const void *)value.ptr;
+        }
+        else {
+            arc_dsc->img_src = lv_obj_get_style_arc_image_src(obj, LV_PART_MAIN);
+        }
     }
     else {
         arc_dsc->color = lv_obj_get_style_arc_color(obj, LV_PART_MAIN);
         arc_dsc->opa = lv_obj_get_style_arc_opa(obj, LV_PART_MAIN);
         arc_dsc->width = lv_obj_get_style_arc_width(obj, LV_PART_MAIN);
         arc_dsc->rounded = lv_obj_get_style_arc_rounded(obj, LV_PART_MAIN);
+        arc_dsc->img_src = lv_obj_get_style_arc_image_src(obj, LV_PART_MAIN);
     }
 }
 
