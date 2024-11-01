71ce2743b021bea893b08a387cd3e23398bd651e
Ana Grad <ana.grad@nxp.com>
2024-11-01 11:54:29 +0200

    feat(g2d) Add clean invalidate cache
    Signed-off-by: Ana Grad <ana.grad@nxp.com>

--- src/draw/nxp/g2d/lv_draw_buf_g2d.c
+++ src/draw/nxp/g2d/lv_draw_buf_g2d.c
@@ -37,6 +37,8 @@ static void * _buf_malloc(size_t size_bytes, lv_color_format_t color_format);
 
 static void _buf_free(void * buf);
 
+static void _invalidate_cache(const lv_draw_buf_t * draw_buf, const lv_area_t * area);
+
 /**********************
  *  STATIC VARIABLES
  **********************/
@@ -55,6 +57,7 @@ void lv_draw_buf_g2d_init_handlers(void)
 
     handlers->buf_malloc_cb = _buf_malloc;
     handlers->buf_free_cb = _buf_free;
+    handlers->invalidate_cache_cb = _invalidate_cache;
 }
 
 /**********************
@@ -63,16 +66,25 @@ void lv_draw_buf_g2d_init_handlers(void)
 
 static void * _buf_malloc(size_t size_bytes, lv_color_format_t color_format)
 {
-    struct g2d_buf* buf = g2d_alloc(size_bytes, 1);
+    LV_UNUSED(color_format);
+
+    size_bytes += LV_DRAW_BUF_ALIGN - 1;
+    struct g2d_buf * buf = g2d_alloc(size_bytes, 1);
     lv_insert_buf_map(buf->buf_vaddr, buf);
     return buf->buf_vaddr;
 }
 
 static void _buf_free(void * buf)
 {
-    struct g2d_buf * buf_g2d = lv_search_buf_map(buf);
-    g2d_free(buf_g2d);
+    lv_free_item(buf);
 }
 
+static void _invalidate_cache(const lv_draw_buf_t * draw_buf, const lv_area_t * area)
+{
+
+    LV_UNUSED(area);
+    struct g2d_buf * buf = lv_search_buf_map(draw_buf->data);
+    g2d_cache_op(buf, G2D_CACHE_FLUSH);
+}
 
 #endif /*LV_USE_DRAW_G2D*/
