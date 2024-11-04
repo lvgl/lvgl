ae2689ac1c3ae032dfee336f811df72ca0d1b84d
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-11-04 18:28:56 +0100

    restructuring

--- src/drivers/display/nxp_elcdif/lv_nxp_elcdif.h
+++ src/drivers/display/nxp_elcdif/lv_nxp_elcdif.h
@@ -30,27 +30,14 @@ extern "C" {
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
-/**
- * API to create display for NXP processors by default. Sets internal buffers and callbacks and everything accordingly
- * @param config : NXP eLCD config
- * @param frame_buffer1 : handle to buffer 1
- * @param frame_buffer2 : handle to buffer 2
- * @param buf_size : frame buffer size in bytes
- * @param mode : LV_DISPLAY_RENDER_MODE (FULL, PARTIAL, DIRECT)
- * @param init_cb : LCD init callback : HW dependent
- */
-lv_display_t * lv_nxp_display_elcdif_create(elcdif_rgb_mode_config_t config, void * frame_buffer1,
-                                            void * frame_buffer2, size_t buf_size, lv_display_render_mode_t mode,
-                                            void (init_cb)(void), lv_display_flush_cb_t flush_cb, lv_display_flush_wait_cb_t wait_cb);
-
 
 /**
- * API to create a display in direct mode. Does NOT set the callbacks and does not initialize the LCD.
- * This function is pure LVGL
- * @param config : NXP eLCD config object
- * @param frame_buffer1 : handle to buffer 1
- * @param frame_buffer2 : handle to buffer 2
- * @param buf_size : buffer size in bytes
+ * Attach LVGL to ELCDIF using DIRECT rendering mode.
+ * ELCDIF should be already initialized.
+ * @param config            NXP eLCD config object
+ * @param frame_buffer1     pointer the first frame buffers
+ * @param frame_buffer2     pointer the second frame buffers
+ * @param buf_size          size of a buffer in bytes
  */
 lv_display_t * lv_nxp_display_elcdif_create_direct(elcdif_rgb_mode_config_t config, void * frame_buffer1,
                                                    void * frame_buffer2, size_t buf_size);
