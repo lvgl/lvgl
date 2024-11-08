4454ee3597a3b6727a14e691ee86c2a4172b3ead
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-11-08 04:10:24 +0100

    fix CI

--- tests/src/test_cases/draw/test_draw_layer.c
+++ tests/src/test_cases/draw/test_draw_layer.c
@@ -48,6 +48,7 @@ void test_draw_layer_bitmap_mask_from_variable(void)
     test_obj_create(&test_image_cogwheel_a8, 80, 600);
 
 
+#ifndef NON_AMD64_BUILD
     TEST_ASSERT_EQUAL_SCREENSHOT("draw/draw_layer_bitmap_mask.png");
 }
 
@@ -62,7 +63,9 @@ void test_draw_layer_bitmap_mask_from_image(void)
     test_obj_create(mask_src, LV_SCALE_NONE, 200);
     test_obj_create(mask_src, 80, 600);
 
+#ifndef NON_AMD64_BUILD
     TEST_ASSERT_EQUAL_SCREENSHOT("draw/draw_layer_bitmap_mask.png");
+#endif
 }
 
 
