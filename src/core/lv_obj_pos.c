lvgl/lvgl

commit 58e2d508e1c6ac9cb45da9566aef5d41776c0d29

perf(draw): improve execute time of obj's transforming (#6638)
Co-authored-by: jaeho83.lim <jaeho83.lim@lge.com>

src/core/lv_obj_pos.c
@@ -33,6 +33,7 @@ static int32_t calc_content_width(lv_obj_t * obj);
 static int32_t calc_content_height(lv_obj_t * obj);
 static void layout_update_core(lv_obj_t * obj);
 static void transform_point_array(const lv_obj_t * obj, lv_point_t * p, size_t p_count, bool inv);
+static bool is_transformed(const lv_obj_t * obj);
 
 /**********************
  *  STATIC VARIABLES
@@ -878,7 +879,12 @@ bool lv_obj_area_is_visible(const lv_obj_t * obj, lv_area_t * area)
     /*The area is not on the object*/
     if(!lv_area_intersect(area, area, &obj_coords)) return false;
 
-    lv_obj_get_transformed_area(obj, area, LV_OBJ_POINT_TRANSFORM_FLAG_RECURSIVE);
+    if(!is_transformed(obj)) {
+        *area = obj->coords;
+    }
+    else {
+        lv_obj_get_transformed_area(obj, area, LV_OBJ_POINT_TRANSFORM_FLAG_RECURSIVE);
+    }
 
     /*Truncate recursively to the parents*/
     lv_obj_t * parent = lv_obj_get_parent(obj);
@@ -893,7 +899,12 @@ bool lv_obj_area_is_visible(const lv_obj_t * obj, lv_area_t * area)
             lv_area_increase(&parent_coords, parent_ext_size, parent_ext_size);
         }
 
-        lv_obj_get_transformed_area(parent, &parent_coords, LV_OBJ_POINT_TRANSFORM_FLAG_RECURSIVE);
+        if(!is_transformed(parent)) {
+            parent_coords = parent->coords;
+        }
+        else {
+            lv_obj_get_transformed_area(parent, &parent_coords, LV_OBJ_POINT_TRANSFORM_FLAG_RECURSIVE);
+        }
         if(!lv_area_intersect(area, area, &parent_coords)) return false;
 
         parent = lv_obj_get_parent(parent);
@@ -975,6 +986,15 @@ void lv_obj_center(lv_obj_t * obj)
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+
+static bool is_transformed(const lv_obj_t * obj)
+{
+    while(obj) {
+        if(obj->spec_attr && obj->spec_attr->layer_type == LV_LAYER_TYPE_TRANSFORM) return true;
+        obj = obj->parent;
+    }
+    return false;
+}
 
 static int32_t calc_content_width(lv_obj_t * obj)
 {

commit 1f84cba49ae5535846a714e9cd18065223c4321f

fix(draw):  fix bug introduced by PR #6638
PR #6638 introduced a bug that caused issue #7230.  Since
I did not see a fix already submitted for it, I am submitting it
here.  This fix is tested under `lvgl/lv_port_pc_visual_studio`
and it resolves both the issue seen with the Knob as well as
with the Roller Widget display updates after the Widget's
value has been changed.

Resolves #7230

src/core/lv_obj_pos.c
@@ -880,7 +880,7 @@ bool lv_obj_area_is_visible(const lv_obj_t * obj, lv_area_t * area)
     if(!lv_area_intersect(area, area, &obj_coords)) return false;
 
     if(!is_transformed(obj)) {
-        *area = obj->coords;
+        *area = obj_coords;
     }
     else {
         lv_obj_get_transformed_area(obj, area, LV_OBJ_POINT_TRANSFORM_FLAG_RECURSIVE);