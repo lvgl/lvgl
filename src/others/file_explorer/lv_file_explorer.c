62d9f10f61d9d81e6fb7575e53f6c67d63fb1a15
Liu Yi <lhdjply@126.com>
2024-11-13 17:48:04 +0800

    fix(file_exploer): fix path issues when returning to open other folders after opening a certain folder (#7258)
    Signed-off-by: lhdjply <lhdjply@126.com>

--- src/others/file_explorer/lv_file_explorer.c
+++ src/others/file_explorer/lv_file_explorer.c
@@ -619,7 +619,7 @@ static void show_dir(lv_obj_t * obj, const char * path)
     /*Move the table to the top*/
     lv_obj_scroll_to_y(explorer->file_table, 0, LV_ANIM_OFF);
 
-    lv_strlcpy(explorer->current_path, path, sizeof(explorer->current_path));
+    lv_strncpy(explorer->current_path, path, sizeof(explorer->current_path));
     lv_label_set_text_fmt(explorer->path_label, LV_SYMBOL_EYE_OPEN" %s", path);
 
     size_t current_path_len = lv_strlen(explorer->current_path);
