99991550625e84f5f69a3d1832e3d4c4dbb28c42
Victor Wheeler <vaw_info998@comcast.net>
2024-11-15 11:43:37 -0700

    feat(code-format):  2 enhancements
    - code-format.cfg doesn't work on Windows because it edits
      every single source file and changes the line endings.  Git,
      perceives this as a change, and makes Windows users have to
      RESET 672 files just in the ./src/ directory and I didn't count the
      number in the other directories.  Resetting files can be dangerous
      to work in progress.  Especially when it involves that many files.
    
      Rationale:  Git -- out of the box, with default configuration -- automatically
      stores source files in its repository with LF-only (Linux) line endings.
      On Windows, each time a file content moves from repository to
      working directory, its line endings are converted to the line endings
      native to the platform it is running on.  And each time file content
      is moved from working directory to repository, the line endings are
      converted back into LF-only (Linux).  This is a desirable trait and
      works very well when Windows and Linux users share source files.
      This change follows in that spirit and makes it so `AStyle` does NOT
      try to edit all source files -- only the files that need formatting.
      I believe this follows the spirit of how it was meant to work.
    
    - code-format.py -- added some command-line argument options
      that allow `AStyle` to be limited to only certain directories if one
      already knows that's where it should look.  This saves time -- important
      when time is precious.  Simultaneously, giving it no arguments makes
      it run on all 4 directories as it did before.  Normal users, or anyone
      running this from a script will see no change.

--- scripts/code-format.py
+++ scripts/code-format.py
@@ -1,19 +1,58 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+
+# Argument enhancement:  to only run `astyle` on a specified directory, to
+# only include changed source code, these arguments have been added.  If
+# run with no arguments, all the normal directories are examined as before:
+# - /demos/
+# - /examples/
+# - /src/
+# - /tests/
+#
+# Args:
+# If ANY args are specified, ONLY run `astyle` on the specified directories.
+# Any combination can be specified.
+include_demos = True
+include_examples = True
+include_src = True
+include_tests = True
+
+args = sys.argv[1:]
+
+# Have any args been specified?
+if args:
+    include_demos = False
+    include_examples = False
+    include_src = False
+    include_tests = False
+
+    if "demos" in args:
+        include_demos = True
+    if "examples" in args:
+        include_examples = True
+    if "src" in args:
+        include_src = True
+    if "tests" in args:
+        include_tests = True
 
 script_dir = os.path.realpath(__file__)
 script_dir = os.path.dirname(script_dir)
 cfg_file = os.path.join(script_dir, 'code-format.cfg')
 
-print("\nFormatting demos")
-os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../demos/*.c,*.cpp,*.h"')
+if include_demos:
+    print("\nFormatting demos")
+    os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../demos/*.c,*.cpp,*.h"')
 
-print("\nFormatting examples")
-os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../examples/*.c,*.cpp,*.h"')
+if include_examples:
+    print("\nFormatting examples")
+    os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../examples/*.c,*.cpp,*.h"')
 
-print("Formatting src")
-os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../src/*.c,*.cpp,*.h"')
+if include_src:
+    print("Formatting src")
+    os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../src/*.c,*.cpp,*.h"')
 
-print("\nFormatting tests")
-os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../tests/*.c,*.cpp,*.h"')
+if include_tests:
+    print("\nFormatting tests")
+    os.system(f'astyle --options={cfg_file} --recursive "{script_dir}/../tests/*.c,*.cpp,*.h"')
