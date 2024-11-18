9eb0ed150557bfc871438ff24c640c2434012c81
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-11-18 12:21:17 +0100

    fix(ebike): add guards to the ebike demo translations (#7319)

--- demos/ebike/translations/lv_i18n.h
+++ demos/ebike/translations/lv_i18n.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include "../../../src/lv_conf_internal.h"
+#if LV_USE_DEMO_EBIKE
 
 #include LV_STDINT_INCLUDE
 
@@ -78,6 +79,7 @@ void __lv_i18n_reset(void);
 #define _(text) lv_i18n_get_text(text)
 #define _p(text, num) lv_i18n_get_text_plural(text, num)
 
+#endif /*LV_USE_DEMO_EBIKE*/
 
 #ifdef __cplusplus
 } /* extern "C" */
