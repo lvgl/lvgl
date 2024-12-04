2a1bc554773c86ab93b7c557a22a5862e07e5fa4
pengyiqiang <pengyiqiang@xiaomi.com>
2024-12-04 20:44:27 +0800

    Add temporary buffer comment
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/draw/lv_draw_label.c
+++ src/draw/lv_draw_label.c
@@ -593,6 +593,9 @@ void lv_draw_unit_draw_letter(lv_draw_task_t * t, lv_draw_glyph_dsc_t * dsc,  co
 
     if(g.resolved_font) {
 #if LV_FONT_FMT_TXT_CACHE_GLYPH_CNT == 0
+        /* When using the built-in font cache, there is no need to use a temporary buffer to store converted font images.
+         * The built-in font cache will manage the cache lifecycle by itself to minimize copying and image conversion.
+         */
         lv_draw_buf_t * draw_buf = NULL;
         if(LV_FONT_GLYPH_FORMAT_NONE < g.format && g.format < LV_FONT_GLYPH_FORMAT_IMAGE) {
             /*Only check draw buf for bitmap glyph*/
