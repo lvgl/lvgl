0b53ca6013bf863c5b76e58de5d1324cb0ce1537
Victor Wheeler <vaw_info998@comcast.net>
2024-12-16 15:55:24 -0700

    feat(anim):  update references to `lv_anim_set_time()`...
    ...to `lv_anim_set_duration()`

--- src/widgets/lottie/lv_lottie.c
+++ src/widgets/lottie/lv_lottie.c
@@ -125,7 +125,7 @@ void lv_lottie_set_src_data(lv_obj_t * obj, const void * src, size_t src_size)
 
     float f_total;
     tvg_animation_get_total_frame(lottie->tvg_anim, &f_total);
-    lv_anim_set_time(lottie->anim, (int32_t)f_total * 1000 / 60); /*60 FPS*/
+    lv_anim_set_duration(lottie->anim, (int32_t)f_total * 1000 / 60); /*60 FPS*/
     lottie->anim->act_time = 0;
     lottie->anim->end_value = (int32_t)f_total;
     lottie->anim->reverse_play_in_progress = false;
@@ -143,7 +143,7 @@ void lv_lottie_set_src_file(lv_obj_t * obj, const char * src)
 
     float f_total;
     tvg_animation_get_total_frame(lottie->tvg_anim, &f_total);
-    lv_anim_set_time(lottie->anim, (int32_t)f_total * 1000 / 60); /*60 FPS*/
+    lv_anim_set_duration(lottie->anim, (int32_t)f_total * 1000 / 60); /*60 FPS*/
     lottie->anim->act_time = 0;
     lottie->anim->end_value = (int32_t)f_total;
     lottie->anim->reverse_play_in_progress = false;
