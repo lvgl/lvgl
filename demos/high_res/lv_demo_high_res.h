581b039ac02479236d236a4da64c073ea6ec3eb4
liamHowatt <liamjmh0@gmail.com>
2025-01-06 18:07:46 -0500

    rename more LV_FS_DEFAULT_DRIVE_LETTER

--- demos/high_res/lv_demo_high_res.h
+++ demos/high_res/lv_demo_high_res.h
@@ -85,7 +85,7 @@ typedef void (*lv_demo_high_res_exit_cb_t)(lv_demo_high_res_api_t * api);
  * Start the High Resolution Demo on the default display, on the active screen.
  * This demo requires `LV_USE_DEMO_HIGH_RES` and `LV_FONT_FMT_TXT_LARGE`
  * to be enabled as well as a filesystem driver to be configured and the
- * `LV_FS_DEFAULT_DRIVE_LETTER` set. The display size should be
+ * `LV_FS_DEFAULT_DRIVER_LETTER` set. The display size should be
  * 800x480, 1280x720, or 1920x1080.
  * @param assets_path       Folder where the image assets are.
  *                          If `NULL`, "lvgl/demos/high_res/assets/" will be used.
@@ -109,7 +109,7 @@ lv_demo_high_res_api_t * lv_demo_high_res(const char * assets_path,
  * Start the High Resolution Demo on the default display, on the active screen.
  * This demo requires `LV_USE_DEMO_HIGH_RES` and `LV_FONT_FMT_TXT_LARGE`
  * to be enabled as well as a filesystem driver to be configured and the
- * `LV_FS_DEFAULT_DRIVE_LETTER` set. The display size should be
+ * `LV_FS_DEFAULT_DRIVER_LETTER` set. The display size should be
  * 800x480, 1280x720, or 1920x1080.
  * @param assets_path       Folder where the image assets are.
  *                          If `NULL`, "lvgl/demos/high_res/assets/" will be used.
