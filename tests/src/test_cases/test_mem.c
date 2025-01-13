56dee3c40a6dc5d7f144b6df7c2bb8c7d4bf128f
Saeed Poorjandaghi <saeed.poorjandaghi@gmail.com>
2025-01-13 13:12:01 -0800

    Update test_mem.c with unaligned memcpy test function
    #7406

--- tests/src/test_cases/test_mem.c
+++ tests/src/test_cases/test_mem.c
@@ -24,4 +24,28 @@ void test_mem_buf_realloc(void)
 #endif
 }
 
+/* #7573: Test memcpy with unaligned addresses */
+void test_memcpy_unaligned(void)
+{
+    uint8_t source[20];
+    uint8_t destination[20];
+
+    /* Initialize source with some values */
+    for (int i = 0; i < 20; i++) {
+        source[i] = (uint8_t)(i + 1);
+    }
+
+    /* Use unaligned addresses by offsetting by 1 */
+    uint8_t *unaligned_source = source + 1;
+    uint8_t *unaligned_destination = destination + 1;
+
+    /* Perform memcpy */
+    lv_memcpy(unaligned_destination, unaligned_source, 18);
+
+    /* Verify that the copied values match the source */
+    for (int i = 0; i < 18; i++) {
+        TEST_ASSERT_EQUAL_UINT8(unaligned_source[i], unaligned_destination[i]);
+    }
+}
+
 #endif
