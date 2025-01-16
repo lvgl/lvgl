769aade4e6cd6568ff7e686646a4d6d01419b7ec
Saeed Poorjandaghi <saeed.poorjandaghi@gmail.com>
2025-01-16 10:09:30 -0800

    Ensure 4-byte alignment in test_memcpy_unaligned using a union
    #7406

--- tests/src/test_cases/test_mem.c
+++ tests/src/test_cases/test_mem.c
@@ -27,23 +27,25 @@ void test_mem_buf_realloc(void)
 /* #7573: Test memcpy with unaligned addresses */
 void test_memcpy_unaligned(void)
 {
-    uint8_t source[20];
-    uint8_t destination[20];
+    union {
+        uint8_t u8[20];
+        uint32_t dummy_u32;
+    } source, destination;
 
     /* Initialize source with some values */
     for (int i = 0; i < 20; i++) {
-        source[i] = (uint8_t)(i + 1);
+        source.u8[i] = (uint8_t)(i + 1);
     }
 
-    /* Use unaligned addresses by offsetting by 1 */
-    uint8_t *unaligned_source = source + 1;
-    uint8_t *unaligned_destination = destination + 1;
+    /* Ensure unaligned addresses */
+    uint8_t *unaligned_source = source.u8 + 1;
+    uint8_t *unaligned_destination = destination.u8 + 1;
 
-    /* Perform memcpy */
-    lv_memcpy(unaligned_destination, unaligned_source, 18);
+    /* Perform lv_memcpy */
+    lv_memcpy(unaligned_destination, unaligned_source, 19);
 
     /* Verify that the copied values match the source */
-    for (int i = 0; i < 18; i++) {
+    for (int i = 0; i < 19; i++) {
         TEST_ASSERT_EQUAL_UINT8(unaligned_source[i], unaligned_destination[i]);
     }
 }
