c6ba330f6e051296c76c3adcec6a948078c425c7
André <andre_miguel_costa@hotmail.com>
2025-01-23 22:53:47 +0100

    chore: regenerate lv_conf_internal

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -66,11 +66,6 @@
     #endif
 #endif
 
-/* Renamed config backwards-compatibility */
-#if !defined(LV_FS_DEFAULT_DRIVER_LETTER) && defined(LV_FS_DEFAULT_DRIVE_LETTER)
-    #define LV_FS_DEFAULT_DRIVER_LETTER LV_FS_DEFAULT_DRIVE_LETTER
-#endif
-
 #ifdef CONFIG_LV_COLOR_DEPTH
     #define LV_KCONFIG_PRESENT
 #endif
