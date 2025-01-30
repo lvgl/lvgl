0861a62f9e58ad0349bfd30468ab6eb39e27b441
André <andre_miguel_costa@hotmail.com>
2025-01-30 10:37:06 +0100

    style: fix formatting

--- src/osal/lv_linux_private.h
+++ src/osal/lv_linux_private.h
@@ -29,16 +29,16 @@ extern "C" {
 
 
 typedef union {
-	struct {
-            /*
-             *  We ignore the iowait column as it's not reliable
-             *  We ignore the guest and guest_nice columns because they're accounted
-             *   for in user and nice respectively
-             */
-		uint32_t user, nice, system, idle, /*iowait,*/ irq, softirq,
-			steal /*, guest, guest_nice*/;
-	} fields;
-	uint32_t buffer[LV_PROC_STAT_PARAMS_LEN];
+    struct {
+        /*
+         *  We ignore the iowait column as it's not reliable
+         *  We ignore the guest and guest_nice columns because they're accounted
+         *   for in user and nice respectively
+         */
+        uint32_t user, nice, system, idle, /*iowait,*/ irq, softirq,
+                 steal /*, guest, guest_nice*/;
+    } fields;
+    uint32_t buffer[LV_PROC_STAT_PARAMS_LEN];
 } lv_proc_stat_t;
 
 /**********************
