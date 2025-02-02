93fb49a49df02a5a3025edfc95c90099b0ead178
André <andre_miguel_costa@hotmail.com>
2025-02-02 17:39:30 +0100

    build: export kconfig keys to .h file

--- cmake/kconfig.cmake
+++ cmake/kconfig.cmake
@@ -1,1 +1,18 @@
-import_kconfig(CONFIG_ ${DOTCONFIG})
+import_kconfig(CONFIG_ ${DOTCONFIG} keys)
+
+set(CONFIG_HEADER "${CMAKE_BINARY_DIR}/config.h")
+
+# Create the header file content
+file(WRITE ${AUTOCONF_H} "/* Auto-generated config.h */\n")
+
+foreach(key ${keys})
+    string(REPLACE "CONFIG_" "" stripped_key ${key})
+    set(value ${${key}})
+    if("${value}" STREQUAL "y")
+        set(value 1)
+    elseif("${value}" STREQUAL "n")
+        set(value 0)
+    endif()
+
+    file(APPEND ${AUTOCONF_H} "#define ${stripped_key} ${${value}}\n")
+endforeach()
