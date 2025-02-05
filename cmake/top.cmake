862187f438fcdf0f97714f567dd466be7a83e4b9
André <andre_miguel_costa@hotmail.com>
2025-02-05 23:06:53 +0100

    feat: add output paths

--- cmake/top.cmake
+++ cmake/top.cmake
@@ -1,6 +1,9 @@
 set(PROJECT_ROOT ${CMAKE_SOURCE_DIR})
 set(KCONFIG_ROOT ${CMAKE_SOURCE_DIR}/Kconfig)
 set(AUTOCONF_H ${CMAKE_CURRENT_BINARY_DIR}/autoconf.h)
+set(OUTPUT_DOTCONFIG ${CMAKE_CURRENT_BINARY_DIR}/.config)
+set(KCONFIG_LIST_OUT ${CMAKE_CURRENT_BINARY_DIR}/kconfig_list)
+set(AUTO_CONF_DIR ${CMAKE_CURRENT_BINARY_DIR})
 set(DOTCONFIG ${CMAKE_SOURCE_DIR}/.config)
 
 # Re-configure (Re-execute all CMakeLists.txt code) when autoconf.h changes
