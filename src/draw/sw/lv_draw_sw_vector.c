95720601c522d80bef246c62177ae37294798180
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-02-13 22:36:38 +0100

    feat(thorvg): use LVGL's malloc/realloc/zalloc/free

--- src/draw/sw/lv_draw_sw_vector.c
+++ src/draw/sw/lv_draw_sw_vector.c
@@ -428,7 +428,7 @@ void lv_draw_sw_vector(lv_draw_task_t * t, const lv_draw_vector_task_dsc_t * dsc
     int32_t height = lv_area_get_height(&layer->buf_area) - 1;
     uint32_t stride = draw_buf->header.stride;
     Tvg_Canvas * canvas = tvg_swcanvas_create();
-    tvg_swcanvas_set_target(canvas, buf, stride / 4, width, height, TVG_COLORSPACE_ARGB8888);
+    tvg_swcanvas_set_target(canvas, buf, stride / 4, width, height, TVG_COLORSPACE_ARGB8888S);
 
     _tvg_rect rc;
     lv_area_to_tvg(&rc, &t->clip_area);
