927d5ad1829977c733a9a84325ed613e886592ff
yushuailong <yyyusl@qq.com>
2025-02-23 19:56:06 +0800

    fix(obj): modify style_cnt to uint16_t
    Signed-off-by: yushuailong <yushuailong1@xiaomi.com>

--- src/core/lv_obj_private.h
+++ src/core/lv_obj_private.h
@@ -65,11 +65,11 @@ struct _lv_obj_t {
     lv_area_t coords;
     lv_obj_flag_t flags;
     lv_state_t state;
+    uint16_t style_cnt;
     uint16_t layout_inv : 1;
     uint16_t readjust_scroll_after_layout : 1;
     uint16_t scr_layout_inv : 1;
     uint16_t skip_trans : 1;
-    uint16_t style_cnt  : 6;
     uint16_t h_layout   : 1;
     uint16_t w_layout   : 1;
     uint16_t is_deleting : 1;
