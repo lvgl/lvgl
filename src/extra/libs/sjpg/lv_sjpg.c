75599a94712d061e9bf85f390f8b04974855ee88
蒋慧赟 <42379289+cnjhb@users.noreply.github.com>
2025-02-27 13:38:36 +0800

    fix(jpg):More rational identification
    The header of the jpg is just 0xffd8,the current way of recognizing it is not proper.

--- src/extra/libs/sjpg/lv_sjpg.c
+++ src/extra/libs/sjpg/lv_sjpg.c
@@ -885,7 +885,7 @@ static void decoder_close(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * dsc
 
 static int is_jpg(const uint8_t * raw_data, size_t len)
 {
-    const uint8_t jpg_signature[] = {0xFF, 0xD8, 0xFF,  0xE0,  0x00,  0x10, 0x4A,  0x46, 0x49, 0x46};
+    const uint8_t jpg_signature[] = {0xFF, 0xD8, };
     if(len < sizeof(jpg_signature)) return false;
     return memcmp(jpg_signature, raw_data, sizeof(jpg_signature)) == 0;
 }
