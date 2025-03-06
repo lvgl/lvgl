4fab9dee5f9a4cf6acf61189492b89354dce3c2d
Cosmin-Daniel Radu <cosmin.radu_1@nxp.com>
2025-03-06 16:30:26 +0200

    refactor(vglite) use dynamic path_data for drawing
    Signed-off-by: Cosmin-Daniel Radu <cosmin.radu_1@nxp.com>

--- src/draw/nxp/vglite/lv_draw_vglite_line.c
+++ src/draw/nxp/vglite/lv_draw_vglite_line.c
@@ -88,6 +88,21 @@ void lv_draw_vglite_line(vglite_draw_task_t * vglite_task)
  *   STATIC FUNCTIONS
  **********************/
 
+static void _vglite_set_line(uint32_t * line_path, uint32_t * path_data_size, const lv_point_t * point1,
+                             const lv_point_t * point2)
+{
+    uint32_t pidx = 0;
+    line_path[pidx++] = VLC_OP_MOVE;
+    line_path[pidx++] = point1->x;
+    line_path[pidx++] = point1->y;
+    line_path[pidx++] = VLC_OP_LINE;
+    line_path[pidx++] = point2->x;
+    line_path[pidx++] = point2->y;
+    line_path[pidx] = VLC_OP_END;
+
+    *path_data_size = pidx * sizeof(int32_t);
+}
+
 static void _vglite_draw_line(vglite_draw_task_t * vglite_task, const lv_point_t * point1, const lv_point_t * point2,
                               const lv_area_t * clip_area, const lv_draw_line_dsc_t * dsc)
 {
@@ -115,13 +130,13 @@ static void _vglite_draw_line(vglite_draw_task_t * vglite_task, const lv_point_t
     /*** Init path ***/
     int32_t width = dsc->width;
 
-    int32_t line_path[] = { /*VG line path*/
-        VLC_OP_MOVE, point1->x, point1->y,
-        VLC_OP_LINE, point2->x, point2->y,
-        VLC_OP_END
-    };
+    uint32_t path_data_size = 0;
+    int32_t * line_path = lv_malloc_zeroed(7 * sizeof(int32_t));
+    LV_ASSERT(line_path != NULL);
+    vglite_task->path_data = line_path;
+    _vglite_set_line(line_path, &path_data_size, point1, point2);
 
-    VGLITE_CHECK_ERROR(vg_lite_init_path(path, VG_LITE_S32, VG_LITE_HIGH, sizeof(line_path), line_path,
+    VGLITE_CHECK_ERROR(vg_lite_init_path(path, VG_LITE_S32, VG_LITE_HIGH, path_data_size, line_path,
                                          (vg_lite_float_t)clip_area->x1, (vg_lite_float_t)clip_area->y1,
                                          ((vg_lite_float_t)clip_area->x2) + 1.0f, ((vg_lite_float_t)clip_area->y2) + 1.0f));
 
