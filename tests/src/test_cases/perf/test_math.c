d21d7779aa31eeded4017f0f9755ad44b27402a7
André <andre_miguel_costa@hotmail.com>
2025-04-04 18:20:01 +0200

    refactor: move perf test sources to test_cases

--- tests/src/test_cases/perf/test_math.c
+++ tests/src/test_cases/perf/test_math.c
@@ -1,8 +1,8 @@
-#include "../framework/lv_test_perf.h"
+#if LV_BUILD_TEST_PERF
+#include "unity/unity.h"
 
 void test_cubic_bezier(void)
 {
 	LV_PERF_ASSERT_MAX_TIME(lv_cubic_bezier, 10, 1, 2, 3, 4, 5);
 }
-#if LV_BUILD_TEST
 #endif
