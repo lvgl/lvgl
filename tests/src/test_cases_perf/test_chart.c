a9851b223d883c4838ca66f31b0705bbbe3b31d5
André <andre_miguel_costa@hotmail.com>
2025-04-06 21:09:02 +0200

    refactor: rename max time asserts for consistency

--- tests/src/test_cases_perf/test_chart.c
+++ tests/src/test_cases_perf/test_chart.c
@@ -20,15 +20,15 @@ void test_chart(void)
     red_series = lv_chart_add_series(chart, red_color,
                                      LV_CHART_AXIS_SECONDARY_Y);
 
-    LV_PERF_ASSERT_MAX_TIME(lv_chart_add_series, 1, chart, red_color,
-                            LV_CHART_AXIS_SECONDARY_Y);
+    TEST_ASSERT_MAX_TIME(lv_chart_add_series, 1, chart, red_color,
+                         LV_CHART_AXIS_SECONDARY_Y);
 
     for(size_t i = 0; i < 10; ++i) {
         uint16_t points_in_series = lv_chart_get_point_count(chart);
         uint16_t new_point_count = points_in_series * 2;
 
-        LV_PERF_ASSERT_MAX_TIME(lv_chart_set_point_count, 1, chart,
-                                new_point_count);
+        TEST_ASSERT_MAX_TIME(lv_chart_set_point_count, 1, chart,
+                             new_point_count);
     }
 }
 #endif
