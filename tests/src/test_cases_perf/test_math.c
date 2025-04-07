d3e623504f1f4fa2bca2793117a8d5a801867518
André <andre_miguel_costa@hotmail.com>
2025-04-07 12:12:35 +0200

    test(perf): make tests pass

--- tests/src/test_cases_perf/test_math.c
+++ tests/src/test_cases_perf/test_math.c
@@ -3,7 +3,7 @@
 
 void test_cubic_bezier(void)
 {
-    TEST_ASSERT_MAX_TIME_ITER(lv_cubic_bezier, 10, 750000, 1, 2, 3, 4,
+    TEST_ASSERT_MAX_TIME_ITER(lv_cubic_bezier, 10, 60000, 1, 2, 3, 4,
                               5);
 }
 #endif
