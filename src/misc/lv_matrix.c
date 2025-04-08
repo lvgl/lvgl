6d15f82474b577bbec0a7d3548ac7bfa6d5e5e1f
luke_sum <luke_sun@realsil.com.cn>
2025-04-08 11:12:00 +0800

    fix(matrix): add error log and use inv_w

--- src/misc/lv_matrix.c
+++ src/misc/lv_matrix.c
@@ -183,12 +183,14 @@ lv_point_precise_t lv_matrix_transform_precise_point(const lv_matrix_t * matrix,
     lv_point_precise_t p;
     lv_value_precise_t w = point->x * matrix->m[2][0] + point->y * matrix->m[2][1] + matrix->m[2][2];
     if(LV_ABS(w) < FLT_EPSILON) {
+        LV_LOG_ERROR("matrix is invalid");
         p.x = 0;
         p.y = 0;
     }
     else {
-        p.x = (lv_value_precise_t)roundf((point->x * matrix->m[0][0] + point->y * matrix->m[0][1] + matrix->m[0][2]) / w);
-        p.y = (lv_value_precise_t)roundf((point->x * matrix->m[1][0] + point->y * matrix->m[1][1] + matrix->m[1][2]) / w);
+        lv_value_precise_t inv_w = 1.0f / w;
+        p.x = (lv_value_precise_t)roundf((point->x * matrix->m[0][0] + point->y * matrix->m[0][1] + matrix->m[0][2]) * inv_w);
+        p.y = (lv_value_precise_t)roundf((point->x * matrix->m[1][0] + point->y * matrix->m[1][1] + matrix->m[1][2]) * inv_w);
     }
     return p;
 }
