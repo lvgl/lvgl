1be85d2d81e15828874b0408386394899821b5ac
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-04-30 20:50:37 +0200

    fix(font): impore static bitmap handling with stride

--- src/draw/sw/lv_draw_sw_letter.c
+++ src/draw/sw/lv_draw_sw_letter.c
@@ -149,10 +149,6 @@ static void LV_ATTRIBUTE_FAST_MEM draw_letter_cb(lv_draw_task_t * t, lv_draw_gly
             case LV_FONT_GLYPH_FORMAT_A3:
             case LV_FONT_GLYPH_FORMAT_A4:
             case LV_FONT_GLYPH_FORMAT_A8:
-            case LV_FONT_GLYPH_FORMAT_A1_ALIGNED:
-            case LV_FONT_GLYPH_FORMAT_A2_ALIGNED:
-            case LV_FONT_GLYPH_FORMAT_A4_ALIGNED:
-            case LV_FONT_GLYPH_FORMAT_A8_ALIGNED:
             case LV_FONT_GLYPH_FORMAT_IMAGE: {
                     if(glyph_draw_dsc->rotation % 3600 == 0 && glyph_draw_dsc->format != LV_FONT_GLYPH_FORMAT_IMAGE) {
                         glyph_draw_dsc->glyph_data = lv_font_get_glyph_bitmap(glyph_draw_dsc->g, glyph_draw_dsc->_draw_buf);
