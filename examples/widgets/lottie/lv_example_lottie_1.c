defaca64414793d3dc7f19e02ae5467a3b0d4440
Felix LVGL <fbiego.fb@gmail.com>
2025-05-02 09:06:06 +0300

    use premultiplied cf

--- examples/widgets/lottie/lv_example_lottie_1.c
+++ examples/widgets/lottie/lv_example_lottie_1.c
@@ -15,12 +15,12 @@ void lv_example_lottie_1(void)
 
 #if LV_DRAW_BUF_ALIGN == 4 && LV_DRAW_BUF_STRIDE_ALIGN == 1
     /*If there are no special requirements, just declare a buffer
-      x4 because the Lottie is rendered in ARGB8888 format*/
+      x4 because the Lottie is rendered in ARGB8888_PREMULTIPLIED format*/
     static uint8_t buf[64 * 64 * 4];
     lv_lottie_set_buffer(lottie, 64, 64, buf);
 #else
     /*For GPUs and special alignment/strid setting use a draw_buf instead*/
-    LV_DRAW_BUF_DEFINE(draw_buf, 64, 64, LV_COLOR_FORMAT_ARGB8888);
+    LV_DRAW_BUF_DEFINE(draw_buf, 64, 64, LV_COLOR_FORMAT_ARGB8888_PREMULTIPLIED);
     lv_lottie_set_draw_buf(lottie, &draw_buf);
 #endif
 
