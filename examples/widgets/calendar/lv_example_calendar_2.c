1e0bb5063d9b737135a2600592ff3e7118c3713a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-05-06 10:38:39 +0200

    feat(xml): add canvas and calendar support

--- examples/widgets/calendar/lv_example_calendar_2.c
+++ examples/widgets/calendar/lv_example_calendar_2.c
@@ -11,9 +11,9 @@ void lv_example_calendar_2(void)
     lv_calendar_set_month_shown(calendar, 2024, 03);
 
 #if LV_USE_CALENDAR_HEADER_DROPDOWN
-    lv_calendar_header_dropdown_create(calendar);
+    lv_calendar_add_header_dropdown(calendar);
 #elif LV_USE_CALENDAR_HEADER_ARROW
-    lv_calendar_header_arrow_create(calendar);
+    lv_calendar_add_header_arrow(calendar);
 #endif
 
     lv_calendar_set_chinese_mode(calendar, true);
