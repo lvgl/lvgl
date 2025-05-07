dcd5632959615da9f52ee99c60712c3889840ad0
Fabian Zäh <Fabian.Zaeh@livetec.de>
2025-05-07 08:31:47 +0000

    fix(test/layout_switch): code formatting (intendation in loops)

--- tests/src/test_layout_switch.c
+++ tests/src/test_layout_switch.c
@@ -17,7 +17,7 @@ static void set_layout_grid(void)
     lv_obj_set_grid_align(active_screen, LV_FLEX_ALIGN_SPACE_BETWEEN, LV_FLEX_ALIGN_SPACE_BETWEEN);
 
     for (int i = 0; i < NUM_OBJECTS; i++) {
-      lv_obj_set_grid_cell (objects[i], LV_GRID_ALIGN_STRETCH, i % 2, 1, LV_GRID_ALIGN_STRETCH, i / 2, 1);
+        lv_obj_set_grid_cell (objects[i], LV_GRID_ALIGN_STRETCH, i % 2, 1, LV_GRID_ALIGN_STRETCH, i / 2, 1);
     }
     lv_obj_update_layout(active_screen);
 }
@@ -48,8 +48,8 @@ static void set_layout_flex(lv_flex_flow_t flex_flow)
     }
 
     for (int i = 0; i < NUM_OBJECTS; i++) {
-      lv_obj_set_flex_grow(objects[i], 1);
-      obj_set_dimension(objects[i], LV_PCT(100)); 
+        lv_obj_set_flex_grow(objects[i], 1);
+        obj_set_dimension(objects[i], LV_PCT(100)); 
     }
 
     lv_obj_update_layout(active_screen);
@@ -59,7 +59,7 @@ void setUp(void)
 {
     active_screen = lv_screen_active();
     for (int i = 0; i < NUM_OBJECTS; i++) {
-      objects[i] = lv_obj_create (active_screen);
+        objects[i] = lv_obj_create (active_screen);
     }
 }
 
