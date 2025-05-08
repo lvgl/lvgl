52f273fff2942a0f9b4a810c47f4d1fce8207f16
liamHowatt <liamjmh0@gmail.com>
2025-05-08 12:28:50 +0200

    feedback: user SPI/param interface, remove targets

---
+++ src/draw/eve/lv_draw_eve_target.h
@@ -0,0 +1,102 @@
+/**
+ * @file lv_draw_eve_target.h
+ *
+ */
+
+#ifndef LV_DRAW_EVE_TARGET_H
+#define LV_DRAW_EVE_TARGET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "../../lv_conf_internal.h"
+#if LV_USE_DRAW_EVE
+
+#include LV_STDBOOL_INCLUDE
+#include LV_STDINT_INCLUDE
+
+/* lv_draw_eve_target_parameters.h should be in the include
+ * paths of your build. I.e., `-I /path/to/dir/that/has/the/file/`
+ *
+ * It should define these defines:
+ *     #define EVE_HSIZE          ...
+ *     #define EVE_VSIZE          ...
+ *     #define EVE_VSYNC0         ...
+ *     #define EVE_VSYNC1         ...
+ *     #define EVE_VOFFSET        ...
+ *     #define EVE_VCYCLE         ...
+ *     #define EVE_HSYNC0         ...
+ *     #define EVE_HSYNC1         ...
+ *     #define EVE_HOFFSET        ...
+ *     #define EVE_HCYCLE         ...
+ *     #define EVE_PCLK           ...
+ *     #define EVE_PCLKPOL        ...
+ *     #define EVE_SWIZZLE        ...
+ *     #define EVE_CSPREAD        ...
+ *     #define EVE_HAS_CRYSTAL    (`#define EVE_HAS_CRYSTAL` for yes, `#undef EVE_HAS_CRYSTAL` for no)
+ *     #define EVE_HAS_GT911      (`#define EVE_HAS_GT911` for yes, `#undef EVE_HAS_GT911` for no)
+ *     #define EVE_GEN            ...
+ *     #define EVE_BACKLIGHT_FREQ ... (optional, if applicable)
+ */
+#include "lv_draw_eve_target_parameters.h" /* see comment above */
+
+/*********************
+ *      DEFINES
+ *********************/
+
+#ifndef EVE_BACKLIGHT_FREQ
+#define EVE_BACKLIGHT_FREQ (4000U)
+#endif
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * The function that will be called to control the EVE IC's "PD_N" pin.
+ * @param powerdown false: set the pin high (powerup)
+ *                  true: set the pin low (powerdown)
+ */
+void lv_draw_eve_target_powerdown(bool powerdown);
+
+/**
+ * The function that will be called to control the EVE IC's SPI "CS_N" pin.
+ * @param selected  false: set the pin high (deselected)
+ *                  true: set the pin low (selected)
+ */
+void lv_draw_eve_target_spi_cs(bool selected);
+
+/**
+ * The function that will be called to transmit SPI data to the EVE IC.
+ * @param src  Pointer to the buffer of bytes that should be transmitted.
+ * @param len  The number of bytes to be transmitted.
+ */
+void lv_draw_eve_target_spi_transmit(uint8_t * src, uint32_t len);
+
+/**
+ * The function that will be called to receive SPI data from the EVE IC.
+ * @param src  Pointer to the buffer that `len` bytes should be received into.
+ * @param len  The number of bytes to receive.
+ */
+void lv_draw_eve_target_spi_receive(uint8_t * dst, uint32_t len);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*LV_USE_DRAW_EVE*/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DRAW_EVE_TARGET_H*/
