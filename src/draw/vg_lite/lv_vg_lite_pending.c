c3d39c70482d88ba9dc8325c89f3231fdcd1c216
Edward Nepomnyashchiy <edwardnepo@MacBook-Air-Edward.local>
2025-05-25 11:33:34 +0300

    Add null check for free_cb in lv_vg_lite_pending_array_clear

--- src/draw/vg_lite/lv_vg_lite_pending.c
+++ src/draw/vg_lite/lv_vg_lite_pending.c
@@ -19,19 +19,20 @@
  *      TYPEDEFS
  **********************/
 
-struct _lv_vg_lite_pending_t {
-    lv_array_t * arr_act;
+struct _lv_vg_lite_pending_t
+{
+    lv_array_t *arr_act;
     lv_array_t arr_1;
     lv_array_t arr_2;
     lv_vg_lite_pending_free_cb_t free_cb;
-    void * user_data;
+    void *user_data;
 };
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 
-static inline void lv_vg_lite_pending_array_clear(lv_vg_lite_pending_t * pending, lv_array_t * arr);
+static inline void lv_vg_lite_pending_array_clear(lv_vg_lite_pending_t *pending, lv_array_t *arr);
 
 /**********************
  *  STATIC VARIABLES
@@ -45,9 +46,9 @@ static inline void lv_vg_lite_pending_array_clear(lv_vg_lite_pending_t * pending
  *   GLOBAL FUNCTIONS
  **********************/
 
-lv_vg_lite_pending_t * lv_vg_lite_pending_create(size_t obj_size, uint32_t capacity_default)
+lv_vg_lite_pending_t *lv_vg_lite_pending_create(size_t obj_size, uint32_t capacity_default)
 {
-    lv_vg_lite_pending_t * pending = lv_malloc_zeroed(sizeof(lv_vg_lite_pending_t));
+    lv_vg_lite_pending_t *pending = lv_malloc_zeroed(sizeof(lv_vg_lite_pending_t));
     LV_ASSERT_MALLOC(pending);
     lv_array_init(&pending->arr_1, capacity_default, obj_size);
     lv_array_init(&pending->arr_2, capacity_default, obj_size);
@@ -55,7 +56,7 @@ lv_vg_lite_pending_t * lv_vg_lite_pending_create(size_t obj_size, uint32_t capac
     return pending;
 }
 
-void lv_vg_lite_pending_destroy(lv_vg_lite_pending_t * pending)
+void lv_vg_lite_pending_destroy(lv_vg_lite_pending_t *pending)
 {
     LV_ASSERT_NULL(pending);
     lv_vg_lite_pending_remove_all(pending);
@@ -65,8 +66,8 @@ void lv_vg_lite_pending_destroy(lv_vg_lite_pending_t * pending)
     lv_free(pending);
 }
 
-void lv_vg_lite_pending_set_free_cb(lv_vg_lite_pending_t * pending, lv_vg_lite_pending_free_cb_t free_cb,
-                                    void * user_data)
+void lv_vg_lite_pending_set_free_cb(lv_vg_lite_pending_t *pending, lv_vg_lite_pending_free_cb_t free_cb,
+                                    void *user_data)
 {
     LV_ASSERT_NULL(pending);
     LV_ASSERT_NULL(free_cb);
@@ -74,14 +75,14 @@ void lv_vg_lite_pending_set_free_cb(lv_vg_lite_pending_t * pending, lv_vg_lite_p
     pending->user_data = user_data;
 }
 
-void lv_vg_lite_pending_add(lv_vg_lite_pending_t * pending, void * obj)
+void lv_vg_lite_pending_add(lv_vg_lite_pending_t *pending, void *obj)
 {
     LV_ASSERT_NULL(pending);
     LV_ASSERT_NULL(obj);
     lv_array_push_back(pending->arr_act, obj);
 }
 
-void lv_vg_lite_pending_remove_all(lv_vg_lite_pending_t * pending)
+void lv_vg_lite_pending_remove_all(lv_vg_lite_pending_t *pending)
 {
     LV_ASSERT_NULL(pending);
 
@@ -89,7 +90,7 @@ void lv_vg_lite_pending_remove_all(lv_vg_lite_pending_t * pending)
     lv_vg_lite_pending_array_clear(pending, &pending->arr_2);
 }
 
-void lv_vg_lite_pending_swap(lv_vg_lite_pending_t * pending)
+void lv_vg_lite_pending_swap(lv_vg_lite_pending_t *pending)
 {
     pending->arr_act = (pending->arr_act == &pending->arr_1) ? &pending->arr_2 : &pending->arr_1;
     lv_vg_lite_pending_array_clear(pending, pending->arr_act);
@@ -99,17 +100,20 @@ void lv_vg_lite_pending_swap(lv_vg_lite_pending_t * pending)
  *   STATIC FUNCTIONS
  **********************/
 
-static inline void lv_vg_lite_pending_array_clear(lv_vg_lite_pending_t * pending, lv_array_t * arr)
+static inline void lv_vg_lite_pending_array_clear(lv_vg_lite_pending_t *pending, lv_array_t *arr)
 {
-    if (!pending->free_cb) return; // fail-safe
+    if (!pending->free_cb)
+        return; // fail-safe
 
     uint32_t size = lv_array_size(arr);
-    if(size == 0) {
+    if (size == 0)
+    {
         return;
     }
 
     /* remove all the pending objects */
-    for(uint32_t i = 0; i < size; i++) {
+    for (uint32_t i = 0; i < size; i++)
+    {
         pending->free_cb(lv_array_at(arr, i), pending->user_data);
     }
 
