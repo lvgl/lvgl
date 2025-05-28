e4e4118f5bf85f6c1d3bbddf0548a063c9faefe5
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-05-28 11:45:09 +0200

    add generic LVGL translation

--- src/others/xml/lv_xml_translation.c
+++ src/others/xml/lv_xml_translation.c
@@ -11,8 +11,8 @@
 
 #include "lv_xml_widget.h"
 #include "lv_xml_parser.h"
+#include "../../others/translation/lv_translation.h"
 #include "../../libs/expat/expat.h"
-#include "../../misc/lv_array.h"
 
 /*********************
  *      DEFINES
@@ -21,20 +21,6 @@
 /**********************
  *      TYPEDEFS
  **********************/
-
-typedef struct {
-    const char * tag;
-    const char ** translations; /**< Translations for each language*/
-} translation_dsc_t;
-
-typedef struct {
-    const char ** languages;
-    uint32_t language_cnt;
-    uint32_t is_pointer; /*In the union translations_p is used*/
-    const char ** tag_p;
-    const char ** translation_p; /*E.g. {{"a", "b"}, {"c", "d"}}*/
-    lv_array_t translation_array;
-} language_pack_t;
 
 /**********************
  *  STATIC PROTOTYPES
@@ -45,8 +31,6 @@ static void end_handler(void * user_data, const char * name);
 /**********************
  *  STATIC VARIABLES
  **********************/
-static lv_ll_t packs_ll;
-static const char * selected_lang = NULL;
 
 /**********************
  *      MACROS
@@ -55,30 +39,6 @@ static const char * selected_lang = NULL;
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
-
-void lv_xml_translation_init(void)
-{
-    lv_ll_init(&packs_ll, sizeof(language_pack_t));
-}
-
-
-lv_result_t lv_xml_translation_register_from_array(const char * languages[], const char * tags[],
-                                                   const char * translations[])
-{
-    language_pack_t * pack = lv_ll_ins_head(&packs_ll);
-    lv_memzero(pack, sizeof(language_pack_t));
-    pack->is_pointer = 1;
-
-    /*Count the languages*/
-    while(languages[pack->language_cnt]) {
-        pack->language_cnt++;
-    }
-
-    pack->languages = languages;
-    pack->tag_p = tags;
-    pack->translation_p = translations;
-    return LV_RESULT_OK;
-}
 
 lv_result_t lv_xml_translation_register_from_file(const char * path)
 {
@@ -129,10 +89,7 @@ lv_result_t lv_xml_translation_register_from_file(const char * path)
 
 lv_result_t lv_xml_translation_register_from_data(const char * xml_def)
 {
-    language_pack_t * pack = lv_ll_ins_head(&packs_ll);
-    lv_memzero(pack, sizeof(language_pack_t));
-    pack->is_pointer = 0;
-    lv_array_init(&pack->translation_array, 16, sizeof(translation_dsc_t));
+    lv_translation_pack_t * pack = lv_translation_add_dynamic();
 
     /* Parse the XML to extract metadata */
     XML_Parser parser = XML_ParserCreate(NULL);
@@ -150,80 +107,6 @@ lv_result_t lv_xml_translation_register_from_data(const char * xml_def)
     return LV_RESULT_OK;
 }
 
-void lv_xml_set_language(const char * lang)
-{
-    if(selected_lang) lv_free((void *)selected_lang);
-    selected_lang = lv_strdup(lang);
-}
-
-const char * lv_xml_get_translation(const char * tag)
-{
-    language_pack_t * pack;
-    bool lang_found = false;
-    LV_LL_READ(&packs_ll, pack) {
-        uint32_t lang;
-        for(lang = 0; lang < pack->language_cnt; lang++) {
-            /*Does this pack contains the language?*/
-            if(lv_streq(pack->languages[lang], selected_lang)) {
-                lang_found = true;
-                /*Find the tag*/
-                if(pack->is_pointer) {
-                    uint32_t t;
-                    for(t = 0; pack->tag_p[t]; t++) {
-                        if(lv_streq(pack->tag_p[t], tag)) {
-                            /*Find the "row" of the tag */
-                            const char ** tr_row = pack->translation_p + pack->language_cnt * t;
-                            const char * tr = tr_row[lang];
-                            if(tr) return tr; /*Found directly*/
-
-                            tr = tr_row[0];
-                            if(tr) {
-                                /*Use the first translation as fall back*/
-                                LV_LOG_WARN("`%s` tag is not found for `%s` language. Using `%s` instead.",
-                                            tag, selected_lang, pack->languages[0]);
-                                return tr;
-                            }
-
-                            LV_LOG_WARN("`%s` tag is not found. Using the tag as translation.", tag);
-                            return tag; /*Return the tag as a worst case option*/
-                        }
-                    }
-                }
-                else {
-                    size_t trans_cnt = lv_array_size(&pack->translation_array);
-                    size_t i;
-                    for(i = 0; i < trans_cnt; i++) {
-                        translation_dsc_t * tr_dsc = lv_array_at(&pack->translation_array, i);
-                        if(lv_streq(tr_dsc->tag, tag)) {
-                            const char * tr = tr_dsc->translations[lang];
-                            if(tr) return tr; /*Found directly*/
-
-                            tr = tr_dsc->translations[0];
-                            if(tr) {
-                                /*Use the first translation as fall back*/
-                                LV_LOG_WARN("`%s` tag is not found for `%s` language. Using `%s` instead.",
-                                            tag, selected_lang, pack->languages[0]);
-                                return tr;
-                            }
-
-                            LV_LOG_WARN("`%s` tag is not found. Using the tag as translation.", tag);
-                            return tag; /*Return the tag as a worst case option*/
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    if(lang_found) {
-        LV_LOG_WARN("`%s` tag is not found, using the tag as translation.", tag);
-    }
-    else {
-        LV_LOG_WARN("`%s` language is not found, using the `%s` as translation.", selected_lang, tag);
-    }
-
-    return tag;
-}
 
 /**********************
  *   STATIC FUNCTIONS
@@ -231,7 +114,7 @@ const char * lv_xml_get_translation(const char * tag)
 
 static void start_handler(void * user_data, const char * name, const char ** attrs)
 {
-    language_pack_t * pack = user_data;
+    lv_translation_pack_t * pack = user_data;
 
     if(lv_streq(name, "translations")) {
         const char * languages = lv_xml_get_value_of(attrs, "languages");
@@ -266,7 +149,7 @@ static void start_handler(void * user_data, const char * name, const char ** att
             return;
         }
 
-        translation_dsc_t tag;
+        lv_translation_tag_dsc_t tag;
         tag.tag = lv_strdup(tag_name);
         tag.translations = lv_malloc(pack->language_cnt * sizeof(const char *));
         lv_array_push_back(&pack->translation_array, &tag);
