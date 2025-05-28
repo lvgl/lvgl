349120cd639c2743e936ab648c01556aff93991e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-05-28 16:47:54 +0200

    reverr examples

--- examples/others/xml/lv_example_xml_2.c
+++ examples/others/xml/lv_example_xml_2.c
@@ -14,23 +14,33 @@ void lv_example_xml_2(void)
     lv_xml_component_register_from_file("A:lvgl/examples/others/xml/my_card.xml");
     lv_xml_component_register_from_file("A:lvgl/examples/others/xml/my_button.xml");
     lv_xml_component_register_from_file("A:lvgl/examples/others/xml/view.xml");
-    lv_xml_test_register_from_file("A:lvgl/examples/others/xml/test1.xml", "A:");
 
     lv_xml_register_font(NULL, "lv_montserrat_18", &lv_font_montserrat_18);
 
-    lv_xml_test_run(1);
+    lv_obj_t * obj = (lv_obj_t *) lv_xml_create(lv_screen_active(), "view", NULL);
+    lv_obj_set_pos(obj, 10, 10);
 
-    uint32_t step_cnt = lv_xml_test_get_step_count();
-    uint32_t i;
-    for(i = 0; i < step_cnt; i++) {
-        if(lv_xml_test_get_step_type(i) == LV_XML_TEST_STEP_TYPE_SCREENSHOT_COMPARE) {
-            if(lv_xml_test_get_status(i)) printf("Step %d passed\n", i);
-            else printf("Step %d failed\n", i);
-        }
-        else {
-            printf("Step %d is not a screenshot compare\n", i);
-        }
-    }
+    const char * my_button_attrs[] = {
+        "x", "10",
+        "y", "-10",
+        "align", "bottom_left",
+        "btn_text", "New button",
+        NULL, NULL,
+    };
 
+    lv_xml_component_unregister("my_button");
+
+    lv_xml_create(lv_screen_active(), "my_button", my_button_attrs);
+
+    const char * slider_attrs[] = {
+        "x", "200",
+        "y", "-15",
+        "align", "bottom_left",
+        "value", "30",
+        NULL, NULL,
+    };
+
+    lv_obj_t * slider = (lv_obj_t *) lv_xml_create(lv_screen_active(), "lv_slider", slider_attrs);
+    lv_obj_set_width(slider, 100);
 }
 #endif
