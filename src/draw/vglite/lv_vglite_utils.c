1b7dcc55c1624cf13663278d00709b24db52823a
Felipe Neves <felipe@lvgl.io>
2025-06-05 11:47:29 -0300

    draw: vglite: add the extra dump functions
    for debug, assertion and tracing
    
    Signed-off-by: Felipe Neves <felipe@lvgl.io>

--- src/draw/vglite/lv_vglite_utils.c
+++ src/draw/vglite/lv_vglite_utils.c
@@ -70,6 +70,260 @@ const char * vglite_error_to_string(vg_lite_error_t error)
     return "VG_LITE_UKNOWN_ERROR";
 }
 
+void vglite_dump_info(void)
+{
+    char name[64];
+    vg_lite_uint32_t chip_id;
+    vg_lite_uint32_t chip_rev;
+    vg_lite_uint32_t cid;
+    vg_lite_get_product_info(name, &chip_id, &chip_rev);
+    vg_lite_get_register(0x30, &cid);
+    LV_LOG_USER("Product Info: %s"
+                " | Chip ID: 0x%" LV_PRIx32
+                " | Revision: 0x%" LV_PRIx32
+                " | CID: 0x%" LV_PRIx32,
+                name, (uint32_t)chip_id, (uint32_t)chip_rev, (uint32_t)cid);
+
+    vg_lite_info_t info;
+    vg_lite_get_info(&info);
+    LV_LOG_USER("VGLite API version: 0x%" LV_PRIx32, (uint32_t)info.api_version);
+    LV_LOG_USER("VGLite API header version: 0x%" LV_PRIx32, (uint32_t)info.header_version);
+    LV_LOG_USER("VGLite release version: 0x%" LV_PRIx32, (uint32_t)info.release_version);
+
+    for(int feature = 0; feature < gcFEATURE_COUNT; feature++) {
+        vg_lite_uint32_t ret = vg_lite_query_feature((vg_lite_feature_t)feature);
+        LV_UNUSED(ret);
+        LV_LOG_USER("Feature-%d: %s\t - %s",
+                    feature, vglite_feature_string((vg_lite_feature_t)feature),
+                    ret ? "YES" : "NO");
+    }
+
+    vg_lite_uint32_t mem_avail = 0;
+    vg_lite_get_mem_size(&mem_avail);
+    LV_LOG_USER("Memory Available: %" LV_PRId32 " Bytes", (uint32_t)mem_avail);
+}
+
+const char * vglite_feature_string(vg_lite_feature_t feature)
+{
+    switch(feature) {
+            FEATURE_ENUM_TO_STRING(IM_INDEX_FORMAT);
+            FEATURE_ENUM_TO_STRING(SCISSOR);
+            FEATURE_ENUM_TO_STRING(BORDER_CULLING);
+            FEATURE_ENUM_TO_STRING(RGBA2_FORMAT);
+            FEATURE_ENUM_TO_STRING(QUALITY_8X);
+            FEATURE_ENUM_TO_STRING(IM_FASTCLAER);
+            FEATURE_ENUM_TO_STRING(RADIAL_GRADIENT);
+            FEATURE_ENUM_TO_STRING(GLOBAL_ALPHA);
+            FEATURE_ENUM_TO_STRING(RGBA8_ETC2_EAC);
+            FEATURE_ENUM_TO_STRING(COLOR_KEY);
+            FEATURE_ENUM_TO_STRING(DOUBLE_IMAGE);
+            FEATURE_ENUM_TO_STRING(YUV_OUTPUT);
+            FEATURE_ENUM_TO_STRING(FLEXA);
+            FEATURE_ENUM_TO_STRING(24BIT);
+            FEATURE_ENUM_TO_STRING(DITHER);
+            FEATURE_ENUM_TO_STRING(USE_DST);
+            FEATURE_ENUM_TO_STRING(PE_CLEAR);
+            FEATURE_ENUM_TO_STRING(IM_INPUT);
+            FEATURE_ENUM_TO_STRING(DEC_COMPRESS);
+            FEATURE_ENUM_TO_STRING(LINEAR_GRADIENT_EXT);
+            FEATURE_ENUM_TO_STRING(MASK);
+            FEATURE_ENUM_TO_STRING(MIRROR);
+            FEATURE_ENUM_TO_STRING(GAMMA);
+            FEATURE_ENUM_TO_STRING(NEW_BLEND_MODE);
+            FEATURE_ENUM_TO_STRING(STENCIL);
+            FEATURE_ENUM_TO_STRING(SRC_PREMULTIPLIED); /*! Valid only if FEATURE_ENUM_TO_STRING(HW_PREMULTIPLY is 0   */
+            FEATURE_ENUM_TO_STRING(HW_PREMULTIPLY); /*! HW multiplier can accept either premultiplied or not */
+            FEATURE_ENUM_TO_STRING(COLOR_TRANSFORMATION);
+            FEATURE_ENUM_TO_STRING(LVGL_SUPPORT);
+            FEATURE_ENUM_TO_STRING(INDEX_ENDIAN);
+            FEATURE_ENUM_TO_STRING(24BIT_PLANAR);
+            FEATURE_ENUM_TO_STRING(PIXEL_MATRIX);
+            FEATURE_ENUM_TO_STRING(NEW_IMAGE_INDEX);
+            FEATURE_ENUM_TO_STRING(PARALLEL_PATHS);
+            FEATURE_ENUM_TO_STRING(STRIPE_MODE);
+            FEATURE_ENUM_TO_STRING(IM_DEC_INPUT);
+            FEATURE_ENUM_TO_STRING(GAUSSIAN_BLUR);
+            FEATURE_ENUM_TO_STRING(RECTANGLE_TILED_OUT);
+            FEATURE_ENUM_TO_STRING(TESSELLATION_TILED_OUT);
+            FEATURE_ENUM_TO_STRING(IM_REPEAT_REFLECT);
+            FEATURE_ENUM_TO_STRING(YUY2_INPUT);
+            FEATURE_ENUM_TO_STRING(YUV_INPUT);
+            FEATURE_ENUM_TO_STRING(YUV_TILED_INPUT);
+            FEATURE_ENUM_TO_STRING(AYUV_INPUT);
+            FEATURE_ENUM_TO_STRING(16PIXELS_ALIGN);
+            FEATURE_ENUM_TO_STRING(DEC_COMPRESS_2_0);
+        default:
+            break;
+    }
+    return "UNKNOW_FEATURE";
+}
+
+const char * vglite_vlc_op_string(uint8_t vlc_op)
+{
+    switch(vlc_op) {
+            VLC_OP_ENUM_TO_STRING(END);
+            VLC_OP_ENUM_TO_STRING(CLOSE);
+            VLC_OP_ENUM_TO_STRING(MOVE);
+            VLC_OP_ENUM_TO_STRING(MOVE_REL);
+            VLC_OP_ENUM_TO_STRING(LINE);
+            VLC_OP_ENUM_TO_STRING(LINE_REL);
+            VLC_OP_ENUM_TO_STRING(QUAD);
+            VLC_OP_ENUM_TO_STRING(QUAD_REL);
+            VLC_OP_ENUM_TO_STRING(CUBIC);
+            VLC_OP_ENUM_TO_STRING(CUBIC_REL);
+
+            VLC_OP_ENUM_TO_STRING(SCCWARC);
+            VLC_OP_ENUM_TO_STRING(SCCWARC_REL);
+            VLC_OP_ENUM_TO_STRING(SCWARC);
+            VLC_OP_ENUM_TO_STRING(SCWARC_REL);
+            VLC_OP_ENUM_TO_STRING(LCCWARC);
+            VLC_OP_ENUM_TO_STRING(LCCWARC_REL);
+            VLC_OP_ENUM_TO_STRING(LCWARC);
+            VLC_OP_ENUM_TO_STRING(LCWARC_REL);
+        default:
+            break;
+    }
+    return "UNKNOW_VLC_OP";
+}
+
+static void path_data_print_cb(void * user_data, uint8_t op_code, const float * data, uint32_t len)
+{
+    LV_UNUSED(user_data);
+
+    LV_LOG("%s, ", vglite_vlc_op_string(op_code));
+    for(uint32_t i = 0; i < len; i++) {
+        LV_LOG("%0.2f, ", data[i]);
+    }
+    LV_LOG("\n");
+}
+
+void vglite_path_dump_info(const vg_lite_path_t * path)
+{
+    LV_ASSERT(path != NULL);
+    LV_ASSERT(path->path != NULL);
+    uint8_t fmt_len = vglite_path_format_len(path->format);
+    size_t len = path->path_length / fmt_len;
+
+    LV_ASSERT(len > 0);
+
+    LV_LOG_USER("address: %p", (void *)path->path);
+    LV_LOG_USER("length: %d", (int)len);
+    LV_LOG_USER("bonding box: (%0.2f, %0.2f) - (%0.2f, %0.2f)",
+                path->bounding_box[0], path->bounding_box[1],
+                path->bounding_box[2], path->bounding_box[3]);
+    LV_LOG_USER("format: %d", (int)path->format);
+    LV_LOG_USER("quality: %d", (int)path->quality);
+    LV_LOG_USER("path_changed: %d", (int)path->path_changed);
+    LV_LOG_USER("pdata_internal: %d", (int)path->pdata_internal);
+    LV_LOG_USER("type: %d", (int)path->path_type);
+    LV_LOG_USER("add_end: %d", (int)path->add_end);
+
+    vglite_path_for_each_data(path, path_data_print_cb, NULL);
+
+    if(path->stroke) {
+        LV_LOG_USER("stroke_path: %p", (void *)path->stroke_path);
+        LV_LOG_USER("stroke_size: %d", (int)path->stroke_size);
+        LV_LOG_USER("stroke_color: 0x%X", (int)path->stroke_color);
+        vglite_stroke_dump_info(path->stroke);
+    }
+}
+
+void vglite_stroke_dump_info(const vg_lite_stroke_t * stroke)
+{
+    LV_ASSERT(stroke != NULL);
+    LV_LOG_USER("stroke: %p", (void *)stroke);
+
+    /* Stroke parameters */
+    LV_LOG_USER("cap_style: 0x%X", (int)stroke->cap_style);
+    LV_LOG_USER("join_style: 0x%X", (int)stroke->join_style);
+    LV_LOG_USER("line_width: %f", stroke->line_width);
+    LV_LOG_USER("miter_limit: %f", stroke->miter_limit);
+
+    LV_LOG_USER("dash_pattern: %p", (void *)stroke->dash_pattern);
+    LV_LOG_USER("pattern_count: %d", (int)stroke->pattern_count);
+    if(stroke->dash_pattern) {
+        for(int i = 0; i < (int)stroke->pattern_count; i++) {
+            LV_LOG_USER("dash_pattern[%d]: %f", i, stroke->dash_pattern[i]);
+        }
+    }
+
+    LV_LOG_USER("dash_phase: %f", stroke->dash_phase);
+    LV_LOG_USER("dash_length: %f", stroke->dash_length);
+    LV_LOG_USER("dash_index: %d", (int)stroke->dash_index);
+    LV_LOG_USER("half_width: %f", stroke->half_width);
+
+    /* Total length of stroke dash patterns. */
+    LV_LOG_USER("pattern_length: %f", stroke->pattern_length);
+
+    /* For fast checking. */
+    LV_LOG_USER("miter_square: %f", stroke->miter_square);
+
+    /* Temp storage of stroke subPath. */
+    LV_LOG_USER("path_points: %p", (void *)stroke->path_points);
+    LV_LOG_USER("path_end: %p", (void *)stroke->path_end);
+    LV_LOG_USER("point_count: %d", (int)stroke->point_count);
+
+    LV_LOG_USER("left_point: %p", (void *)stroke->left_point);
+    LV_LOG_USER("right_point: %p", (void *)stroke->right_point);
+    LV_LOG_USER("stroke_points: %p", (void *)stroke->stroke_points);
+    LV_LOG_USER("stroke_end: %p", (void *)stroke->stroke_end);
+    LV_LOG_USER("stroke_count: %d", (int)stroke->stroke_count);
+
+    /* Divide stroke path according to move or move_rel for avoiding implicit closure. */
+    LV_LOG_USER("path_list_divide: %p", (void *)stroke->path_list_divide);
+
+    /* pointer to current divided path data. */
+    LV_LOG_USER("cur_list: %p", (void *)stroke->cur_list);
+
+    /* Flag that add end_path in driver. */
+    LV_LOG_USER("add_end: %d", (int)stroke->add_end);
+    LV_LOG_USER("dash_reset: %d", (int)stroke->dash_reset);
+
+    /* Sub path list. */
+    LV_LOG_USER("stroke_paths: %p", (void *)stroke->stroke_paths);
+
+    /* Last sub path. */
+    LV_LOG_USER("last_stroke: %p", (void *)stroke->last_stroke);
+
+    /* Swing area handling. */
+    LV_LOG_USER("swing_handling: %d", (int)stroke->swing_handling);
+    LV_LOG_USER("swing_deltax: %f", stroke->swing_deltax);
+    LV_LOG_USER("swing_deltay: %f", stroke->swing_deltay);
+    LV_LOG_USER("swing_start: %p", (void *)stroke->swing_start);
+    LV_LOG_USER("swing_stroke: %p", (void *)stroke->swing_stroke);
+    LV_LOG_USER("swing_length: %f", stroke->swing_length);
+    LV_LOG_USER("swing_centlen: %f", stroke->swing_centlen);
+    LV_LOG_USER("swing_count: %d", (int)stroke->swing_count);
+    LV_LOG_USER("need_swing: %d", (int)stroke->need_swing);
+    LV_LOG_USER("swing_ccw: %d", (int)stroke->swing_ccw);
+
+    LV_LOG_USER("stroke_length: %f", stroke->stroke_length);
+    LV_LOG_USER("stroke_size: %d", (int)stroke->stroke_size);
+
+    LV_LOG_USER("fattened: %d", (int)stroke->fattened);
+    LV_LOG_USER("closed: %d", (int)stroke->closed);
+}
+
+void vglite_buffer_dump_info(const vg_lite_buffer_t * buffer)
+{
+    LV_LOG_USER("memory: %p", (buffer)->memory);
+    LV_LOG_USER("address: 0x%08x", (int)(buffer)->address);
+    LV_LOG_USER("size: W%d x H%d", (int)((buffer)->width), (int)((buffer)->height));
+    LV_LOG_USER("stride: %d", (int)((buffer)->stride));
+    LV_LOG_USER("format: %d (%s)",
+                (int)((buffer)->format),
+                vglite_buffer_format_string((buffer)->format));
+    LV_LOG_USER("tiled: %d", (int)((buffer)->tiled));
+}
+
+void vglite_matrix_dump_info(const vg_lite_matrix_t * matrix)
+{
+    for(int i = 0; i < 3; i++) {
+        LV_LOG_USER("| %f, %f, %f |",
+                    (matrix)->m[i][0], (matrix)->m[i][1], (matrix)->m[i][2]);
+    }
+}
+
 #if LV_USE_VGLITE_DRAW_ASYNC
 bool vglite_cmd_buf_is_flushed(void)
 {
