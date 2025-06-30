211ae6a08e936187eda32a0ed72db72ff54127fa
Gauthier Provost <gpr@interel.com>
2025-06-30 11:07:33 +0800

    Add state trickle down feature

--- src/core/lv_obj.h
+++ src/core/lv_obj.h
@@ -116,16 +116,17 @@ typedef enum {
     LV_OBJ_FLAG_FLEX_IN_NEW_TRACK = (1L << 21),     /**< Start a new flex track on this item*/
 #endif
     LV_OBJ_FLAG_EVENT_TRICKLE   = (1L << 22), /**< Propagate the events to the children too*/
+    LV_OBJ_FLAG_STATE_TRICKLE   = (1L << 23), /**< Propagate the states to the children too*/
 
-    LV_OBJ_FLAG_LAYOUT_1        = (1L << 23), /**< Custom flag, free to use by layouts*/
-    LV_OBJ_FLAG_LAYOUT_2        = (1L << 24), /**< Custom flag, free to use by layouts*/
+    LV_OBJ_FLAG_LAYOUT_1        = (1L << 24), /**< Custom flag, free to use by layouts*/
+    LV_OBJ_FLAG_LAYOUT_2        = (1L << 25), /**< Custom flag, free to use by layouts*/
 
-    LV_OBJ_FLAG_WIDGET_1        = (1L << 25), /**< Custom flag, free to use by widget*/
-    LV_OBJ_FLAG_WIDGET_2        = (1L << 26), /**< Custom flag, free to use by widget*/
-    LV_OBJ_FLAG_USER_1          = (1L << 27), /**< Custom flag, free to use by user*/
-    LV_OBJ_FLAG_USER_2          = (1L << 28), /**< Custom flag, free to use by user*/
-    LV_OBJ_FLAG_USER_3          = (1L << 29), /**< Custom flag, free to use by user*/
-    LV_OBJ_FLAG_USER_4          = (1L << 30), /**< Custom flag, free to use by user*/
+    LV_OBJ_FLAG_WIDGET_1        = (1L << 26), /**< Custom flag, free to use by widget*/
+    LV_OBJ_FLAG_WIDGET_2        = (1L << 27), /**< Custom flag, free to use by widget*/
+    LV_OBJ_FLAG_USER_1          = (1L << 28), /**< Custom flag, free to use by user*/
+    LV_OBJ_FLAG_USER_2          = (1L << 29), /**< Custom flag, free to use by user*/
+    LV_OBJ_FLAG_USER_3          = (1L << 30), /**< Custom flag, free to use by user*/
+    LV_OBJ_FLAG_USER_4          = (1L << 31), /**< Custom flag, free to use by user*/
 } lv_obj_flag_t;
 
 #if LV_USE_OBJ_PROPERTY
