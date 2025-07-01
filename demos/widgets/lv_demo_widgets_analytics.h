a35e6ea6eba90306ade3191c5e331d9176df528f
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-07-01 07:09:08 +0200

    restucture

---
+++ demos/widgets/lv_demo_widgets_analytics.h
@@ -0,0 +1,39 @@
+/**
+ * @file lv_demo_widgets_analytics.h
+ *
+ */
+
+#ifndef LV_DEMO_WIDGETS_ANALYTICS_H
+#define LV_DEMO_WIDGETS_ANALYTICS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_widgets_analytics_create(lv_obj_t * parent);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DEMO_WIDGETS_ANALYTICS_H*/
---
+++ demos/widgets/lv_demo_widgets_profile.h
@@ -0,0 +1,39 @@
+/**
+ * @file lv_demo_widgets_profile.h
+ *
+ */
+
+#ifndef LV_DEMO_WIDGETS_PROFILE_H
+#define LV_DEMO_WIDGETS_PROFILE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_widgets_profile_create(lv_obj_t * parent);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DEMO_WIDGETS_PROFILE_H*/
