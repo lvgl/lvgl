589f4da40bfb83f898f910d8fddecf8c08c60ce3
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-07-01 07:09:08 +0200

    restucture

---
+++ demos/widgets/lv_demo_widgets_shop.h
@@ -0,0 +1,39 @@
+/**
+ * @file lv_demo_widgets_shop.h
+ *
+ */
+
+#ifndef LV_DEMO_WIDGETS_SHOP_H
+#define LV_DEMO_WIDGETS_SHOP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_widgets_shop_create(lv_obj_t * parent);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DEMO_WIDGETS_SHOP_H*/
