61ad6c46887c0ddb60c4b5b752931feafea8a51d
zhangjipeng <zhangjipeng@xiaomi.com>
2025-07-15 14:10:14 +0800

    fix code format

--- tests/src/test_cases/draw/test_draw_svg.c
+++ tests/src/test_cases/draw/test_draw_svg.c
@@ -310,17 +310,17 @@ void test_draw_shapes(void)
     lv_svg_node_delete(svg);
 
     const char * svg_shapes_12 = \
-                                "<svg width=600 height=200 viewBox=\"0 0 1200 400\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
-                                "<rect x=\"1\" y=\"1\" width=\"1198\" height=\"398\" fill=\"none\" stroke=\"blue\" stroke-width=\"1\"/>"
-                                "<path d=\"M300,200 h-150 a150,150 0 1,0 150,-150 z\" fill=\"red\" stroke=\"blue\" stroke-width=\"5\"/>"
-                                "<path d=\"M275,175 v-150 a150,150 0 0,0 -150,150 z\""
-                                " fill=\"yellow\" stroke=\"blue\" stroke-width=\"5\"/>"
-                                "<path d=\"M600,350 l 50,-25"
-                                " a25,25 -30 0,1 50,-25 l 50,-25"
-                                " a25,50 -30 0,1 50,-25 l 50,-25"
-                                " a25,75 -30 0,1 50,-25 l 50,-25"
-                                " a25,100 -30 0,1 50,-25 l 50,-25\""
-                                " fill=\"none\" stroke=\"red\" stroke-width=\"5\"/></svg>";
+                                 "<svg width=600 height=200 viewBox=\"0 0 1200 400\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
+                                 "<rect x=\"1\" y=\"1\" width=\"1198\" height=\"398\" fill=\"none\" stroke=\"blue\" stroke-width=\"1\"/>"
+                                 "<path d=\"M300,200 h-150 a150,150 0 1,0 150,-150 z\" fill=\"red\" stroke=\"blue\" stroke-width=\"5\"/>"
+                                 "<path d=\"M275,175 v-150 a150,150 0 0,0 -150,150 z\""
+                                 " fill=\"yellow\" stroke=\"blue\" stroke-width=\"5\"/>"
+                                 "<path d=\"M600,350 l 50,-25"
+                                 " a25,25 -30 0,1 50,-25 l 50,-25"
+                                 " a25,50 -30 0,1 50,-25 l 50,-25"
+                                 " a25,75 -30 0,1 50,-25 l 50,-25"
+                                 " a25,100 -30 0,1 50,-25 l 50,-25\""
+                                 " fill=\"none\" stroke=\"red\" stroke-width=\"5\"/></svg>";
 
     svg = lv_svg_load_data(svg_shapes_12, lv_strlen(svg_shapes_12));
     TEST_ASSERT_NOT_EQUAL(NULL, svg);
