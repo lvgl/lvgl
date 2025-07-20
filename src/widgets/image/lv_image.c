7a8ede5881f36dfc84d679d3465c144e069925bf
André <andre_miguel_costa@hotmail.com>
2025-07-20 14:18:39 +0200

    fix(lv_image): fix unused var warning when LV_LOG_WARN isn't available

--- src/widgets/image/lv_image.c
+++ src/widgets/image/lv_image.c
@@ -187,11 +187,12 @@ void lv_image_set_src(lv_obj_t * obj, const void * src)
     lv_image_header_t header;
     lv_result_t res = lv_image_decoder_get_info(src, &header);
     if(res != LV_RESULT_OK) {
-#if LV_USE_LOG
-        char buf[24];
-        LV_LOG_WARN("failed to get image info: %s",
-                    src_type == LV_IMAGE_SRC_FILE ? (const char *)src : (lv_snprintf(buf, sizeof(buf), "%p", src), buf));
-#endif /*LV_USE_LOG*/
+        if(src_type == LV_IMAGE_SRC_FILE) {
+            LV_LOG_WARN("failed to get image info: %s", (const char *)src);
+        }
+        else {
+            LV_LOG_WARN("failed to get image info: %p", src);
+        }
         return;
     }
 
