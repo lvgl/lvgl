65ca2df46831fcf25c5eebddd7c2b925acc6754c
André <andre_miguel_costa@hotmail.com>
2025-07-30 06:19:15 +0200

    chore: remove unused libs

--- src/libs/gltf/lv_gltf_view/ibl/lv_gltf_ibl_sampler.c
+++ src/libs/gltf/lv_gltf_view/ibl/lv_gltf_ibl_sampler.c
@@ -22,8 +22,6 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../stb_image/stb_image.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "../../stb_image/stb_image_resize.h"
 
 /*********************
  *      DEFINES
