886336801577dfd375043da51bfcdc7af27f0fa9
André <andre_miguel_costa@hotmail.com>
2025-08-06 13:50:04 +0200

    fix: correctly handle resizing compiled shaders array

--- src/libs/gltf/lv_gltf_data/lv_gltf_data_shader.cpp
+++ src/libs/gltf/lv_gltf_data/lv_gltf_data_shader.cpp
@@ -38,20 +38,20 @@
 void lv_gltf_store_compiled_shader(lv_gltf_model_t * data, size_t identifier, lv_gltf_compiled_shader_t * shader)
 {
     const size_t index = identifier - 1;
-    if(index >= lv_array_size(&data->compiled_shaders)) {
-        lv_array_resize(&data->compiled_shaders, index);
-        lv_array_push_back(&data->compiled_shaders, shader);
+    bool has_to_resize = index >= lv_array_size(&data->compiled_shaders);
+    if(!has_to_resize) {
+        lv_array_assign(&data->compiled_shaders, index, shader);
     }
-    else {
-        void * dest = lv_array_at(&data->compiled_shaders, index);
-        lv_memcpy(dest, shader, sizeof(*shader));
+    while(index >= lv_array_size(&data->compiled_shaders)) {
+        lv_array_push_back(&data->compiled_shaders, shader);
     }
 }
 
 lv_gltf_compiled_shader_t * lv_gltf_get_compiled_shader(lv_gltf_model_t * data, size_t identifier)
 {
-    LV_ASSERT(identifier - 1 < lv_array_size(&data->compiled_shaders));
-    return (lv_gltf_compiled_shader_t *)lv_array_at(&data->compiled_shaders, identifier - 1);
+    const size_t index = identifier - 1;
+    LV_ASSERT(index < lv_array_size(&data->compiled_shaders));
+    return (lv_gltf_compiled_shader_t *)lv_array_at(&data->compiled_shaders, index);
 }
 
 /**********************
