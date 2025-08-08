481f4f40e68ee06fd8d31968a6873aba66547e36
André <andre_miguel_costa@hotmail.com>
2025-08-08 21:13:32 +0200

    fix: remove useless initialization call

--- src/libs/gltf/lv_gltf_view/lv_gltf_view.cpp
+++ src/libs/gltf/lv_gltf_view/lv_gltf_view.cpp
@@ -478,7 +478,6 @@ static void lv_gltf_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
     lv_gltf_ibl_generate_env_textures(&view->env_textures, NULL, 0);
 
     lv_array_init(&view->models, LV_GLTF_INITIAL_MODEL_CAPACITY, sizeof(lv_gltf_model_t *));
-    new(&view->ibm_by_skin_then_node) std::map<int32_t, std::map<fastgltf::Node *, fastgltf::math::fmat4x4> >;
 
     LV_TRACE_OBJ_CREATE("end");
 }
