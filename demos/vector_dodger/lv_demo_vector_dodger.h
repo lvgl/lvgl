d9e0f2b5c6a70f3ac42551310876c955f0940de3
Benign X <1341398182@qq.com>
2025-08-14 22:10:05 +0800

    demo(game): add vector-based dodger game demo
    Signed-off-by: Benign X <1341398182@qq.com>

---
+++ demos/vector_dodger/lv_demo_vector_dodger.h
@@ -0,0 +1,41 @@
+
+/**
+ * @file lv_demo_vector_dodger.h
+ *
+ */
+
+#ifndef LV_DEMO_VECTOR_DODGER_H
+#define LV_DEMO_VECTOR_DODGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void lv_demo_vector_dodger(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_VECTOR_DODGER_H*/
