00284c81d54d5cd6590dea97a2cffc4c60cd79b3
Richard Allen <richard@bryghtlabs.com>
2025-08-22 09:12:12 -0500

    feat(roller): function to get option string

--- tests/src/test_cases/widgets/test_roller.c
+++ tests/src/test_cases/widgets/test_roller.c
@@ -57,6 +57,23 @@ void test_roller_get_options(void)
     TEST_ASSERT_EQUAL_STRING(default_roller_options, lv_roller_get_options(roller));
 }
 
+void test_roller_get_option_str(void)
+{
+    char option_str[OPTION_BUFFER_SZ] = {0x00};
+    char * expected_strs[] = {"One", "Two", "Three"};
+    uint16_t expected_str_count = sizeof(expected_strs) / sizeof(expected_strs[0]);
+
+    /* Select the last option, index starts at 0 */
+    uint16_t option_count = lv_roller_get_option_count(roller);
+    TEST_ASSERT_EQUAL(expected_str_count, option_count);
+
+    for(uint16_t i = 0; i < option_count; ++i) {
+        memset(option_str, 0x00, sizeof(option_str));
+        lv_roller_get_option_str(roller, i, option_str, OPTION_BUFFER_SZ);
+        TEST_ASSERT_EQUAL_STRING(expected_strs[i], option_str);
+    }
+}
+
 void test_roller_get_selected_option(void)
 {
     char actual_str[OPTION_BUFFER_SZ] = {0x00};
