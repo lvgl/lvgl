9e19c677c9f874d3ab0907de16976bda68edd6b5
Felipe Neves <felipe@lvgl.io>
2025-08-28 17:45:46 -0300

    draw: pxp: fix alignment issue on cache flush.
    * Update PXP to raise an error if the draw buffer alignment does not meet PXP reqs.
    * Use the target D-cache line size to calculate address alignment before flush
    
    Signed-off-by: Felipe Neves <felipe@lvgl.io>

--- src/draw/nxp/pxp/lv_pxp_cfg.h
+++ src/draw/nxp/pxp/lv_pxp_cfg.h
@@ -27,6 +27,10 @@ extern "C" {
 #include "fsl_cache.h"
 #include "fsl_pxp.h"
 
+#if (LV_DRAW_BUF_ALIGN != 32)
+#error "If PXP is enabled the draw buffers should be aligned to 32-byte boundary, please set LV_DRAW_BUF_ALIGN to 32 in lv_conf.h"
+#endif
+
 #include "../../../misc/lv_log.h"
 
 /*********************
