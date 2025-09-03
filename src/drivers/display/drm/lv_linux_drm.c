6c13ed2d65cf76093b508455f15340e9328a9045
liamHowatt <liamjmh0@gmail.com>
2025-09-03 21:02:52 +0200

    formatting

--- src/drivers/display/drm/lv_linux_drm.c
+++ src/drivers/display/drm/lv_linux_drm.c
@@ -1323,7 +1323,7 @@ static int drm_atomic_commit(drm_dev_t * drm_dev, uint32_t fb_id, uint32_t flags
         return ret;
     }
 
-    if (drm_dev->kms_in_fence_fd != -1) {
+    if(drm_dev->kms_in_fence_fd != -1) {
         close(drm_dev->kms_in_fence_fd);
         drm_dev->kms_in_fence_fd = -1;
     }
