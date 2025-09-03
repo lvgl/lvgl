5752394ee07a6ca280e6c0c6b5e2eb63b9865ee4
André <andre_miguel_costa@hotmail.com>
2025-09-03 10:59:04 +0200

    fix(draw/opengles): add missing texture MAG_FILTER parameter

--- src/draw/opengles/lv_draw_opengles.c
+++ src/draw/opengles/lv_draw_opengles.c
@@ -614,11 +614,10 @@ static unsigned int create_texture(int32_t w, int32_t h, const void * data)
      * have full ARGB pixels since the alpha channel is required for blending.
      */
     GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, data));
-
     GL_CALL(glGenerateMipmap(GL_TEXTURE_2D));
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 20));
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST));
-
+    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
     GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
 
     return texture;
