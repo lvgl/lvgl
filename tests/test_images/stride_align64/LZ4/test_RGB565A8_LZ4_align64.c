fccd5f2383a47938f6096e7cb9a8d3fb29c9190f
André Costa <andre_miguel_costa@hotmail.com>
2025-09-05 16:10:32 +0200

    test(images): update test images according to LVGLImage script (#8790)

--- tests/test_images/stride_align64/LZ4/test_RGB565A8_LZ4_align64.c
+++ tests/test_images/stride_align64/LZ4/test_RGB565A8_LZ4_align64.c
@@ -1,12 +1,13 @@
 
 #if defined(LV_LVGL_H_INCLUDE_SIMPLE)
 #include "lvgl.h"
+#elif defined(LV_LVGL_H_INCLUDE_SYSTEM)
+#include <lvgl.h>
 #elif defined(LV_BUILD_TEST)
 #include "../lvgl.h"
 #else
 #include "lvgl/lvgl.h"
 #endif
-
 
 #ifndef LV_ATTRIBUTE_MEM_ALIGN
 #define LV_ATTRIBUTE_MEM_ALIGN
@@ -153,13 +154,17 @@ uint8_t test_RGB565A8_LZ4_align64_map[] = {
 };
 
 const lv_image_dsc_t test_RGB565A8_LZ4_align64 = {
-  .header.magic = LV_IMAGE_HEADER_MAGIC,
-  .header.cf = LV_COLOR_FORMAT_RGB565A8,
-  .header.flags = 0 | LV_IMAGE_FLAGS_COMPRESSED,
-  .header.w = 71,
-  .header.h = 60,
-  .header.stride = 144,
+  .header = {
+    .magic = LV_IMAGE_HEADER_MAGIC,
+    .cf = LV_COLOR_FORMAT_RGB565A8,
+    .flags = 0 | LV_IMAGE_FLAGS_COMPRESSED,
+    .w = 71,
+    .h = 60,
+    .stride = 144,
+    .reserved_2 = 0,
+  },
   .data_size = sizeof(test_RGB565A8_LZ4_align64_map),
   .data = test_RGB565A8_LZ4_align64_map,
+  .reserved = NULL,
 };
 
