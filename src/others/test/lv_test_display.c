c17fe1ee01a2b6ecada1d67521d657eb2aec74e3
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-09 17:47:42 +0800

    add glfw_window for test_display
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/others/test/lv_test_display.c
+++ src/others/test/lv_test_display.c
@@ -11,6 +11,7 @@
 
 #include "../../core/lv_global.h"
 #include "../../lvgl_private.h"
+#include "../../drivers/opengles/lv_opengles_glfw.c"
 #include <stdlib.h>
 
 /*********************
@@ -44,7 +45,37 @@ static void delete_event_cb(lv_event_t * e);
 
 lv_display_t * lv_test_display_create(int32_t hor_res, int32_t ver_res)
 {
+#if LV_USE_GLFW
+    LV_UNUSED(dummy_flush_cb);
+    LV_UNUSED(buf_changed_event_cb);
+    LV_UNUSED(delete_event_cb);
 
+    /* create a window and initialize OpenGL */
+    lv_opengles_window_t * window = lv_opengles_glfw_window_create(hor_res, ver_res, true);
+
+    /* create a display that flushes to a texture */
+    lv_display_t * disp = lv_opengles_texture_create(hor_res, ver_res);
+    lv_display_set_default(disp);
+
+    /* add the texture to the window */
+    unsigned int texture_id = lv_opengles_texture_get_texture_id(disp);
+    lv_opengles_window_texture_t * window_texture = lv_opengles_window_add_texture(window, texture_id, hor_res, ver_res);
+
+    /* get the mouse indev of the window texture */
+    lv_indev_t * mouse = lv_opengles_window_texture_get_mouse_indev(window_texture);
+    lv_indev_set_group(mouse, lv_group_get_default());
+    lv_indev_set_display(mouse, disp);
+
+    lv_draw_buf_t * draw_buf = lv_display_get_buf_active(disp);
+    LV_ASSERT_NULL(draw_buf);
+    _state.draw_buf = *draw_buf;
+
+    /**
+     * Remove the timestamp callback set internally by lv_glfw to
+     * avoid affecting testcases that are strongly related to timestamps
+     */
+    lv_tick_set_cb(NULL);
+#else
     lv_display_t * disp = lv_display_create(hor_res, ver_res);
     lv_display_set_color_format(disp, LV_COLOR_FORMAT_XRGB8888);
 
@@ -63,6 +94,7 @@ lv_display_t * lv_test_display_create(int32_t hor_res, int32_t ver_res)
     lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_COLOR_FORMAT_CHANGED, NULL);
     lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_RESOLUTION_CHANGED, NULL);
     lv_display_add_event_cb(disp, delete_event_cb, LV_EVENT_DELETE, NULL);
+#endif /* LV_USE_DRAW_OPENGLES */
 
     return disp;
 }
--- tests/src/lv_test_conf_opengles.h
+++ tests/src/lv_test_conf_opengles.h
@@ -1,10 +1,18 @@
 #ifndef LV_TEST_CONF_OPENGLES_H
 #define LV_TEST_CONF_OPENGLES_H
 
-#if !defined(NON_AMD64_BUILD) && !defined(_MSC_VER) && !defined(_WIN32)
-    #define LV_USE_OPENGLES               1
-    #define LV_USE_OPENGLES_DEBUG         1
-    #define LV_USE_GLFW                   1
+#ifndef LV_USE_OPENGLES
+    #if !defined(NON_AMD64_BUILD) && !defined(_MSC_VER) && !defined(_WIN32)
+        #define LV_USE_OPENGLES               1
+        #define LV_USE_OPENGLES_DEBUG         1
+        #define LV_USE_GLFW                   1
+        #define LV_DRAW_BUF_ALIGN             4
+
+        #ifdef LV_DRAW_BUF_STRIDE_ALIGN
+            #undef LV_DRAW_BUF_STRIDE_ALIGN
+            #define LV_DRAW_BUF_STRIDE_ALIGN 1 /* Not supported stride alignment yet */
+        #endif/* LV_DRAW_BUF_STRIDE_ALIGN */
+    #endif
 #endif
 
 #endif /*LV_TEST_CONF_OPENGLES_H*/
