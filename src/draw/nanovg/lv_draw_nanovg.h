c6ae779182d16ebc51871d6c5b163c44161e22db
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-11 17:33:43 +0800

    feat(draw): add nanovg rendering backend
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

---
+++ src/draw/nanovg/lv_draw_nanovg.h
@@ -0,0 +1,43 @@
+/**
+ * @file lv_draw_nanovg.h
+ *
+ */
+
+#ifndef LV_DRAW_NANOVG_H
+#define LV_DRAW_NANOVG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "../../lv_conf_internal.h"
+
+#include "../lv_draw.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void lv_draw_nanovg_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DRAW_NANOVG_H*/
