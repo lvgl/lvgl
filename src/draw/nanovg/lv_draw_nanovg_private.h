35cf5c588e4d20074a8755853d286e3cbc01a3c8
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-11 20:49:53 +0800

    add independent drawing method template file

---
+++ src/draw/nanovg/lv_draw_nanovg_private.h
@@ -0,0 +1,37 @@
+/**
+ * @file lv_draw_nanovg_private.h
+ *
+ */
+
+#ifndef LV_DRAW_NANOVG_PRIVATE_H
+#define LV_DRAW_NANOVG_PRIVATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DRAW_NANOVG_PRIVATE_H*/
---
+++ src/draw/nanovg/lv_draw_nanovg_utils.h
@@ -0,0 +1,37 @@
+/**
+ * @file lv_draw_nanovg_utils.h
+ *
+ */
+
+#ifndef LV_DRAW_NANOVG_UTILS_H
+#define LV_DRAW_NANOVG_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_DRAW_NANOVG_UTILS_H*/
