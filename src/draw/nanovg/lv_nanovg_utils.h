bbd18b4d148c0f57ab05d684a6788336a0a0a48a
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-12 13:37:28 +0800

    add border support

--- src/draw/nanovg/lv_nanovg_utils.h
+++ src/draw/nanovg/lv_nanovg_utils.h
@@ -1,10 +1,10 @@
 /**
- * @file lv_draw_nanovg_utils.h
+ * @file lv_nanovg_utils.h
  *
  */
 
-#ifndef LV_DRAW_NANOVG_UTILS_H
-#define LV_DRAW_NANOVG_UTILS_H
+#ifndef LV_NANOVG_UTILS_H
+#define LV_NANOVG_UTILS_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,6 +20,7 @@ extern "C" {
 
 #include "../../misc/lv_assert.h"
 #include "../../misc/lv_matrix.h"
+#include "../../misc/lv_color.h"
 #include "../../libs/nanovg/nanovg.h"
 
 /*********************
@@ -50,9 +51,35 @@ static inline void lv_nanovg_matrix_convert(lv_nanovg_matrixt_t * dest, const lv
     dest->xform[5] = src->m[1][2];
 }
 
+static inline NVGcolor lv_nanovg_color_convert(lv_color_t color, lv_opa_t opa, bool pre_mul)
+{
+    if(pre_mul && opa < LV_OPA_COVER) {
+        color.red = LV_UDIV255(color.red * opa);
+        color.green = LV_UDIV255(color.green * opa);
+        color.blue = LV_UDIV255(color.blue * opa);
+    }
+
+    return nvgRGBA(color.red, color.green, color.blue, opa);
+}
+
 void lv_nanovg_transform(NVGcontext * ctx, const lv_matrix_t * matrix);
 
 void lv_nanovg_path_append_rect(NVGcontext * ctx, float x, float y, float w, float h, float r);
+
+void lv_nanovg_path_append_arc_right_angle(NVGcontext * ctx,
+                                           float start_x, float start_y,
+                                           float center_x, float center_y,
+                                           float end_x, float end_y);
+
+void lv_nanovg_path_append_arc(NVGcontext * ctx,
+                               float cx, float cy,
+                               float radius,
+                               float start_angle,
+                               float sweep,
+                               bool pie);
+
+void lv_nanovg_fill(NVGcontext * ctx, enum NVGwinding winding, enum NVGcompositeOperation composite_operation,
+                    NVGcolor color);
 
 /**********************
  *      MACROS
@@ -64,4 +91,4 @@ void lv_nanovg_path_append_rect(NVGcontext * ctx, float x, float y, float w, flo
 } /*extern "C"*/
 #endif
 
-#endif /*LV_DRAW_NANOVG_UTILS_H*/
+#endif /*LV_NANOVG_UTILS_H*/
