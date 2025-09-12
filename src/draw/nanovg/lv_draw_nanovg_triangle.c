7f7c2d27e9d26c41909b8248f57fb40325383bca
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-12 14:31:30 +0800

    add triangle support

--- src/draw/nanovg/lv_draw_nanovg_triangle.c
+++ src/draw/nanovg/lv_draw_nanovg_triangle.c
@@ -11,6 +11,8 @@
 
 #if LV_USE_DRAW_NANOVG
 
+#include "lv_nanovg_utils.h"
+
 /*********************
 *      DEFINES
 *********************/
@@ -37,7 +39,42 @@
 
 void lv_draw_nanovg_triangle(lv_draw_task_t * t, const lv_draw_triangle_dsc_t * dsc)
 {
+    LV_PROFILER_DRAW_BEGIN;
+    lv_area_t tri_area;
+    tri_area.x1 = (int32_t)LV_MIN3(dsc->p[0].x, dsc->p[1].x, dsc->p[2].x);
+    tri_area.y1 = (int32_t)LV_MIN3(dsc->p[0].y, dsc->p[1].y, dsc->p[2].y);
+    tri_area.x2 = (int32_t)LV_MAX3(dsc->p[0].x, dsc->p[1].x, dsc->p[2].x);
+    tri_area.y2 = (int32_t)LV_MAX3(dsc->p[0].y, dsc->p[1].y, dsc->p[2].y);
 
+    lv_area_t clip_area;
+    if(!lv_area_intersect(&clip_area, &tri_area, &t->clip_area)) {
+        LV_PROFILER_DRAW_END;
+        return;
+    }
+
+    lv_draw_nanovg_unit_t * u = (lv_draw_nanovg_unit_t *)t->draw_unit;
+
+    nvgBeginPath(u->vg);
+    nvgMoveTo(u->vg, dsc->p[0].x, dsc->p[0].y);
+    nvgLineTo(u->vg, dsc->p[1].x, dsc->p[1].y);
+    nvgLineTo(u->vg, dsc->p[2].x, dsc->p[2].y);
+    nvgClosePath(u->vg);
+
+    if(dsc->grad.dir != LV_GRAD_DIR_NONE) {
+#if LV_USE_VECTOR_GRAPHIC
+#else
+        LV_LOG_WARN("Gradient fill is not supported without VECTOR_GRAPHIC");
+#endif
+    }
+    else { /* normal fill */
+        lv_nanovg_fill(
+            u->vg,
+            NVG_CCW,
+            NVG_SOURCE_OVER,
+            lv_nanovg_color_convert(dsc->color, dsc->opa, true));
+    }
+
+    LV_PROFILER_DRAW_END;
 }
 
 /**********************
