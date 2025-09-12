117ea6fdf6499fcd4764485af46c85019e30681e
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-12 15:20:44 +0800

    add box shadow support

--- src/draw/nanovg/lv_draw_nanovg_box_shadow.c
+++ src/draw/nanovg/lv_draw_nanovg_box_shadow.c
@@ -11,6 +11,8 @@
 
 #if LV_USE_DRAW_NANOVG
 
+#include "lv_nanovg_utils.h"
+
 /*********************
 *      DEFINES
 *********************/
@@ -37,6 +39,51 @@
 
 void lv_draw_nanovg_box_shadow(lv_draw_task_t * t, const lv_draw_box_shadow_dsc_t * dsc, const lv_area_t * coords)
 {
+    LV_PROFILER_DRAW_BEGIN;
+
+    /*Calculate the rectangle which is blurred to get the shadow in `shadow_area`*/
+    lv_area_t core_area;
+    core_area.x1 = coords->x1  + dsc->ofs_x - dsc->spread;
+    core_area.x2 = coords->x2  + dsc->ofs_x + dsc->spread;
+    core_area.y1 = coords->y1  + dsc->ofs_y - dsc->spread;
+    core_area.y2 = coords->y2  + dsc->ofs_y + dsc->spread;
+
+    /*Calculate the bounding box of the shadow*/
+    lv_area_t shadow_area;
+    shadow_area.x1 = core_area.x1 - dsc->width / 2 - 1;
+    shadow_area.x2 = core_area.x2 + dsc->width / 2 + 1;
+    shadow_area.y1 = core_area.y1 - dsc->width / 2 - 1;
+    shadow_area.y2 = core_area.y2 + dsc->width / 2 + 1;
+
+    /*Get clipped draw area which is the real draw area.
+     *It is always the same or inside `shadow_area`*/
+    lv_area_t draw_area;
+    if(!lv_area_intersect(&draw_area, &shadow_area, &t->clip_area)) {
+        LV_PROFILER_DRAW_END;
+        return;
+    }
+
+    lv_draw_nanovg_unit_t * u = (lv_draw_nanovg_unit_t *)t->draw_unit;
+
+    const NVGcolor icol = lv_nanovg_color_convert(dsc->color, dsc->opa);
+    const NVGcolor ocol = lv_nanovg_color_convert(dsc->color, 0);
+
+    const int32_t w = lv_area_get_width(&draw_area);
+    const int32_t h = lv_area_get_height(&draw_area);
+
+    NVGpaint paint = nvgBoxGradient(
+                         u->vg,
+                         shadow_area.x1, shadow_area.y1,
+                         w, h,
+                         dsc->radius, 1, icol, ocol);
+
+    nvgBeginPath(u->vg);
+    lv_nanovg_path_append_rect(u->vg, shadow_area.x1, shadow_area.y1, w, h, dsc->radius);
+
+    nvgFillPaint(u->vg, paint);
+    nvgFill(u->vg);
+
+    LV_PROFILER_DRAW_END;
 }
 
 /**********************
