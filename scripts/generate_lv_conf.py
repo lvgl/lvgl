9f9394a9fa994d3ad37f7de026be0e03a28967fe
André Costa <andre_miguel_costa@hotmail.com>
2025-09-16 10:07:24 +0200

    chore(lvconf_gen): generate empty lv_conf.h if it doesn't exist (#8885)

--- scripts/generate_lv_conf.py
+++ scripts/generate_lv_conf.py
@@ -3,6 +3,7 @@
 import os
 import re
 import argparse
+from pathlib import Path
 
 DIR_SCRIPTS = os.path.dirname(__file__)
 REPO_ROOT = os.path.join(DIR_SCRIPTS, "..")
@@ -48,7 +49,8 @@ def get_args():
     if not os.path.exists(args.template):
         fatal(f"Template file not found at {args.template}")
     if not os.path.exists(args.config):
-        fatal(f"User config file not found at {args.config}")
+        print(f"{args.config} not found. Generating it")
+        Path(args.config).touch()
     if not os.path.exists(args.defaults):
         fatal(f"User defaults not found at {args.defaults}")
 
