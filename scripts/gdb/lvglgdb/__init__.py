c8f8e1db10a8cd470e0e3c4f4ccf8a0bb6b79f41
Benign X <1341398182@qq.com>
2025-09-24 18:20:28 +0800

    update expose

--- scripts/gdb/lvglgdb/__init__.py
+++ scripts/gdb/lvglgdb/__init__.py
@@ -1,9 +1,6 @@
-import gdb
-
-from .lvgl import curr_inst, LVDisplay, LVDrawBuf, LVList, LVObject, dump_style_info
-from .cmds import DumpObj, DumpDisplayBuf, InfoDrawUnit, InfoStyle
-from .debugger import Debugger
 from .value import Value
+from .lvgl import curr_inst, LVDisplay, LVDrawBuf, LVList, LVObject, dump_style_info
+from . import cmds as cmds
 
 __all__ = [
     "curr_inst",
@@ -12,26 +9,5 @@ __all__ = [
     "LVList",
     "LVObject",
     "dump_style_info",
-    "DumpObj",
-    "DumpDisplayBuf",
-    "InfoDrawUnit",
-    "InfoStyle",
     "Value",
 ]
-
-# Set pagination off and python print-stack full
-gdb.execute("set pagination off")
-gdb.write("set pagination off\n")
-gdb.execute("set python print-stack full")
-gdb.write("set python print-stack full\n")
-
-# Debugger
-Debugger()
-
-# Dumps
-DumpObj()
-DumpDisplayBuf()
-
-# Infos
-InfoStyle()
-InfoDrawUnit()
