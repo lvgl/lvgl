60e83840fab00a2764cbdcfe6c3e154acc0f92a3
pengyiqiang <pengyiqiang@xiaomi.com>
2025-10-24 17:59:15 +0800

    feat(test_mem): improve memory test coverage
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- tests/src/test_cases/test_mem.c
+++ tests/src/test_cases/test_mem.c
@@ -15,12 +15,58 @@ void tearDown(void)
 }
 
 /* #3324 */
-void test_mem_buf_realloc(void)
+void test_mem_realloc(void)
 {
 #ifdef LVGL_CI_USING_DEF_HEAP
+    uint32_t mem = lv_test_get_free_mem();
+
     void * buf1 = lv_malloc(20);
-    void * buf2 = lv_realloc(buf1, LV_MEM_SIZE + 16384);
+
+    void * buf2 = lv_realloc(buf1, LV_MEM_SIZE + 1);
     TEST_ASSERT_NULL(buf2);
+
+    /* Realloc failed, but should free buf1 */
+    void * buf3 = lv_reallocf(buf1, LV_MEM_SIZE + 1);
+    TEST_ASSERT_NULL(buf3);
+
+    void * buf4 = lv_reallocf(NULL, 30);
+    TEST_ASSERT_NOT_NULL(buf4);
+    lv_free(buf4);
+
+    TEST_ASSERT_MEM_LEAK_LESS_THAN(mem, 0);
+#endif
+}
+
+void test_mem_alloc_failed(void)
+{
+#ifdef LVGL_CI_USING_DEF_HEAP
+    uint32_t mem = lv_test_get_free_mem();
+    TEST_ASSERT_NULL(lv_malloc(LV_MEM_SIZE + 1));
+    TEST_ASSERT_NULL(lv_malloc_zeroed(LV_MEM_SIZE + 1));
+    TEST_ASSERT_MEM_LEAK_LESS_THAN(mem, 0);
+#endif
+}
+
+void test_mem_test(void)
+{
+#ifdef LVGL_CI_USING_DEF_HEAP
+    uint32_t mem = lv_test_get_free_mem();
+    uint32_t * zero_mem = lv_malloc_zeroed(0);
+    TEST_ASSERT_NOT_NULL(zero_mem);
+
+    /* Test magic value */
+    TEST_ASSERT_EQUAL_UINT32(ZERO_MEM_SENTINEL, *zero_mem);
+    TEST_ASSERT_EQUAL(LV_RESULT_OK, lv_mem_test());
+
+    /* Test wrong memory, test should fail */
+    *zero_mem = 0;
+    TEST_ASSERT_EQUAL(LV_RESULT_INVALID, lv_mem_test());
+
+    /* Restore magic value */
+    *zero_mem = ZERO_MEM_SENTINEL;
+    lv_free(zero_mem);
+
+    TEST_ASSERT_MEM_LEAK_LESS_THAN(mem, 0);
 #endif
 }
 
