5f715c48f30d57f770e1c94d8f4c0d44d20a8609
André <andre_miguel_costa@hotmail.com>
2025-10-30 08:57:29 +0100

    refactor: use new api in style example

--- examples/styles/lv_example_style_14.c
+++ examples/styles/lv_example_style_14.c
@@ -4,6 +4,7 @@
 
 static lv_style_t style_btn;
 
+
 /*Will be called when the styles of the base theme are already added
   to add new styles*/
 static void new_theme_apply_cb(lv_theme_t * th, lv_obj_t * obj)
@@ -15,25 +16,38 @@ static void new_theme_apply_cb(lv_theme_t * th, lv_obj_t * obj)
     }
 }
 
+static void on_display_delete(lv_event_t * e)
+{
+    lv_display_t * display = (lv_display_t *) lv_event_get_target(e);
+    lv_theme_t * display_theme = lv_display_get_theme(display);
+    lv_theme_destroy(display_theme);
+}
+
 static void new_theme_init_and_set(void)
 {
+    lv_display_t * display = lv_display_get_default();
+
     /*Initialize the styles*/
     lv_style_init(&style_btn);
     lv_style_set_bg_color(&style_btn, lv_palette_main(LV_PALETTE_GREEN));
     lv_style_set_border_color(&style_btn, lv_palette_darken(LV_PALETTE_GREEN, 3));
     lv_style_set_border_width(&style_btn, 3);
 
-    /*Initialize the new theme from the current theme*/
+    /* Initialize the new theme with the current theme as its parent
+     * The user is reponsible for freeing the theme when it's no longer needed */
     lv_theme_t * th_act = lv_display_get_theme(NULL);
-    static lv_theme_t th_new;
-    th_new = *th_act;
+    lv_theme_t * th_new = lv_theme_create();
+    lv_theme_copy(th_new, th_act);
+    lv_theme_set_parent(th_new, th_act);
 
-    /*Set the parent theme and the style apply callback for the new theme*/
-    lv_theme_set_parent(&th_new, th_act);
-    lv_theme_set_apply_cb(&th_new, new_theme_apply_cb);
+    /*Set the style apply callback for the new theme*/
+    lv_theme_set_apply_cb(th_new, new_theme_apply_cb);
 
     /*Assign the new theme to the current display*/
-    lv_display_set_theme(NULL, &th_new);
+    lv_display_set_theme(display, th_new);
+
+    /* Assing a delete event callback so that we can delete the theme when the display is deleted*/
+    lv_display_add_event_cb(display, on_display_delete, LV_EVENT_DELETE, NULL);
 }
 
 /**
