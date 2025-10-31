b1fddba2034fa22ba147a555325b5d370f80a9e7
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-10-31 21:27:21 +0100

    feat(test): fix naming and add more error handling

--- src/debugging/test/lv_test_screenshot_compare.h
+++ src/debugging/test/lv_test_screenshot_compare.h
@@ -24,6 +24,34 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
+/**
+ * Return value of `lv_test_screenshot_compare`
+ */
+typedef enum {
+    /**
+     * The screenshot is different than the reference image
+     */
+    LV_TEST_SCREENSHOT_RESULT_FAILED,
+
+    /**
+     * The screenshot is the same as than the reference image.
+     * It also returned if `LV_TEST_SCREENSHOT_CREATE_REFERENCE_IMAGE` is enabled
+     * and the reference image was missing.
+     */
+    LV_TEST_SCREENSHOT_RESULT_PASSED,
+
+    /**
+     * If `LV_TEST_SCREENSHOT_CREATE_REFERENCE_IMAGE` is not enabled
+     * and the reference image is missing.
+     */
+    LV_TEST_SCREENSHOT_RESULT_NO_REFERENCE_IMAGE,
+
+    /**
+     * The dimension of the reference image is incorrect
+     */
+    LV_TEST_SCREENSHOT_RESULT_BAD_REFERENCE_IMAGE_SIZE
+} lv_test_screenshot_result_t;
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
@@ -36,11 +64,12 @@ extern "C" {
  * It requires libPNG.
  *
  * @param fn_ref    path to the reference image. Will be appended toREF_IMGS_PATH if set.
- * @return          true: the reference image and the display are the same; false: they are different (`<image_name>_err.png` is created).
+ * @return          An element of `lv_test_screenshot_compare_result_t`
+ * true: the reference image and the display are the same; false: they are different (`<image_name>_err.png` is created).
  * @note            This function assumes that the default display is the test display that was created by
  *                  `lv_test_display_create()`
  */
-bool lv_test_screenshot_compare(const char * fn_ref);
+lv_test_screenshot_result_t lv_test_screenshot_compare(const char * fn_ref);
 
 /**********************
  *      MACROS
