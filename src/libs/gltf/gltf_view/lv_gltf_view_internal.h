b7ac6aeb6c59b1e5e2da2db6af74e33688a96208
Matt Kimball <greyworld@gmail.com>
2025-11-11 14:54:10 -0500

    feat(gltf): testing branch for direct buffer writes (experimental)

--- src/libs/gltf/gltf_view/lv_gltf_view_internal.h
+++ src/libs/gltf/gltf_view/lv_gltf_view_internal.h
@@ -26,6 +26,12 @@
  *********************/
 
 
+/* This is a temporary flag that should only be enabled for testing purposes.
+ * Ultimately we'll probably need this to be run-time selectable, per instance.
+ * But for now it's all on/off to test with. Note: This disables caching for gltf
+ * views, so only use it for continously updated setups. */
+#define LV_GLTF_DIRECT_BUFFER_WRITES 1
+
 /**********************
  *      TYPEDEFS
  **********************/
