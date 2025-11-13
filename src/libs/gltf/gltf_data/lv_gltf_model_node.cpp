933fc80cff874be0ccff237e9b7636ae5068b8a0
André <andre_miguel_costa@hotmail.com>
2025-11-13 17:37:13 +0100

    refactor: rename data to model

--- src/libs/gltf/gltf_data/lv_gltf_model_node.cpp
+++ src/libs/gltf/gltf_data/lv_gltf_model_node.cpp
@@ -11,11 +11,11 @@
 #if LV_USE_GLTF
 #include "fastgltf/types.hpp"
 #include "lv_gltf_model.h"
-#include "lv_gltf_data_internal.hpp"
 #include "../../../misc/lv_array.h"
 #include "../../../misc/lv_assert.h"
 #include "../../../misc/lv_types.h"
 #include "../../../stdlib/lv_string.h"
+#include "../../../stdlib/lv_sprintf.h"
 
 /*********************
  *      DEFINES
@@ -41,16 +41,16 @@
  *   GLOBAL FUNCTIONS
  **********************/
 
-void lv_gltf_model_nodes_init(lv_gltf_model_t * data, size_t size)
+void lv_gltf_model_nodes_init(lv_gltf_model_t * model, size_t size)
 {
-    lv_array_init(&data->nodes, size, sizeof(lv_gltf_model_node_t));
+    lv_array_init(&model->nodes, size, sizeof(lv_gltf_model_node_t));
 }
 
 void lv_gltf_model_node_init(lv_gltf_model_node_t * node, fastgltf::Node * fastgltf_node, const char * path,
                              const char * ip)
 {
     LV_ASSERT_NULL(node);
-    node->node = fastgltf_node;
+    node->fastgltf_node = fastgltf_node;
     node->path = lv_strdup(path);
     node->ip = lv_strdup(ip);
 
@@ -58,25 +58,30 @@ void lv_gltf_model_node_init(lv_gltf_model_node_t * node, fastgltf::Node * fastg
     LV_ASSERT_MALLOC(ip);
 }
 
-void lv_gltf_model_node_add(lv_gltf_model_t * data, const lv_gltf_model_node_t * model_node)
+void lv_gltf_model_node_add(lv_gltf_model_t * model, const lv_gltf_model_node_t * model_node)
 {
-    LV_ASSERT_NULL(data);
-    lv_array_push_back(&data->nodes, model_node);
+    lv_array_push_back(&model->nodes, model_node);
 }
 
-lv_gltf_model_node_t * lv_gltf_model_node_get_by_index(lv_gltf_model_t * data, size_t index)
+lv_gltf_model_node_t * lv_gltf_model_node_get_by_index(lv_gltf_model_t * model, size_t index)
 {
-    LV_ASSERT_NULL(data);
-    if(index >= lv_array_size(&data->nodes)) {
+    if(!model) {
+        LV_LOG_WARN("Failed to get node from NULL model");
         return nullptr;
     }
-    return (lv_gltf_model_node_t *)lv_array_at(&data->nodes, index);
+
+    const uint32_t count = lv_array_size(&model->nodes);
+    if(index >= count) {
+        LV_LOG_WARN("Invalid index. Max should be %" LV_PRIu32, count - 1);
+        return nullptr;
+    }
+    return (lv_gltf_model_node_t *)lv_array_at(&model->nodes, index);
 }
 
-lv_gltf_model_node_t * lv_gltf_model_node_get_by_ip(lv_gltf_model_t * data, const char * ip)
+lv_gltf_model_node_t * lv_gltf_model_node_get_by_ip(lv_gltf_model_t * model, const char * ip)
 {
-    for(size_t i = 0; i < data->nodes.size; ++i) {
-        lv_gltf_model_node_t * entry = (lv_gltf_model_node_t *) lv_array_at(&data->nodes, i);
+    for(size_t i = 0; i < model->nodes.size; ++i) {
+        lv_gltf_model_node_t * entry = (lv_gltf_model_node_t *) lv_array_at(&model->nodes, i);
         if(lv_streq(entry->ip, ip)) {
             return entry;
         }
@@ -84,11 +89,11 @@ lv_gltf_model_node_t * lv_gltf_model_node_get_by_ip(lv_gltf_model_t * data, cons
     return nullptr;
 }
 
-lv_gltf_model_node_t * lv_gltf_model_node_get_by_path(lv_gltf_model_t * data, const char * path)
+lv_gltf_model_node_t * lv_gltf_model_node_get_by_path(lv_gltf_model_t * model, const char * path)
 {
 
-    for(size_t i = 0; i < data->nodes.size; ++i) {
-        lv_gltf_model_node_t * entry = (lv_gltf_model_node_t *) lv_array_at(&data->nodes, i);
+    for(size_t i = 0; i < model->nodes.size; ++i) {
+        lv_gltf_model_node_t * entry = (lv_gltf_model_node_t *) lv_array_at(&model->nodes, i);
         if(lv_streq(entry->path, path)) {
             return entry;
         }
@@ -104,6 +109,7 @@ const char * lv_gltf_model_node_get_path(lv_gltf_model_node_t * node)
     }
     return node->path;
 }
+
 const char * lv_gltf_model_node_get_ip(lv_gltf_model_node_t * node)
 {
 
