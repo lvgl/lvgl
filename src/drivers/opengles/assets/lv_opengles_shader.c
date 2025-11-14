3a79936279351181086c60c68f86eed0e304d20f
Matt Kimball <greyworld@gmail.com>
2025-11-14 05:06:23 -0500

    refactor(opengles): restructured vertex shader conditionals

--- src/drivers/opengles/assets/lv_opengles_shader.c
+++ src/drivers/opengles/assets/lv_opengles_shader.c
@@ -183,22 +183,16 @@ static const char * src_vertex_shader_v300es = R"(
     {
         gl_Position = vec4((u_VertexTransform * vec3(position.xy, 1)).xy, position.zw);
         v_TexCoord = texCoord;
+        is_gray = (abs(u_ColorDepth - 8.0) < 0.1) ? 1 : 0;
 
         if (u_IsFill) {
-            if (abs(u_ColorDepth - 8.0) < 0.1) {
-                is_gray = 1;
+            if (is_gray == 1) {
                 fill_color_alpha = vec4(u_FillColor.rrr, 1.0) * u_Opa;
             } else {
-                is_gray = 0;
                 fill_color_alpha = vec4((u_FillColor.rgb * u_Opa), u_Opa);
             }
         } else {
             fill_color_alpha = vec4(0.0, 0.0, 0.0, -1.0);
-            if (abs(u_ColorDepth - 8.0) < 0.1) {
-                is_gray = 1;
-            } else {
-                is_gray = 0;
-            }
         }
     }
 )";
@@ -221,7 +215,7 @@ static const char *src_fragment_shader_v300es = R"(
 
     void main()
     {
-        if (-1.0 != fill_color_alpha.a) {
+        if (fill_color_alpha.a != -1.0) {
             color = fill_color_alpha;
         } else {
             color = texture(u_Texture, v_TexCoord);
@@ -230,7 +224,7 @@ static const char *src_fragment_shader_v300es = R"(
              * derivative calculation errors from interpolator increments flipping direction.
              * color = textureLod(u_Texture, v_TexCoord, u_LodLevel);
              */
-            if (0 != is_gray) {
+            if (is_gray != 0) {
                 color.r *= u_Opa;
                 color.gba = vec3(color.rr, u_Opa);
             } else {
