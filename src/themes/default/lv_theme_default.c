9d9bc318eb26a13e80f9e9db97240db9c06323c7
escherstair <22594460+escherstair@users.noreply.github.com>
2025-11-20 15:53:18 +0100

    handle polar chart in themes

--- src/themes/default/lv_theme_default.c
+++ src/themes/default/lv_theme_default.c
@@ -96,6 +96,10 @@ typedef struct {
     lv_style_t chart_series, chart_indic, chart_bg;
 #endif
 
+#if LV_USE_POLAR_CHART
+    lv_style_t polar_chart_series, polar_chart_indic, polar_chart_bg;
+#endif
+
 #if LV_USE_DROPDOWN
     lv_style_t dropdown_list;
 #endif
@@ -448,6 +452,27 @@ static void style_init(my_theme_t * theme)
     int32_t chart_size = LV_DPX_CALC(theme->disp_dpi, 8);
     lv_style_set_size(&theme->styles.chart_series, chart_size, chart_size);
     lv_style_set_pad_column(&theme->styles.chart_series, LV_DPX_CALC(theme->disp_dpi, 2));
+
+    style_init_reset(&theme->styles.chart_indic);
+    lv_style_set_radius(&theme->styles.chart_indic, LV_RADIUS_CIRCLE);
+    lv_style_set_size(&theme->styles.chart_indic, chart_size, chart_size);
+    lv_style_set_bg_color(&theme->styles.chart_indic, theme->base.color_primary);
+    lv_style_set_bg_opa(&theme->styles.chart_indic, LV_OPA_COVER);
+#endif
+
+#if LV_USE_POLAR_CHART
+    style_init_reset(&theme->styles.polar_chart_bg);
+    lv_style_set_border_post(&theme->styles.polar_chart_bg, false);
+    lv_style_set_pad_column(&theme->styles.polar_chart_bg, LV_DPX_CALC(theme->disp_dpi, 10));
+    lv_style_set_line_color(&theme->styles.polar_chart_bg, theme->color_grey);
+
+    style_init_reset(&theme->styles.polar_chart_series);
+    lv_style_set_line_width(&theme->styles.polar_chart_series, LV_DPX_CALC(theme->disp_dpi, 3));
+    lv_style_set_radius(&theme->styles.polar_chart_series, LV_DPX_CALC(theme->disp_dpi, 3));
+
+    int32_t chart_size = LV_DPX_CALC(theme->disp_dpi, 8);
+    lv_style_set_size(&theme->styles.polar_chart_series, chart_size, chart_size);
+    lv_style_set_pad_column(&theme->styles.polar_chart_series, LV_DPX_CALC(theme->disp_dpi, 2));
 
     style_init_reset(&theme->styles.chart_indic);
     lv_style_set_radius(&theme->styles.chart_indic, LV_RADIUS_CIRCLE);
--- src/themes/mono/lv_theme_mono.c
+++ src/themes/mono/lv_theme_mono.c
@@ -58,6 +58,9 @@ typedef struct {
 #if LV_USE_CHART
     lv_style_t chart_indic;
 #endif
+#if LV_USE_POLAR_CHART
+    lv_style_t polar_chart_indic;
+#endif
 } my_theme_styles_t;
 
 struct _my_theme_t {
@@ -174,6 +177,14 @@ static void style_init(my_theme_t * theme, bool dark_bg, const lv_font_t * font)
     lv_style_set_size(&theme->styles.chart_indic, lv_display_dpx(theme->base.disp, 8), lv_display_dpx(theme->base.disp, 8));
     lv_style_set_bg_color(&theme->styles.chart_indic, COLOR_FG);
     lv_style_set_bg_opa(&theme->styles.chart_indic, LV_OPA_COVER);
+#endif
+
+#if LV_USE_POLAR_CHART
+    style_init_reset(&theme->styles.polar_chart_indic);
+    lv_style_set_radius(&theme->styles.polar_chart_indic, LV_RADIUS_CIRCLE);
+    lv_style_set_size(&theme->styles.polar_chart_indic, lv_display_dpx(theme->base.disp, 8), lv_display_dpx(theme->base.disp, 8));
+    lv_style_set_bg_color(&theme->styles.polar_chart_indic, COLOR_FG);
+    lv_style_set_bg_opa(&theme->styles.polar_chart_indic, LV_OPA_COVER);
 #endif
 }
 
