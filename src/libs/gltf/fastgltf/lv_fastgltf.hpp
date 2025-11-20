fc467695ed209f38fc7d048d1345385499584ee0
Matt Kimball <greyworld@gmail.com>
2025-11-20 18:04:16 -0500

    refactor(gltf): animation and math fixes

--- src/libs/gltf/fastgltf/lv_fastgltf.hpp
+++ src/libs/gltf/fastgltf/lv_fastgltf.hpp
@@ -18,6 +18,34 @@
 
 namespace fastgltf
 {
+/**
+ * Computes the transform matrix for a given node
+ */
+FASTGLTF_EXPORT inline auto getLocalTransformMatrix(const Node& node) {
+	return visit_exhaustive(visitor {
+		[&](const math::fmat4x4& matrix) {
+				return matrix;
+		},
+		[&](const TRS& trs) {
+			/* This may appear backwards, like it is applying the scale last, but it should 
+			 * be first.  However, these operations alter the matrix *in place*, they are *not* 
+			 * matrix multiplications and they should *not* be applied in the standard 
+			 * post-multiplicative order, so this is correct as shown.  This method is used for
+			 * performance reasons, since it uses less multiplications and gets the same results.
+			 * This function is being added to fastgltf and will be removed from this file soon.
+			 */
+			return scale(rotate(translate(math::fmat4x4(), trs.translation), trs.rotation), trs.scale);
+		}
+	}, node.transform);
+}
+
+FASTGLTF_EXPORT inline void removeScale(fastgltf::math::fmat4x4& matrix) {
+	auto scale = math::fvec3( length(matrix.col(0)), length(matrix.col(1)), length(matrix.col(2)) );
+	matrix.col(0) /= scale.x();
+	matrix.col(1) /= scale.y();
+	matrix.col(2) /= scale.z();
+}
+
 FASTGLTF_EXPORT template <typename AssetType, typename Callback>
 #if FASTGLTF_HAS_CONCEPTS
 requires std::same_as<std::remove_cvref_t<AssetType>, Asset> &&
@@ -114,9 +142,26 @@ inline void custom_iterate_scene_nodes(AssetType&& asset, std::size_t sceneIndex
             }
         }
     };
-    // auto tempmat = fastgltf::math::fmat4x4(*initial);
-    //for (auto& sceneNode : scene.nodeIndices) function(sceneNode, &tempmat, function);
     for(auto & sceneNode : scene.nodeIndices) function(sceneNode, *initial, function);
+}
+
+FASTGLTF_EXPORT template <typename AssetType, typename Callback>
+#if FASTGLTF_HAS_CONCEPTS
+    requires std::same_as<std::remove_cvref_t<AssetType>, Asset>
+    && std::is_invocable_v<Callback, fastgltf::Node &, uint32_t placement>
+#endif
+inline void iterate_scene_nodes_get_placement(AssetType&& asset, std::size_t sceneIndex, Callback callback)
+{
+	uint32_t placement = 0;
+    auto & scene = asset.scenes[sceneIndex];
+    auto & nodes = asset.nodes;
+    auto function = [&](std::size_t nodeIndex, auto & self) -> void {
+        auto & node = nodes[nodeIndex];
+        std::invoke(callback, node, placement);
+		placement++;
+		for(auto & child : node.children) self(child, self);
+    };
+    for(auto & sceneNode : scene.nodeIndices) function(sceneNode, function);
 }
 }
 
