8236e5df4266b823b4fefee667772da2f0c3034a
escherstair <22594460+escherstair@users.noreply.github.com>
2025-11-25 12:52:01 +0100

    fix build test

--- tests/src/test_cases_perf/test_polarchart.c
+++ tests/src/test_cases_perf/test_polarchart.c
@@ -17,10 +17,10 @@ void setUp(void)
 void test_polarchart(void)
 {
     lv_polarchart_add_series(polarchart, red_color,
-                             LV_CHART_AXIS_RADIAL);
+                             LV_POLARCHART_AXIS_RADIAL);
 
     TEST_ASSERT_MAX_TIME(lv_polarchart_add_series, 1, polarchart, red_color,
-                         LV_CHART_AXIS_RADIAL);
+                         LV_POLARCHART_AXIS_RADIAL);
 
     for(size_t i = 0; i < 10; ++i) {
         uint16_t points_in_series = lv_polarchart_get_point_count(polarchart);
