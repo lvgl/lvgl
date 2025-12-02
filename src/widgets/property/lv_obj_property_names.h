62f75bfe438f21c978f160fe81fc52692555423f
Neo Xu <neo.xu1990@gmail.com>
2025-12-02 13:16:59 +0800

    feat(line): add property interface support
    Add property support for line widget:
    - Y_INVERT: y coordinate inversion flag
    
    Note: POINTS property not added as it requires two parameters (array + count).
    
    Also add test_line_properties() test function.

--- src/widgets/property/lv_obj_property_names.h
+++ src/widgets/property/lv_obj_property_names.h
@@ -19,6 +19,7 @@
     extern const lv_property_name_t lv_keyboard_property_names[4];
     extern const lv_property_name_t lv_label_property_names[4];
     extern const lv_property_name_t lv_led_property_names[2];
+    extern const lv_property_name_t lv_line_property_names[1];
     extern const lv_property_name_t lv_obj_property_names[74];
     extern const lv_property_name_t lv_roller_property_names[3];
     extern const lv_property_name_t lv_slider_property_names[8];
