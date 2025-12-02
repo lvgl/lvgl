e697e1b38be85722e1651fece6be52c7b9c934a5
Neo Xu <neo.xu1990@gmail.com>
2025-12-02 13:11:19 +0800

    feat(led): add property interface support
    Add property support for led widget:
    - COLOR: LED color (with helper getter)
    - BRIGHTNESS: LED brightness
    
    Also add test_led_properties() test function.
    
    Signed-off-by: Neo Xu <neo.xu1990@gmail.com>

--- src/widgets/led/lv_led.h
+++ src/widgets/led/lv_led.h
@@ -36,6 +36,14 @@ extern "C" {
 
 LV_ATTRIBUTE_EXTERN_DATA extern const lv_obj_class_t lv_led_class;
 
+#if LV_USE_OBJ_PROPERTY
+enum _lv_property_led_id_t {
+    LV_PROPERTY_ID(LED, COLOR,      LV_PROPERTY_TYPE_COLOR, 0),
+    LV_PROPERTY_ID(LED, BRIGHTNESS, LV_PROPERTY_TYPE_INT,   1),
+    LV_PROPERTY_LED_END,
+};
+#endif
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
