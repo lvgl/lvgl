39e359af46b6b02812995e023acac4c01829e68c
cnjhb <hikariz@qq.com>
2025-12-04 09:35:43 +0800

    update

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -171,6 +171,14 @@ static const lv_property_ops_t properties[] = {
         .getter = lv_obj_get_scroll_end_helper,
     },
     {
+        .id = LV_PROPERTY_OBJ_SCROLL_X,
+        .getter = lv_obj_get_scroll_x,
+    },
+    {
+        .id = LV_PROPERTY_OBJ_SCROLL_Y,
+        .getter = lv_obj_get_scroll_y,
+    },
+    {
         .id = LV_PROPERTY_OBJ_EXT_DRAW_SIZE,
         .getter = lv_obj_get_ext_draw_size,
     },
