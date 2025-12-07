0e0db1afa9b898d28af6c42c61641da45009ce10
Neo Xu <neo.xu1990@gmail.com>
2025-12-08 00:25:11 +0800

    chore(widgets): use helper for properties in class
    Signed-off-by: Neo Xu <neo.xu1990@gmail.com>

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -86,7 +86,7 @@ static void delete_on_screen_unloaded_event_cb(lv_event_t * e);
  *  STATIC VARIABLES
  **********************/
 #if LV_USE_OBJ_PROPERTY
-static const lv_property_ops_t properties[] = {
+static const lv_property_ops_t lv_obj_properties[] = {
     {
         .id = LV_PROPERTY_OBJ_PARENT,
         .setter = lv_obj_set_parent,
@@ -213,18 +213,7 @@ const lv_obj_class_t lv_obj_class = {
     .instance_size = (sizeof(lv_obj_t)),
     .base_class = NULL,
     .name = "lv_obj",
-#if LV_USE_OBJ_PROPERTY
-    .prop_index_start = LV_PROPERTY_OBJ_START,
-    .prop_index_end = LV_PROPERTY_OBJ_END,
-    .properties = properties,
-    .properties_count = sizeof(properties) / sizeof(properties[0]),
-
-#if LV_USE_OBJ_PROPERTY_NAME
-    .property_names = lv_obj_property_names,
-    .names_count = sizeof(lv_obj_property_names) / sizeof(lv_property_name_t),
-#endif
-
-#endif
+    LV_PROPERTY_CLASS_FIELDS(obj, OBJ)
 };
 
 /**********************
