3d460cadc80ef6baea5bc9dbc24b2622bc7a6463
红桃六 <liuhongchao@xiaomi.com>
2025-12-20 14:58:35 +0800

    fix(demo): fix compiler warnings regarding the use of LV_UNUSED (#9403)
    Signed-off-by: liuhongchao <liuhongchao@xiaomi.com>

--- examples/widgets/switch/lv_example_switch_1.c
+++ examples/widgets/switch/lv_example_switch_1.c
@@ -5,8 +5,8 @@ static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t * obj = lv_event_get_target_obj(e);
+    LV_UNUSED(obj);
     if(code == LV_EVENT_VALUE_CHANGED) {
-        LV_UNUSED(obj);
         LV_LOG_USER("State: %s\n", lv_obj_has_state(obj, LV_STATE_CHECKED) ? "On" : "Off");
     }
 }
