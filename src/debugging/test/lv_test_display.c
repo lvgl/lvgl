0475806cf3827505f44ad8758355ed60195de3cd
André <andre_miguel_costa@hotmail.com>
2025-12-22 10:02:28 +0100

    feat: use pbuffers in test display with draw opengles

--- src/debugging/test/lv_test_display.c
+++ src/debugging/test/lv_test_display.c
@@ -10,12 +10,8 @@
 #if LV_USE_TEST
 
 #include "../../core/lv_global.h"
-#include "../../drivers/opengles/lv_opengles_debug.h"
-#include "../../drivers/opengles/lv_opengles_texture.h"
-#include "../../drivers/opengles/lv_opengles_private.h"
-#include "../../drivers/opengles/lv_opengles_driver.h"
 #include <stdlib.h>
-#include "../../lv_init.h"
+#include "../../drivers/opengles/lv_opengles_pbuffer.h"
 
 /*********************
  *      DEFINES
@@ -33,6 +29,11 @@ static lv_draw_buf_t create_test_draw_buf(int32_t hor_res, int32_t ver_res, lv_c
 static void buf_changed_event_cb(lv_event_t * e);
 static void delete_event_cb(lv_event_t * e);
 
+static void refr_ready_event_cb(lv_event_t * e);
+
+static lv_display_t * create_sw_display(int32_t hor_res, int32_t ver_res);
+static lv_display_t * create_opengl_display(int32_t hor_res, int32_t ver_res);
+
 /**********************
  *  STATIC VARIABLES
  **********************/
@@ -48,34 +49,23 @@ static void delete_event_cb(lv_event_t * e);
 
 lv_display_t * lv_test_display_create(int32_t hor_res, int32_t ver_res)
 {
-    const lv_color_format_t cf = LV_COLOR_FORMAT_ARGB8888;
+    const lv_color_format_t cf = LV_COLOR_FORMAT_XRGB8888;
     _state.draw_buf = create_test_draw_buf(hor_res, ver_res, cf);
 
-#if LV_USE_DRAW_OPENGLES
-    glfwInit();
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    _state.window = glfwCreateWindow(hor_res, ver_res, "", NULL, NULL);
-    glfwMakeContextCurrent(_state.window);
+    lv_display_t * disp;
+    if(LV_USE_DRAW_OPENGLES && LV_USE_EGL) {
+        disp = create_opengl_display(hor_res, ver_res);
+    }
+    else {
+        disp = create_sw_display(hor_res, ver_res);
+    }
 
-    if(!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-        glfwDestroyWindow(_state.window);
-        lv_draw_buf_destroy(&_state.draw_buf);
+    if(!disp) {
+        lv_free(_state.draw_buf.unaligned_data);
         return NULL;
     }
-    lv_opengles_init();
-    lv_display_t * disp = lv_opengles_texture_create(hor_res, ver_res);
-    lv_display_set_render_mode(disp, LV_DISPLAY_RENDER_MODE_FULL);
-#else
-    lv_display_t * disp = lv_display_create(hor_res, ver_res);
-    lv_display_set_render_mode(disp, LV_DISPLAY_RENDER_MODE_DIRECT);
-#endif
 
-    lv_display_set_draw_buffers(disp, &_state.draw_buf, NULL);
     lv_display_set_color_format(disp, cf);
-    lv_display_set_flush_cb(disp, flush_cb);
-    lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_COLOR_FORMAT_CHANGED, NULL);
-    lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_RESOLUTION_CHANGED, NULL);
-    lv_display_add_event_cb(disp, delete_event_cb, LV_EVENT_DELETE, NULL);
 
     return disp;
 }
@@ -83,6 +73,34 @@ lv_display_t * lv_test_display_create(int32_t hor_res, int32_t ver_res)
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+static lv_display_t * create_sw_display(int32_t hor_res, int32_t ver_res)
+{
+    lv_display_t * disp = lv_display_create(hor_res, ver_res);
+    if(!disp) {
+        LV_LOG_ERROR("Failed to create display");
+        return NULL;
+    }
+
+    lv_display_set_draw_buffers(disp, &_state.draw_buf, NULL);
+    lv_display_set_render_mode(disp, LV_DISPLAY_RENDER_MODE_DIRECT);
+    lv_display_set_flush_cb(disp, flush_cb);
+    lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_COLOR_FORMAT_CHANGED, NULL);
+    lv_display_add_event_cb(disp, buf_changed_event_cb, LV_EVENT_RESOLUTION_CHANGED, NULL);
+    lv_display_add_event_cb(disp, delete_event_cb, LV_EVENT_DELETE, NULL);
+    return disp;
+
+}
+static lv_display_t * create_opengl_display(int32_t hor_res, int32_t ver_res)
+{
+    lv_display_t * disp = lv_opengles_pbuffer_create(hor_res, ver_res);
+    if(!disp) {
+        LV_LOG_ERROR("Failed to create pbuffer display");
+        return NULL;
+    }
+    /* Listen for refresh complete events to copy data back to CPU */
+    lv_display_add_event_cb(disp, refr_ready_event_cb, LV_EVENT_REFR_READY, NULL);
+    return disp;
+}
 
 static lv_draw_buf_t create_test_draw_buf(int32_t hor_res, int32_t ver_res, lv_color_format_t cf)
 {
@@ -117,18 +135,12 @@ static void buf_changed_event_cb(lv_event_t * e)
 static void delete_event_cb(lv_event_t * e)
 {
     LV_UNUSED(e);
-    lv_display_t * disp = lv_event_get_target(e);
-    lv_draw_buf_t * draw_buf = lv_display_get_buf_active(disp);
-    lv_free(draw_buf->unaligned_data);
-    draw_buf->unaligned_data = NULL;
-#if LV_USE_DRAW_OPENGLES
-    if(_state.window) {
-        glfwDestroyWindow(_state.window);
-        _state.window = NULL;
+    lv_draw_buf_t * draw_buf = &_state.draw_buf;
+
+    if(draw_buf->unaligned_data) {
+        lv_free(draw_buf->unaligned_data);
+        draw_buf->unaligned_data = NULL;
     }
-    lv_deinit();
-    glfwTerminate();
-#endif
 }
 
 static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * color_p)
@@ -136,32 +148,17 @@ static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * colo
     LV_UNUSED(area);
     LV_UNUSED(color_p);
     lv_display_flush_ready(disp);
-#if LV_USE_DRAW_OPENGLES
-    if(!lv_display_flush_is_last(disp)) {
-        return;
-    }
-    const lv_color_format_t cf = lv_display_get_color_format(disp);
-    uint32_t width = _state.draw_buf.header.w;
-    uint32_t height = _state.draw_buf.header.h;
-    uint8_t * data = _state.draw_buf.data;
-
-    lv_opengles_render_params_t params  = {
-        .h_flip = false,
-        .v_flip = false,
-        .rb_swap = false
-    };
-    lv_opengles_render_display(disp, &params);
-
-    if(cf == LV_COLOR_FORMAT_XRGB8888 || cf == LV_COLOR_FORMAT_ARGB8888) {
-        GL_CALL(glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data));
-    }
-    else if(cf == LV_COLOR_FORMAT_RGB888) {
-        GL_CALL(glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data));
-    }
-    else if(cf == LV_COLOR_FORMAT_RGB565) {
-        GL_CALL(glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, data));
-    }
-#endif
 }
+
+#if LV_USE_DRAW_OPENGLES && LV_USE_EGL
+static void refr_ready_event_cb(lv_event_t * e)
+{
+    lv_display_t * disp = lv_event_get_target(e);
+    lv_result_t res = lv_opengles_pbuffer_read_to_draw_buf(disp, &_state.draw_buf);
+    if(res != LV_RESULT_OK) {
+        LV_LOG_ERROR("Failed to read pbuffer to draw buffer");
+    }
+}
+#endif
 
 #endif /*LV_USE_TEST*/
