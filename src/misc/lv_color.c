803d0a0672130e22afc42a140ce083fb4b20d626
dynamo <95570158+dynamo0003@users.noreply.github.com>
2026-01-09 07:30:00 +0100

    fix (misc): lv_color_format_get_bpp() returns wrong bpp for LV_COLOR_FORMAT_RGB565A8

--- src/misc/lv_color.c
+++ src/misc/lv_color.c
@@ -65,7 +65,6 @@ uint8_t lv_color_format_get_bpp(lv_color_format_t cf)
         case LV_COLOR_FORMAT_NEMA_TSC12:
         case LV_COLOR_FORMAT_NEMA_TSC12A:
             return 12;
-        case LV_COLOR_FORMAT_RGB565A8:
         case LV_COLOR_FORMAT_RGB565:
         case LV_COLOR_FORMAT_RGB565_SWAPPED:
         case LV_COLOR_FORMAT_YUY2:
@@ -73,7 +72,7 @@ uint8_t lv_color_format_get_bpp(lv_color_format_t cf)
         case LV_COLOR_FORMAT_ARGB1555:
         case LV_COLOR_FORMAT_ARGB4444:
             return 16;
-
+        case LV_COLOR_FORMAT_RGB565A8:
         case LV_COLOR_FORMAT_ARGB8565:
         case LV_COLOR_FORMAT_RGB888:
             return 24;
