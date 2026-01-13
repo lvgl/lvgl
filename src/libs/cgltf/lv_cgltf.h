acdfd3d0bbb400067e0ddf9eeb9f221e088b2c88
pengyiqiang <pengyiqiang@xiaomi.com>
2026-01-13 13:55:29 +0800

    upload cgltf lib

---
+++ src/libs/cgltf/lv_cgltf.h
@@ -0,0 +1,37 @@
+/**
+ * @file lv_cgltf.h
+ *
+ */
+
+#ifndef LV_CGLTF_H
+#define LV_CGLTF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_CGLTF_H*/
