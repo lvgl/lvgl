ed4391c3b981314f20c6a7e1792e75ebbc361140
James Roy <rruuaanng@outlook.com>
2026-01-13 11:23:43 +0800

    Fix AI check error

--- src/widgets/msgbox/lv_msgbox.h
+++ src/widgets/msgbox/lv_msgbox.h
@@ -68,9 +68,9 @@ lv_obj_t * lv_msgbox_add_text(lv_obj_t * obj, const char * text);
  * Add a formatted text to the content area of message box. Multiple texts will be created below each other.
  * @param obj           pointer to a message box
  * @param fmt           `printf`-like format string
- * @return              the created button
+ * @return              the created label
  */
-lv_obj_t * lv_msgbox_add_text_fmt(lv_obj_t * obj, const char * fmt, ...);
+lv_obj_t * lv_msgbox_add_text_fmt(lv_obj_t * obj, const char * fmt, ...) LV_FORMAT_ATTRIBUTE(2, 3);
 
 /**
  * Add a button to the footer of to the message box. It also creates a footer.
