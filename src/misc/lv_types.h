de31a62843848aa5e5b0c3e2e36aa2cb9320fb31
André <andre_miguel_costa@hotmail.com>
2026-01-15 11:04:13 +0100

    feat: remove leftover typedefs and defines

--- src/misc/lv_types.h
+++ src/misc/lv_types.h
@@ -384,12 +384,6 @@ typedef struct _lv_sysmon_perf_info_t lv_sysmon_perf_info_t;
 #endif /*LV_USE_SYSMON*/
 
 
-typedef struct _lv_xml_component_scope_t lv_xml_component_scope_t;
-
-typedef struct _lv_xml_parser_state_t lv_xml_parser_state_t;
-
-typedef struct _lv_xml_load_t lv_xml_load_t;
-
 #if LV_USE_EVDEV
 typedef struct _lv_evdev_discovery_t lv_evdev_discovery_t;
 #endif
