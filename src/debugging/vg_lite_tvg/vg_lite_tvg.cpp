963f4d7670f84c066f1baabe6731e8b56fa9120a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2026-01-21 13:02:47 +0100

    fix code formatting

--- src/debugging/vg_lite_tvg/vg_lite_tvg.cpp
+++ src/debugging/vg_lite_tvg/vg_lite_tvg.cpp
@@ -936,11 +936,12 @@ extern "C" {
         TVG_CHECK_RETURN_VG_ERROR(shape_append_path(shape, path, matrix));
         if(path->stroke) {
             TVG_CHECK_RETURN_VG_ERROR(shape->stroke(path->stroke->cap_style));
-            TVG_CHECK_RETURN_VG_ERROR(shape->stroke(B(path->stroke_color), G(path->stroke_color), R(path->stroke_color), A(path->stroke_color)));
+            TVG_CHECK_RETURN_VG_ERROR(shape->stroke(B(path->stroke_color), G(path->stroke_color), R(path->stroke_color),
+                                                    A(path->stroke_color)));
             TVG_CHECK_RETURN_VG_ERROR(shape->stroke(path->stroke->line_width));
         }
 
-		TVG_CHECK_RETURN_VG_ERROR(shape->transform(matrix_conv(matrix)));
+        TVG_CHECK_RETURN_VG_ERROR(shape->transform(matrix_conv(matrix)));
         TVG_CHECK_RETURN_VG_ERROR(shape->fill(fill_rule_conv(fill_rule)););
         TVG_CHECK_RETURN_VG_ERROR(shape->blend(blend_method_conv(blend)));
         TVG_CHECK_RETURN_VG_ERROR(shape->fill(TVG_COLOR(color)));
