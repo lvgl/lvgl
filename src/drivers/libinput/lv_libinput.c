6ea3101932966fcd6a010f3d8ea5fa2e450b9e09
Jef Driesen <jefdriesen@telenet.be>
2026-03-04 08:48:47 +0100

    fix(libinput): Close the directory handle
    The closedir call is needed to free the directory handle and avoid a
    memory leak.

--- src/drivers/libinput/lv_libinput.c
+++ src/drivers/libinput/lv_libinput.c
@@ -264,6 +264,7 @@ static bool _rescan_devices(void)
             perror("could not allocate memory for device node path");
             libinput_unref(context);
             _reset_scanned_devices();
+            closedir(dir);
             return false;
         }
         strcpy(path, "/dev/input/");
@@ -293,11 +294,13 @@ static bool _rescan_devices(void)
             free(path);
             libinput_unref(context);
             _reset_scanned_devices();
+            closedir(dir);
             return false;
         }
     }
 
     libinput_unref(context);
+    closedir(dir);
     return true;
 }
 
