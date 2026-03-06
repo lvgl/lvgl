69981eee8ce4241ae9767a1b185a0f98e40895bf
Felix Biego <fbiego.fb@gmail.com>
2026-03-06 11:50:28 +0300

    fix -1 if 0
    - update test images

--- src/widgets/image/lv_image.c
+++ src/widgets/image/lv_image.c
@@ -529,8 +529,8 @@ void lv_image_get_pivot(lv_obj_t * obj, lv_point_t * pivot)
 
     lv_image_t * img = (lv_image_t *)obj;
 
-    pivot->x = lv_pct_to_px(img->pivot.x, img->w - 1);
-    pivot->y = lv_pct_to_px(img->pivot.y, img->h - 1);
+    pivot->x = lv_pct_to_px(img->pivot.x, img->w > 0 ? img->w - 1 : 0);
+    pivot->y = lv_pct_to_px(img->pivot.y, img->h > 0 ? img->h - 1 : 0);
 }
 
 int32_t lv_image_get_scale(lv_obj_t * obj)
