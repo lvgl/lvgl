2522f4d23efea12e0d78dc0ad328f1e60a673ed5
André <andre_miguel_costa@hotmail.com>
2026-03-20 12:33:14 +0100

    build: add glfw support

--- env_support/cmake/dependencies.cmake
+++ env_support/cmake/dependencies.cmake
@@ -31,6 +31,10 @@ if(CONFIG_LV_USE_LINUX_DRM_GBM_BUFFERS OR CONFIG_LV_LINUX_DRM_USE_EGL)
   include(${CMAKE_CURRENT_LIST_DIR}/dependencies/gbm.cmake)
 endif()
 
+if(CONFIG_LV_USE_GLFW)
+  include(${CMAKE_CURRENT_LIST_DIR}/dependencies/glfw.cmake)
+endif()
+
 # ====== Indev ====== #
 if(CONFIG_LV_USE_EVDEV)
   include(${CMAKE_CURRENT_LIST_DIR}/dependencies/evdev.cmake)
