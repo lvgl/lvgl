a65f828780df2f49ef56b0be6c870d479794f1db
André <andre_miguel_costa@hotmail.com>
2026-03-20 12:35:04 +0100

    build: add x11 support

--- env_support/cmake/dependencies.cmake
+++ env_support/cmake/dependencies.cmake
@@ -35,6 +35,10 @@ if(CONFIG_LV_USE_WAYLAND)
   include(${CMAKE_CURRENT_LIST_DIR}/dependencies/wayland.cmake)
 endif()
 
+if(CONFIG_LV_USE_X11)
+  include(${CMAKE_CURRENT_LIST_DIR}/dependencies/x11.cmake)
+endif()
+
 if(CONFIG_LV_USE_GLFW)
   include(${CMAKE_CURRENT_LIST_DIR}/dependencies/glfw.cmake)
 endif()
