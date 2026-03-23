70f8d16475c159d1d329eaba7ced6937d464c156
André <andre_miguel_costa@hotmail.com>
2026-03-23 11:28:44 +0100

    build(cmake): add gbm support

--- env_support/cmake/dependencies.cmake
+++ env_support/cmake/dependencies.cmake
@@ -30,3 +30,7 @@ endif()
 if(CONFIG_LV_USE_LINUX_DRM)
   include(${CMAKE_CURRENT_LIST_DIR}/dependencies/drm.cmake)
 endif()
+
+if(CONFIG_LV_USE_LINUX_DRM_GBM_BUFFERS OR CONFIG_LV_LINUX_DRM_USE_EGL)
+  include(${CMAKE_CURRENT_LIST_DIR}/dependencies/gbm.cmake)
+endif()
