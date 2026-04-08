070d6a4ad1065702c80c36e0e60f49b5a047aa43
Matt Kimball <greyworld@gmail.com>
2026-04-08 00:40:41 -0400

    use wayland API to free keyboard proxy ref

--- src/drivers/wayland/lv_wayland_keyboard.c
+++ src/drivers/wayland/lv_wayland_keyboard.c
@@ -127,9 +127,15 @@ void lv_wayland_seat_keyboard_delete(lv_wl_seat_keyboard_t * seat_keyboard)
 {
     lv_wayland_update_indevs(keyboard_read, NULL);
 
-    xkb_keymap_unref(seat_keyboard->xkb_keymap);
-    xkb_state_unref(seat_keyboard->xkb_state);
-    lv_free(seat_keyboard->wl_keyboard);
+    if(seat_keyboard->xkb_keymap) {
+        xkb_keymap_unref(seat_keyboard->xkb_keymap);
+    }
+    if(seat_keyboard->xkb_state) {
+        xkb_state_unref(seat_keyboard->xkb_state);
+    }
+    if(seat_keyboard->wl_keyboard) {
+        wl_keyboard_destroy(seat_keyboard->wl_keyboard);
+    }
 
     lv_free(seat_keyboard);
 }
