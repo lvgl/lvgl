107bf4f05f2077021a66aa2625afcf00c293d12e
Joshua Watt <JPEWhacker@gmail.com>
2026-04-16 15:04:54 -0600

    opengles: Remove GL_BGRA usage
    OpenGLES 2.0 doesn't specify GL_BGRA as a valid format, and it's no
    longer needed since LVGL correctly renders textures into the native RGBA
    format.

--- src/drivers/opengles/lv_opengles_texture.c
+++ src/drivers/opengles/lv_opengles_texture.c
@@ -276,7 +276,7 @@ static void flush_cb(lv_display_t * disp, const lv_area_t * area, uint8_t * px_m
         GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB565, disp->hor_res, disp->ver_res, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5,
                              texture->fb1));
 #elif LV_COLOR_DEPTH == 32
-        GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, disp->hor_res, disp->ver_res, 0, GL_BGRA, GL_UNSIGNED_BYTE,
+        GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, disp->hor_res, disp->ver_res, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                              texture->fb1));
 #else
 #error("Unsupported color format")
