26111246f17bf539d289ceed3ba333efc797f49b
André Costa <andre_miguel_costa@hotmail.com>
2026-04-24 12:48:39 +0200

    ci(docs): add mdx linter and enable checks for PRs (#10013)

--- scripts/build_html_examples.sh
+++ scripts/build_html_examples.sh
@@ -45,12 +45,12 @@ python ./lvgl/scripts/generate_lv_conf.py \
   --config $LV_CONF_PATH \
   --defaults lvgl/configs/ci/docs/lv_conf_docs.defaults
 
-mkdir cmbuild
-cd cmbuild
-emcmake cmake .. -DLV_BUILD_CONF_PATH=$LV_CONF_PATH -DLVGL_CHOSEN_DEMO=lv_example_noop -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-emmake make -j$(nproc)
-rm -rf CMakeFiles
-cd ..
+
+emcmake cmake -B cmbuild -GNinja -DLV_BUILD_CONF_PATH=$LV_CONF_PATH -DLVGL_CHOSEN_DEMO=lv_example_noop -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+cmake --build cmbuild
+
+rm -rf cmbuild/CMakeFiles
+
 if [ "$ARG_1" != "--symlink" ]; then
   cp -a cmbuild ../docs/built_lv_examples
 else
