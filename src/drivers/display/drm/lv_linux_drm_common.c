60820c8d4e05588ce649a8f2c7e58cd6615b7427
Chisheng Chen <johnny1001s000602@gmail.com>
2026-04-30 15:37:35 +0800

    feat(drm): add libdrm fallback for drm device probe
    The current implementation relies on sysfs to find a drm device
    which has a connector with CONNECTED state. However, in some minimal
    Linux on embedded platforms, sysfs is disabled to reduce image size
    and RAM usage.
    
    This makes lv_linux_drm_find_device_path automatically fallback to
    native drm device probe using libdrm API to find the available drm
    device if the sysfs is not available.
    
    Signed-off-by: Chisheng Chen <johnny1001s000602@gmail.com>

--- src/drivers/display/drm/lv_linux_drm_common.c
+++ src/drivers/display/drm/lv_linux_drm_common.c
@@ -13,8 +13,9 @@
 
 #include <dirent.h>
 #include <xf86drmMode.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#include "lv_linux_drm.h"
 #include "../../../stdlib/lv_sprintf.h"
 
 /*********************
@@ -22,6 +23,7 @@
  *********************/
 
 #define LV_DRM_CLASS_DIR "/sys/class/drm"
+#define LV_DRM_CARD_DIR "/dev/dri"
 #define LV_DRM_CARD_PATH "/dev/dri/card"
 
 /**********************
@@ -33,6 +35,7 @@
  **********************/
 
 static char * find_by_class(void);
+static char * find_by_drm_dev(void);
 
 /**********************
  *  STATIC VARIABLES
@@ -48,12 +51,70 @@ static char * find_by_class(void);
 
 char * lv_linux_drm_find_device_path(void)
 {
-    return find_by_class();
+    char * card_path;
+    if((card_path = find_by_class()))
+        return card_path;
+    return find_by_drm_dev();
 }
 
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+
+static char * find_by_drm_dev(void)
+{
+    DIR * d = opendir(LV_DRM_CARD_DIR);
+    if(!d) {
+        return NULL;
+    }
+    struct dirent * ent;
+    while((ent = readdir(d)) != NULL) {
+        if(!lv_strcmp(ent->d_name, ".")
+           || !lv_strcmp(ent->d_name, "..")
+           || lv_strncmp(ent->d_name, "card", 4))
+            continue;
+
+        const size_t buf_size = lv_strlen(LV_DRM_CARD_DIR "/") + lv_strlen(ent->d_name) + 1;
+        char * card_path = lv_zalloc(buf_size);
+        if(!card_path)
+            continue;
+        lv_snprintf(card_path, buf_size, LV_DRM_CARD_DIR "/%s", ent->d_name);
+        int fd = open(card_path, O_RDWR);
+
+        if(fd < 0)
+            goto open_fail;
+
+        if(fcntl(fd, F_SETFD, FD_CLOEXEC) < 0)
+            goto get_res_fail;
+
+        drmModeRes * res = drmModeGetResources(fd);
+        if(!res)
+            goto get_res_fail;
+
+        for(int i = 0; i < res->count_connectors; i++) {
+            drmModeConnector * conn = drmModeGetConnector(fd, res->connectors[i]);
+            if(!conn)
+                continue;
+            if(conn->connection == DRM_MODE_CONNECTED) {
+                drmModeFreeConnector(conn);
+                drmModeFreeResources(res);
+                close(fd);
+                closedir(d);
+                return card_path;
+            }
+            drmModeFreeConnector(conn);
+        }
+        drmModeFreeResources(res);
+get_res_fail:
+        close(fd);
+open_fail:
+        lv_free(card_path);
+
+    }
+
+    closedir(d);
+    return NULL;
+}
 
 static char * find_by_class(void)
 {
@@ -77,6 +138,8 @@ static char * find_by_class(void)
 
         const size_t buf_size = lv_strlen(LV_DRM_CARD_PATH) + 3;
         char * card_path = lv_zalloc(buf_size);
+        if(!card_path)
+            continue;
         if(ent->d_name[5] != '-') {
             /* Double digit card*/
             lv_snprintf(card_path, buf_size, LV_DRM_CARD_PATH "%c%c", ent->d_name[4], ent->d_name[5]);
