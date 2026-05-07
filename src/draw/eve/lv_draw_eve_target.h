9daceecab1b069f54da095aeb408f3cea8525953
André Costa <andre_miguel_costa@hotmail.com>
2026-05-07 09:31:25 +0200

    fix(eve): fix path to lvgl.h (#10058)

--- src/draw/eve/lv_draw_eve_target.h
+++ src/draw/eve/lv_draw_eve_target.h
@@ -1,2 +1,2 @@
 #warning Include public headers from the `src` folder is deprecated. This file will be removed soon. To ensure your application keeps working, you can choose to include the new header in <include/lvgl/drivers/display/lv_draw_eve_target.h> or include the main header file <include/lvgl/lvgl.h>
-#include "../../include/core/lvgl/lvgl.h"
+#include "../../include/lvgl/lvgl.h"
