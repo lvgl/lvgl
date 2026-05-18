5644fc16f87b47703f4a404a8bf80d29de56e2fa
André Costa <andre_miguel_costa@hotmail.com>
2026-05-18 10:20:07 +0200

    build: revert using target_link_libraries for g2d

--- env_support/cmake/dependencies/g2d.cmake
+++ env_support/cmake/dependencies/g2d.cmake
@@ -3,12 +3,8 @@ find_path(G2D_INCLUDE_DIR NAMES g2d.h g2dExt.h QUIET)
 
 if(G2D_LIBRARY AND G2D_INCLUDE_DIR)
   message(STATUS "lvgl: G2D: found at ${G2D_LIBRARY}")
-  # Create an interface target for G2D since it's found manually
-  add_library(G2D::G2D INTERFACE IMPORTED)
-  set_target_properties(
-    G2D::G2D PROPERTIES INTERFACE_LINK_LIBRARIES "${G2D_LIBRARY}"
-                        INTERFACE_INCLUDE_DIRECTORIES "${G2D_INCLUDE_DIR}")
-  lvgl_link_libraries(PRIVATE TARGETS G2D::G2D)
+  target_include_directories(lvgl PRIVATE ${G2D_INCLUDE_DIR})
+  target_link_libraries(lvgl PRIVATE ${G2D_LIBRARY})
   return()
 endif()
 
