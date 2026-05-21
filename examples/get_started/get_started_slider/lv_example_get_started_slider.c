5eb7a125bb07378db5654206f2076cdc65bf17ee
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2026-05-21 13:31:44 +0200

    api map + minor fixes

--- examples/get_started/get_started_slider/lv_example_get_started_slider.c
+++ examples/get_started/get_started_slider/lv_example_get_started_slider.c
@@ -28,16 +28,15 @@ void lv_example_get_started_slider(void)
     }
 
     lv_obj_t * screen = lv_screen_active();
-    lv_obj_set_flex_flow(screen, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_style_flex_cross_place(screen, LV_FLEX_ALIGN_CENTER, 0);
-    lv_obj_set_style_pad_row(screen, 16, 0);
 
     /* 💡 Drag the slider; the label tracks it because both share `subject_value`. */
     lv_obj_t * lv_label_0 = lv_label_create(screen);
     lv_label_bind_text(lv_label_0, &subject_value, "%d");
+    lv_obj_align(lv_label_0, LV_ALIGN_CENTER, 0, -30);
 
     lv_obj_t * lv_slider_0 = lv_slider_create(screen);
     lv_obj_set_width(lv_slider_0, lv_pct(90));
     lv_slider_bind_value(lv_slider_0, &subject_value);
+    lv_obj_center(lv_slider_0);
 }
 #endif
