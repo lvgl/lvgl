8d6b8192423c1613fe2fe8c4dc2085f659ac214d
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2026-05-31 15:08:57 +1000

    fix(lv_draw_sw): Useful error messages

--- src/draw/sw/blend/lv_draw_sw_blend_to_i1.c
+++ src/draw/sw/blend/lv_draw_sw_blend_to_i1.c
@@ -367,7 +367,7 @@ void LV_ATTRIBUTE_FAST_MEM lv_draw_sw_blend_image_to_i1(lv_draw_sw_blend_image_d
             i1_image_blend(dsc);
             break;
         default:
-            LV_LOG_WARN("Not supported source color format");
+            LV_LOG_WARN("Not supported source color format 0x%02X", dsc->src_color_format);
             break;
     }
 }
