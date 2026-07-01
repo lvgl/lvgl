080b545afb2f92ff24872ca675d3d69a90f476f4
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2026-07-01 10:41:48 +0200

    minot fixes based on feedback

--- examples/get_started/lv_example_get_started.h
+++ examples/get_started/lv_example_get_started.h
@@ -10,13 +10,33 @@
 extern "C" {
 #endif
 
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
 void lv_example_get_started_button(void);
 void lv_example_get_started_hello_world(void);
 void lv_example_get_started_slider(void);
 void lv_example_get_started_styles(void);
+
+/**********************
+ *      MACROS
+ **********************/
 
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
 
 #endif /*LV_EXAMPLE_GET_STARTED_H*/
+
