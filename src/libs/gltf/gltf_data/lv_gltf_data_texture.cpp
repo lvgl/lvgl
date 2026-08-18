37f15b52df3181f9718c9f8ad22d4ed0dc4d1502
André Costa <andre_miguel_costa@hotmail.com>
2026-08-18 11:35:10 +0100

    refactor(gltf): remove unused internal functions

--- src/libs/gltf/gltf_data/lv_gltf_data_texture.cpp
+++ src/libs/gltf/gltf_data/lv_gltf_data_texture.cpp
@@ -50,36 +50,6 @@ GLuint lv_gltf_data_create_texture(lv_gltf_model_t * data)
     return texture;
 }
 
-void lv_gltf_data_rgb_to_bgr(uint8_t * pixel_buffer, size_t byte_total_count, bool has_alpha)
-{
-    size_t bytes_per_pixel = has_alpha ? 4 : 3;
-    size_t pixel_count = (byte_total_count / bytes_per_pixel);
-    if(bytes_per_pixel == 4) {
-        for(size_t p = 0; p < pixel_count; p++) {
-            size_t index = p << 2;
-            uint8_t r = pixel_buffer[index + 0];
-            uint8_t g = pixel_buffer[index + 1];
-            uint8_t b = pixel_buffer[index + 2];
-            uint8_t a = pixel_buffer[index + 3];
-            pixel_buffer[index + 0] = b;
-            pixel_buffer[index + 1] = g;
-            pixel_buffer[index + 2] = r;
-            pixel_buffer[index + 3] = a;
-        }
-    }
-    else {
-        for(size_t p = 0; p < pixel_count; p++) {
-            size_t index = p * 3;
-            uint8_t r = pixel_buffer[index + 0];
-            uint8_t g = pixel_buffer[index + 1];
-            uint8_t b = pixel_buffer[index + 2];
-            pixel_buffer[index + 0] = b;
-            pixel_buffer[index + 1] = g;
-            pixel_buffer[index + 2] = r;
-        }
-    }
-}
-
 /**********************
  *   STATIC FUNCTIONS
  **********************/
