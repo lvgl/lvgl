009b6a1142f488403c10e19bc4f31cf4f9e006a3
André Costa <andre_miguel_costa@hotmail.com>
2026-08-18 17:48:17 +0100

    feat(stdlib): standardize arg checks

--- src/stdlib/builtin/lv_string_builtin.c
+++ src/stdlib/builtin/lv_string_builtin.c
@@ -52,6 +52,9 @@
 
 void * LV_ATTRIBUTE_FAST_MEM lv_memcpy(void * dst, const void * src, size_t len)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     volatile uint8_t * d8 = dst;
     const uint8_t * s8 = src;
 
@@ -114,6 +117,8 @@ void * LV_ATTRIBUTE_FAST_MEM lv_memcpy(void * dst, const void * src, size_t len)
 
 void LV_ATTRIBUTE_FAST_MEM lv_memset(void * dst, uint8_t v, size_t len)
 {
+    LV_ASSERT(dst != NULL);
+
     uint8_t * d8 = (uint8_t *)dst;
     uintptr_t d_align = (lv_uintptr_t) d8 & ALIGN_MASK;
 
@@ -144,6 +149,9 @@ void LV_ATTRIBUTE_FAST_MEM lv_memset(void * dst, uint8_t v, size_t len)
 
 void * LV_ATTRIBUTE_FAST_MEM lv_memmove(void * dst, const void * src, size_t len)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     if(dst < src || (char *)dst > ((char *)src + len)) {
         return lv_memcpy(dst, src, len);
     }
@@ -170,6 +178,9 @@ void * LV_ATTRIBUTE_FAST_MEM lv_memmove(void * dst, const void * src, size_t len
 
 int lv_memcmp(const void * p1, const void * p2, size_t len)
 {
+    LV_ASSERT(p1 != NULL);
+    LV_ASSERT(p2 != NULL);
+
     const char * s1 = (const char *) p1;
     const char * s2 = (const char *) p2;
     while(--len > 0 && (*s1 == *s2)) {
@@ -182,6 +193,8 @@ int lv_memcmp(const void * p1, const void * p2, size_t len)
 /* See https://en.cppreference.com/w/c/string/byte/strlen for reference */
 size_t lv_strlen(const char * str)
 {
+    LV_ASSERT(str != NULL);
+
     size_t i = 0;
     while(str[i]) i++;
 
@@ -190,6 +203,8 @@ size_t lv_strlen(const char * str)
 
 size_t lv_strnlen(const char * str, size_t max_len)
 {
+    LV_ASSERT(str != NULL);
+
     size_t i = 0;
     while(i < max_len && str[i]) i++;
 
@@ -198,6 +213,9 @@ size_t lv_strnlen(const char * str, size_t max_len)
 
 size_t lv_strlcpy(char * dst, const char * src, size_t dst_size)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     size_t i = 0;
     if(dst_size > 0) {
         for(; i < dst_size - 1 && src[i]; i++) {
@@ -211,6 +229,9 @@ size_t lv_strlcpy(char * dst, const char * src, size_t dst_size)
 
 char * lv_strncpy(char * dst, const char * src, size_t dst_size)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     size_t i;
     for(i = 0; i < dst_size && src[i]; i++) {
         dst[i] = src[i];
@@ -223,6 +244,9 @@ char * lv_strncpy(char * dst, const char * src, size_t dst_size)
 
 char * lv_strcpy(char * dst, const char * src)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     char * tmp = dst;
     while((*dst++ = *src++) != '\0');
     return tmp;
@@ -230,6 +254,9 @@ char * lv_strcpy(char * dst, const char * src)
 
 int lv_strcmp(const char * s1, const char * s2)
 {
+    LV_ASSERT(s1 != NULL);
+    LV_ASSERT(s2 != NULL);
+
     while(*s1 && (*s1 == *s2)) {
         s1++;
         s2++;
@@ -239,6 +266,9 @@ int lv_strcmp(const char * s1, const char * s2)
 
 int lv_strncmp(const char * s1, const char * s2, size_t len)
 {
+    LV_ASSERT(s1 != NULL);
+    LV_ASSERT(s2 != NULL);
+
     if(len == 0) {
         return 0;
     }
@@ -255,6 +285,8 @@ int lv_strncmp(const char * s1, const char * s2, size_t len)
 
 char * lv_strdup(const char * src)
 {
+    LV_ASSERT(src != NULL);
+
     size_t len = lv_strlen(src) + 1;
     char * dst = lv_malloc(len);
     if(dst == NULL) return NULL;
@@ -265,6 +297,8 @@ char * lv_strdup(const char * src)
 
 char * lv_strndup(const char * src, size_t max_len)
 {
+    LV_ASSERT(src != NULL);
+
     size_t len = lv_strnlen(src, max_len);
     char * dst = lv_malloc(len + 1);
     if(dst == NULL) return NULL;
@@ -276,12 +310,18 @@ char * lv_strndup(const char * src, size_t max_len)
 
 char * lv_strcat(char * dst, const char * src)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     lv_strcpy(dst + lv_strlen(dst), src);
     return dst;
 }
 
 char * lv_strncat(char * dst, const char * src, size_t src_len)
 {
+    LV_ASSERT(dst != NULL);
+    LV_ASSERT(src != NULL);
+
     char * tmp = dst;
     while(*dst != '\0') {
         dst++;
@@ -296,6 +336,8 @@ char * lv_strncat(char * dst, const char * src, size_t src_len)
 
 char * lv_strchr(const char * s, int c)
 {
+    LV_ASSERT(s != NULL);
+
     for(; ; s++) {
         if(*s == c) {
             return (char *)s;
