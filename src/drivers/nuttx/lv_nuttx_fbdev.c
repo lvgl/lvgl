587732677ebded426a5eea3949017803f4428494
André Costa <andre_miguel_costa@hotmail.com>
2026-08-20 18:46:46 +0200

    feat(nuttx): standardize arg checks

--- src/drivers/nuttx/lv_nuttx_fbdev.c
+++ src/drivers/nuttx/lv_nuttx_fbdev.c
@@ -100,6 +100,8 @@ lv_display_t * lv_nuttx_fbdev_create(void)
 
 int lv_nuttx_fbdev_set_file(lv_display_t * disp, const char * file)
 {
+    LV_CHECK_ARG(disp != NULL, return -EINVAL);
+    LV_CHECK_ARG(file != NULL, return -EINVAL);
     int ret;
     LV_ASSERT(disp && file);
     lv_nuttx_fb_t * dsc = lv_display_get_driver_data(disp);
