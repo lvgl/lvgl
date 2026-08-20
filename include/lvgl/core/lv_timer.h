c43a4f16d3dd8067ed8dd448e7eadde275189320
André Costa <andre_miguel_costa@hotmail.com>
2026-08-20 17:54:09 +0200

    feat(timer): standardize arg checks
    (cherry picked from commit b7b4acac8a112de7222b122ed7c8d2caaf872407)

--- include/lvgl/core/lv_timer.h
+++ include/lvgl/core/lv_timer.h
@@ -74,7 +74,7 @@ LV_ATTRIBUTE_TIMER_HANDLER void lv_timer_periodic_handler(void);
 /**
  * Set the resume callback to the timer handler
  * @param cb the function to call when timer handler is resumed
- * @param data pointer to a resume data
+ * @param data pointer to a resume data @nullable
  */
 void lv_timer_handler_set_resume_cb(lv_timer_handler_resume_cb_t cb, void * data);
 
@@ -183,7 +183,8 @@ uint32_t lv_timer_get_time_until_next(void);
 
 /**
  * Iterate through the timers
- * @param timer NULL to start iteration or the previous return value to get the next timer
+ * @param timer the previous return value. @nullable When NULL the iteration starts
+ *              from the first timer.
  * @return the next timer or NULL if there is no more timer
  */
 lv_timer_t * lv_timer_get_next(lv_timer_t * timer);
@@ -211,9 +212,10 @@ bool lv_timer_get_paused(lv_timer_t * timer);
  * the associated resources.
  *
  * @param timer      Pointer to the timer object
- * @param data       User-defined data pointer to associate with the timer
+ * @param data       User-defined data pointer to associate with the timer @nullable
  * @param free_cb    Callback function for cleaning up ext_data when timer is deleted.
- *                   Receives ext_data as parameter. NULL means no cleanup required.
+ *                   Receives ext_data as parameter. @nullable When NULL no cleanup
+ *                   is performed.
  */
 void lv_timer_set_external_data(lv_timer_t * timer, void * data, void (* free_cb)(void * data));
 #endif
