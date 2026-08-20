265ef8f0c167c6b6635066e73906272b02a5e3ed
André Costa <andre_miguel_costa@hotmail.com>
2026-08-20 16:37:05 +0200

    feat(image): standardize arg checks
    (cherry picked from commit 69c9df970accce59d29d2ccbfac92ba071cf10ae)

--- src/widgets/image/lv_image.c
+++ src/widgets/image/lv_image.c
@@ -525,6 +525,7 @@ int32_t lv_image_get_rotation(lv_obj_t * obj)
 void lv_image_get_pivot(lv_obj_t * obj, lv_point_t * pivot)
 {
     LV_CHECK_OBJ(obj, MY_CLASS, return);
+    LV_CHECK_ARG(pivot != NULL, return);
 
     lv_image_t * img = (lv_image_t *)obj;
 
@@ -651,13 +652,10 @@ const lv_image_dsc_t * lv_image_get_bitmap_map_src(lv_obj_t * obj)
 #if LV_USE_OBSERVER
 lv_observer_t * lv_image_bind_src(lv_obj_t * obj, lv_subject_t * subject)
 {
-    LV_ASSERT_NULL(subject);
     LV_CHECK_OBJ(obj, MY_CLASS, return NULL);
-
-    if(subject->type != LV_SUBJECT_TYPE_POINTER) {
-        LV_LOG_WARN("Incompatible subject type: %d", subject->type);
-        return NULL;
-    }
+    LV_CHECK_ARG(subject != NULL, return NULL);
+    LV_CHECK_ARG(subject->type == LV_SUBJECT_TYPE_POINTER, return NULL,
+                 "Incompatible subject type: %d", subject->type);
 
     lv_observer_t * observer = lv_subject_add_observer_obj(subject, image_src_observer_cb, obj, NULL);
     return observer;
@@ -671,6 +669,7 @@ lv_observer_t * lv_image_bind_src(lv_obj_t * obj, lv_subject_t * subject)
 
 static void lv_image_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
 {
+    LV_ASSERT(obj != NULL);
     LV_UNUSED(class_p);
     LV_TRACE_OBJ_CREATE("begin");
 
@@ -697,6 +696,7 @@ static void lv_image_constructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
 
 static void lv_image_destructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
 {
+    LV_ASSERT(obj != NULL);
     LV_UNUSED(class_p);
     lv_image_t * img = (lv_image_t *)obj;
     if(img->src_type == LV_IMAGE_SRC_FILE || img->src_type == LV_IMAGE_SRC_SYMBOL) {
@@ -708,6 +708,7 @@ static void lv_image_destructor(const lv_obj_class_t * class_p, lv_obj_t * obj)
 
 static void lv_image_event(const lv_obj_class_t * class_p, lv_event_t * e)
 {
+    LV_ASSERT(e != NULL);
     LV_UNUSED(class_p);
 
     lv_event_code_t code = lv_event_get_code(e);
@@ -794,6 +795,7 @@ static void lv_image_event(const lv_obj_class_t * class_p, lv_event_t * e)
 
 static void draw_image(lv_event_t * e)
 {
+    LV_ASSERT(e != NULL);
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t * obj = lv_event_get_current_target(e);
     lv_image_t * img = (lv_image_t *)obj;
@@ -959,6 +961,7 @@ static void draw_image(lv_event_t * e)
 
 static void scale_update(lv_obj_t * obj, int32_t scale_x, int32_t scale_y)
 {
+    LV_ASSERT(obj != NULL);
     lv_image_t * img = (lv_image_t *)obj;
 
     lv_obj_update_layout(obj);  /*Be sure the object's size is calculated*/
@@ -994,6 +997,7 @@ static void scale_update(lv_obj_t * obj, int32_t scale_x, int32_t scale_y)
 
 static void update_align(lv_obj_t * obj)
 {
+    LV_ASSERT(obj != NULL);
     lv_image_t * img = (lv_image_t *)obj;
     if(img->align == LV_IMAGE_ALIGN_STRETCH) {
         lv_image_set_rotation(obj, 0);
@@ -1039,6 +1043,7 @@ static void update_align(lv_obj_t * obj)
 
 static void reset_image_attributes(lv_obj_t * obj)
 {
+    LV_ASSERT(obj != NULL);
     lv_image_t * img = (lv_image_t *)obj;
     if(img->src_type == LV_IMAGE_SRC_SYMBOL || img->src_type == LV_IMAGE_SRC_FILE) {
         lv_free((void *)img->src);
@@ -1056,6 +1061,9 @@ static void reset_image_attributes(lv_obj_t * obj)
 
 static void image_src_observer_cb(lv_observer_t * observer, lv_subject_t * subject)
 {
+    LV_ASSERT(observer != NULL);
+    LV_ASSERT(observer->target != NULL);
+    LV_ASSERT(subject != NULL);
     if(subject->type == LV_SUBJECT_TYPE_POINTER) {
         lv_image_set_src(observer->target, subject->value.pointer);
     }
@@ -1066,11 +1074,14 @@ static void image_src_observer_cb(lv_observer_t * observer, lv_subject_t * subje
 #if LV_USE_OBJ_PROPERTY
 static void lv_image_set_pivot_helper(lv_obj_t * obj, lv_point_t * pivot)
 {
+    LV_ASSERT(obj != NULL);
+    LV_ASSERT(pivot != NULL);
     lv_image_set_pivot(obj, pivot->x, pivot->y);
 }
 
 static lv_point_t lv_image_get_pivot_helper(lv_obj_t * obj)
 {
+    LV_ASSERT(obj != NULL);
     lv_point_t pivot;
     lv_image_get_pivot(obj, &pivot);
     return pivot;
