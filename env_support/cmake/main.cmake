98a01bb75e35c1e4797a192bc7c67f050a339f0e
ychsiao168 <yungchunghsiao@protonmail.com>
2026-09-01 17:59:47 +0800

    build(cmake): skip thorvg build when LV_USE_THORVG is disabled (#10570)
    Co-authored-by: ychsiao168 <yungchunghsiao@brainchild.com.tw>

--- env_support/cmake/main.cmake
+++ env_support/cmake/main.cmake
@@ -56,6 +56,7 @@ option(LV_BUILD_TESTS "Build the unit tests in the tests directory" OFF)
 
 option(CONFIG_LV_BUILD_DEMOS "Build demos" ON)
 option(CONFIG_LV_BUILD_EXAMPLES "Build examples" ON)
+option(CONFIG_LV_USE_THORVG "Enable ThorVG" ON)
 option(CONFIG_LV_USE_THORVG_INTERNAL "Use the internal version of ThorVG" ON)
 option(CONFIG_LV_USE_PRIVATE_API "If set - install the private headers" OFF)
 
@@ -302,7 +303,12 @@ target_include_directories(lvgl SYSTEM PUBLIC
 # mentioned in this file
 get_target_property(COMP_DEF lvgl COMPILE_DEFINITIONS)
 
-if(CONFIG_LV_USE_THORVG_INTERNAL)
+set(LV_BUILD_THORVG_INTERNAL OFF)
+if(CONFIG_LV_USE_THORVG AND CONFIG_LV_USE_THORVG_INTERNAL)
+    set(LV_BUILD_THORVG_INTERNAL ON)
+endif()
+
+if(LV_BUILD_THORVG_INTERNAL)
     message(STATUS "Enabling the building of ThorVG internal")
 
     add_library(lvgl_thorvg ${THORVG_SOURCES})
@@ -470,7 +476,7 @@ install(EXPORT lvglTargets
 )
 
 # Install library thorvg
-if(CONFIG_LV_USE_THORVG_INTERNAL)
+if(LV_BUILD_THORVG_INTERNAL)
 
     set_target_properties(
         lvgl_thorvg
