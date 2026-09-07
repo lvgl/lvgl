589c871d4f91f5b3b5f1a0bd31ca51a079df8457
André Costa <andre_miguel_costa@hotmail.com>
2026-09-07 14:52:36 +0200

    test(canvas): add dropshadow canvas test

--- tests/src/test_cases/widgets/test_canvas.c
+++ tests/src/test_cases/widgets/test_canvas.c
@@ -791,4 +791,32 @@ void test_line_bigger_than_display_resolution(void)
      * at (hor_res+1, ver_res+1) on a buffer of size (hor_res+1)x(ver_res+1)*/
 }
 
+void test_canvas_draw_sub_layer(void)
+{
+    lv_obj_t * canvas = lv_canvas_create(g_screen_active);
+
+    LV_DRAW_BUF_DEFINE_STATIC(draw_buf, 100, 100, LV_COLOR_FORMAT_ARGB8888);
+    LV_DRAW_BUF_INIT_STATIC(draw_buf);
+    canvas_draw_buf_reshape(&draw_buf);
+    lv_canvas_set_draw_buf(canvas, &draw_buf);
+    lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
+
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+    TEST_ASSERT_EQUAL_PTR(lv_obj_get_display(canvas), layer.display);
+
+    lv_draw_rect_dsc_t rect_dsc;
+    lv_draw_rect_dsc_init(&rect_dsc);
+    rect_dsc.bg_color = lv_color_hex(0xFF0000);
+    rect_dsc.bg_opa = LV_OPA_COVER;
+    rect_dsc.base.drop_shadow_opa = LV_OPA_COVER;
+    rect_dsc.base.drop_shadow_blur_radius = 5;
+    lv_area_t rect_area = {30, 30, 70, 70};
+    lv_draw_rect(&layer, &rect_dsc, &rect_area);
+
+    lv_canvas_finish_layer(canvas, &layer);
+
+    TEST_ASSERT_EQUAL_SCREENSHOT("widgets/canvas_dropshadow.png")
+}
+
 #endif
