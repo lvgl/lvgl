f9b0f1b4c1fb3cd18ca9d2f91bb88168c7d9f86e
André Costa <andre_miguel_costa@hotmail.com>
2026-09-08 22:16:55 +0200

    fix(demo/gltf): return early if model can't be loaded (#10653)

--- demos/gltf/lv_demo_gltf.c
+++ demos/gltf/lv_demo_gltf.c
@@ -129,7 +129,10 @@ lv_obj_t * lv_demo_gltf(const char * path)
     lv_obj_set_scrollable(viewer, false);
     lv_gltf_set_background_mode(viewer, LV_GLTF_BG_MODE_ENVIRONMENT);
     lv_gltf_model_t * model = lv_gltf_load_model_from_file(viewer, path);
-    LV_ASSERT_NULL(model);
+    if(!model) {
+        lv_obj_delete(viewer);
+        return NULL;
+    }
 
     init_subjects(viewer);
     create_control_panel(viewer);
