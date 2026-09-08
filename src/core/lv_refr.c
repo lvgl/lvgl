eaf8204b426d2d70d991d05f428ba497b3e99521
André Costa <andre_miguel_costa@hotmail.com>
2026-09-08 08:45:20 +0200

    fix(refr): ensure there's an active screen before updating its layout

--- src/core/lv_refr.c
+++ src/core/lv_refr.c
@@ -405,7 +405,7 @@ void lv_display_refr_timer(lv_timer_t * timer)
 
     /*Refresh the screen's layout if required*/
     LV_PROFILER_LAYOUT_BEGIN_TAG("layout");
-    lv_obj_update_layout(disp_refr->act_scr);
+    if(disp_refr->act_scr) lv_obj_update_layout(disp_refr->act_scr);
     if(disp_refr->prev_scr) lv_obj_update_layout(disp_refr->prev_scr);
 
     lv_obj_update_layout(disp_refr->bottom_layer);
