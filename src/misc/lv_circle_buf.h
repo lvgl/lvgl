bd4565fa043b385ed34fa65cfc937cf10c94cf3d
André Costa <andre_miguel_costa@hotmail.com>
2026-09-16 17:09:49 +0200

    feat(deprecated-headers)!: remove deprecated header files from src folder
    BREAKING CHANGE: User still using these deprecated headers can use the
    headers inside the include/lvgl folder

--- src/misc/lv_circle_buf.h
+++
@@ -1,3 +0,0 @@
-#warning "lv_circle_buf" is no longer part of the public API and will be removed in the future.
-
-#include "lv_circle_buf_private.h"
--- src/misc/lv_rb.h
+++
@@ -1,3 +0,0 @@
-#warning "lv_rb" is no longer part of the public API and will be removed in the future.
-
-#include "lv_rb_private.h"
